b4855f5008696f208a1cf4087e5be842ac9ccdc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 14:06:08 +0000

    Add package fz-route/0.0.1/fz_route-0.0.1.tar.gz

---
+++ packages/fz-route/fz_route-0.0.1.tar.gz/fz_route-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'FZ Route Forecast'
+LONG_DESCRIPTION = 'Base Working file data generation for route forecast'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="fz_route", 
+        version=VERSION,
+        author="Sami",
+        author_email="<edsami17@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[
+            'pandas',
+            'numpy',
+            'sqlalchemy'
+        ], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
