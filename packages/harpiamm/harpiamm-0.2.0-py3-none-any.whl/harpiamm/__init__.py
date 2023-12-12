550265454616ed75d2ca1a2c656c399ce91db52b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 14:06:15 +0000

    Add package harpiamm/0.2.0/harpiamm-0.2.0-py3-none-any.whl

---
+++ packages/harpiamm/harpiamm-0.2.0-py3-none-any.whl/harpiamm/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""HARPIA Microscopy Module Python library.
+
+Contact: lukas.kontenis@lightcon.com, support@lightcon.com
+
+Copyright (c) 2019-2023 Light Conversion
+All rights reserved.
+www.lightcon.com
+"""
+__version__ = '0.2.0'
+
+# from . import harpiamm
+# from . import harpiamm_ui
+# from . import harpiamm_ui_cli
+# from . import delay_line_alignment
+# from . import cam_focus_test
+
+# Qt GUI supports depends on PySide2
+# from . import harpiamm_ui_qt
