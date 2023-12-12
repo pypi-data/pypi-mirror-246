03fb6ef568b3a20966f460d19224076dfdbecf4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 14:05:37 +0000

    Add package afeng-py-tools/0.0.31/afeng_py_tools-0.0.31-py3-none-any.whl

---
+++ packages/afeng-py-tools/afeng_py_tools-0.0.31-py3-none-any.whl/afeng_tools/fastapi_tool/core/fastapi_enums.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class FastapiConfigKeyEnum(Enum):
+    error404_context_data_func = 'error404_context_data_func'
+    error500_context_data_func = 'error500_context_data_func'
+    error501_context_data_func = 'error500_context_data_func'
