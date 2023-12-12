4788b6f2cb91d840c747914372be39e78ea8ad9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 14:06:16 +0000

    Add package blackjax-nightly/1.0.0.post41/blackjax-nightly-1.0.0.post41.tar.gz

---
+++ packages/blackjax-nightly/blackjax-nightly-1.0.0.post41.tar.gz/blackjax-nightly-1.0.0.post41/tests/mcmc/test_metrics.py
@@ -0,0 +1,71 @@
+import chex
+import jax.numpy as jnp
+import numpy as np
+from absl.testing import absltest, parameterized
+from jax import random
+from jax.scipy import linalg
+
+from blackjax.mcmc import metrics
+
+
+class GaussianEuclideanMetricsTest(chex.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.key = random.key(0)
+        self.dtype = "float32"
+
+    @parameterized.named_parameters(
+        {"testcase_name": "0d", "shape": ()},
+        {"testcase_name": "3d", "shape": (1, 2, 3)},
+    )
+    def test_gaussian_euclidean_ndim_invalid(self, shape):
+        """Test Gaussian Euclidean Function returns correct function invalid ndim"""
+        x = jnp.ones(shape=shape)
+        with self.assertRaisesRegex(
+            ValueError, "The mass matrix has the wrong number of dimensions"
+        ):
+            _ = metrics.gaussian_euclidean(x)
+
+    @chex.all_variants(with_pmap=False)
+    def test_gaussian_euclidean_dim_1(self):
+        """Test Gaussian Euclidean Function with ndim 1"""
+        inverse_mass_matrix = jnp.asarray([1 / 4], dtype=self.dtype)
+        momentum, kinetic_energy, _ = metrics.gaussian_euclidean(inverse_mass_matrix)
+
+        arbitrary_position = jnp.asarray([12345], dtype=self.dtype)
+        momentum_val = self.variant(momentum)(self.key, arbitrary_position)
+
+        # 2 is square root inverse of 1/4
+        expected_momentum_val = 2 * random.normal(self.key)
+
+        kinetic_energy_val = self.variant(kinetic_energy)(momentum_val)
+        velocity = inverse_mass_matrix * momentum_val
+        expected_kinetic_energy_val = 0.5 * velocity * momentum_val
+
+        assert momentum_val == expected_momentum_val
+        assert kinetic_energy_val == expected_kinetic_energy_val
+
+    @chex.all_variants(with_pmap=False)
+    def test_gaussian_euclidean_dim_2(self):
+        """Test Gaussian Euclidean Function with ndim 2"""
+        inverse_mass_matrix = jnp.asarray(
+            [[1 / 9, 0.5], [0.5, 1 / 4]], dtype=self.dtype
+        )
+        momentum, kinetic_energy, _ = metrics.gaussian_euclidean(inverse_mass_matrix)
+
+        arbitrary_position = jnp.asarray([12345, 23456], dtype=self.dtype)
+        momentum_val = self.variant(momentum)(self.key, arbitrary_position)
+
+        L_inv = linalg.cholesky(linalg.inv(inverse_mass_matrix), lower=True)
+        expected_momentum_val = L_inv @ random.normal(self.key, shape=(2,))
+
+        kinetic_energy_val = self.variant(kinetic_energy)(momentum_val)
+        velocity = jnp.dot(inverse_mass_matrix, momentum_val)
+        expected_kinetic_energy_val = 0.5 * jnp.matmul(velocity, momentum_val)
+
+        np.testing.assert_allclose(expected_momentum_val, momentum_val)
+        np.testing.assert_allclose(kinetic_energy_val, expected_kinetic_energy_val)
+
+
+if __name__ == "__main__":
+    absltest.main()
