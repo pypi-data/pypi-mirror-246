72c590aee30a0bef2364fde6375bd60bcc3818d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 14:05:47 +0000

    Add package capellambse/0.5.41/capellambse-0.5.41-py3-none-any.whl

---
+++ packages/capellambse/capellambse-0.5.41-py3-none-any.whl/capellambse/pvmt/__init__.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: Copyright DB Netz AG and the capellambse contributors
+# SPDX-License-Identifier: Apache-2.0
+"""Provides easy access to the Polarsys Capella PVMT extension.
+
+The public API of this submodule uses raw LXML elements. For a more
+object oriented and user friendly way to access property values in a
+model, see the :class:`capellambse.MelodyModel` class.
+"""
+
+from .exceptions import ScopeError
+from .model import PVMTExtension, load_pvmt_from_model
