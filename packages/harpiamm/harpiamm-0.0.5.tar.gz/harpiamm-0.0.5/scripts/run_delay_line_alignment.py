b30234e073fb740f054d68fb3a2224e62df0956f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 14:05:42 +0000

    Add package harpiamm/0.0.5/harpiamm-0.0.5.tar.gz

---
+++ packages/harpiamm/harpiamm-0.0.5.tar.gz/harpiamm-0.0.5/scripts/run_delay_line_alignment.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#===============================================================================
+# Measure HARPIA probe delay line straightness.
+#-------------------------------------------------------------------------------
+#
+# Copyright (c) 2019-2020 Light Conversion
+# All rights reserved.
+# www.lightcon.com
+#===============================================================================
+
+from harpiamm.delay_line_alignment import delay_line_alignment
+
+delay_line_alignment(cam_port='20042')
