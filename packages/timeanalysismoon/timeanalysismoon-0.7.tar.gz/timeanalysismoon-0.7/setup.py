6083827815ff46b646d9e810c4952a71e08ff1b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 14:05:52 +0000

    Add package timeanalysismoon/0.7/timeanalysismoon-0.7.tar.gz

---
+++ packages/timeanalysismoon/timeanalysismoon-0.7.tar.gz/timeanalysismoon-0.7/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='timeanalysismoon',
+    version='0.7',
+    packages=['timeanalysismoon'],
+    author='CoderMungan',
+    author_email='codermungan@gmail.com',
+    description='A time analysis decorator for Python functions',
+    long_description='A decorator that measures and prints the execution time of a function.',
+    url='https://github.com/CoderMungan/timeanalysismoon',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    entry_points={
+        'console_scripts': [
+            'timeanalysismoon=timeanalysismoon.cli:main',
+        ],
+    },
+)
