a1c1a8308233be15cc4f7b77de5fee96e4ad3ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 14:06:18 +0000

    Add package cdklabs.cdk-validator-cfnguard/0.0.58/cdklabs.cdk_validator_cfnguard-0.0.58-py3-none-any.whl

---
+++ packages/cdklabs.cdk-validator-cfnguard/cdklabs.cdk_validator_cfnguard-0.0.58-py3-none-any.whl/cdklabs/cdk_validator_cfnguard/_jsii/__init__.py
@@ -0,0 +1,26 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cdklabs/cdk-validator-cfnguard",
+    "0.0.58",
+    __name__[0:-6],
+    "cdk-validator-cfnguard@0.0.58.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
