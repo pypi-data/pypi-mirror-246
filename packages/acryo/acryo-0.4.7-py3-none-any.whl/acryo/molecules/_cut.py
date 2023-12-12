fc0377e9bcea79ba287087576223852fcc439518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 14:05:52 +0000

    Add package acryo/0.4.7/acryo-0.4.7-py3-none-any.whl

---
+++ packages/acryo/acryo-0.4.7-py3-none-any.whl/acryo/molecules/_cut.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+from typing import (
+    TYPE_CHECKING,
+    Iterator,
+    NamedTuple,
+    cast,
+)
+
+if TYPE_CHECKING:
+    from polars.dataframe.group_by import GroupBy
+    from .core import Molecules
+
+
+class MoleculeCutGroup:
+    """A groupby-like object for molecules created by :meth:`cutby`."""
+
+    def __init__(self, group: GroupBy, label: str):
+        self._group = group
+        self._label = label
+
+    def __iter__(self) -> Iterator[tuple[CutEdges, Molecules]]:
+        from .core import Molecules
+
+        for key, df in self._group:
+            key = cast(str, key)
+            gt, le = map(float, key[1:-1].split(", "))
+            mole = Molecules.from_dataframe(df.drop(self._label))
+            yield CutEdges(gt, le), mole
+
+
+class CutEdges(NamedTuple):
+    """Tuple of cut edges."""
+
+    gt: float
+    le: float
+
+    def __repr__(self) -> str:
+        return f"({self.gt}, {self.le}]"
