cd2f761c6a75d8077770b97167b87fb656d74ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 14:05:40 +0000

    Add package services-communication/2.2.2/services_communication-2.2.2.tar.gz

---
+++ packages/services-communication/services_communication-2.2.2.tar.gz/services_communication-2.2.2/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='services_communication',
+    version='2.2.2',
+    packages=find_packages(),
+    url='https://github.com/ksinn/django-microservices-communication',
+    author='ksinn',
+    author_email='ksinnd@gmail.com',
+    description='Pub/Sub for microservice on django',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    install_requires=[
+        "Django",
+        "djangorestframework",
+        "djangorestframework-camel-case",
+        "pika",
+        "requests",
+    ],
+    setup_requires=['wheel'],
+)
