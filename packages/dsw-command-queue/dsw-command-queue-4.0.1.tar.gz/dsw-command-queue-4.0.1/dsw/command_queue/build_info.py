e1e325b54551476f14316488c540482985360127
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 04:03:52 +0000

    Add package dsw-command-queue/4.0.1/dsw-command-queue-4.0.1.tar.gz

---
+++ packages/dsw-command-queue/dsw-command-queue-4.0.1.tar.gz/dsw-command-queue-4.0.1/dsw/command_queue/build_info.py
@@ -0,0 +1,17 @@
+# Generated file
+# - do not overwrite
+# - do not include in git
+from collections import namedtuple
+
+BuildInfo = namedtuple(
+    'BuildInfo',
+    ['version', 'built_at', 'sha', 'branch', 'tag'],
+)
+
+BUILD_INFO = BuildInfo(
+    version='v4.0.1~d19c8b2',
+    built_at='2023-12-12 15:43:39Z',
+    sha='d19c8b2407c819c62698a944b3d99f9f83672045',
+    branch='HEAD',
+    tag='v4.0.1',
+)
