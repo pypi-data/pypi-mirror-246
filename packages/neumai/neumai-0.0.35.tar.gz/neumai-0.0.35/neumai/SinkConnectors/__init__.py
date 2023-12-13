869489ae883dd40bfe8023c846078eaf59176483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 07:03:40 +0000

    Add package neumai/0.0.35/neumai-0.0.35.tar.gz

---
+++ packages/neumai/neumai-0.0.35.tar.gz/neumai-0.0.35/neumai/SinkConnectors/__init__.py
@@ -0,0 +1,7 @@
+from .PineconeSink import PineconeSink
+from .QdrantSink import QdrantSink
+from .WeaviateSink import WeaviateSink
+from .SingleStoreSink import SingleStoreSink
+from .SupabaseSink import SupabaseSink
+from .SinkConnector import SinkConnector
+from .SinkConnectorEnum import SinkConnectorEnum
