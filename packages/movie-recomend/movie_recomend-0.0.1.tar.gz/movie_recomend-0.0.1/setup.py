44de64e10b0834a7d3cdf6c67948b9942668a686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 16:03:29 +0000

    Add package movie-recomend/0.0.1/movie_recomend-0.0.1.tar.gz

---
+++ packages/movie-recomend/movie_recomend-0.0.1.tar.gz/movie_recomend-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    author='Lilith Petrosyan, Zhora Stepanyan, Petros Petrosyan, Artashes Mezhlumyan',
+    description='Movie recomendation system',
+    name='movie_recomend',
+    version='0.0.1',
+    packages=find_packages(),
+    
+)
