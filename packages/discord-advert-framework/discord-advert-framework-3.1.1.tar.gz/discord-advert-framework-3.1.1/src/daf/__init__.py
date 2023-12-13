35a910e14b822999dba0956194c5f3ba3c2f5dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 14:04:42 +0000

    Add package discord-advert-framework/3.1.1/discord-advert-framework-3.1.1.tar.gz

---
+++ packages/discord-advert-framework/discord-advert-framework-3.1.1.tar.gz/discord-advert-framework-3.1.1/src/daf/__init__.py
@@ -0,0 +1,20 @@
+"""
+Discord Advertisement Framework
+"""
+import _discord as discord
+
+from . import misc
+from . import events
+
+from .client import *
+from .core import *
+from .dtypes import *
+from .guild import *
+from .message import *
+from .logging import *
+from .web import *
+from .convert import *
+from .remote import *
+
+
+VERSION = "3.1.1"
