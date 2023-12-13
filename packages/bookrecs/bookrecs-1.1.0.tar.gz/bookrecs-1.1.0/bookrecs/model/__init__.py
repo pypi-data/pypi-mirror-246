caafbddf2cae2aeb5109c27c7dd5dbb3934e694e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 05:02:54 +0000

    Add package bookrecs/1.1.0/bookrecs-1.1.0.tar.gz

---
+++ packages/bookrecs/bookrecs-1.1.0.tar.gz/bookrecs-1.1.0/bookrecs/model/__init__.py
@@ -0,0 +1,1 @@
+from .model import get_combined_recommendations
---
+++ packages/bookrecs/bookrecs-1.1.0.tar.gz/bookrecs-1.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    author='Mane Davtyan',
+    description='BookStore Recommendation System',
+    name='bookrecs',
+    version='1.1.0',
+    packages=find_packages(include=['bookrecs', 'bookrecs.*']),
+    install_requires=[
+        'fastapi==0.105.0',
+        'numpy==1.24.4',
+        'pandas==2.0.3',
+        'pydantic==2.5.2',
+        'scikit_learn==1.3.2',
+        'setuptools==41.2.0',
+        'SQLAlchemy==2.0.23',
+        'uvicorn==0.24.0.post1',
+    ],
+)
