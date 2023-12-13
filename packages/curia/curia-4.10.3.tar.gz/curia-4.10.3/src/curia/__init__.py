476d262937b20228750f5a803c0ac1b6cfe19f9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 05:03:05 +0000

    Add package curia/4.10.3/curia-4.10.3.tar.gz

---
+++ packages/curia/curia-4.10.3.tar.gz/curia-4.10.3/src/curia/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "4.10.3"
+__api_version__ = "v3.10.4"
+
+
+from curia import api
---
+++ packages/curia/curia-4.10.3.tar.gz/curia-4.10.3/src/curia/infra/databricks/task.py
@@ -0,0 +1,73 @@
+import dataclasses
+from typing import ClassVar, List
+
+from pyspark.sql import SparkSession
+
+from curia.infra.task import TaskDefinition, TaskInputDefinition
+from curia.infra.types import TaskType, DatabricksTaskCodeSourceType
+
+
+@dataclasses.dataclass
+class DatabricksTaskDefinition(TaskDefinition):  # pylint: disable=too-many-instance-attributes
+    """
+    Task definition for a task executed through a databricks job
+    """
+    code_src_type: DatabricksTaskCodeSourceType = DatabricksTaskCodeSourceType.S3WHEEL
+    code_src_cfg: dict = dataclasses.field(default_factory=dict)
+    min_workers: int = 2
+    max_workers: int = 4
+    task_type: ClassVar[TaskType] = TaskType.DATABRICKS
+
+    def __post_init__(self):
+        self.inputs.append(
+            TaskInputDefinition(
+                name="code_version",
+                description="""
+                The code version to use for this task
+                """,
+                type="literal",
+                optional=False
+            )
+        )
+
+    # pylint: disable=arguments-differ
+    def run(self,
+            task_execution_id: str,
+            api_token: str,
+            api_endpoint: str,
+            spark: SparkSession,
+            dbutils: 'DBUtils') -> None:
+        """
+        Run the analytics task flow definition
+        :param task_execution_id: The task execution ID
+        :param api_token: The API key to use to retrieve the task inputs from the Curia API
+        :param api_endpoint: The API endpoint to use to retrieve the task inputs from the Curia API
+        :return: The result
+        """
+        assert self._function is not None, "TaskDefinition must decorate a function"
+        resolved_args = self.resolve_arguments(task_execution_id, api_token, api_endpoint)
+        results = self._function(**resolved_args, spark=spark, dbutils=dbutils)
+        self.upload_results(task_execution_id, api_token, api_endpoint, results)
+
+    def build_workflow_data_block(self, inputs: List[TaskInputDefinition], task_specific_context: dict):
+        if "databricks_job_id" not in task_specific_context:
+            raise ValueError("Databricks job ID must be provided to DatabricksTaskDefinition")
+        databricks_job_id = task_specific_context["databricks_job_id"]
+        return {
+            "notebook_params": {task_input.name: "{{parameters." + task_input.name + "}}" for task_input in inputs},
+            "job_id": databricks_job_id
+        }
+
+    def build_task_inputs(self, inputs: List[TaskInputDefinition], task_specific_context: dict):
+        if "databricks_job_id" not in task_specific_context:
+            raise ValueError("Databricks job ID must be provided to DatabricksTaskDefinition")
+        databricks_job_id = task_specific_context["databricks_job_id"]
+        return {
+            "notebook_params": {
+                task_input.name: {
+                    "required": not task_input.optional,
+                    "descr": task_input.description
+                } for task_input in inputs
+            },
+            "job_id": databricks_job_id
+        }
