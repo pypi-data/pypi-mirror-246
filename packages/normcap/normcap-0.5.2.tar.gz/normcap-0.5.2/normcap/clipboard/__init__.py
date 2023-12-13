2ac414196d5abac07acb66b57b4526db08764e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 14:03:50 +0000

    Add package normcap/0.5.2/normcap-0.5.2.tar.gz

---
+++ packages/normcap/normcap-0.5.2.tar.gz/normcap-0.5.2/normcap/clipboard/__init__.py
@@ -0,0 +1,13 @@
+from normcap.clipboard import handlers
+from normcap.clipboard.main import (
+    copy,
+    get_compatible_handlers,
+    has_compatible_handler,
+)
+
+__all__ = [
+    "copy",
+    "get_compatible_handlers",
+    "has_compatible_handler",
+    "handlers",
+]
