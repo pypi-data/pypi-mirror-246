5974842b4268a3915ca0b8e204eca43cbf8cd165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 16:03:54 +0000

    Add package kkappkit/0.23.1/kkappkit-0.23.1-py3-none-any.whl

---
+++ packages/kkappkit/kkappkit-0.23.1-py3-none-any.whl/kkappkit/res/skeleton/src/gui.py
@@ -0,0 +1,21 @@
+#
+# GENERATED: DO NOT EDIT
+#
+import os
+import os.path as osp
+import sys
+import time
+# 3rd party
+import kkpyui as ui
+import kkpyutil as util
+# project
+import control as ctrl
+
+
+def main():
+# {{view}}
+    pass
+
+
+if __name__ == "__main__":
+    main()
