dd6928b3b2c9b94ff2e164077aa3c7442a92e6f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 04:03:05 +0000

    Add package prometheus-givenergy/1.0.3/prometheus-givenergy-1.0.3.tar.gz

---
+++ packages/prometheus-givenergy/prometheus-givenergy-1.0.3.tar.gz/prometheus-givenergy-1.0.3/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+echo "Info: Creating venv..."
+rm -rf tmp.dev.venv
+
+/usr/bin/python3 -m venv tmp.dev.venv --upgrade-deps
+
+echo "Info: Installing development libs into venv..."
+PY=${PWD}/tmp.dev.venv/bin/python
+tmp.dev.venv/bin/python -m pip install --quiet \
+    pymodbus \
+    crccheck
+
+tmp.dev.venv/bin/python -m pip list
+
+echo
+echo PY=${PY}
+echo PYTHONPATH=src \${PY} -m prometheus_givenergy "$@"
