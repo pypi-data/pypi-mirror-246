956ceca60077ce5b44658c11fea02aac033d2a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 03:03:30 +0000

    Add package CatInstaller/1.7.1/CatInstaller-1.7.1.tar.gz

---
+++ packages/CatInstaller/CatInstaller-1.7.1.tar.gz/CatInstaller-1.7.1/CatInstaller/__init__.py
@@ -0,0 +1,8 @@
+__copyright__    = 'Copyright (C) 2023 TechCat56'
+__version__      = '1.7.1'
+__license__      = 'MIT'
+__author__       = 'TechCat56'
+__author_email__ = 'techcat28@gmail.com'
+__url__          = 'http://github.com/diamondgotcat/CatInstaller'
+
+__all__ = ['CatInstaller']
