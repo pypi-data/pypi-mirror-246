3a3a3e8f200db24bfe654861cbe89e75920c0442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 16:04:08 +0000

    Add package i18n-json/0.0.8/i18n_json-0.0.8.tar.gz

---
+++ packages/i18n-json/i18n_json-0.0.8.tar.gz/i18n_json-0.0.8/setup.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from setuptools import setup, Extension
+import Cython.Build
+
+# read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "readme.md").read_text()
+
+version = '0.0.8'
+
+ext = Extension(name="i18n_json", sources=["i18n_json.py"])
+
+setup(
+    name='i18n_json',
+    version=version,
+    description='Sychronized, streaming dictionary that uses shared memory as a backend',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='wanghaisheng',
+    author_email='admin@tiktokastudio.com',
+    url='https://github.com/wanghaisheng/json-dict-in-memory',
+    cmdclass={'build_ext': Cython.Build.build_ext},
+    package_dir={'i18n_json': '.'},
+    packages=['i18n_json'],
+    zip_safe=False,
+    ext_modules=Cython.Build.cythonize(ext, compiler_directives={'language_level' : "3"}),
+    setup_requires=['cython>=0.24.1'],
+    python_requires=">=3.8",
+)
