4bf816ab49c66d050ae56f8a55797e8cd36d4538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 05:03:02 +0000

    Add package corx/1.0.0b2/corx-1.0.0b2-py3-none-any.whl

---
+++ packages/corx/corx-1.0.0b2-py3-none-any.whl/corx/__init__.py
@@ -0,0 +1,18 @@
+from . import aggregate
+from . import bootstrap
+from . import command
+from . import dispatcher
+from . import event
+from . import query
+from . import test
+
+__all__ = [
+    'test',
+    'command',
+    'query',
+    'event',
+    'dispatcher',
+    'aggregate',
+]
+
+bootstrap.bootstrap()
