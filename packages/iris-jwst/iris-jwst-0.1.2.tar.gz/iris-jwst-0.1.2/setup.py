b237283694f85c54b8e41905b47fd2da29cecf8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 07:03:37 +0000

    Add package iris-jwst/0.1.2/iris-jwst-0.1.2.tar.gz

---
+++ packages/iris-jwst/iris-jwst-0.1.2.tar.gz/iris-jwst-0.1.2/setup.py
@@ -0,0 +1,35 @@
+import os, sys
+from setuptools import setup, find_packages
+from setuptools.command.install import install as _install
+
+
+def _post_install(dir):
+    from subprocess import call
+    call(['python ./iris-jwst/HITRAN/Create_ir_model_files.py'])
+
+
+class install(_install):
+    def run(self):
+        _install.run(self)
+        self.execute(_post_install, (self.install_lib,),
+                     msg="Downloading HITRAN data...")
+        
+setup(
+    name='iris-jwst',
+    version='0.1.2',
+    author='Carlos E. Muñoz-Romero',
+    author_email='carlos.munoz_romero@cfa.harvard.edu',
+    description='iris-jwst is a GPU-accelerated package to generate high-resolution \
+                 opacity-weighted molecular line intensities to model JWST MIRI MRS spectra.',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+    cmdclass={'install': install}
+)
+
+
+
