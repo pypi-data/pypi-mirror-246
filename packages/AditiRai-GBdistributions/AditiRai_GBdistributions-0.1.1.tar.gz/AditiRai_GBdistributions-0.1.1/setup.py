db45b91ee83374bdc8c5037026beaafb16cd805c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 04:03:31 +0000

    Add package AditiRai-GBdistributions/0.1.1/AditiRai_GBdistributions-0.1.1.tar.gz

---
+++ packages/AditiRai-GBdistributions/AditiRai_GBdistributions-0.1.1.tar.gz/AditiRai_GBdistributions-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='AditiRai_GBdistributions',
+      version='0.1.1',
+      description='Gaussian & Binomial distributions',
+      packages=['AditiRai_GBdistributions'],
+      author = 'Aditi Rai',
+      author_email = 'aditi.rai2021@vitstudent.ac.in',
+      zip_safe=False)
