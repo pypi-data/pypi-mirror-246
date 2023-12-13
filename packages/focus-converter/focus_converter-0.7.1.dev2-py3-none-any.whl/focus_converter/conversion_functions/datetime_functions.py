ba60b0f18a7deff32ecce1c2acb493bcd30d6581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 06:04:12 +0000

    Add package focus-converter/0.7.1.dev2/focus_converter-0.7.1.dev2-py3-none-any.whl

---
+++ packages/focus-converter/focus_converter-0.7.1.dev2-py3-none-any.whl/focus_converter/conversion_functions/datetime_functions.py
@@ -0,0 +1,62 @@
+import polars as pl
+
+from focus_converter.configs.base_config import ConversionPlan
+from focus_converter.conversion_functions.validations import ColumnValidator
+
+
+class DateTimeConversionFunctions:
+    @staticmethod
+    def convert_timezone(
+        plan: ConversionPlan, column_alias, column_validator: ColumnValidator
+    ) -> pl.col:
+        # add to column validator and check if source column exists
+        column_validator.map_non_sql_plan(plan=plan, column_alias=column_alias)
+
+        return (
+            pl.col(plan.column)
+            .dt.cast_time_unit("ms")
+            .dt.convert_time_zone(plan.conversion_args)
+            .alias(column_alias)
+        )
+
+    @staticmethod
+    def assign_timezone(
+        plan: ConversionPlan, column_alias, column_validator: ColumnValidator
+    ) -> pl.col:
+        # add to column validator and check if source column exists
+        column_validator.map_non_sql_plan(plan=plan, column_alias=column_alias)
+
+        return (
+            pl.col(plan.column)
+            .dt.cast_time_unit("ms")
+            .dt.replace_time_zone(plan.conversion_args, ambiguous="earliest")
+            .alias(column_alias)
+        )
+
+    @staticmethod
+    def assign_utc_timezone(
+        plan: ConversionPlan, column_alias, column_validator: ColumnValidator
+    ) -> pl.col:
+        # add to column validator and check if source column exists
+        column_validator.map_non_sql_plan(plan=plan, column_alias=column_alias)
+
+        return (
+            pl.col(plan.column)
+            .dt.cast_time_unit("ms")
+            .dt.replace_time_zone("UTC")
+            .alias(column_alias)
+        )
+
+    @staticmethod
+    def parse_datetime(
+        plan: ConversionPlan, column_alias, column_validator: ColumnValidator
+    ) -> pl.col:
+        # add to column validator and check if source column exists
+        column_validator.map_non_sql_plan(plan=plan, column_alias=column_alias)
+
+        return (
+            pl.col(plan.column)
+            .str.strptime(pl.Datetime, plan.conversion_args)
+            .dt.cast_time_unit("ms")
+            .alias(column_alias)
+        )
