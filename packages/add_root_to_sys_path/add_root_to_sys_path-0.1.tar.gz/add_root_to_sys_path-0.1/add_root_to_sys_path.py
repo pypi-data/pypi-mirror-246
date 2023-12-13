639d77673e381cec574e81d2af5560fbb1387f12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 06:04:00 +0000

    Add package add_root_to_sys_path/0.1/add_root_to_sys_path-0.1.tar.gz

---
+++ packages/add_root_to_sys_path/add_root_to_sys_path-0.1.tar.gz/add_root_to_sys_path-0.1/add_root_to_sys_path.py
@@ -0,0 +1,17 @@
+"""Searches a given file's parents for a directory with a given name and adds it to sys.path."""
+
+__version__ = "0.1"
+
+import sys
+from pathlib import Path
+
+def init(file, root_dir_name, min_depth=0, debug=False):
+    parents = Path(file).resolve().parents
+    for i in range(min_depth, len(parents)):
+        if parents[i].name == root_dir_name:
+            if debug:
+                print(f"add-root-to-sys-path: Found root dir '{root_dir_name}' at '{parents[i]}'")
+            sys.path.append(str(Path(file).resolve().parents[i]))
+            break
+    if debug:
+        print(f"add-root-to-sys-path: WARNING: root dir '{root_dir_name}' not found.")
