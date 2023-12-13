f22f5ffdf284e731644b30fb9548b85b9398006e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 05:02:55 +0000

    Add package pimms-learn/0.2.0/pimms-learn-0.2.0.tar.gz

---
+++ packages/pimms-learn/pimms-learn-0.2.0.tar.gz/pimms-learn-0.2.0/vaep/__main__.py
@@ -0,0 +1,8 @@
+# click
+# main command displaying the models
+# each model has its own subcommand
+# common argument go to the main command
+# specific arguments go to the subcommand
+# output -> produces real_na predictions
+# Questions:
+# Should papermill notebooks be reused or not?
