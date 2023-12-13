8046231ea9865826f0c8e9a3acef225d55a5574c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 06:03:21 +0000

    Add package alchemyrohan/0.3.2/alchemyrohan-0.3.2.tar.gz

---
+++ packages/alchemyrohan/alchemyrohan-0.3.2.tar.gz/alchemyrohan-0.3.2/alchemyrohan/__init__.py
@@ -0,0 +1,20 @@
+
+
+"""
+
+Alchemyrohan is an extension package for SqlAlchemy 
+which automatically creates the database 
+models according to the database schema.
+
+More in < README.md > file
+
+"""
+
+
+__author__ = 'Alan Wamberger'
+__version__ = '0.3.2'
+__license__ = 'MIT'
+
+
+from alchemyrohan.assemble import *
+from alchemyrohan.utils import *
