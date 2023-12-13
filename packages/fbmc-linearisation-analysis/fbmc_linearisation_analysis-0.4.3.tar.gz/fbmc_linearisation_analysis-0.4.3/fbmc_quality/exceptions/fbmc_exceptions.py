b5a206ba6c0f691192e8e637e89715b459df08a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 03:03:49 +0000

    Add package fbmc-linearisation-analysis/0.4.3/fbmc_linearisation_analysis-0.4.3.tar.gz

---
+++ packages/fbmc-linearisation-analysis/fbmc_linearisation_analysis-0.4.3.tar.gz/fbmc_linearisation_analysis-0.4.3/fbmc_quality/exceptions/fbmc_exceptions.py
@@ -0,0 +1,14 @@
+class ENTSOELookupException(Exception):
+    pass
+
+
+class WrongTimezoneException(Exception):
+    pass
+
+
+class NaiveTimestampException(Exception):
+    pass
+
+
+class NoInferrableFrequency(Exception):
+    pass
