ba64ae80e15c2d9220650e21307715f3fd6eee99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 06:04:06 +0000

    Add package check-system/1.0.0/check-system-1.0.0.tar.gz

---
+++ packages/check-system/check-system-1.0.0.tar.gz/check-system-1.0.0/src/check_system/__init__.py
@@ -0,0 +1,7 @@
+"""check_system - Tool for checking that a system is ok"""
+
+__version__ = "1.0.0"
+
+from check_system.check_system import MARKERS, cli_wrapper
+
+__all__ = ["cli_wrapper", "MARKERS"]
---
+++ packages/check-system/check-system-1.0.0.tar.gz/check-system-1.0.0/tests/test_lib.py
@@ -0,0 +1,6 @@
+"""Tests for the library."""
+
+
+def test_dummy() -> None:
+    """Dummy test to get CI pipeline to pass until we have added tests."""
+    assert True
