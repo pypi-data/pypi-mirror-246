5177d851acd31f1a9314fe40c1f4264201b73905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 16:03:49 +0000

    Add package consolechess/0.2.24/consolechess-0.2.24.tar.gz

---
+++ packages/consolechess/consolechess-0.2.24.tar.gz/consolechess-0.2.24/consolechess/__main__.py
@@ -0,0 +1,20 @@
+"""Play a game of chess in Python module mode, i.e. `python -m chess`."""
+
+from sys import argv
+
+from . import asciiconsole, console, tui
+
+
+def main() -> None:
+    """Start chess."""
+    args = " ".join(argv)
+    if "ascii" in args:
+        asciiconsole.main()
+    elif "console" in args or "-c" in args:
+        console.main()
+    else:
+        tui.main()
+
+
+if __name__ == "__main__":
+    main()
