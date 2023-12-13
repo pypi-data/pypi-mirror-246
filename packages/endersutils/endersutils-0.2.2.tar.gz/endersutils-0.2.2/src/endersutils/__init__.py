ba363b3deb81825de3cec0beb82aca633f80570a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 16:03:31 +0000

    Add package endersutils/0.2.2/endersutils-0.2.2.tar.gz

---
+++ packages/endersutils/endersutils-0.2.2.tar.gz/endersutils-0.2.2/src/endersutils/__init__.py
@@ -0,0 +1,23 @@
+from .el_funcs import fixedrange, fixedindex
+from .el_globs import globs
+from .el_classes import pairs, component
+from urllib.request import urlopen
+import json
+
+url = urlopen("https://raw.githubusercontent.com/endert1099/EndersUtils/main/versions.json")
+versions = json.loads(url.read())
+
+class EndersUtils():
+    def __init__(self):
+        self.fixedrange = fixedrange.fixedrange
+        self.version = globs.LIBVERSION
+        self.intversion = globs.LIBNUMVERSION
+        self.pair = pairs.Pair
+        self.fixedindex = fixedindex.fixedindex
+        self.components = component.Components
+
+# The main class used, although other instances can be made!
+endersutils = EndersUtils()
+
+if endersutils.intversion not in versions:
+    raise ImportError(f"EndersUtils - Version {endersutils.intversion} is not up to date! Please use any of the following versions: {versions}")
