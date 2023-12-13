e0f5d897a139abe0c9bf15ee8d287033f3c74b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 05:03:20 +0000

    Add package aiopki/0.0.2a14/aiopki-0.0.2a14.tar.gz

---
+++ packages/aiopki/aiopki-0.0.2a14.tar.gz/aiopki-0.0.2a14/aiopki/ext/cryptography/cryptokey.py
@@ -0,0 +1,37 @@
+# Copyright (C) 2023 Cochise Ruhulessin
+#
+# All rights reserved. No warranty, explicit or implicit, provided. In
+# no event shall the author(s) be liable for any claim or damages.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+import aiopki
+from aiopki import CryptoKeyVersion
+from aiopki.types import IAlgorithm
+
+
+class LocalKey(aiopki.VersionedCryptoKey):
+
+    async def discover(self) -> None:
+        for k in self.versions:
+            self._index[k.thumbprint] = k
+
+    async def sign(
+        self,
+        version: CryptoKeyVersion,
+        message: bytes,
+        algorithm: IAlgorithm
+    ) -> bytes:
+        assert version.key is not None
+        return await version.key.sign(message, algorithm)
+
+    async def verify(
+        self,
+        version: CryptoKeyVersion,
+        signature: bytes,
+        message: bytes,
+        algorithm: IAlgorithm
+    ) -> bool:
+        assert version.key is not None
+        return await version.key.verify(signature, message, algorithm)
