fdde6aa611eade455514ab834ed5198c531b24f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 07:03:40 +0000

    Add package bayesian-ab-testing/1.1.2/bayesian_ab_testing-1.1.2.tar.gz

---
+++ packages/bayesian-ab-testing/bayesian_ab_testing-1.1.2.tar.gz/bayesian_ab_testing-1.1.2/bayesian_ab_testing/utils.py
@@ -0,0 +1,69 @@
+"""Non-structured functionality"""
+
+import sqlite3
+from .config import db_path
+
+
+
+class ISQL_Etiquette:
+    """Parent class for sql-facing classes"""
+    def __init__(self):
+        pass
+
+
+    def chk_conn(conn) -> bool:
+        """Check if connection is still alive
+
+        Args:
+            conn (sqlite3.connection): connection
+
+        Returns:
+            bool: True if is alive, false otherwise
+        """        
+        
+        try:
+            conn.cursor()
+            return True
+        except Exception as ex:
+            return False
+
+
+    def refresh_conn(self):
+        """Make sure the connection is still alive"""
+        if not ISQL_Etiquette.chk_conn(self.cnxn):
+            self.cnxn = sqlite3.connect(db_path)
+
+
+    def exec(self, query: str, *args, **kwargs) -> sqlite3.Cursor:
+        """Executres a given query and commits immediately after
+
+        Args:
+            query (str): query string
+
+        Returns:
+            sqlite3.Cursor: cursor
+        """       
+        self.refresh_conn()
+
+        cur = self.cnxn.execute(query, *args, **kwargs)
+        self.cnxn.commit() 
+
+        return cur
+    
+
+    def exec_many(self, query: str, *args, **kwargs) -> sqlite3.Cursor:
+        """Executres many queries and commits immediately after
+
+        Args:
+            query (str): query string
+
+        Returns:
+            sqlite3.Cursor: cursor
+        """        
+
+        self.refresh_conn()
+
+        cur = self.cnxn.executemany(query, *args, **kwargs)
+        self.cnxn.commit()
+
+        return cur
---
+++ packages/bayesian-ab-testing/bayesian_ab_testing-1.1.2.tar.gz/bayesian_ab_testing-1.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    author="""Anush Aghinyan, 
+                Anahit Navoyan, 
+                Yeva Avetisyan, 
+                Naira Maria Barseghyan, 
+                Mher Movsisyan""",
+    author_email="""mher@movsisyan.info""", # needed to change this because of 
+            # ERROR    HTTPError: 400 Bad Request from https://upload.pypi.org/legacy/
+            #  'anush_aghinyan@edu.aua.am, ana...m, mher_movsisyan@edu.aua.am, ' is an invalid value for Author-email. Error: Use a valid email address See
+            #  https://packaging.python.org/specifications/core-metadata for more information.
+    description='AB testing tool, designed for testing data of call tracking services',
+    name='bayesian_ab_testing',
+    version='1.1.2',
+    packages=find_packages(include=['bayesian_ab_testing','bayesian_ab_testing.*']),
+    long_description="""# Bayesian AB Testing  \n\nAB testing tool, designed for testing data of call tracking services\n""",
+    long_description_content_type='text/markdown',
+    python_requires=">=3.11",
+    install_requires=open("requirements.txt", "r").read().split("\n")
+)
