279a310981b5ce6fe020f106de928f5fcc35bd94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 03:03:44 +0000

    Add package examAnalyzerINF219h23/1.0.4/examAnalyzerINF219h23-1.0.4.tar.gz

---
+++ packages/examAnalyzerINF219h23/examAnalyzerINF219h23-1.0.4.tar.gz/examAnalyzerINF219h23-1.0.4/examAnalyzerINF219h23/classes/Constants.py
@@ -0,0 +1,5 @@
+class Constants:
+    """
+    during development we needed this class for constants such as paths for Json files, but now the class is
+    redundant. For future development it will remain for practicle purposes. 
+    """
