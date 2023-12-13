9bfbe72dbf5bd4bdc46520d9d2ff928b5862f265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 16:04:05 +0000

    Add package geography/0.0.81/geography-0.0.81.tar.gz

---
+++ packages/geography/geography-0.0.81.tar.gz/geography-0.0.81/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Operating System :: Microsoft :: Windows :: Windows 10",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3"
+]
+
+setup(
+    name="geography",
+    version="0.0.81",
+    description="Get info about countries and states",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="",
+    author="FrenchFries8854",
+    author_email="frenchfries8854@gmail.com",
+    license="MIT",
+    classifiers=classifiers,
+    keywords="geography",
+    packages=find_packages(),
+    install_requires=[""]
+)
