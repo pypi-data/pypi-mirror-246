fe6a14d1b53f6a983d0e88d6e3b828db5ccba0bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 03:03:37 +0000

    Add package psyke/0.5.2.dev3/psyke-0.5.2.dev3-py3-none-any.whl

---
+++ packages/psyke/psyke-0.5.2.dev3-py3-none-any.whl/psyke/clustering/__init__.py
@@ -0,0 +1,15 @@
+from abc import ABC
+from typing import Iterable
+
+from psyke import Clustering, Target
+from psyke.extraction.hypercubic import HyperCube
+from psyke.hypercubepredictor import HyperCubePredictor
+
+
+class HyperCubeClustering(HyperCubePredictor, Clustering, ABC):
+
+    def __init__(self, output: Target = Target.CONSTANT, normalization=None):
+        HyperCubePredictor.__init__(self, output=output, normalization=normalization)
+
+    def get_hypercubes(self) -> Iterable[HyperCube]:
+        raise NotImplementedError('get_hypercubes')
