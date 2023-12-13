358874a70f13a211c55094069613b7e22e461638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 04:03:14 +0000

    Add package devrevteamthirtyone/0.4.0/devrevteamthirtyone-0.4.0-py3-none-any.whl

---
+++ packages/devrevteamthirtyone/devrevteamthirtyone-0.4.0-py3-none-any.whl/devrev_team_thirty_one/__init__.py
@@ -0,0 +1,4 @@
+from .Argument import Argument
+from .ReturnType import ReturnType
+from .Tool import Tool
+from .utils import convert_python_to_solution
---
+++ packages/devrevteamthirtyone/devrevteamthirtyone-0.4.0-py3-none-any.whl/devrev_team_thirty_one/pipeline/pipeline.py
@@ -0,0 +1,12 @@
+from .imports import *
+from .model import Model
+from .retriever import Retriever
+class Pipeline:
+    def __init__(self,config):
+        self.config = config
+        self.rtr = Retriever(config.embedding_model)
+        self.model = Model(self.config)
+    def __call__(self,query, tools,examples):
+        retrieved_tools = self.rtr(query,tools,self.config.num_retrieved)
+        output = self.model(query,self.rtr.strip_embeddings(retrieved_tools),examples)
+        return json.loads(output)
