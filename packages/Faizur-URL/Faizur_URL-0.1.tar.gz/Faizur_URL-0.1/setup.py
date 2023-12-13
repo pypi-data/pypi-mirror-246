cb27153c59cbba33b8bb28577dc11e9656e50fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 05:03:04 +0000

    Add package Faizur-URL/0.1/Faizur_URL-0.1.tar.gz

---
+++ packages/Faizur-URL/Faizur_URL-0.1.tar.gz/Faizur_URL-0.1/setup.py
@@ -0,0 +1,23 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='Faizur_URL',
+    version='0.1',
+    packages=['Faizur_URL'],
+    install_requires=[
+        'pyshorteners',
+    ],
+    author='Faizur',
+    author_email='Faizursearch@gmail.com',
+    description='A Python module for URL shortening with various services BY Faizur',
+    long_description='URL shortening module made by Faizur',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
