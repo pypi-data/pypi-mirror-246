f68e77f231dcef811456d846f0f554beaf8d96a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 14:03:56 +0000

    Add package woodwork/0.27.0/woodwork-0.27.0-py3-none-any.whl

---
+++ packages/woodwork/woodwork-0.27.0-py3-none-any.whl/woodwork/tests/testing_utils/__init__.py
@@ -0,0 +1,32 @@
+# flake8: noqa
+import pytest
+
+from woodwork.tests.testing_utils.table_utils import (
+    _check_close,
+    check_empty_box_plot_dict,
+    concat_dataframe_or_series,
+    dep_between_cols,
+    is_property,
+    is_public_method,
+    to_pandas,
+    validate_subset_schema,
+)
+
+
+def pd_to_dask(series):
+    dd = pytest.importorskip("dask.dataframe", reason="Dask not installed, skipping")
+    return dd.from_pandas(series, npartitions=1)
+
+
+def pd_to_spark(series):
+    ps = pytest.importorskip("pyspark.pandas", reason="Spark not installed, skipping")
+    return ps.from_pandas(convert_tuples_to_lists(series))
+
+
+def convert_tuples_to_lists(series):
+    def apply_func(value):
+        if type(value) is tuple:
+            return list(value)
+        return value
+
+    return series.apply(apply_func)
