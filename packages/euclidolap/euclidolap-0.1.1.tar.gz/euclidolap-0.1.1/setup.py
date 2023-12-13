a81d345c07e11c6ab606cdaa883cd21b19885316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 16:03:55 +0000

    Add package euclidolap/0.1.1/euclidolap-0.1.1.tar.gz

---
+++ packages/euclidolap/euclidolap-0.1.1.tar.gz/euclidolap-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='euclidolap',
+    version='0.1.1',
+    packages=find_packages(),
+
+    author="euclidolap",
+    author_email="euclidolap@outlook.com",
+
+    description="Python for EuclidOLAP.",
+
+    url="https://github.com/EuclidOLAP/EuclidOLAP",
+
+    keywords="olap"
+)
