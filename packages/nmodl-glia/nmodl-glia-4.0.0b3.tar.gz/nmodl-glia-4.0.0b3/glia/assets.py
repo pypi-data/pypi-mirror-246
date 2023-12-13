7c993e1290a2db95e93c6d1ba98428b026c3b0db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 04:03:50 +0000

    Add package nmodl-glia/4.0.0b3/nmodl-glia-4.0.0b3.tar.gz

---
+++ packages/nmodl-glia/nmodl-glia-4.0.0b3.tar.gz/nmodl-glia-4.0.0b3/glia/assets.py
@@ -0,0 +1,234 @@
+import os
+import shutil
+import subprocess
+import typing
+from pathlib import Path
+from tempfile import TemporaryDirectory, mkdtemp
+
+from . import _mpi
+from ._fs import get_cache_path, read_cache, update_cache
+from ._hash import get_directory_hash, get_package_hash, get_package_mods_hash
+from .exceptions import *
+
+if typing.TYPE_CHECKING:
+    import arbor
+
+
+class _ModList(list):
+    def __init__(self, package: "Package", *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.pkg = package
+        for item in self:
+            if not isinstance(item, Mod):
+                raise PackageFileError(f"Package mod '{item}' is not a valid `glia.Mod`.")
+            item.set_package(package)
+
+    def __setitem__(self, key, value):
+        if not isinstance(value, Mod):
+            raise PackageFileError(f"Package mod '{value}' is not a valid `glia.Mod`.")
+        super().__setitem__(key, value)
+        value.set_package(self.pkg)
+
+    def append(self, item):
+        item.set_package(self.pkg)
+        super().append(item)
+
+    def extend(self, itr):
+        super().extend(i.set_package(self.pkg) or i for i in itr)
+
+
+class Package:
+    def __init__(self, name: str, root: Path, *, mods: list["Mod"] = None, builtin=False):
+        self._name = name
+        self._root = Path(root)
+        self.mods: list["Mod"] = _ModList(self, [] if mods is None else mods)
+        # Exceptional flag for the NEURON builtins.
+        # They need a definition to be `insert`ed,
+        # but have no mod files to be compiled.
+        self.builtin = builtin
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def hash(self):
+        return get_package_hash(self)
+
+    @property
+    def mod_hash(self):
+        return get_package_mods_hash(self)
+
+    @property
+    def root(self):
+        return self._root
+
+
+class Mod:
+    def __init__(
+        self,
+        relpath: str,
+        asset_name,
+        *,
+        variant="0",
+        is_point_process=False,
+        is_artificial_cell=False,
+        dialect: typing.Union[typing.Literal["arbor"], typing.Literal["neuron"]] = None,
+        builtin=False,
+    ):
+        self._pkg: typing.Optional[Package] = None
+        self.relpath = relpath
+        self.asset_name = asset_name
+        self.variant = variant
+        self.is_point_process = is_point_process
+        self.is_artificial_cell = is_artificial_cell
+        self.dialect = dialect
+        self.builtin = builtin
+
+    def set_package(self, package: Package):
+        self._pkg = package
+
+    @property
+    def pkg(self):
+        return self._pkg
+
+    @property
+    def pkg_name(self):
+        return self._pkg.name
+
+    @property
+    def mech_id(self):
+        return (self.asset_name, self.variant, self.pkg_name)
+
+    @property
+    def mod_name(self):
+        if self.builtin:
+            return self.asset_name
+        return f"glia__{self.pkg_name}__{self.asset_name}__{self.variant}"
+
+    @property
+    def path(self) -> Path:
+        return self.pkg.root / self.relpath
+
+
+class Catalogue:
+    def __init__(self, name, source_file):
+        self._name = name
+        self._source = os.path.dirname(source_file)
+        self._cache = get_cache_path(self._name, prefix="_arb")
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def source(self):
+        return self._source
+
+    def load(self) -> "arbor.catalogue":
+        import arbor
+
+        if not self.is_fresh():
+            self.build()
+        return arbor.load_catalogue(self._get_library_path())
+
+    def _get_library_path(self):
+        return os.path.join(self._cache, f"{self._name}-catalogue.so")
+
+    def is_fresh(self):
+        if not os.path.exists(self._get_library_path()):
+            return False
+        try:
+            cache_data = read_cache()
+            # Backward compatibility with old installs that
+            # have a JSON file without cat_hashes in it.
+            cached = cache_data.get("cat_hashes", dict()).get(self._name, None)
+            return cached == self._hash()
+        except FileNotFoundError as _:
+            return False
+
+    def _hash(self):
+        import arbor
+
+        source_hash = get_directory_hash(self.get_mod_path())
+        arbor_hash = str(arbor.config())
+        return source_hash + arbor_hash
+
+    def get_mod_path(self):
+        return os.path.abspath(os.path.join(self._source, "mod"))
+
+    def build(self, verbose=None, debug=False, gpu=None):
+        # Turn verbosity on if debug is on, unless it's explicitly toggled off.
+        verbose = False if verbose is False else verbose or debug
+
+        def run_build():
+            self._build_local(verbose, debug, gpu)
+
+        build_err = None
+        try:
+            if _mpi.main_node:
+                run_build()
+        except Exception as err:
+            _mpi.bcast(err)
+            raise err from None
+        else:
+            build_err = _mpi.bcast(build_err)
+            if build_err:
+                raise BuildCatalogueError(
+                    "Catalogue build error, look for main node error."
+                ) from None
+
+    def _build_local(self, verbose, debug, gpu):
+        tmp_dir = TemporaryDirectory
+        if debug:
+            # Temp dir context manager that doesn't clean up the build folder so that
+            # the generated cpp code can be debugged
+            class TmpDir:
+                def __enter__(*args, **kwargs):
+                    return mkdtemp()
+
+                def __exit__(*args, **kwargs):
+                    pass
+
+            tmp_dir = TmpDir
+
+        mod_path = self.get_mod_path()
+        with tmp_dir() as tmp:
+            pwd = os.getcwd()
+            os.chdir(tmp)
+            cmd = f"arbor-build-catalogue {self._name} {mod_path}"
+            try:
+                subprocess.run(
+                    cmd
+                    + (" --quiet" if not verbose else "")
+                    + (" --verbose" if verbose else "")
+                    + (" --debug" if debug else "")
+                    + (f" --gpu={gpu}" if gpu else ""),
+                    shell=True,
+                    check=True,
+                    capture_output=not verbose,
+                )
+            except subprocess.CalledProcessError as e:
+                msg_p = [f"ABC errored out with exitcode {e.returncode}"]
+                if verbose:
+                    msg_p += ["Check log above for error."]
+                else:
+                    msg_p += [
+                        f"Command: {cmd}\n---- ABC output ----",
+                        e.stdout.decode(),
+                        "---- ABC error  ----",
+                        e.stderr.decode(),
+                    ]
+                msg = "\n\n".join(msg_p)
+                raise BuildCatalogueError(msg) from None
+            os.makedirs(self._cache, exist_ok=True)
+            shutil.copy2(f"{self._name}-catalogue.so", self._cache)
+            os.chdir(pwd)
+            if debug:
+                print(f"Debug copy of catalogue in '{tmp}'")
+        # Cache directory hash of current mod files so we only rebuild on source code
+        # changes.
+        cache_data = read_cache()
+        cat_hashes = cache_data.setdefault("cat_hashes", dict())
+        cat_hashes[self._name] = self._hash()
+        update_cache(cache_data)
---
+++ packages/nmodl-glia/nmodl-glia-4.0.0b3.tar.gz/nmodl-glia-4.0.0b3/tests/test_cli.py
@@ -0,0 +1,181 @@
+import importlib.util
+import os
+import unittest
+from pathlib import Path
+
+from click.testing import CliRunner
+
+import glia._cli
+import glia._mpi
+from glia import Mod, Package
+
+from . import _shared
+
+
+def run_cli_command(command, xfail=False, **kwargs):
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(glia._cli.glia, command, **kwargs)
+        if not xfail and result.exception:
+            raise result.exception
+        return result
+
+
+def import_tmp_package(path):
+    path = Path(path) / "__init__.py"
+    spec = importlib.util.spec_from_file_location("__test_package__", path)
+    m = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(m)
+    return m.package
+
+
+class TestCLI(unittest.TestCase):
+    """
+    Test CLI commands
+    """
+
+    def test_glia(self):
+        result = run_cli_command([])
+        self.assertNotEqual("", result.output, "Should show help message")
+        self.assertEqual(0, result.exit_code)
+
+    @unittest.skipIf(glia._mpi.parallel_run, "Skip in parallel")
+    @_shared.skipUnlessTestMods
+    def test_compile(self):
+        # todo: test this test
+        result = run_cli_command(["compile"])
+        self.assertEqual(0, result.exit_code)
+        self.assertRegex(result.output, r"(\d+) out of (\1) passed")
+
+    @unittest.skipIf(glia._mpi.parallel_run, "Skip in parallel")
+    @_shared.skipIfInstalled()
+    def test_compile_nopkg(self):
+        # todo: test this test
+        result = run_cli_command(["compile"])
+        self.assertEqual(0, result.exit_code)
+        self.assertRegex(result.output, r"0 out of 0 passed")
+
+    def test_list(self):
+        result = run_cli_command(["list"])
+        self.assertEqual(0, result.exit_code)
+
+    @_shared.skipUnlessTestMods
+    def test_show(self):
+        # todo: test this test
+        result = run_cli_command(["show", "AMPA"])
+        self.assertEqual(0, result.exit_code)
+
+    def test_show_unknown(self):
+        result = run_cli_command(["show", "doesntexist"], xfail=True)
+        self.assertIn('Unknown ASSET "doesntexist"', result.output)
+        self.assertEqual(2, result.exit_code)
+
+    @_shared.skipUnlessTestMods
+    def test_show_pkg(self):
+        # todo: test this test
+        result = run_cli_command(["show-pkg", "glia_test_mods"])
+        self.assertEqual(0, result.exit_code)
+
+    def test_show_unknown_pkg(self):
+        result = run_cli_command(["show-pkg", "doesntexist"], xfail=True)
+        self.assertIn('Unknown PACKAGE "doesntexist"', result.output)
+        self.assertEqual(2, result.exit_code)
+
+
+class TestPackagingCLI(unittest.TestCase):
+    def test_new_package(self):
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            result = runner.invoke(glia._cli.glia, ["pkg", "new"], input="\n\n\n\n\n\n\n")
+            self.assertEqual(
+                ["glia_package"],
+                os.listdir(),
+                "Should have created the package folder",
+            )
+            self.assertIn(
+                "glia_package",
+                os.listdir("glia_package"),
+                "Should have created the module folder",
+            )
+            os.chdir("glia_package")
+            package = import_tmp_package("glia_package")
+            self.assertEqual(Package, type(package))
+            self.assertEqual(0, result.exit_code)
+
+    def test_add_mod(self):
+        mod_path = Path(__file__).parent / "data" / "mods" / "Na.mod"
+        mod_source = mod_path.read_text()
+        assert "SUFFIX Na\n" in mod_path.read_text(), "Source of Na was mutated"
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            runner.invoke(glia._cli.glia, ["pkg", "new"], input="\n\n\n\n\n\n\n")
+            os.chdir("./glia_package")
+            result = runner.invoke(
+                glia._cli.glia,
+                [
+                    "pkg",
+                    "add",
+                    str(mod_path),
+                    "--name",
+                    "Na",
+                ],
+            )
+
+            self.assertEqual(0, result.exit_code)
+            self.assertEqual([".gitkeep", "Na__0.mod"], os.listdir("glia_package/mods"))
+            package = import_tmp_package("glia_package")
+            self.assertEqual(Package, type(package), "Broken package declaration")
+            self.assertEqual(1, len(package.mods), "Should have added mod")
+            mod = package.mods[0]
+            mod: Mod
+            self.assertEqual(Mod, type(mod), "Broken mod declaration")
+            self.assertEqual("Na", mod.asset_name, "Wrong name")
+            self.assertEqual("0", mod.variant, "Wrong variant")
+            self.assertEqual("glia_package", mod.pkg_name, "Wrong package name")
+            self.assertEqual(
+                "glia_package/mods/Na__0.mod",
+                str(mod.path.relative_to(Path().resolve())),
+                "Wrong mod path",
+            )
+            self.assertTrue(
+                mod.path.exists(),
+                "Mod path not created",
+            )
+            mod_text = mod.path.read_text()
+            self.assertIn(f"SUFFIX {mod.mod_name}", mod_text, "Mod suffix not written")
+            self.assertEqual(
+                mod_source, mod_path.read_text(), "Adding mod file changed mod source"
+            )
+
+    def test_add_synapse(self):
+        mod_path = Path(__file__).parent / "data" / "mods" / "AMPA.mod"
+        mod_source = mod_path.read_text()
+        assert (
+            "POINT_PROCESS AMPA\n" in mod_path.read_text()
+        ), "Source of AMPA was mutated"
+        runner = CliRunner()
+        with runner.isolated_filesystem():
+            runner.invoke(glia._cli.glia, ["pkg", "new"], input="\n\n\n\n\n\n\n")
+            os.chdir("./glia_package")
+            result = runner.invoke(
+                glia._cli.glia,
+                [
+                    "pkg",
+                    "add",
+                    str(mod_path),
+                    "--name",
+                    "AMPA",
+                ],
+            )
+
+            self.assertEqual(0, result.exit_code)
+            package = import_tmp_package("glia_package")
+            mod = package.mods[0]
+            mod: Mod
+            mod_text = mod.path.read_text()
+            self.assertIn(
+                f"POINT_PROCESS {mod.mod_name}", mod_text, "Mod suffix not written"
+            )
+            self.assertEqual(
+                mod_source, mod_path.read_text(), "Adding mod file changed mod source"
+            )
