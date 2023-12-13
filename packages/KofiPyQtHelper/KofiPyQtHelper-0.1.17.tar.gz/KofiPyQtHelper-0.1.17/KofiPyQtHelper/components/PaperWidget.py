f91eabac12e28f98efa63dd15581c6bf3c250bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 17:05:55 +0000

    Add package KofiPyQtHelper/0.1.17/KofiPyQtHelper-0.1.17.tar.gz

---
+++ packages/KofiPyQtHelper/KofiPyQtHelper-0.1.17.tar.gz/KofiPyQtHelper-0.1.17/KofiPyQtHelper/components/PaperWidget.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+Author       : Kofi
+Date         : 2023-07-13 12:09:49
+LastEditors  : Kofi
+LastEditTime : 2023-07-13 12:09:50
+Description  : 
+"""
+
+
+class PaperWidget:
+    def __init__(self) -> None:
+        super(PaperWidget, self).__init__()
+
+
+class PaperListWidget:
+    def __init__(self) -> None:
+        pass
