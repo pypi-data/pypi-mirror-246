3ab9abf86f30a69f5325976862db92eb7ebd96f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 16:04:28 +0000

    Add package fake-bge-module/20231213/fake-bge-module-20231213.tar.gz

---
+++ packages/fake-bge-module/fake-bge-module-20231213.tar.gz/fake-bge-module-20231213/bgui/__init__.py
@@ -0,0 +1,16 @@
+import sys
+import typing
+from . import theme
+from . import text_input
+from . import system
+from . import label
+from . import text_block
+from . import frame
+from . import widget
+from . import image_button
+from . import list_box
+from . import image
+from . import progress_bar
+from . import frame_button
+
+GenericType = typing.TypeVar("GenericType")
---
+++ packages/fake-bge-module/fake-bge-module-20231213.tar.gz/fake-bge-module-20231213/bl_operators/__init__.py
@@ -0,0 +1,49 @@
+import sys
+import typing
+from . import object
+from . import wm
+from . import screen_play_rendered_anim
+from . import object_randomize_transform
+from . import sequencer
+from . import spreadsheet
+from . import constraint
+from . import uvcalc_follow_active
+from . import file
+from . import uvcalc_lightmap
+from . import view3d
+from . import assets
+from . import presets
+from . import object_quick_effects
+from . import object_align
+from . import userpref
+from . import add_mesh_torus
+from . import vertexpaint_dirt
+from . import uvcalc_transform
+from . import anim
+from . import console
+from . import node
+from . import mesh
+from . import geometry_nodes
+from . import rigidbody
+from . import image
+from . import freestyle
+from . import clip
+from . import bmesh
+
+GenericType = typing.TypeVar("GenericType")
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
