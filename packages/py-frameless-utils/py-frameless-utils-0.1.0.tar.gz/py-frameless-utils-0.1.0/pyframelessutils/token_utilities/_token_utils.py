d8948954e15a86942f93e5ad7d57a7d7ec57d228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 04:03:53 +0000

    Add package py-frameless-utils/0.1.0/py-frameless-utils-0.1.0.tar.gz

---
+++ packages/py-frameless-utils/py-frameless-utils-0.1.0.tar.gz/py-frameless-utils-0.1.0/pyframelessutils/token_utilities/_token_utils.py
@@ -0,0 +1,23 @@
+from ._custom_errors import TokenMissing
+from jwt import decode
+
+def token_parser(token:str, keyword:str|None, verify_signature: bool = False) -> str|dict|None:
+    """
+    Method to parse Bearer token
+    :param token: token value
+    :param keyword: keyword to parse value of (if None - returns whole dict with keys)
+    :param verify_signature: verify signature
+    :return: value by keyword, dictionary or None
+    """
+
+    if not token :
+        raise TokenMissing("Missing token","No token value for parsing.")
+
+    token = token.replace("Bearer ", "")
+
+    decoded = decode(
+        token.replace("Bearer ", ""),
+        algorithms=["HS256"],
+        options={"verify_signature": verify_signature},
+    )
+    return decoded.get(keyword, None) if keyword else decoded
