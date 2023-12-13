38ec3408bb3636da8449b659b6b0e52f5ef97a09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 04:03:11 +0000

    Add package AditiRai-GBdistributions/0.0.1/AditiRai-GBdistributions-0.0.1.tar.gz

---
+++ packages/AditiRai-GBdistributions/AditiRai-GBdistributions-0.0.1.tar.gz/AditiRai-GBdistributions-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='AditiRai-GBdistributions',
+      version='0.0.1',
+      description='Gaussian distributions',
+      packages=['AditiRai-GBdistributions'],
+      author = 'Aditi Rai',
+      author_email = 'aditi.rai2021@vitstudent.ac.in',
+      zip_safe=False)
