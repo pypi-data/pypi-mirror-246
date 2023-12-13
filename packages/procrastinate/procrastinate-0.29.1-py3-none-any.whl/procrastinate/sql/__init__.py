8bbd6d2b6c3da7f1e07227d628e331e0a1c1a39c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 17:06:17 +0000

    Add package procrastinate/0.29.1/procrastinate-0.29.1-py3-none-any.whl

---
+++ packages/procrastinate/procrastinate-0.29.1-py3-none-any.whl/procrastinate/sql/__init__.py
@@ -0,0 +1,36 @@
+import re
+import sys
+from typing import Dict
+
+# https://github.com/pypa/twine/pull/551
+if sys.version_info[:2] < (3, 9):  # coverage: exclude
+    import importlib_resources
+else:  # coverage: exclude
+    import importlib.resources as importlib_resources
+
+QUERIES_REGEX = re.compile(r"(?:\n|^)-- ([a-z0-9_]+) --\n(?:-- .+\n)*", re.MULTILINE)
+
+
+def parse_query_file(query_file: str) -> Dict["str", "str"]:
+    split = iter(QUERIES_REGEX.split(query_file))
+    next(split)  # Consume the header of the file
+    result = {}
+    try:
+        while True:
+            key = next(split)
+            value = next(split).strip()
+            result[key] = value
+    except StopIteration:
+        pass
+    return result
+
+
+def get_queries() -> Dict["str", "str"]:
+    return parse_query_file(
+        (importlib_resources.files("procrastinate.sql") / "queries.sql").read_text(
+            encoding="utf-8"
+        )
+    )
+
+
+queries = get_queries()
