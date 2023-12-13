eb8aa4394a7b7f7f9966d71b34bdfb998b453c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 03:03:06 +0000

    Add package the-pytech-calculator/0.0.2/the_pytech_calculator-0.0.2-py3-none-any.whl

---
+++ packages/the-pytech-calculator/the_pytech_calculator-0.0.2-py3-none-any.whl/the_pytech_calculator/divide.py
@@ -0,0 +1,6 @@
+"""Simple module for dividing 2 numbers."""
+
+
+def divide(x, y):
+    """Divides x into y and returns the quotient."""
+    return x // y
---
+++ packages/the-pytech-calculator/the_pytech_calculator-0.0.2-py3-none-any.whl/the_pytech_calculator/extras/hello_world.py
@@ -0,0 +1,6 @@
+"""Simple module to print a string."""
+
+
+def hello(name):
+    """Prints hello + name of user."""
+    return f"Hello, {name}!"
