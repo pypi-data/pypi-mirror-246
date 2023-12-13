16d3c36690316dd686b010cf7592e967da604545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 03:03:05 +0000

    Add package netsys-client/0.41.0/netsys_client-0.41.0.tar.gz

---
+++ packages/netsys-client/netsys_client-0.41.0.tar.gz/netsys_client-0.41.0/netsys_client/nf/controller.py
@@ -0,0 +1,12 @@
+from . import NFMethods
+
+class NFController(NFMethods):
+    
+    def __init__(self, data):
+
+        self._data = data
+        self._file_path = self.map_nf_path()
+
+    def execute(self):
+
+        self.write_conf(self._file_path, self._data)
