ba5688008a2fc2defcf710c84849d4e9a612ac6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 14:04:19 +0000

    Add package PyPlumIO/0.5.11/PyPlumIO-0.5.11.tar.gz

---
+++ packages/PyPlumIO/PyPlumIO-0.5.11.tar.gz/PyPlumIO-0.5.11/docs/source/conf.py
@@ -0,0 +1,41 @@
+from importlib.metadata import version as pkg_version
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../"))
+
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "PyPlumIO"
+copyright = "2023, Denis Paavilainen"
+author = "Denis Paavilainen"
+release = pkg_version("pyplumio")
+version = ".".join(release.split(".")[:2])
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.autosectionlabel",
+]
+autosummary_generate = True
+
+templates_path = ["_templates"]
+exclude_patterns: list[str] = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
