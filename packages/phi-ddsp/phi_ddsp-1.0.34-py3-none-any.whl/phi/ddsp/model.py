900f8a4984137e91066e663c9b4850d26559d1e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 16:03:50 +0000

    Add package phi-ddsp/1.0.34/phi_ddsp-1.0.34-py3-none-any.whl

---
+++ packages/phi-ddsp/phi_ddsp-1.0.34-py3-none-any.whl/phi/ddsp/model.py
@@ -0,0 +1,186 @@
+import torch
+import torch.nn as nn
+from .core import mlp, gru, scale_function, remove_above_nyquist, upsample
+from .core import harmonic_synth, amp_to_impulse_response, fft_convolve
+from .core import resample
+import math
+
+
+class Reverb(nn.Module):
+
+    def __init__(self, length, sampling_rate, initial_wet=0, initial_decay=5):
+        super().__init__()
+        self.length = length
+        self.sampling_rate = sampling_rate
+
+        self.noise = nn.Parameter((torch.rand(length) * 2 - 1).unsqueeze(-1))
+        self.decay = nn.Parameter(torch.tensor(float(initial_decay)))
+        self.wet = nn.Parameter(torch.tensor(float(initial_wet)))
+
+        t = torch.arange(self.length) / self.sampling_rate
+        t = t.reshape(1, -1, 1)
+        self.register_buffer("t", t)
+
+    def build_impulse(self):
+        t = torch.exp(-nn.functional.softplus(-self.decay) * self.t * 500)
+        noise = self.noise * t
+        impulse = noise * torch.sigmoid(self.wet)
+        impulse[:, 0] = 1
+        return impulse
+
+    def forward(self, x):
+        lenx = x.shape[1]
+        impulse = self.build_impulse()
+        impulse = nn.functional.pad(impulse, (0, 0, 0, lenx - self.length))
+
+        x = fft_convolve(x.squeeze(-1), impulse.squeeze(-1)).unsqueeze(-1)
+
+        return x
+
+
+class DDSP(nn.Module):
+
+    def __init__(self, hidden_size, n_harmonic, n_bands, n_conditional_params, sampling_rate,
+                 block_size):
+        super().__init__()
+        self.register_buffer("sampling_rate", torch.tensor(sampling_rate))
+        self.register_buffer("block_size", torch.tensor(block_size))
+
+        self.n_conditional_params = n_conditional_params + 2
+
+        self.in_mlps = nn.ModuleList([mlp(1, hidden_size, 3)] * self.n_conditional_params)
+        self.gru = gru(self.n_conditional_params, hidden_size)
+        self.out_mlp = mlp(hidden_size + self.n_conditional_params, hidden_size, 3)
+
+        self.proj_matrices = nn.ModuleList([
+            nn.Linear(hidden_size, n_harmonic + 1),
+            nn.Linear(hidden_size, n_bands),
+        ])
+
+        self.reverb = Reverb(sampling_rate, sampling_rate)
+
+        self.register_buffer("cache_gru", torch.zeros(1, 1, hidden_size))
+        self.register_buffer("phase", torch.zeros(2))
+
+    def forward(self, pitch, conditional_parameters, loudness):
+        # Concat pitch and loudness
+        hidden_list = [
+            self.in_mlps[0](pitch),
+            self.in_mlps[1](loudness),
+        ]
+
+        # Concat each conditional parameter
+        for i in range(2, self.n_conditional_params):
+            hidden_list.append(self.in_mlps[i](conditional_parameters[:, i - 2, :, :]))
+
+        # Concatenate all intermediate results
+        hidden = torch.cat(hidden_list, dim=-1)
+
+        # Concat pitch and loudness again
+        gru_output = self.gru(hidden)[0]
+        hidden_list = [gru_output, pitch, loudness]
+
+        # Concat each conditional parameter ... again
+        for i in range(2, self.n_conditional_params):
+            hidden_list.append(conditional_parameters[:, i - 2, :, :])
+
+        # Concatenate all intermediate results
+        hidden = torch.cat(hidden_list, dim=-1)
+
+        hidden = self.out_mlp(hidden)
+
+        # harmonic part
+        param = scale_function(self.proj_matrices[0](hidden))
+
+        total_amp = param[..., :1]
+        amplitudes = param[..., 1:]
+
+        amplitudes = remove_above_nyquist(
+            amplitudes,
+            pitch,
+            self.sampling_rate,
+        )
+        amplitudes /= amplitudes.sum(-1, keepdim=True)
+        amplitudes *= total_amp
+
+        amplitudes = upsample(amplitudes, self.block_size)
+        pitch = upsample(pitch, self.block_size)
+
+        harmonic = harmonic_synth(pitch, amplitudes, self.sampling_rate)
+
+        # noise part
+        param = scale_function(self.proj_matrices[1](hidden) - 5)
+
+        impulse = amp_to_impulse_response(param, self.block_size)
+        noise = torch.rand(
+            impulse.shape[0],
+            impulse.shape[1],
+            self.block_size,
+        ).to(impulse) * 2 - 1
+
+        noise = fft_convolve(noise, impulse).contiguous()
+        noise = noise.reshape(noise.shape[0], -1, 1)
+
+        signal = harmonic + noise
+
+        #reverb part
+        signal = self.reverb(signal)
+
+        return signal
+
+    def realtime_forward(self, pitch, centroid, loudness):
+        hidden = torch.cat([
+            self.in_mlps[0](pitch),
+            self.in_mlps[1](loudness),
+            self.in_mlps[2](centroid),
+        ], -1)
+
+        gru_out, cache = self.gru(hidden, self.cache_gru)
+        self.cache_gru.copy_(cache)
+
+        hidden = torch.cat([gru_out, pitch, loudness], -1)
+        hidden = self.out_mlp(hidden)
+
+        # harmonic part
+        param = scale_function(self.proj_matrices[0](hidden))
+
+        total_amp = param[..., :1]
+        amplitudes = param[..., 1:]
+
+        amplitudes = remove_above_nyquist(
+            amplitudes,
+            pitch,
+            self.sampling_rate,
+        )
+        amplitudes /= amplitudes.sum(-1, keepdim=True)
+        amplitudes *= total_amp
+
+        amplitudes = upsample(amplitudes, self.block_size)
+        pitch = upsample(pitch, self.block_size)
+
+        n_harmonic = amplitudes.shape[-1]
+        omega = torch.cumsum(2 * math.pi * pitch / self.sampling_rate, 1)
+
+        omega = omega + self.phase
+        self.phase.copy_(omega[0, -1, 0] % (2 * math.pi))
+
+        omegas = omega * torch.arange(1, n_harmonic + 1).to(omega)
+
+        harmonic = (torch.sin(omegas) * amplitudes).sum(-1, keepdim=True)
+
+        # noise part
+        param = scale_function(self.proj_matrices[1](hidden) - 5)
+
+        impulse = amp_to_impulse_response(param, self.block_size)
+        noise = torch.rand(
+            impulse.shape[0],
+            impulse.shape[1],
+            self.block_size,
+        ).to(impulse) * 2 - 1
+
+        noise = fft_convolve(noise, impulse).contiguous()
+        noise = noise.reshape(noise.shape[0], -1, 1)
+
+        signal = harmonic + noise
+
+        return signal
