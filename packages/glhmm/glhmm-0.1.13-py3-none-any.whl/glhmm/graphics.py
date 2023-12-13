0172e5e1fe95114bed464a7134d930c221355dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 07:03:47 +0000

    Add package glhmm/0.1.13/glhmm-0.1.13-py3-none-any.whl

---
+++ packages/glhmm/glhmm-0.1.13-py3-none-any.whl/glhmm/graphics.py
@@ -0,0 +1,325 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Basic graphics - Gaussian Linear Hidden Markov Model
+@author: Diego Vidaurre 2023
+"""
+import numpy as np
+import seaborn as sb
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+import pandas as pd
+
+from . import utils
+# import utils
+
+
+
+def show_trans_prob_mat(hmm,only_active_states=False,show_diag=True,show_colorbar=True):
+    """Displays the transition probability matrix of a given HMM.
+
+    Parameters:
+    -----------
+    hmm: HMM object
+        An instance of the HMM class containing the transition probability matrix to be visualized.
+    only_active_states : bool, optional, default=False
+        Whether to display only active states or all states in the matrix.
+    show_diag : bool, optional, defatult=True
+        Whether to display the diagonal elements of the matrix or not.
+    show_colorbar : bool, optional, default=True
+        Whether to display the colorbar next to the matrix or not.
+    """
+    
+    P = np.copy(hmm.P)
+    if only_active_states:
+        P = P[hmm.active_states,hmm.active_states]
+        
+    K = P.shape[0]
+
+    if not show_diag:
+        for k in range(P.shape[0]):
+            P[k,k] = 0
+            P[k,:] = P[k,:] / np.sum(P[k,:])
+
+    _,ax = plt.subplots()
+    g = sb.heatmap(ax=ax,data=P,\
+        cmap='bwr',xticklabels=np.arange(K), yticklabels=np.arange(K),
+        square=True,cbar=show_colorbar)
+    for k in range(K):
+        g.plot([0, K],[k, k], '-k')
+        g.plot([k, k],[0, K], '-k')
+
+    ax.axhline(y=0, color='k',linewidth=4)
+    ax.axhline(y=K, color='k',linewidth=4)
+    ax.axvline(x=0, color='k',linewidth=4)
+    ax.axvline(x=K, color='k',linewidth=4)
+
+
+def show_Gamma(Gamma, line_overlay=None, tlim=None, Hz=1, palette='viridis'):
+    """Displays the activity of the hidden states as a function of time.
+    
+    Parameters:
+    -----------
+    Gamma : array of shape (n_samples, n_states)
+        The state timeseries probabilities.
+    line_overlay : array of shape (n_samples, 1)
+        A secondary related data type to overlay as a line.
+    tlim : 2x1 array or None, default=None
+        The time interval to be displayed. If None (default), displays the 
+        entire sequence.
+    Hz : int, default=1
+        The frequency of the signal, in Hz.
+    palette : str, default = 'Oranges'
+        The name of the color palette to use.
+    """
+
+    T,K = Gamma.shape
+
+    # Setup colors
+    x = np.round(np.linspace(0.0, 256-1, K)).astype(int)
+    # cmap = plt.get_cmap('plasma').colors
+    cmap = plt.get_cmap(palette)
+    cmap = cmap(np.arange(0, cmap.N))[:, :3]
+    
+    colors = np.zeros((K,3))
+    for k in range(K):
+        colors[k,:] = cmap[x[k]]
+
+    # Setup data according to given limits
+    if tlim is not None:
+        T = tlim[1] - tlim[0]
+        data = Gamma[tlim[0] : tlim[1], :]
+        if line_overlay is not None:
+            line = line_overlay[tlim[0] : tlim[1]].copy()
+    else: 
+        data = Gamma
+    
+    df = pd.DataFrame(data, index=np.arange(T)/Hz)
+    df = df.divide(df.sum(axis=1), axis=0)
+    
+    # Plot Gamma area
+    ax = df.plot(
+        kind='area',
+        stacked=True,
+        ylim=(0,1),
+        legend=False,
+        color=colors
+    )
+    
+    # Overlay line if given
+    if line_overlay is not None:
+        df2 = pd.DataFrame(line, index=np.arange(T)/Hz)
+        ax2 = ax.twinx()
+        df2.plot(ax=ax2, legend=False, color="black")
+        ax2.set(ylabel = '')
+    
+    # Adjust axis specifications
+    ax.yaxis.set_major_formatter(mtick.PercentFormatter(1))
+    ax.set(
+        title  = "",
+        xlabel = 'Time [s]',
+        ylabel = 'State probability')
+    ax.margins(0,0)
+    
+    plt.show()
+
+
+def show_temporal_statistic(Gamma,indices,statistic='FO',type_plot='barplot'):
+    """Plots a statistic over time for a set of sessions.
+
+    Parameters:
+    -----------
+    Gamma : array of shape (n_samples, n_states)
+        The state timeseries probabilities.
+    indices: numpy.ndarray of shape (n_sessions,)
+        The session indices to plot.
+    statistic: str, default='FO'
+        The statistic to compute and plot. Can be 'FO', 'switching_rate' or 'FO_entropy'.
+    type_plot: str, default='barplot'
+        The type of plot to generate. Can be 'barplot', 'boxplot' or 'matrix'.
+
+    Raises:
+    -------
+    Exception
+        - Statistic is not one of 'FO', 'switching_rate' or 'FO_entropy'.
+        - type_plot is 'boxplot' and there are less than 10 sessions.
+        - type_plot is 'matrix' and there is only one session.
+    """
+    
+    s = eval("utils.get_" + statistic)(Gamma,indices)
+    if statistic not in ["FO","switching_rate","FO_entropy"]:
+        raise Exception("statistic has to be 'FO','switching_rate' or 'FO_entropy'") 
+    N,K = s.shape
+
+    sb.set(style='whitegrid')
+    if type_plot=='boxplot':
+        if N < 10:
+            raise Exception("Too few sessions for a boxplot; use barplot") 
+        sb.boxplot(data=s,palette='plasma')
+    elif type_plot=='barplot':
+        sb.barplot(data=np.concatenate((s,s)),palette='plasma', errorbar=None)
+    elif type_plot=='matrix':
+        if N < 2:
+            raise Exception("There is only one session; use barplot") 
+        fig,ax = plt.subplots()
+        labels_x = np.round(np.linspace(0,N,5)).astype(int)
+        pos_x = np.linspace(0,N,5)
+        if K > 10: labels_y = np.linspace(0,K-1,5)
+        else: labels_y = np.arange(K)
+        im = plt.imshow(s.T,aspect='auto')
+        plt.xticks(pos_x, labels_x)
+        plt.yticks(labels_y, labels_y)
+        ax.set_xlabel('Sessions')
+        ax.set_ylabel('States')
+        fig.tight_layout()
+
+
+def show_beta(hmm,only_active_states=True,recompute_states=False,
+              X=None,Y=None,Gamma=None,show_average=None,alpha=1.0):
+    """
+    Displays the beta coefficients of a given HMM.
+    The beta coefficients can be extracted directly from the HMM structure or reestimated from the data;
+    for the latter, X, Y and Gamma need to be provided as parameters. 
+    This is useful for example if one has run the model on PCA space, 
+    but wants to show coefficients in the original space.
+    
+    Parameters:
+    -----------
+    hmm: HMM object
+        An instance of the HMM class containing the beta coefficients to be visualized.
+    only_active_states: bool, optional, default=False
+        If True, only the beta coefficients of active states are shown.
+    recompute_states: bool, optional, default=False
+        If True, the betas will be recomputed from the data and the state time courses
+    X: numpy.ndarray, optional, default=None
+        The timeseries of set of variables 1.
+    Y: numpy.ndarray, optional, default=None
+        The timeseries of set of variables 2.
+    Gamma: numpy.ndarray, optional, default=None
+        The state time courses
+    show_average: bool, optional, default=None
+        If True, an additional row of the average beta coefficients is shown.
+    alpha: float, optional, default=0.1
+        The regularisation parameter to be applied if the betas are to be recomputed.
+
+    """
+    
+    if show_average is None:
+        show_average = not ((X is None) or (Y is None))
+    
+    K = hmm.get_betas().shape[2]
+    
+    if recompute_states:
+        if (Y is None) or (X is None) or (Gamma is None):
+            raise Exception("The data (X,Y) and the state time courses (Gamma) need \
+                             to be provided if recompute_states is True ")
+        (p,q) = (X.shape[1],Y.shape[1])
+        beta = np.zeros((p,q,K))
+        for k in range(K):
+            if hmm.hyperparameters["model_mean"] != 'no':
+                m = (np.expand_dims(Gamma[:,k],axis=1).T @ Yr) / np.sum(Gamma[:,k])
+                Yr = Y - np.expand_dims(m, axis=0)
+            else:
+                Yr = Y
+            beta[:,:,k] =  np.linalg.inv((X * np.expand_dims(Gamma[:,k],axis=1)).T @ X + alpha * np.eye(p)) @ \
+                ((X * np.expand_dims(Gamma[:,k],axis=1)).T @ Yr)
+    else:
+        beta = hmm.get_betas()
+        (p,q,_) = beta.shape
+
+    if only_active_states:
+        idx = np.where(hmm.active_states)[0]
+        beta = beta[:,:,idx]
+        K = beta.shape[2]
+    else:
+        idx = np.arange(K)
+
+    if show_average:
+        Yr = Y - np.expand_dims(np.mean(Y,axis=0), axis=0) 
+        b0 = np.linalg.inv(X.T @ X + alpha * np.eye(p)) @ (X.T @ Yr)
+        K += 1
+        B = np.zeros((p,q,K))
+        B[:,:,0:K-1] = beta
+        B[:,:,-1] = b0 
+    else:
+        B = beta 
+
+    Bstar1 = np.zeros((p,q,K,K))
+    for k in range(K): Bstar1[:,:,k,:] = B
+    Bstar2 = np.zeros((p,q,K,K))
+    for k in range(K): Bstar2[:,:,:,k] = B   
+
+    I1 = np.zeros((p,q,K,K),dtype=object)
+    for j in range(q): I1[:,j,:,:] = str(j)
+    I2 = np.zeros((p,q,K,K),dtype=object)
+    for k in range(K): 
+        if show_average and (k==(K-1)): 
+            I2[:,:,k,:] = 'Average'
+        else:
+            I2[:,:,k,:] = 'State ' + str(k)
+    I3 = np.zeros((p,q,K,K),dtype=object)
+    for k in range(K): 
+        if show_average and (k==(K-1)): 
+            I3[:,:,:,k] = 'Average'
+        else:
+            I3[:,:,:,k] = 'State ' + str(k)
+
+    Bstar1 = np.expand_dims(np.reshape(Bstar1,p*q*K*K,order='F'),axis=0)
+    Bstar2 = np.expand_dims(np.reshape(Bstar2,p*q*K*K,order='F'),axis=0)
+    I1 = np.expand_dims(np.reshape(I1,p*q*K*K,order='F'),axis=0)
+    I2 = np.expand_dims(np.reshape(I2,p*q*K*K,order='F'),axis=0)
+    I3 = np.expand_dims(np.reshape(I3,p*q*K*K,order='F'),axis=0)
+
+    B = np.concatenate((Bstar1,Bstar2,I1,I2,I3),axis=0).T
+    df = pd.DataFrame(B,columns=('x','y','Variable','beta x','beta y'))
+
+    g = sb.relplot(x='x', 
+        y='y', 
+        s=25,
+        hue='Variable', 
+        col="beta x", row="beta y",
+        data=df,
+        palette='cool')
+    
+    for item, ax in g.axes_dict.items():
+        ax.grid(False, axis='x')
+        ax.set_title('')
+
+    # def show_r2(r2=None,hmm=None,Gamma=None,X=None,Y=None,indices=None,show_average=False):
+
+    #     if r2 is None:
+    #         if (Y is None) or (indices is None):
+    #             raise Exception("Y and indices (and maybe X) has to be specified if r2 is not provided")
+    #         r2 = hmm.get_r2(X,Y,Gamma,indices)
+
+    #     if show_average:
+    #         if (Y is None) or (indices is None):
+    #             raise Exception("Y and indices (and maybe X) has to be specified if the average is to computed") 
+
+    #         r20 = hmm.get_r2(X,Y,Gamma,indices)
+
+    #         for j in range(N):
+
+    #             tt_j = range(indices[j,0],indices[j,1])
+
+    #             if X is not None:
+    #                 Xj = np.copy(X[tt_j,:])
+
+    #             d = np.copy(Y[tt_j,:])
+    #             if self.hyperparameters["model_mean"] == 'shared':
+    #                 d -= np.expand_dims(self.mean[0]['Mu'],axis=0)
+    #             if self.hyperparameters["model_beta"] == 'shared':
+    #                 d -= (Xj @ self.beta[0]['Mu'])
+    #             for k in range(K):
+    #                 if self.hyperparameters["model_mean"] == 'state': 
+    #                     d -= np.expand_dims(self.mean[k]['Mu'],axis=0) * np.expand_dims(Gamma[:,k],axis=1)
+    #                 if self.hyperparameters["model_beta"] == 'state':
+    #                     d -= (Xj @ self.beta[k]['Mu']) * np.expand_dims(Gamma[:,k],axis=1)
+    #             d = np.sum(d**2,axis=0)
+
+    #             d0 = np.copy(Y[tt_j,:])
+    #             if self.hyperparameters["model_mean"] != 'no':
+    #                 d0 -= np.expand_dims(m,axis=0)
+    #             d0 = np.sum(d0**2,axis=0)
+
+    #             r2[j,:] = 1 - (d / d0)
