4b0f714a0f0d377e3f51c8bde1053f76d610c4e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 15:04:10 +0000

    Add package nyc-prop-prediction/0.1.3/nyc_prop_prediction-0.1.3.tar.gz

---
+++ packages/nyc-prop-prediction/nyc_prop_prediction-0.1.3.tar.gz/nyc_prop_prediction-0.1.3/nyc_prop_prediction/nyc_prop_prediction.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import time
+import math
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import tree
+from sklearn.ensemble import GradientBoostingRegressor
+from joblib import dump, load
+
+
+class predict:
+ 
+    def __init__(self):
+        
+        self.inputmodel_grad = load('inputmodel_grad.joblib')
+        self.zipmodel_grad = load('zipmodel_grad.joblib')
+
+    def predict_price(self,input_type,model,inputs):
+        if input_type == "zipcode":
+            if model == 'rf':
+                return None#zipmodel_rf.predict(inputs)
+            elif model =='gradient':
+                return self.zipmodel_grad.predict(inputs)
+        elif input_type == "data":
+            if model == 'rf':
+                return None#inputmodel_rf.predict(inputs)
+            elif model =='gradient':
+                return (self.inputmodel_grad).predict(inputs)
+        else:
+            return None
