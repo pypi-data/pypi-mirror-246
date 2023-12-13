dac8b5f2b235e2d46e7fe6f973bfff936153f41c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 05:03:06 +0000

    Add package pytest-ver/0.0.62/pytest-ver-0.0.62.tar.gz

---
+++ packages/pytest-ver/pytest-ver-0.0.62.tar.gz/pytest-ver-0.0.62/pytest_ver/lib/build_info.py
@@ -0,0 +1,10 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.8.10'
+    os_name = 'posix:ubuntu'
+    version = '0.0.62'
+    git_sha = '83a1c110df12a37966d74c2e3bfab6b3e3beab40'
+    git_branch = 'master'
+    git_uncommitted = [
+    ]
+    git_unpushed = [
+    ]
---
+++ packages/pytest-ver/pytest-ver-0.0.62.tar.gz/pytest-ver-0.0.62/tools/common.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+
+
+# --------------------
+## holds all common variables for a module/app
+@dataclass
+class Common:
+    ## the version string held in mod_dir_name/lib/version.json
+    version = '0.0.62'
+    ## the module name (with dashes)
+    mod_name = 'pytest-ver'
+    ## the local directory name for the module (with underscores)
+    mod_dir_name = 'pytest_ver'
+    ## flag indicating this is a python module (True) or an app (False)
+    is_module = True
+
+    # == typically do not change from here on
+
+    ## the license for the module
+    license = 'MIT'
+    ## the license string for the classifier section
+    classifier_license = 'License :: OSI Approved :: MIT License'
+    ## the url for the homepage link
+    homepage_url = f'https://bitbucket.org/arrizza-public/{mod_name}/src/master'
+    ## the url for the download link
+    download_url = f'https://bitbucket.org/arrizza-public/{mod_name}/get/master.zip'
+    ## the author name
+    author = 'JA'
+    ## the contact email
+    email = 'cppgent0@gmail.com'
+
+    # == set in main.init()
+
+    ## the long version of the version string
+    long_version = 'unknown'
+    ## the long description of the module (usually content of README)
+    long_desc = 'unknown'
+    ## the format of the long desc (usually markdown)
+    long_desc_type = 'unknown'
