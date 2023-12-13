15b7e791497c4fdada8ba3ef05724503fbe458cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 15:03:56 +0000

    Add package CRFActions/3.0.0/CRFActions-3.0.0.tar.gz

---
+++ packages/CRFActions/CRFActions-3.0.0.tar.gz/CRFActions-3.0.0/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 10 17:15:23 2023
+
+@author: Stacey.Yang
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='CRFActions',
+    version='3.0.0',
+    packages=find_packages(),  
+    url='https://github.com/Doraemon-code/QianYang',
+    author='Stacey',
+    author_email='yq13408614660@163.com'
+)
