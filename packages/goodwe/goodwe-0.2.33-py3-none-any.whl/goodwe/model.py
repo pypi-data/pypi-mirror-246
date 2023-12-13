4e60272dde081a4cb989b5f384f9e2dbb45f2ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 07:03:55 +0000

    Add package goodwe/0.2.33/goodwe-0.2.33-py3-none-any.whl

---
+++ packages/goodwe/goodwe-0.2.33-py3-none-any.whl/goodwe/model.py
@@ -0,0 +1,37 @@
+from .inverter import Inverter
+
+# Serial number tags to identify inverter type
+ET_MODEL_TAGS = ["ETU", "ETL", "ETR", "ETC", "EHU", "EHR", "EHB", "BTU", "BTN", "BTC", "BHU", "AES", "ABP", "HHI",
+                 "HSB", "HUA", "CUA",
+                 "ESN", "EMN", "ERN", "EBN",  # ES Gen 2
+                 "HLB", "HMB", "HBB", "SPN"]  # Gen 2
+ES_MODEL_TAGS = ["ESU", "EMU", "ESA", "BPS", "BPU", "EMJ", "IJL"]
+DT_MODEL_TAGS = ["DTU", "DTS", "MSU", "MST", "DSN", "DTN", "DST", "NSU", "SSN", "SST", "SSX", "SSY", "PSB", "PSC"]
+
+SINGLE_PHASE_MODELS = ["DSN", "DST", "NSU", "SSN", "SST", "SSX", "SSY",  # DT
+                       "MSU", "MST", "PSB", "PSC",
+                       "EHU", "EHR", "HSB",  # ET
+                       "ESN", "EMN", "ERN", "EBN", "HLB", "HMB", "HBB", "SPN"]  # ES Gen 2
+
+MPPT3_MODELS = ["MSU", "MST", "PSC",
+                "25KET", "29KET", "29K9ET", "30KET"]
+
+MPPT4_MODELS = ["HSB"]
+
+BAT_2_MODELS = ["25KET", "29KET", "29K9ET", "30KET"]
+
+
+def is_single_phase(inverter: Inverter) -> bool:
+    return any(model in inverter.serial_number for model in SINGLE_PHASE_MODELS)
+
+
+def is_3_mptt(inverter: Inverter) -> bool:
+    return any(model in inverter.serial_number for model in MPPT3_MODELS)
+
+
+def is_4_mptt(inverter: Inverter) -> bool:
+    return any(model in inverter.serial_number for model in MPPT4_MODELS)
+
+
+def is_2_battery(inverter: Inverter) -> bool:
+    return any(model in inverter.serial_number for model in BAT_2_MODELS)
