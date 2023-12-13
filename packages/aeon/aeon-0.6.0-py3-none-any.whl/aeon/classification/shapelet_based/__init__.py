ce6555769d8f9fc3b73d5cea256ad112bf34add1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 04:03:47 +0000

    Add package aeon/0.6.0/aeon-0.6.0-py3-none-any.whl

---
+++ packages/aeon/aeon-0.6.0-py3-none-any.whl/aeon/classification/shapelet_based/__init__.py
@@ -0,0 +1,7 @@
+"""Shapelet based time series classifiers."""
+
+__all__ = ["MrSQMClassifier", "ShapeletTransformClassifier", "RDSTClassifier"]
+
+from aeon.classification.shapelet_based._mrsqm import MrSQMClassifier
+from aeon.classification.shapelet_based._rdst import RDSTClassifier
+from aeon.classification.shapelet_based._stc import ShapeletTransformClassifier
