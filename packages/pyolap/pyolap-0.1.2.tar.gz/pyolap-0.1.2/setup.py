a92fa00da96f079478ddd964ccd16ba812b3157c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 16:03:49 +0000

    Add package pyolap/0.1.2/pyolap-0.1.2.tar.gz

---
+++ packages/pyolap/pyolap-0.1.2.tar.gz/pyolap-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyolap',
+    version='0.1.2',
+    packages=find_packages(),
+
+    author="euclidolap",
+    author_email="euclidolap@outlook.com",
+
+    description="Python for EuclidOLAP.",
+
+    url="https://github.com/EuclidOLAP/EuclidOLAP",
+
+    keywords="olap"
+)
