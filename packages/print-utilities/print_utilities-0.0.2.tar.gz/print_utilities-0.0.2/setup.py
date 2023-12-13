f4d15e9300ef36f7de756f3288c3adb2d86094bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 06:03:44 +0000

    Add package print-utilities/0.0.2/print_utilities-0.0.2.tar.gz

---
+++ packages/print-utilities/print_utilities-0.0.2.tar.gz/print_utilities-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='print_utilities',
+    version='0.0.2',
+    license='MIT License',
+    author='CarlosAllberto',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='dasilvacarlosalberto344@gmail.com',
+    keywords='print utilities',
+    description='Uma biblioteca com varias utilidades para printar',
+    packages=find_packages(),)
