8f453be33b262ea5f7d4eee3e56862e62a054845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 03:02:52 +0000

    Add package baca2-package-manager/0.2.16/baca2_package_manager-0.2.16-py3-none-any.whl

---
+++ packages/baca2-package-manager/baca2_package_manager-0.2.16-py3-none-any.whl/baca2PackageManager/manager_exceptions.py
@@ -0,0 +1,13 @@
+class FileAlreadyExist(Exception):
+    pass
+
+class NoTestFound(Exception):
+    pass
+
+
+class NoSetFound(Exception):
+    pass
+
+
+class TestExistError(FileAlreadyExist):
+    pass
