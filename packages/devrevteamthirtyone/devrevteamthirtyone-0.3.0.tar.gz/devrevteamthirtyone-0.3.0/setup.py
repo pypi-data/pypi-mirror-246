28d93f346cd428d4375323c8b1ac8d28813b6be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 04:03:10 +0000

    Add package devrevteamthirtyone/0.3.0/devrevteamthirtyone-0.3.0.tar.gz

---
+++ packages/devrevteamthirtyone/devrevteamthirtyone-0.3.0.tar.gz/devrevteamthirtyone-0.3.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="devrevteamthirtyone",
+    version="0.3.0",
+    author="Team31",
+    author_email="team31-interiit@gmail.com",
+    description="Submission for team31",
+    long_description="This takes tools and queries and returns a solution",
+    packages=find_packages(),
+    install_requires=["tiktoken", "litellm", "numpy"],
+    package_dir={"": "src"}
+)
