a7345f32323d7008c207abed1cd79a32a7ba40c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 16:03:38 +0000

    Add package gytoolkit/1.2.0/gytoolkit-1.2.0-py3-none-any.whl

---
+++ packages/gytoolkit/gytoolkit-1.2.0-py3-none-any.whl/gytoolkit/datamanager/__init__.py
@@ -0,0 +1,6 @@
+from .net_value import NetValueLoader
+from .prodinfo import ProdInfoLoader
+from .trade import TradeLoader
+from .fundevent import DividendLoader, PerfFeeLoader
+
+from .utils import get_otc_nv, ppwnvformatter, get_otc_trade
