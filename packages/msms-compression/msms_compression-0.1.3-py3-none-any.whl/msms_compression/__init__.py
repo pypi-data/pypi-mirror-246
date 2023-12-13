8d96de4454bde52b97aa7161e5ee92a2c7ff3a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 06:04:10 +0000

    Add package msms-compression/0.1.3/msms_compression-0.1.3-py3-none-any.whl

---
+++ packages/msms-compression/msms_compression-0.1.3-py3-none-any.whl/msms_compression/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.1.3'
+
+from .compressors import SpectrumCompressor, SpectrumCompressorUrl, SpectrumCompressorGzip, SpectrumCompressorUrlGzip, \
+    SpectrumCompressorUrlLzstring
+
