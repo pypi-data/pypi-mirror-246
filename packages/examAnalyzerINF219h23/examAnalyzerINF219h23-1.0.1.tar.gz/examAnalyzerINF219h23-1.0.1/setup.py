ed9e0f61dcf5bf8f5bdd4b34b9b94d2ff3cc41f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 03:03:30 +0000

    Add package examAnalyzerINF219h23/1.0.1/examAnalyzerINF219h23-1.0.1.tar.gz

---
+++ packages/examAnalyzerINF219h23/examAnalyzerINF219h23-1.0.1.tar.gz/examAnalyzerINF219h23-1.0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.1'
+DESCRIPTION = 'Exam analyzer is a tool that extract data from Json files from Inspera, and then creates visualizations with descriptive texts'
+
+# Setting up
+setup(
+    name="examAnalyzerINF219h23",
+    version=VERSION,
+    author="Tobias, Jonas, Brage, Prem",
+    author_email="premeide@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'examAnalyzerINF219h23': ['templates/index.html']},
+    install_requires=['Flask'],
+    keywords=['python', 'education', 'exam analysis'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
