0b26dc27c182f0e30f482670ec6fbe224f424a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 05:03:14 +0000

    Add package pytest-ver/0.0.61/pytest-ver-0.0.61.tar.gz

---
+++ packages/pytest-ver/pytest-ver-0.0.61.tar.gz/pytest-ver-0.0.61/pytest_ver/lib/build_info.py
@@ -0,0 +1,10 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.8.10'
+    os_name = 'posix:ubuntu'
+    version = '0.0.61'
+    git_sha = 'e195ae81a176c39c70f6ac5b1138cbc8a6d7c027'
+    git_branch = 'master'
+    git_uncommitted = [
+    ]
+    git_unpushed = [
+    ]
