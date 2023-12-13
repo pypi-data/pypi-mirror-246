a0cf0d05a13a6abb9b7f40fc227311f099663a04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 14:04:15 +0000

    Add package socket-oneline/0.0.17/socket-oneline-0.0.17.tar.gz

---
+++ packages/socket-oneline/socket-oneline-0.0.17.tar.gz/socket-oneline-0.0.17/setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_packages
+from setuptools import setup
+
+print('     setup: version:  v0.0.17')
+print('     setup: module :  socket_oneline')
+
+# @formatter:off
+setup(
+    description='Client server base class over socket',
+    keywords=['socket', 'client server', 'simple'],
+    install_requires=[
+        'pytest-ver',
+        'pytest',
+    ],
+    classifiers=[
+        # Choose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+    ],
+
+    # common attributes from here on
+    name='socket-oneline',
+    packages=find_packages(include='./socket_oneline*', ),
+    include_package_data=True,
+    exclude_package_data={'./socket_oneline/lib': ['.gitignore']},
+    version='0.0.17',
+    license='MIT',
+    long_description='unknown',
+    long_description_content_type='unknown',
+    author='JA',
+    author_email='cppgent0@gmail.com',
+    url='https://bitbucket.org/arrizza-public/socket-oneline/src/master',
+    download_url='https://bitbucket.org/arrizza-public/socket-oneline/get/master.zip',
+)
+# @formatter:on
+
+print('     setup: done')
---
+++ packages/socket-oneline/socket-oneline-0.0.17.tar.gz/socket-oneline-0.0.17/socket_oneline/lib/build_info.py
@@ -0,0 +1,10 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.8.10'
+    os_name = 'posix:ubuntu'
+    version = '0.0.17'
+    git_sha = '727efe8113ba1c9d07c53061065cf81d2c21210b'
+    git_branch = 'master'
+    git_uncommitted = [
+    ]
+    git_unpushed = [
+    ]
