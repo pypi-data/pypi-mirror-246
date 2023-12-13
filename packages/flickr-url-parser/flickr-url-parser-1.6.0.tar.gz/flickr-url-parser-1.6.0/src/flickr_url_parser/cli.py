480af32bed38aa47590e2783c1f63faccbcc6856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 04:03:27 +0000

    Add package flickr-url-parser/1.6.0/flickr-url-parser-1.6.0.tar.gz

---
+++ packages/flickr-url-parser/flickr-url-parser-1.6.0.tar.gz/flickr-url-parser-1.6.0/src/flickr_url_parser/cli.py
@@ -0,0 +1,30 @@
+from typing import List
+
+import json
+import sys
+import textwrap
+
+from flickr_url_parser import parse_flickr_url, __version__
+
+
+def run_cli(argv: List[str]) -> int:
+    try:
+        url = argv[1]
+    except IndexError:
+        print(f"Usage: {__file__} <URL>", file=sys.stderr)
+        return 1
+
+    if url == "--help":
+        print(textwrap.dedent(parse_flickr_url.__doc__).strip())  # type: ignore[arg-type]
+        return 0
+    elif url == "--version":
+        print(f"flickr_url_parser {__version__}")
+        return 0
+    else:
+        print(json.dumps(parse_flickr_url(url)))
+        return 0
+
+
+def main() -> None:  # pragma: no cover
+    rc = run_cli(argv=sys.argv)
+    sys.exit(rc)
