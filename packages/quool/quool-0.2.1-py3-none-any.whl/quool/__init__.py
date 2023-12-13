56178122f15df5635b222e18d2f5b13922744ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 17:05:55 +0000

    Add package quool/0.2.1/quool-0.2.1-py3-none-any.whl

---
+++ packages/quool/quool-0.2.1-py3-none-any.whl/quool/__init__.py
@@ -0,0 +1,41 @@
+from .tools import (
+    Logger,
+    parse_commastr,
+    parse_date,
+    reduce_mem_usage,
+    format_code,
+    strip_stock_code,
+)
+
+from .database import (
+    Table,
+    PanelTable,
+    FrameTable,
+    DiffTable,
+)
+
+from .collector import (
+    Request,
+)
+
+from .backtest import (
+    BackTrader,
+    Relocator,
+    Strategy,
+    Indicator,
+    Analyzer,
+    Observer,
+    OrderTable,
+    CashValueRecorder,
+)
+
+from .ops import (
+    abs, neg, sign, sqrt, ssqrt,
+    square, csrank, csnorm, 
+    add, sub, mul, div, power, 
+    maximum, minimum, log, sum,
+    mean, wma, ema, var, skew, kurt,
+    max, min, delta, delay, rank,
+    scale, product, decay_linear,
+    std, tsnorm, ifelse, correlation, covariance
+)
