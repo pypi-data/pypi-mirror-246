03ffa57c52153a193ecd0937b31befe68e183ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 14:03:56 +0000

    Add package qbraid/0.5.0.dev20231213012035/qbraid-0.5.0.dev20231213012035-py3-none-any.whl

---
+++ packages/qbraid/qbraid-0.5.0.dev20231213012035-py3-none-any.whl/qbraid/programs/__init__.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2023 qBraid
+#
+# This file is part of the qBraid-SDK
+#
+# The qBraid-SDK is free software released under the GNU General Public License v3
+# or later. You can redistribute and/or modify it under the terms of the GPL v3.
+# See the LICENSE file in the project root or <https://www.gnu.org/licenses/gpl-3.0.html>.
+#
+# THERE IS NO WARRANTY for the qBraid-SDK, as per Section 15 of the GPL v3.
+
+"""
+=====================================
+Programs  (:mod:`qbraid.programs`)
+=====================================
+
+.. currentmodule:: qbraid.programs
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   QuantumProgram
+   CirqCircuit
+   OpenQasm2Program
+   random_circuit
+   circuits_allclose
+
+"""
+from qbraid.programs.abc_program import QuantumProgram
+from qbraid.programs.cirq import CirqCircuit
+from qbraid.programs.qasm2 import OpenQasm2Program
+from qbraid.programs.testing import circuits_allclose, random_circuit
