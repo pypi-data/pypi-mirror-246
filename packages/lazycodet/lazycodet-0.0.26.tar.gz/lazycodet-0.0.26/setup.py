44b4b573ea4e9bac29e265262666e83560ba67b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 16:03:46 +0000

    Add package lazycodet/0.0.26/lazycodet-0.0.26.tar.gz

---
+++ packages/lazycodet/lazycodet-0.0.26.tar.gz/lazycodet-0.0.26/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lazycodet',
+    version='0.0.26',
+    author='Nguyen Tan Tai',
+    author_email='nguyentantaitcag2000@gmail.com',
+    packages=['lazycodet'],
+    install_requires=[
+        'unidecode'
+        # Các dependencies nếu có
+    ],
+)
