399f3968699531d6a4313cff3ec08a99ee365f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 14:04:20 +0000

    Add package accurate-timed-loop/0.0.15/accurate-timed-loop-0.0.15.tar.gz

---
+++ packages/accurate-timed-loop/accurate-timed-loop-0.0.15.tar.gz/accurate-timed-loop-0.0.15/accurate_timed_loop/lib/build_info.py
@@ -0,0 +1,10 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.8.10'
+    os_name = 'posix:ubuntu'
+    version = '0.0.15'
+    git_sha = '3785c08247e0f6353a598f085c5141dc76c26349'
+    git_branch = 'master'
+    git_uncommitted = [
+    ]
+    git_unpushed = [
+    ]
---
+++ packages/accurate-timed-loop/accurate-timed-loop-0.0.15.tar.gz/accurate-timed-loop-0.0.15/setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages
+from setuptools import setup
+
+print('     setup: version:  v0.0.15')
+print('     setup: module :  accurate_timed_loop')
+
+# @formatter:off
+setup(
+    description='Accurate timed loop',
+    keywords=['accurate loop', 'utility'],
+    install_requires=[
+        'pytest-ver',
+        'pytest',
+        'on_the_fly_stats',
+    ],
+    classifiers=[
+        # Choose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+    ],
+
+    # common attributes from here on
+    name='accurate-timed-loop',
+    packages=find_packages(include='./accurate_timed_loop*', ),
+    include_package_data=True,
+    exclude_package_data={'./accurate_timed_loop/lib': ['.gitignore']},
+    version='0.0.15',
+    license='MIT',
+    long_description='unknown',
+    long_description_content_type='unknown',
+    author='JA',
+    author_email='cppgent0@gmail.com',
+    url='https://bitbucket.org/arrizza-public/accurate-timed-loop/src/master',
+    download_url='https://bitbucket.org/arrizza-public/accurate-timed-loop/get/master.zip',
+)
+# @formatter:on
+
+print('     setup: done')
