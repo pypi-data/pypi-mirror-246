cd56bde30dbdf30c69409c3a5415b587055ee681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 16:04:28 +0000

    Add package aspose-ocr-python-cpp/0.0.0/aspose-ocr-python-cpp-0.0.0.tar.gz

---
+++ packages/aspose-ocr-python-cpp/aspose-ocr-python-cpp-0.0.0.tar.gz/aspose-ocr-python-cpp-0.0.0/aspose-ocr-python-cpp/__init__.py
@@ -0,0 +1,9 @@
+"""
+aspose-ocr-python-cpp.
+
+An Aspose OCR python library via C++.
+"""
+
+__version__ = "0.0.0"
+__author__ = 'Igor Furin'
+__credits__ = 'Aspose OCR Team'
---
+++ packages/aspose-ocr-python-cpp/aspose-ocr-python-cpp-0.0.0.tar.gz/aspose-ocr-python-cpp-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='aspose-ocr-python-cpp',
+    version='0.0.0',    
+    description='Aspose.OCR for Python via C++ is a powerful yet easy-to-use and cost-effective API for extracting text from scanned images, photos, screenshots, PDF documents, and other files.',
+    url='https://github.com/aspose-ocr/Aspose.OCR-for-Python-via-Cpp',
+    author='Aspose OCR Team',
+    author_email='igor.furin@aspose.com',
+    license='Other/Proprietary License',
+    packages=['aspose-ocr-python-cpp']
+)
