d42c94cee3914860d42924a0362e2e8fbbefb6e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 05:03:07 +0000

    Add package comgra/0.10.1/comgra-0.10.1.tar.gz

---
+++ packages/comgra/comgra-0.10.1.tar.gz/comgra-0.10.1/src/comgra/objects.py
@@ -0,0 +1,261 @@
+import abc
+import collections
+import dataclasses
+from typing import List, Dict, Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from comgra.recorder import ComgraRecorder
+
+from comgra import utilities
+
+
+@dataclasses.dataclass
+class ParameterRepresentation:
+    name: str
+    full_unique_name: str
+    shape: List[int]
+
+
+@dataclasses.dataclass
+class ModuleRepresentation:
+    name: str
+    full_unique_name: str
+    submodules: Dict[str, 'ModuleRepresentation']
+    parameters: Dict[str, ParameterRepresentation]
+
+
+@dataclasses.dataclass
+class TensorRepresentation:
+    full_unique_name: str
+    node_name: str
+    role_within_node: str
+    iteration: int
+    configuration_type: str
+    type_of_tensor: str
+    shape: List[int]
+    index_of_batch_dimension: Optional[int]
+    value_dimensions: List[int]
+    recording_type: str
+    items_to_record: List[str]
+    record_per_batch_index: bool
+    is_a_dependency_of: List[str] = dataclasses.field(default_factory=list)
+
+    def get_size_of_tensor(self):
+        assert len(self.shape) == 2
+        return self.shape[1 - self.index_of_batch_dimension]
+
+
+@dataclasses.dataclass
+class Node:
+    full_unique_name: str
+    type_of_tensor: str
+    items_to_record: List[str]
+
+
+@dataclasses.dataclass
+class TensorRecordings:
+    training_step_to_type_of_execution: Dict[int, str] = dataclasses.field(default_factory=dict)
+    training_step_to_iteration_to_configuration_type: Dict[int, Dict[int, str]] = dataclasses.field(default_factory=dict)
+    recordings: Optional[utilities.PseudoDb] = None
+
+    @utilities.runtime_analysis_decorator
+    def update_with_more_recordings(self, other: 'TensorRecordings'):
+        self.recordings.merge(other.recordings)
+
+
+@dataclasses.dataclass
+class StatusAndGraph:
+    configuration_type: str
+    modules_and_parameters: Dict[str, ModuleRepresentation]
+    name_to_node: Dict[str, Node]
+    types_of_tensor_recordings: List[str]
+    nodes: List[str]
+    connections: List[List[str]]
+    dag_format: List[List[str]] = dataclasses.field(default_factory=list)
+
+    def __post_init__(self):
+        for name, v in self.name_to_node.items():
+            assert name == v.full_unique_name, (name, v.full_unique_name,)
+
+    def build_dag_format(self, recorder: 'ComgraRecorder', name_to_tensor_representation: Dict[str, TensorRepresentation]):
+        dependencies_of = collections.defaultdict(list)
+        for a in self.connections:
+            dependencies_of[a[1]].append(a[0])
+        #
+        # Sanity check for nodes vs tensors
+        #
+        node_to_tensor_names = collections.defaultdict(list)
+        for tr in name_to_tensor_representation.values():
+            assert tr.role_within_node not in node_to_tensor_names[tr.node_name], \
+                f"A node has two tensors with the same name in it: {tr.node_name}, {tr.role_within_node}"
+            node_to_tensor_names[tr.node_name].append(tr.role_within_node)
+        #
+        # Logic for grouping:
+        # * Inputs first (None of these have dependencies)
+        # * One set for each group of parameters in the same module (None of these have dependencies)
+        # * Intermediates and Outputs are sorted in DAG logic
+        # * Move each set of parameters to the right as far as possible without violating DAG logic
+        # * Targets (None of these have dependencies)
+        # * Losses
+        #
+        nodes_list_list = []
+        debug = 0
+        nodes_list_list.append([k for k, v in name_to_tensor_representation.items() if v.type_of_tensor == 'input'])
+        used_nodes = {a: True for a in nodes_list_list[0]}
+        nodes_list_for_parameters_and_targets = []
+        for prefix in recorder.prefixes_for_grouping_module_parameters_visually:
+            next_set_of_nodes = []
+            nodes_list_for_parameters_and_targets.append(next_set_of_nodes)
+            for a in name_to_tensor_representation.values():
+                if a.type_of_tensor == 'parameter' and a.full_unique_name not in used_nodes:
+                    if a.full_unique_name.startswith(prefix):
+                        used_nodes[a.full_unique_name] = True
+                        next_set_of_nodes.append(a.full_unique_name)
+        for a in name_to_tensor_representation.values():
+            if a.type_of_tensor == 'parameter':
+                assert a.full_unique_name in used_nodes, \
+                    f"The parameter {a.full_unique_name} is not covered by any of the provided prefixes: " \
+                    f"{recorder.prefixes_for_grouping_module_parameters_visually}"
+        nodes_list_for_parameters_and_targets.append([k for k, v in name_to_tensor_representation.items() if v.type_of_tensor == 'target'])
+        nodes_to_sort = [k for k, v in name_to_tensor_representation.items() if v.type_of_tensor in ['intermediate', 'output']]
+        nodes_without_open_dependencies = {k: True for k, v in name_to_tensor_representation.items() if not dependencies_of[k]}
+        c = 0
+        while c < len(nodes_to_sort):
+            debug += 1
+            assert debug < 100000, \
+                f"The graph is too large. The most likely cause for this error is that register_tensor() " \
+                f"was called on a tensor that can't be discovered through backtracking from the losses. " \
+                f"To fix this, find the responsible tensor and call register_tensor() on it " \
+                f"with is_target=True instead.\n" \
+                f"{c}, {len(nodes_to_sort)}\n{[a for a in nodes_list_list if a]}\n{nodes_to_sort}\n" \
+                f"{[c for c in nodes_to_sort if c not in [b for a in nodes_list_list for b in a]]}"
+            next_set_of_nodes = []
+            nodes_list_list.append(next_set_of_nodes)
+            for n in nodes_to_sort:
+                if n not in nodes_without_open_dependencies:
+                    is_now_a_root = True
+                    for dependency in dependencies_of[n]:
+                        if dependency not in nodes_without_open_dependencies:
+                            is_now_a_root = False
+                            break
+                    if is_now_a_root:
+                        next_set_of_nodes.append(n)
+                        c += 1
+            for n in next_set_of_nodes:
+                nodes_without_open_dependencies[n] = True
+        assert c == len(nodes_to_sort)
+        for list_of_parameters_and_targets in nodes_list_for_parameters_and_targets:
+            farthest_possible_index = 0
+            for i, nodes in enumerate(nodes_list_list):
+                farthest_possible_index = i
+                shared_dependencies_of_nodes = {b for a in nodes for b in dependencies_of[a]}
+                if any(a in shared_dependencies_of_nodes for a in list_of_parameters_and_targets):
+                    break
+            nodes_list_list.insert(farthest_possible_index, list_of_parameters_and_targets)
+        nodes_list_list.append([k for k, v in name_to_tensor_representation.items() if v.type_of_tensor == 'loss'])
+        nodes_list_list = [a for a in nodes_list_list if len(a) > 0]
+        for i, nodes0 in enumerate(nodes_list_list):
+            for j, nodes1 in enumerate(nodes_list_list):
+                if i < j:
+                    shared_dependencies_of_nodes = {b for a in nodes0 for b in dependencies_of[a]}
+                    assert not any(a in shared_dependencies_of_nodes for a in nodes1), \
+                        f"The construction of the DAG is faulty. Probably the easiest way to debug this is " \
+                        f"to deactivate this assert and check what the graph looks like. " \
+                        f"Some arrows for dependencies should be pointing in the wrong direction."
+        #
+        # At this point the nodes_list_list contains tensor_names, not node_names.
+        # Translate between these, and make sure there are no contradictions.
+        #
+        used_node_names = {}
+        dag_format = []
+        for nodes_list in nodes_list_list:
+            tmp = []
+            dag_format.append(tmp)
+            for tensor_name in nodes_list:
+                node_name = name_to_tensor_representation[tensor_name].node_name
+                if node_name in used_node_names:
+                    assert node_name in tmp, \
+                        f"The node_name {node_name} is used for two tensors that get sorted in different ways. " \
+                        f"This should not be possible if the tensors have the same role / are used " \
+                        f"in the same way."
+                else:
+                    tmp.append(node_name)
+                used_node_names[node_name] = True
+        self.dag_format = dag_format
+        # Also adjust connections to be based on node instead of tensors
+        node_level_connections = [
+            list(a)
+            for a in list(dict.fromkeys([
+                (name_to_tensor_representation[a[0]].node_name, name_to_tensor_representation[a[1]].node_name)
+                for a in self.connections
+            ]))
+        ]
+        for from_node, to_node in node_level_connections:
+            all_from_tensors = [tr.full_unique_name for tr in name_to_tensor_representation.values() if tr.node_name == from_node]
+            all_to_tensors = [tr.full_unique_name for tr in name_to_tensor_representation.values() if tr.node_name == to_node]
+            for from_tensor in all_from_tensors:
+                for to_tensor in all_to_tensors:
+                    assert [from_tensor, to_tensor] in self.connections, \
+                        f"Not all tensors that were combined into nodes have a connection in common." \
+                        f"\n{from_node}, {to_node}\n{from_tensor}, {to_tensor}" \
+                        f"\n{[a[0] for a in self.connections if a[1] == to_tensor]}" \
+                        f"\n{[a[1] for a in self.connections if a[0] == from_tensor]}"
+        self.connections = node_level_connections
+        assert sum([len(a) for a in dag_format]) == len(self.nodes), \
+            (sum([len(a) for a in dag_format]), len(self.nodes), dag_format, self.nodes)
+
+
+class DecisionMakerForRecordings(abc.ABC):
+    pass
+
+    @abc.abstractmethod
+    def is_record_on_this_iteration(self, training_step, type_of_execution):
+        pass
+
+
+@dataclasses.dataclass
+class DecisionMakerForRecordingsHardcoded(DecisionMakerForRecordings):
+    fixed_training_steps: set[int]
+
+    def is_record_on_this_iteration(self, training_step, type_of_execution):
+        return training_step in self.fixed_training_steps
+
+
+@dataclasses.dataclass
+class DecisionMakerForRecordingsFrequencyPerType(DecisionMakerForRecordings):
+    min_training_steps_difference: int
+    identifier_to_last_recorded_step: Dict = dataclasses.field(default_factory=dict)
+
+    def is_record_on_this_iteration(self, training_step, type_of_execution):
+        if type_of_execution is None:
+            return False
+        last_recorded_step = self.identifier_to_last_recorded_step.get(type_of_execution, None)
+        if last_recorded_step == training_step:
+            return True
+        if last_recorded_step is None or training_step >= last_recorded_step + self.min_training_steps_difference:
+            self.identifier_to_last_recorded_step[type_of_execution] = training_step
+            return True
+        return False
+
+
+class DecisionMakerForRecordingsExponentialFalloff(DecisionMakerForRecordings):
+    maximum_number_of_recordings: int
+    current_valid_steps: List
+    current_step_size: int = 1
+
+    def __init__(self, maximum_number_of_recordings, starting_step_size):
+        super().__init__()
+        assert maximum_number_of_recordings > 1
+        self.maximum_number_of_recordings = maximum_number_of_recordings
+        self.current_valid_steps = []
+        self.current_step_size = starting_step_size
+
+    def is_record_on_this_iteration(self, training_step, type_of_execution):
+        if self.current_step_size * (self.maximum_number_of_recordings - 1) < training_step:
+            self.current_step_size *= 2
+        self.current_valid_steps = [
+            k for k in self.current_valid_steps
+            if k % self.current_step_size == 0
+        ]
+        assert len(self.current_valid_steps) <= self.maximum_number_of_recordings
+        return (training_step % self.current_step_size) == 0
---
+++ packages/comgra/comgra-0.10.1.tar.gz/comgra-0.10.1/src/comgra/utilities.py
@@ -0,0 +1,202 @@
+import collections
+from datetime import datetime, timedelta
+import math
+from typing import List, Any, Tuple, Dict, Set, Optional
+
+from plotly import express as px
+
+DEBUG_MODE = True
+PRINT_EACH_TIME = False
+
+FUNCTION_NAME_TO_TOTAL_TIME = collections.defaultdict(lambda: timedelta(seconds=0))
+FUNCTION_NAME_TO_TOTAL_NUM_CALLS = collections.defaultdict(lambda: 0)
+
+_CURRENT_INDENT = 0
+_CALL_STACK = []
+
+
+def runtime_analysis_decorator(function):
+    if not DEBUG_MODE:
+        return function
+    function_name = function.__name__
+
+    def wrapper(*args, **kwargs):
+        global _CURRENT_INDENT
+        global _CALL_STACK
+        start = datetime.now()
+        if PRINT_EACH_TIME:
+            print("  " * _CURRENT_INDENT + f"START {function.__name__}")
+        _CURRENT_INDENT += 1
+        _CALL_STACK.append(function.__name__)
+        try:
+            res = function(*args, **kwargs)
+        except Exception as e:
+            raise
+        finally:
+            _CURRENT_INDENT -= 1
+            _CALL_STACK.pop()
+            end = datetime.now()
+            duration = end - start
+            FUNCTION_NAME_TO_TOTAL_TIME[function_name] += duration
+            FUNCTION_NAME_TO_TOTAL_NUM_CALLS[function_name] += 1
+            # Subtract the time spent in a function from its parent function.
+            # This is to avoid double-counting, since the parent has its own time tracking.
+            # This is particularly bad if a function is recursive,
+            # because it will look many times more expensive than it is.
+            if _CALL_STACK:
+                parent = _CALL_STACK[-1]
+                FUNCTION_NAME_TO_TOTAL_TIME[parent] -= duration
+            total_duration_in_this_function = FUNCTION_NAME_TO_TOTAL_TIME[function_name]
+            if PRINT_EACH_TIME:
+                print(
+                    "  " * _CURRENT_INDENT + f"END {function_name} after {duration}, "
+                                             f"total duration {total_duration_in_this_function}"
+                )
+        return res
+    return wrapper
+
+
+def print_total_runtimes():
+    names_and_times_and_num_calls = [(k, v.total_seconds(), FUNCTION_NAME_TO_TOTAL_NUM_CALLS[k])
+                                     for k, v in FUNCTION_NAME_TO_TOTAL_TIME.items()]
+    names_and_times_and_num_calls.sort(key=lambda a: a[1], reverse=True)
+    for name, time, num_calls in names_and_times_and_num_calls:
+        print(f"{name:>50} -  {num_calls:>10}  -  {time:>15}")
+
+
+def the(a):
+    lngth = len(a)
+    if lngth != 1:
+        raise ValueError(f"Item should have exactly one element, but has {lngth}")
+    return list(a)[0]
+
+
+class PseudoDb:
+    def __init__(self, attributes):
+        self.attributes: List[str] = attributes
+        self.record_set: Optional[Dict[Tuple, Any]] = {}
+        # Set later, when create_index() is used, to replace record_set
+        self.index_attributes: Optional[List[str]] = None
+        self.index_tree: Optional[Dict] = None
+
+    def merge(self, other: 'PseudoDb'):
+        # Sanity checks
+        assert self.attributes == other.attributes
+        my_recs = set(self.record_set.keys())
+        other_recs = set(other.record_set.keys())
+        assert all(len(a) == len(self.attributes) for a in my_recs)
+        assert all(len(a) == len(self.attributes) for a in other_recs)
+        merged_recs = my_recs | other_recs
+        if len(my_recs) + len(other_recs) != len(merged_recs):
+            print(f"The recordings overlap:\n{len(my_recs)=}\n{len(other_recs)=}\n{len(merged_recs)=}")
+            assert False
+        # Update
+        self.record_set.update(other.record_set)
+
+    def serialize(self):
+        frequent_values = {}
+        encoded_records = []
+        for key, val in self.record_set.items():
+            encoded_keys = []
+            for attr, attr_val in zip(self.attributes, key):
+                if attr_val not in frequent_values:
+                    frequent_values[attr_val] = len(frequent_values)
+                encoded_keys.append(frequent_values[attr_val])
+            encoded_records.append([encoded_keys, val])
+        frequent_values = {str(v): k for k, v in frequent_values.items()}
+        res = [self.attributes, frequent_values, encoded_records]
+        return res
+
+    def deserialize(self, json_data):
+        attributes, frequent_values, encoded_records = tuple(json_data)
+        frequent_values = {int(k): v for k, v in frequent_values.items()}
+        self.attributes = attributes
+        self.record_set = {
+            tuple([frequent_values[a] for a in val[0]]): val[1] for val in encoded_records
+        }
+        return self
+
+    def add_record(self, attr_values, result):
+        assert len(attr_values) == len(self.attributes)
+        assert attr_values not in self.record_set, attr_values
+        self.record_set[attr_values] = result
+
+    @runtime_analysis_decorator
+    def create_index(self, index_attributes):
+        assert all(a in self.attributes for a in index_attributes)
+        self.index_attributes = list(index_attributes)
+        index_attribute_indices = [self.attributes.index(a) for a in self.index_attributes]
+        self.index_tree = {}
+        for key, val in self.record_set.items():
+            tree = self.index_tree
+            for idx in index_attribute_indices:
+                tree = tree.setdefault(key[idx], {})
+            tree[key] = val
+        self.record_set = None
+
+    @runtime_analysis_decorator
+    def get_matches(self, filters: Dict[str, Any]) -> Tuple[List[Tuple[Tuple[Any, ...], Any]], Dict[str, Set]]:
+        filters_with_indices = {self.attributes.index(k): v for k, v in filters.items()}
+        # Filter using an index.
+        # While doing so, up to max_num_mismatches_to_allow many attributes may be mismatched
+        # while going through the index.
+        # (This is important for determining possible_attribute_values later).
+        max_num_mismatches_to_allow = 1
+        if self.index_attributes is None:
+            assert self.index_tree is None
+            record_set = self.record_set
+        else:
+            assert self.record_set is None
+            index_attribute_indices = [self.attributes.index(a) for a in self.index_attributes]
+            trees_to_process = [([], self.index_tree)]
+            for idx in index_attribute_indices:
+                next_level_of_trees_to_process = []
+                has_filter = (idx in filters_with_indices)
+                if has_filter:
+                    filter_value = filters_with_indices[idx]
+                    for mismatches, tree in trees_to_process:
+                        if filter_value in tree:
+                            next_level_of_trees_to_process.append((mismatches, tree[filter_value]))
+                        if len(mismatches) < max_num_mismatches_to_allow:
+                            for a, subtree in tree.items():
+                                if a != filter_value or ((a is None) != (filter_value is None)):
+                                    next_level_of_trees_to_process.append((mismatches + [idx], subtree))
+                else:
+                    for mismatches, tree in trees_to_process:
+                        for subtree in tree.values():
+                            next_level_of_trees_to_process.append((mismatches, subtree))
+                trees_to_process = next_level_of_trees_to_process
+            record_set = {}
+            for mismatches, d in trees_to_process:
+                record_set.update(d)
+        # Search for records
+        list_of_matches = []
+        possible_attribute_values = {k: set() for k in self.attributes}
+        for attr_values, result in record_set.items():
+            num_mismatches = 0
+            for idx, filter_value in filters_with_indices.items():
+                if attr_values[idx] != filter_value:
+                    num_mismatches += 1
+                    if num_mismatches == 2:
+                        break
+            if num_mismatches == 0:
+                list_of_matches.append((attr_values, result))
+            if num_mismatches < 2:
+                for idx, attr_name, in enumerate(self.attributes):
+                    # If the record either matches, or doesn't match but only because of the attribute in question,
+                    # then that attribute value is a legal value for selection.
+                    if num_mismatches == 0 or \
+                            (num_mismatches == 1 and idx in filters_with_indices and attr_values[idx] != filters_with_indices[idx]):
+                        possible_attribute_values[attr_name].add(attr_values[idx])
+        return list_of_matches, possible_attribute_values
+
+
+def number_to_hex(number):
+    if math.isnan(number):
+        return "#000000"
+    if number < -1:
+        number = -1
+    elif number > 1:
+        number = 1
+    colorscale = px.colors.sequential.Viridis
+    return colorscale[int((number + 1) / 2 * (len(colorscale) - 1))]
