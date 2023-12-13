71a86627ce74c9df2e3cdea56ecab24d825f5a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 04:03:06 +0000

    Add package sqlmodelx/0.0.6/sqlmodelx-0.0.6.tar.gz

---
+++ packages/sqlmodelx/sqlmodelx-0.0.6.tar.gz/sqlmodelx-0.0.6/sqlmodelx/main.py
@@ -0,0 +1,208 @@
+from typing import Any, Dict, List, Set, Tuple, Type
+
+from pydantic import BaseConfig
+from pydantic.fields import SHAPE_SINGLETON, ModelField, Undefined
+from pydantic.main import ModelMetaclass
+from pydantic.typing import ForwardRef, resolve_annotations
+from sqlalchemy import Column, inspect
+from sqlalchemy.ext.hybrid import hybrid_method, hybrid_property
+from sqlalchemy.orm import (
+    ColumnProperty,
+    Mapped,
+    RelationshipProperty,
+    declared_attr,
+    relationship,
+)
+from sqlalchemy.orm.decl_api import DeclarativeMeta
+from sqlalchemy.util import classproperty, memoized_property
+from sqlmodel import SQLModel as _SQLModel
+from sqlmodel.main import RelationshipInfo
+from sqlmodel.main import SQLModelMetaclass as _SQLModelMetaclass
+from sqlmodel.main import get_column_from_field as _get_column_from_field
+from typing_extensions import get_origin
+
+from sqlmodelx.enums import Choices
+from sqlmodelx.sqltypes import ChoiceType
+
+try:
+    from functools import cached_property
+except ImportError:
+    cached_property = memoized_property
+
+SaColumnTypes = (
+    Column,
+    ColumnProperty,
+    hybrid_property,
+    declared_attr,
+)
+
+
+def get_column_from_field(field: ModelField) -> Any:
+    sa_column = getattr(field.field_info, "sa_column", Undefined)
+    if isinstance(sa_column, SaColumnTypes):  # support ColumnProperty
+        return sa_column
+    if field.default and isinstance(field.default, SaColumnTypes):
+        return field.default
+    if isinstance(field.type_, type) and field.shape == SHAPE_SINGLETON:
+        # Support for choices enums
+        if issubclass(field.type_, Choices):
+            setattr(field.field_info, "sa_type", ChoiceType(field.type_))
+    return _get_column_from_field(field)
+
+
+class _SQLModelBasesInfo:
+    def __init__(self, bases):
+        self.is_table = False
+        self.tablename = None
+        self.columns = {}
+        self.sqlmodel_relationships = {}
+        for base in bases:
+            config = getattr(base, "__config__", None)
+            if config and getattr(config, "table", False):
+                self.is_table = True
+                self.tablename = base.__tablename__
+                # noinspection PyProtectedMember
+                self.columns.update(base.__table__.columns._index)
+                self.sqlmodel_relationships.update(base.__sqlmodel_relationships__)
+
+
+class SQLModelMetaclass(_SQLModelMetaclass):
+    # From Pydantic
+    def __new__(
+        cls,
+        name: str,
+        bases: Tuple[Type[Any], ...],
+        class_dict: Dict[str, Any],
+        **kwargs: Any,
+    ) -> Any:
+        relationships: Dict[str, RelationshipInfo] = {}
+        dict_for_pydantic = {}
+        original_annotations = resolve_annotations(class_dict.get("__annotations__", {}), class_dict.get("__module__", None))
+        pydantic_annotations = {}
+        relationship_annotations = {}
+        for k, v in class_dict.items():
+            if isinstance(v, RelationshipInfo):
+                relationships[k] = v
+            else:
+                dict_for_pydantic[k] = v
+        for k, v in original_annotations.items():
+            if k in relationships:
+                relationship_annotations[k] = v
+            else:
+                pydantic_annotations[k] = v
+        dict_used = {
+            **dict_for_pydantic,
+            "__weakref__": None,
+            "__sqlmodel_relationships__": relationships,
+            "__annotations__": pydantic_annotations,
+        }
+        # Duplicate logic from Pydantic to filter config kwargs because if they are
+        # passed directly including the registry Pydantic will pass them over to the
+        # superclass causing an error
+        allowed_config_kwargs: Set[str] = {
+            key
+            for key in dir(BaseConfig)
+            if not (key.startswith("__") and key.endswith("__"))  # skip dunder methods and attributes
+        }
+        pydantic_kwargs = kwargs.copy()
+        config_kwargs = {key: pydantic_kwargs.pop(key) for key in pydantic_kwargs.keys() & allowed_config_kwargs}
+        new_cls = ModelMetaclass.__new__(cls, name, bases, dict_used, **config_kwargs)
+        new_cls.__annotations__ = {
+            **relationship_annotations,
+            **pydantic_annotations,
+            **new_cls.__annotations__,
+        }
+
+        def get_config(name: str, default: Any = Undefined) -> Any:
+            kwarg_value = kwargs.get(name, default)
+            if kwarg_value is not Undefined:
+                # If it was passed by kwargs, ensure it's also set in config
+                setattr(new_cls.__config__, name, kwarg_value)
+                return kwarg_value
+            return getattr(new_cls.__config__, name, default)
+
+        config_table = get_config("table", False)
+        if config_table is True:
+            # # If it was passed by kwargs, ensure it's also set in config
+            # new_cls.__config__.table = config_table
+            for k, v in new_cls.__fields__.items():
+                # Ensure that the column is unique
+                if not hasattr(new_cls, k):
+                    col = get_column_from_field(v)
+                    setattr(new_cls, k, col)
+            # Set a config flag to tell FastAPI that this should be read with a field
+            # in orm_mode instead of preemptively converting it to a dict.
+            # This could be done by reading new_cls.__config__.table in FastAPI, but
+            # that's very specific about SQLModel, so let's have another config that
+            # other future tools based on Pydantic can use.
+            new_cls.__config__.read_with_orm_mode = True
+
+        config_registry = get_config("registry")
+        if config_registry is not Undefined:
+            # config_registry = cast(registry, config_registry)
+            # # If it was passed by kwargs, ensure it's also set in config
+            setattr(new_cls, "_sa_registry", config_registry)
+            setattr(new_cls, "metadata", config_registry.metadata)
+            setattr(new_cls, "__abstract__", True)
+        return new_cls
+
+    # noinspection PyMissingConstructor
+    def __init__(cls, classname: str, bases: Tuple[type, ...], dict_: Dict[str, Any], **kw: Any) -> None:
+        # Only one of the base classes (or the current one) should be a table model
+        # this allows FastAPI cloning a SQLModel for the response_model without
+        cls._bases = _SQLModelBasesInfo(bases)
+        if kw.get("table", False):
+            for rel_name, rel_info in cls.__sqlmodel_relationships__.items():
+                if rel_info.sa_relationship:
+                    # There's a SQLAlchemy relationship declared, that takes precedence
+                    # over anything else, use that and continue with the next attribute
+                    setattr(cls, rel_name, rel_info.sa_relationship)
+                    continue
+                raw_ann = cls.__annotations__[rel_name]
+                origin = get_origin(raw_ann)
+                if origin is Mapped:
+                    ann = raw_ann.__args__[0]
+                else:
+                    ann = raw_ann
+                    # Plain forward references, for models not yet defined, are not
+                    # handled well by SQLAlchemy without Mapped, so, wrap the
+                    # annotations in Mapped here
+                    cls.__annotations__[rel_name] = Mapped[ann]  # type: ignore[valid-type]
+                temp_field = ModelField.infer(
+                    name=rel_name,
+                    value=rel_info,
+                    annotation=ann,
+                    class_validators=None,
+                    config=BaseConfig,
+                )
+                relationship_to = temp_field.type_
+                if isinstance(temp_field.type_, ForwardRef):
+                    relationship_to = temp_field.type_.__forward_arg__
+                rel_kwargs: Dict[str, Any] = {}
+                if rel_info.back_populates:
+                    rel_kwargs["back_populates"] = rel_info.back_populates
+                if rel_info.link_model:
+                    ins = inspect(rel_info.link_model)
+                    local_table = getattr(ins, "local_table", None)
+                    if local_table is None:
+                        raise RuntimeError("Couldn't find the secondary table for " f"model {rel_info.link_model}")
+                    rel_kwargs["secondary"] = local_table
+                rel_args: List[Any] = []
+                if rel_info.sa_relationship_args:
+                    rel_args.extend(rel_info.sa_relationship_args)
+                if rel_info.sa_relationship_kwargs:
+                    rel_kwargs.update(rel_info.sa_relationship_kwargs)
+                rel_value: RelationshipProperty = relationship(relationship_to, *rel_args, **rel_kwargs)
+                setattr(cls, rel_name, rel_value)
+            DeclarativeMeta.__init__(cls, classname, bases, dict_, **kw)
+            if cls._bases.is_table:
+                cls.__sqlmodel_relationships__.update(cls._bases.sqlmodel_relationships)
+        else:
+            ModelMetaclass.__init__(cls, classname, bases, dict_)
+
+
+class SQLModel(_SQLModel, metaclass=SQLModelMetaclass):
+    __table_args__ = {"extend_existing": True}
+
+    class Config:
+        keep_untouched = (classproperty, cached_property, memoized_property, hybrid_method, *SaColumnTypes)
---
+++ packages/sqlmodelx/sqlmodelx-0.0.6.tar.gz/sqlmodelx-0.0.6/tests/test_main.py
@@ -0,0 +1,201 @@
+from datetime import datetime
+from typing import List
+
+from sqlalchemy import Column, ForeignKey, Integer, String, func
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import (
+    InstrumentedAttribute,
+    column_property,
+    declared_attr,
+    deferred,
+    relationship,
+)
+from sqlmodel import Field, Relationship, Session, select
+
+from sqlmodelx import SQLModel
+
+# from sqlmodel import SQLModel
+from sqlmodelx.main import SQLModelMetaclass
+
+
+class PkMixin(SQLModel):
+    id: int = Field(default=None, primary_key=True, nullable=False)
+
+
+class BaseUser(PkMixin):
+    username: str = Field(default="", nullable=False)
+    password: str = Field(default="", nullable=False)
+    create_time: datetime = Field(default_factory=datetime.now, nullable=False)
+    group_id: int = Field(default=None, nullable=True, foreign_key="group.id")
+
+
+class User(BaseUser, table=True):
+    __tablename__ = "user"
+    group: "Group" = Relationship(back_populates="users")
+
+
+class Group(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True, nullable=False)
+    name: str = Field(default="", nullable=False)
+    create_time: datetime = Field(default_factory=datetime.now, nullable=False)
+    users: List[User] = Relationship(back_populates="group", sa_relationship_kwargs={"enable_typechecks": False})
+
+
+def test_class_and_metaclass(engine):
+    """Test class and metaclass"""
+    from sqlmodel import SQLModel as _SQLModel
+    from sqlmodel.main import SQLModelMetaclass as _SQLModelMetaclass
+
+    assert isinstance(User, SQLModelMetaclass)
+    assert isinstance(User, _SQLModelMetaclass)
+    assert issubclass(User, SQLModel)
+    assert issubclass(User, _SQLModel)
+
+
+def test_base_is_table_and_subclass_is_table(engine):
+    """Test base class and subclass are both ORM database tables"""
+
+    # Extend the user ORM model to add a field
+    class NickNameUser(User, table=True):
+        nickname: str = Field(default="")
+
+    # Extend the user ORM model to add a field
+    class AvatarUser(NickNameUser, table=True):
+        avatar: str = Field(default="")
+
+    # Create the database tables
+    SQLModel.metadata.drop_all(engine)
+    SQLModel.metadata.create_all(engine)
+
+    avatar_user = AvatarUser(
+        username="Deadpond",
+        password="Dive Wilson",
+        nickname="nickname",
+        avatar="avatar",
+        group=Group(name="admin"),
+    )
+
+    with Session(engine) as session:
+        session.add(avatar_user)
+        session.commit()
+        session.refresh(avatar_user)
+        assert avatar_user.id is not None
+        # The relationship property of the base class will also be inherited
+        assert avatar_user.group.id is not None
+
+        nickname_user = session.query(NickNameUser).first()
+        assert nickname_user.nickname == avatar_user.nickname
+        # The relationship property of the base class will also be inherited
+        assert nickname_user.group.id == avatar_user.group.id
+
+        user = session.exec(select(User)).first()
+        assert user.username == avatar_user.username
+        assert user.group.id == avatar_user.group.id
+
+
+def test_base_is_table_and_subclass_is_not_table(engine):
+    """Test base class is an ORM database table, the subclass is not"""
+
+    # Create a pydantic model quickly through inheritance
+    class NickNameUserSchema(User, table=False):
+        nickname: str = Field(default="")
+        group_: Group = Field(default=None, alias="group")
+
+    user = User(username="Deadpond", password="Dive Wilson", group=Group(name="admin"))
+
+    with Session(engine) as session:
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+        assert user.id is not None
+        assert user.group.id is not None
+
+        user_ex = NickNameUserSchema.from_orm(user, update={"nickname": "nickname"})
+        assert user_ex.id == user.id
+        assert user_ex.nickname == "nickname"
+        assert user_ex.group_.id == user.group.id
+
+
+def test_relationship_sa_relationship(engine):
+    class MyUser(BaseUser, table=True):
+        __tablename__ = "user"
+        group: "Group" = Relationship(sa_relationship=relationship("Group"))
+
+    # Create the database tables
+    SQLModel.metadata.drop_all(engine)
+    SQLModel.metadata.create_all(engine)
+
+    user = MyUser(
+        username="Deadpond",
+        password="Dive Wilson",
+        group=Group(name="admin"),
+    )
+
+    with Session(engine) as session:
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+        assert user.id is not None
+        assert user.group is not None
+
+
+def test_SaColumnTypes(engine):
+    class MyUser(PkMixin, table=True):
+        __tablename__ = "user"
+
+        id = Column(Integer, primary_key=True)
+
+        firstname: str = Field(default="")
+        lastname: str = Field(default="")
+
+        info: str = deferred(Column(String(100)))  # lazy load column
+
+        group_count: int = Field(sa_column=column_property(select(func.count(Group.id))))  # type: ignore
+        """The type of the field is int, but the type of the column is sqlalchemy.orm.ColumnProperty"""
+
+        group_count2 = column_property(select(func.count(Group.id)))
+
+        @hybrid_property
+        def fullname(self):
+            return self.firstname + " " + self.lastname
+
+        @declared_attr
+        def group_id(self):
+            return Column(ForeignKey("group.id"))
+
+    # Create the database tables
+    SQLModel.metadata.drop_all(engine)
+    SQLModel.metadata.create_all(engine)
+    # test declared_attr
+    assert isinstance(MyUser.group_id, InstrumentedAttribute)
+
+    user = MyUser(
+        firstname="Deadpond",
+        lastname="Dive Wilson",
+        info="info",
+        group_count=1,  # readonly
+    )
+
+    with Session(engine) as session:
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+
+        # test attribute
+        assert user.id is not None
+        assert user.fullname == "Deadpond Dive Wilson"
+        assert user.group_count == 0
+        assert user.group_count2 == 0
+
+        # test dict
+        dct = user.dict()
+        assert dct["id"] is not None
+        assert "fullname" not in dct
+        assert dct["group_count"] == 0
+
+        # test deferred column
+        assert "info" not in dct
+        assert user.info == "info"  # load info column
+        dct = user.dict()
+        assert "info" in dct
+        assert dct["info"] == "info"
