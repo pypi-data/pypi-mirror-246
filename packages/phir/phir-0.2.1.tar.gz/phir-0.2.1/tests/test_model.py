c8505b602b69cd476dad8183c8bd955202f65f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 05:03:21 +0000

    Add package phir/0.2.1/phir-0.2.1.tar.gz

---
+++ packages/phir/phir-0.2.1.tar.gz/phir-0.2.1/tests/test_model.py
@@ -0,0 +1,22 @@
+##############################################################################
+#
+# Copyright (c) 2023 Quantinuum LLC All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+#
+##############################################################################
+
+"""Basic validation tests."""
+
+import json
+from pathlib import Path
+
+from phir.model import PHIRModel
+
+
+def test_spec_example() -> None:  # noqa: D103
+    # From https://github.com/CQCL/phir/blob/main/spec.md#overall-phir-example-with-quantinuums-extended-openqasm-20
+    with Path("tests/example.json").open() as f:
+        data = json.load(f)  # type: ignore [misc]
+
+    PHIRModel.model_validate(data)  # type: ignore [misc]
