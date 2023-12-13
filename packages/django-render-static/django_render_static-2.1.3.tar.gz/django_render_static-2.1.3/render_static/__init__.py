cbd382637ff0c21fc88470ad9789cbadd0d6d0de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 06:04:12 +0000

    Add package django-render-static/2.1.3/django_render_static-2.1.3.tar.gz

---
+++ packages/django-render-static/django_render_static-2.1.3.tar.gz/django_render_static-2.1.3/render_static/__init__.py
@@ -0,0 +1,36 @@
+r"""
+    ____                 __             _____ __        __  _
+   / __ \___  ____  ____/ /__  _____   / ___// /_____ _/ /_(_)____
+  / /_/ / _ \/ __ \/ __  / _ \/ ___/   \__ \/ __/ __ `/ __/ / ___/
+ / _, _/  __/ / / / /_/ /  __/ /      ___/ / /_/ /_/ / /_/ / /__
+/_/ |_|\___/_/ /_/\__,_/\___/_/      /____/\__/\__,_/\__/_/\___/
+
+"""
+from datetime import datetime
+
+from .context import resolve_context
+from .resource import resource
+from .transpilers.defines_to_js import DefaultDefineTranspiler
+from .transpilers.enums_to_js import EnumClassWriter
+from .transpilers.urls_to_js import ClassURLWriter, SimpleURLWriter
+
+VERSION = (2, 1, 3)
+
+__title__ = 'Django Render Static'
+__version__ = '.'.join(str(i) for i in VERSION)
+__author__ = 'Brian Kohan'
+__license__ = 'MIT'
+__copyright__ = f'Copyright 2020-{datetime.now().year} Brian Kohan'
+
+
+class Jinja2DependencyNeeded:  # pylint: disable=R0903
+    """
+    Jinja2 is an optional dependency - lazy fail if its use is attempted
+    without it being present on the python path.
+    """
+
+    def __init__(self, *args, **kwargs):
+        raise ImportError(  # pragma: no cover
+            'To use the Jinja2 backend you must install the Jinja2 python '
+            'package.'
+        )
