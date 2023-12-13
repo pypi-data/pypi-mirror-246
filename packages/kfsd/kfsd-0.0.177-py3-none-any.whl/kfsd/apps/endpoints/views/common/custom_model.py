f204d275ede47525a245d23cc59bbda1761fc745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 04:03:57 +0000

    Add package kfsd/0.0.177/kfsd-0.0.177-py3-none-any.whl

---
+++ packages/kfsd/kfsd-0.0.177-py3-none-any.whl/kfsd/apps/endpoints/views/common/custom_model.py
@@ -0,0 +1,22 @@
+from rest_framework.response import Response
+from rest_framework import status
+
+from kfsd.apps.endpoints.views.common.model import ModelViewSet
+
+
+class CustomModelViewSet(ModelViewSet):
+    lookup_field = "identifier"
+    lookup_value_regex = "[^/]+"
+
+    def perform_create(self, serializer):
+        request = self.request
+        validated_data = serializer.validated_data
+        if "created_by" not in validated_data:
+            createdBy = None
+            if request.token_user.isAuthenticated():
+                createdBy = request.token_user.getUserId()
+            elif hasattr(request, "api_key"):
+                createdBy = request.api_key_user.getUserId()
+            validated_data["created_by"] = createdBy
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
