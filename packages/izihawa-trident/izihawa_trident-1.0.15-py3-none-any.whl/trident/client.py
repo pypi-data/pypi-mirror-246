b53efc559ce2900855c76375236d5da63a681730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 05:03:06 +0000

    Add package izihawa-trident/1.0.15/izihawa_trident-1.0.15-py3-none-any.whl

---
+++ packages/izihawa-trident/izihawa_trident-1.0.15-py3-none-any.whl/trident/client.py
@@ -0,0 +1,25 @@
+from aiobaseclient import BaseClient
+from aiobaseclient.exceptions import ExternalServiceError
+
+
+class TridentClient(BaseClient):
+    async def response_processor(self, response):
+        if response.status == 404:
+            return None
+        elif response.status != 200:
+            data = await response.read()
+            if hasattr(response, 'request'):
+                raise ExternalServiceError(response.request.url, response.status, data)
+            else:
+                raise ExternalServiceError(None, response.status, data)
+        return response
+
+    async def store(self, key: str, data: bytes) -> dict:
+        url = f'/kv/{key}'
+        response = await self.put(url, data=data)
+        return await response.json()
+
+    async def read(self, key: str) -> bytes:
+        url = f'/kv/{key}'
+        response = await self.get(url)
+        return await response.read()
