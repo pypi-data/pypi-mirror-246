5f35527817d8e5875ad10608d19d03b292a0d8ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 14:04:24 +0000

    Add package pyrogram-aylak/2.0.106.21/pyrogram_aylak-2.0.106.21-py3-none-any.whl

---
+++ packages/pyrogram-aylak/pyrogram_aylak-2.0.106.21-py3-none-any.whl/pyrogram/types/bots_and_keyboards/request_channel_info.py
@@ -0,0 +1,46 @@
+#  Pyrogram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#
+#  This file is part of Pyrogram.
+#
+#  Pyrogram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pyrogram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
+
+from ..object import Object
+
+
+class RequestChannelInfo(Object):
+    """Contains information about a channel peer type.
+
+    Parameters:
+        button_id (``int``):
+            Identifier of button.
+
+        is_creator (``bool``):
+            If True, returns the list of chats where this user is a channel creator.
+
+        has_username (``bool``):
+            If True, returns the list of chats where chat has username.
+    """
+
+    def __init__(
+        self, *,
+        button_id: int,
+        is_creator: bool = None,
+        has_username: bool = None,
+    ):
+        super().__init__()
+
+        self.button_id = button_id
+        self.is_creator = is_creator
+        self.has_username = has_username
