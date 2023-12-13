929db7940ed6b8bcb77f67158792a6b5a32578ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 04:03:30 +0000

    Add package phantasma-py/1.0.3/phantasma-py-1.0.3.tar.gz

---
+++ packages/phantasma-py/phantasma-py-1.0.3.tar.gz/phantasma-py-1.0.3/VM/vmtype.py
@@ -0,0 +1,13 @@
+from enum import IntEnum
+
+
+class VMType(IntEnum):
+    NONE = 0
+    Struct = 1
+    Bytes = 2
+    Number = 3
+    String = 4
+    Timstamp = 5
+    Bool = 6
+    Enum = 7
+    Object = 8
