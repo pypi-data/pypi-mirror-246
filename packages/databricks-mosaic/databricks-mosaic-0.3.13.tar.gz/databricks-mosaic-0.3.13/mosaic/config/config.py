ddf5ec5cbe95bd2fa7bd1715b5eba321b123b910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 04:03:04 +0000

    Add package databricks-mosaic/0.3.13/databricks-mosaic-0.3.13.tar.gz

---
+++ packages/databricks-mosaic/databricks-mosaic-0.3.13.tar.gz/databricks-mosaic-0.3.13/mosaic/config/config.py
@@ -0,0 +1,13 @@
+from IPython import InteractiveShell
+from pyspark.sql import SparkSession, SQLContext
+
+from mosaic.core.mosaic_context import MosaicContext
+from mosaic.utils.display_handler import DisplayHandler
+
+mosaic_spark: SparkSession
+mosaic_context: MosaicContext
+sql_context: SQLContext
+display_handler: DisplayHandler
+ipython_hook: InteractiveShell
+notebook_utils = None
+default_gdal_init_script_path: str = "/dbfs/FileStore/geospatial/mosaic/gdal/"
