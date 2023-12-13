682b8581bd1bea9190d6fb90ee98ee17b5eeea05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 15:03:54 +0000

    Add package CheckListProce/0.0.7/CheckListProce-0.0.7.tar.gz

---
+++ packages/CheckListProce/CheckListProce-0.0.7.tar.gz/CheckListProce-0.0.7/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  8 14:55:19 2023
+
+@author: QianYang
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="CheckListProce",
+    version="0.0.7",
+    packages=find_packages(),
+)
