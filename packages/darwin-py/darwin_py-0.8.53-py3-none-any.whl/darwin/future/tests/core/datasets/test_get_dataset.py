0e1fb350f0afb5b3c299e3e6aa683fdffa69af75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 04:03:10 +0000

    Add package darwin-py/0.8.53/darwin_py-0.8.53-py3-none-any.whl

---
+++ packages/darwin-py/darwin_py-0.8.53-py3-none-any.whl/darwin/future/tests/core/datasets/test_get_dataset.py
@@ -0,0 +1,44 @@
+import responses
+from pydantic import ValidationError
+from pytest import raises
+
+from darwin.future.core.client import ClientCore
+from darwin.future.core.datasets import get_dataset
+from darwin.future.data_objects.dataset import DatasetCore
+from darwin.future.exceptions import BadRequest
+from darwin.future.tests.core.fixtures import *
+
+from .fixtures import *
+
+
+def test_it_gets_a_dataset(base_client: ClientCore, basic_dataset: DatasetCore) -> None:
+    with responses.RequestsMock() as rsps:
+        rsps.add(
+            rsps.GET,
+            base_client.config.api_endpoint + "datasets",
+            json=basic_dataset,
+            status=200,
+        )
+
+        dataset = get_dataset(base_client, "test-dataset")
+
+        assert dataset.name == "test-dataset"
+        assert dataset.slug == "1337"
+
+
+def test_it_raises_an_error_on_http_error(base_client: ClientCore) -> None:
+    with responses.RequestsMock() as rsps:
+        rsps.add(
+            rsps.GET,
+            base_client.config.api_endpoint + "datasets",
+            json={},
+            status=400,
+        )
+        with raises(BadRequest):
+            get_dataset(base_client, "test-dataset")
+            get_dataset(base_client, "test-dataset")
+
+
+def test_it_raises_an_error_on_pydantic_error(sad_client_pydantic: ClientCore) -> None:
+    with raises(ValidationError):
+        get_dataset(sad_client_pydantic, "test-dataset")
