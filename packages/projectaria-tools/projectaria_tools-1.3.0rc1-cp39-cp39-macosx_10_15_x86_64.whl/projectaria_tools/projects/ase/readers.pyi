a83dbcbafbfc0960fe0f8474db104465ff34e8a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 05:03:05 +0000

    Add package projectaria-tools/1.3.0rc1/projectaria_tools-1.3.0rc1-cp39-cp39-macosx_10_15_x86_64.whl

---
+++ packages/projectaria-tools/projectaria_tools-1.3.0rc1-cp39-cp39-macosx_10_15_x86_64.whl/projectaria_tools/projects/ase/readers.pyi
@@ -0,0 +1,14 @@
+from __future__ import annotations
+from projectaria_tools.core.sophus import SE3
+import numpy as np
+import os as os
+from projectaria_tools.core import mps
+__all__ = ['SE3', 'mps', 'np', 'os', 'read_language_file', 'read_points_file', 'read_trajectory_file']
+def _read_trajectory_line(line):
+    ...
+def read_language_file(filepath):
+    ...
+def read_points_file(filepath):
+    ...
+def read_trajectory_file(filepath):
+    ...
