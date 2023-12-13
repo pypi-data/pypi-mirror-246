69ab7b37865eceb22e30532913dfdb3614370951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 06:03:21 +0000

    Add package transitfit/3.4.1/transitfit-3.4.1.tar.gz

---
+++ packages/transitfit/transitfit-3.4.1.tar.gz/transitfit-3.4.1/transitfit/__init__.py
@@ -0,0 +1,15 @@
+'''
+TransitFit package
+
+This package is designed to fit transit light curves using BATMAN
+'''
+name = 'transitfit'
+__version__ = '3.4.1'
+
+from .retriever import Retriever
+from .priorinfo import PriorInfo
+from .lightcurve import LightCurve
+from ._pipeline import run_retrieval
+from ._utils import split_lightcurve_file, calculate_logg, AU_to_host_radii
+from .lightcurve import LightCurve
+from .error_analysis import get_quantiles_on_best_val
