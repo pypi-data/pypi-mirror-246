0d8234b2b85dd094ec2e0aaf893e4827972f3160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 06:04:07 +0000

    Add package battleships-edf1101/1.0/battleships-edf1101-1.0.tar.gz

---
+++ packages/battleships-edf1101/battleships-edf1101-1.0.tar.gz/battleships-edf1101-1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setuptools.setup(
+    name="battleships-edf1101",
+    version="1.0",
+    author="Ed Fillingham",
+    author_email="ef494@exeter.ac.uk",
+    description="2023 ECM1400 Coursework - Battleships",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/edf1101/Battleships",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11.4",
+)
