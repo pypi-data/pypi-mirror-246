3a117781c74c4919a45e75182d3d8ed3f69d1487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 04:03:57 +0000

    Add package lymph-model/1.0.0a4/lymph-model-1.0.0a4.tar.gz

---
+++ packages/lymph-model/lymph-model-1.0.0a4.tar.gz/lymph-model-1.0.0a4/lymph/matrix.py
@@ -0,0 +1,275 @@
+"""
+Methods & classes to manage matrices of the :py:class:`~lymph.models.Unilateral` class.
+"""
+# pylint: disable=too-few-public-methods
+from __future__ import annotations
+
+import warnings
+
+import numpy as np
+import pandas as pd
+
+from lymph import models
+from lymph.helper import (
+    AbstractLookupDict,
+    get_state_idx_matrix,
+    row_wise_kron,
+    tile_and_repeat,
+)
+
+
+def generate_transition(instance: models.Unilateral) -> np.ndarray:
+    """Compute the transition matrix of the lymph model."""
+    num_lnls = len(instance.graph.lnls)
+    num_states = 3 if instance.graph.is_trinary else 2
+    transition_matrix = np.ones(shape=(num_states**num_lnls, num_states**num_lnls))
+
+    for i, lnl in enumerate(instance.graph.lnls.values()):
+        current_state_idx = get_state_idx_matrix(
+            lnl_idx=i,
+            num_lnls=num_lnls,
+            num_states=num_states,
+        )
+        new_state_idx = current_state_idx.T
+
+        # This needs to be initialized with a one where no transition happens
+        # and a zero where a transition happens. This is because of how differently
+        # the transition matrix entries are computed for no spread vs. spread.
+        lnl_transition_matrix = new_state_idx == current_state_idx
+
+        for edge in lnl.inc:
+            if edge.is_tumor_spread:
+                edge_transition_grid = edge.transition_tensor[
+                    0, current_state_idx, new_state_idx
+                ]
+            else:
+                parent_node_i = list(instance.graph.lnls.values()).index(edge.parent)
+                parent_state_idx = get_state_idx_matrix(
+                    lnl_idx=parent_node_i,
+                    num_lnls=num_lnls,
+                    num_states=num_states,
+                )
+                edge_transition_grid = edge.transition_tensor[
+                    parent_state_idx, current_state_idx, new_state_idx
+                ]
+
+            lnl_transition_matrix = np.where(
+                # For transitions, we need to compute the probability that none of
+                # the incoming edges of an LNL spread. This is done by multiplying
+                # the probabilities of all edges not spreading and taking the
+                # complement: 1 - (1 - p_1) * (1 - p_2) * ...
+                # If an LNL remains in its current state, the probability is
+                # simply the product of all incoming edges not spreading.
+                new_state_idx == current_state_idx + 1,
+                1 - (1 - lnl_transition_matrix) * (1 - edge_transition_grid),
+                lnl_transition_matrix * edge_transition_grid,
+            )
+
+        transition_matrix *= lnl_transition_matrix
+
+    return transition_matrix
+
+
+def generate_observation(instance: models.Unilateral) -> np.ndarray:
+    """Generate the observation matrix of the lymph model."""
+    num_lnls = len(instance.graph.lnls)
+    base = 2 if instance.graph.is_binary else 3
+    shape = (base ** num_lnls, 1)
+    observation_matrix = np.ones(shape=shape)
+
+    for modality in instance.modalities.values():
+        mod_obs_matrix = np.ones(shape=(1,1))
+        for _ in instance.graph.lnls:
+            mod_obs_matrix = np.kron(mod_obs_matrix, modality.confusion_matrix)
+
+        observation_matrix = row_wise_kron(observation_matrix, mod_obs_matrix)
+
+    return observation_matrix
+
+
+def compute_encoding(
+    lnls: list[str],
+    pattern: pd.Series | dict[str, bool | int | str],
+    base: int = 2,
+) -> np.ndarray:
+    """Compute the encoding of a particular ``pattern`` of involvement.
+
+    A ``pattern`` holds information about the involvement of each LNL and the function
+    transforms this into a binary encoding which is ``True`` for all possible complete
+    states/diagnoses that are compatible with the given ``pattern``.
+
+    In the binary case (``base=2``), the value behind ``pattern[lnl]`` can be one of
+    the following things:
+    - ``False``: The LNL is healthy.
+    - ``"healthy"``: The LNL is healthy.
+    - ``True``: The LNL is involved.
+    - ``"involved"``: The LNL is involved.
+    - ``pd.isna(pattern[lnl]) == True``: The involvement of the LNL is unknown.
+
+    In the trinary case (``base=3``), the value behind ``pattern[lnl]`` can be one of
+    these things:
+    - ``False``: The LNL is healthy.
+    - ``"healthy"``: The LNL is healthy.
+    - ``True``: The LNL is involved (micro- or macroscopic).
+    - ``"involved"``: The LNL is involved (micro- or macroscopic).
+    - ``"micro"``: The LNL is involved microscopically only.
+    - ``"macro"``: The LNL is involved macroscopically only.
+    - ``"notmacro"``: The LNL is healthy or involved microscopically.
+
+    Missing values are treated as unknown involvement.
+
+    Examples:
+    >>> compute_encoding(["II", "III"], {"II": True, "III": False})
+    array([False, False,  True, False])
+    >>> compute_encoding(["II", "III"], {"II": "involved"})
+    array([False, False,  True,  True])
+    >>> compute_encoding(
+    ...     lnls=["II", "III"],
+    ...     pattern={"II": True, "III": False},
+    ...     base=3,
+    ... )
+    array([False, False, False,  True, False, False,  True, False, False])
+    >>> compute_encoding(
+    ...     lnls=["II", "III"],
+    ...     pattern={"II": "micro", "III": "notmacro"},
+    ...     base=3,
+    ... )
+    array([False, False, False,  True,  True, False, False, False, False])
+    """
+    num_lnls = len(lnls)
+    encoding = np.ones(shape=base ** num_lnls, dtype=bool)
+
+    if base == 2:
+        element_map = {
+            "healthy": np.array([True, False]),
+            False: np.array([True, False]),
+            "involved": np.array([False, True]),
+            True: np.array([False, True]),
+        }
+    elif base == 3:
+        element_map = {
+            "healthy": np.array([True, False, False]),
+            False: np.array([True, False, False]),
+            "involved": np.array([False, True, True]),
+            True: np.array([False, True, True]),
+            "micro": np.array([False, True, False]),
+            "macro": np.array([False, False, True]),
+            "notmacro": np.array([True, True, False]),
+        }
+    else:
+        raise ValueError(f"Invalid base {base}.")
+
+    for j, lnl in enumerate(lnls):
+        if lnl not in pattern or pd.isna(pattern[lnl]):
+            continue
+
+        try:
+            element = element_map[pattern[lnl]]
+        except KeyError as key_err:
+            raise ValueError(
+                f"Invalid pattern for LNL {lnl}: {pattern[lnl]}",
+            ) from key_err
+
+        encoding = np.logical_and(
+            encoding,
+            tile_and_repeat(
+                mat=element,
+                tile=(1, base ** j),
+                repeat=(1, base ** (num_lnls - j - 1)),
+            )[0],
+        )
+    return encoding
+
+
+def generate_data_encoding(model: models.Unilateral, t_stage: str) -> np.ndarray:
+    """Generate the data matrix for a specific T-stage from patient data.
+
+    The :py:attr:`~lymph.models.Unilateral.patient_data` needs to contain the column
+    ``"_model"``, which is constructed when loading the data into the model. From this,
+    a data matrix is constructed for the given ``t_stage``.
+
+    The returned matrix has the shape :math:`2^{N \\cdot \\mathcal{O}} \\times M`,
+    where :math:`N` is the number of lymph node levels, :math:`\\mathcal{O}` is the
+    number of diagnostic modalities and :math:`M` is the number of patients with the
+    given ``t_stage``.
+    """
+    if not model.patient_data["_model", "#", "t_stage"].isin([t_stage]).any():
+        raise ValueError(f"No patients with T-stage {t_stage} in patient data.")
+
+    has_t_stage = model.patient_data["_model", "#", "t_stage"] == t_stage
+    patients_with_t_stage = model.patient_data[has_t_stage]
+
+    result = np.ones(
+        shape=(model.observation_matrix.shape[1], len(patients_with_t_stage)),
+        dtype=bool,
+    )
+
+    for i, (_, patient_row) in enumerate(patients_with_t_stage["_model"].iterrows()):
+        patient_encoding = np.ones(shape=1, dtype=bool)
+        for modality_name in model.modalities.keys():
+            if modality_name not in patient_row:
+                continue
+            diagnose_encoding = compute_encoding(
+                lnls=model.graph.lnls.keys(),
+                pattern=patient_row[modality_name],
+                base=2,   # observations are always binary!
+            )
+            patient_encoding = np.kron(patient_encoding, diagnose_encoding)
+
+        result[:,i] = patient_encoding
+
+    return result
+
+
+class DataEncodingUserDict(AbstractLookupDict):
+    """``UserDict`` that dynamically generates the data matrices for each T-stage.
+
+    The data matrix is a binary encoding of all complete diagnoses that are plausible
+    for an actual patient with possibly incomplete information. So, every columns may
+    contain multiple ones, each for a complete diagnosis that could have led to the
+    observed data.
+
+    When multiplying a distribution over possible complete observations with the data
+    matrix, the result is essentially an array of likelihoods for each patient.
+
+    See Also:
+        :py:attr:`~lymph.models.Unilateral.data_matrices`
+    """
+    model: models.Unilateral
+
+    def __setitem__(self, __key, __value) -> None:
+        warnings.warn("Setting the data matrices is not supported.")
+
+    def __missing__(self, t_stage: str):
+        """Create the data matrix for a specific T-stage if necessary."""
+        try:
+            self.data[t_stage] = generate_data_encoding(self.model, t_stage)
+        except ValueError as val_err:
+            raise KeyError(f"No data matrix for T-stage {t_stage}.") from val_err
+
+        return self[t_stage]
+
+
+class DiagnoseUserDict(AbstractLookupDict):
+    """``UserDict`` that dynamically generates the diagnose matrices for each T-stage.
+
+    A diagnose matrix for a T-stage is simply the product of the observation matrix and
+    the data matrix for that T-stage. Precomputing and caching this matrix is useful,
+    because it does not depend on the model parameters and can be reused until either
+    the diagnostic modalities (meaning the observation matrix needs to be updated) or
+    the patient data (meaning the data matrix needs to be updated) change.
+
+    See Also:
+        :py:attr:`~lymph.models.Unilateral.diagnose_matrices`
+    """
+    model: models.Unilateral
+
+    def __setitem__(self, __key, __value) -> None:
+        warnings.warn("Setting the diagnose matrices is not supported.")
+
+    def __missing__(self, t_stage: str) -> np.ndarray:
+        """If the matrix for a ``t_stage`` is missing, try to generate it lazily."""
+        self.data[t_stage] = (
+            self.model.observation_matrix @ self.model.data_matrices[t_stage]
+        )
+        return self[t_stage]
---
+++ packages/lymph-model/lymph-model-1.0.0a4.tar.gz/lymph-model-1.0.0a4/tests/binary_bilateral_test.py
@@ -0,0 +1,282 @@
+"""
+Test the bilateral model.
+"""
+import unittest
+
+import fixtures
+import numpy as np
+
+from lymph import models
+from lymph.modalities import Clinical
+
+
+class BilateralInitTest(fixtures.BilateralModelMixin, unittest.TestCase):
+    """Test the delegation of attrs from the unilateral class to the bilateral one."""
+
+    def setUp(self):
+        super().setUp()
+        self.load_patient_data()
+
+    def test_delegation(self):
+        """Test that the unilateral model delegates the attributes."""
+        self.assertEqual(
+            self.model.is_binary, self.model.ipsi.is_binary
+        )
+        self.assertEqual(
+            self.model.is_trinary, self.model.ipsi.is_trinary
+        )
+        self.assertEqual(
+            self.model.max_time, self.model.ipsi.max_time
+        )
+        self.assertEqual(
+            list(self.model.t_stages), list(self.model.ipsi.t_stages)
+        )
+
+    def test_lnl_edge_sync(self):
+        """Check if synced LNL edges update their respective parameters."""
+        rng = np.random.default_rng(42)
+        for ipsi_edge in self.model.ipsi.graph.lnl_edges.values():
+            contra_edge = self.model.contra.graph.lnl_edges[ipsi_edge.name]
+            ipsi_edge.set_params(spread=rng.random())
+            self.assertEqual(
+                ipsi_edge.get_params("spread"),
+                contra_edge.get_params("spread"),
+            )
+
+    def test_modality_sync(self):
+        """Make sure the modalities are synced between the two sides."""
+        rng = np.random.default_rng(42)
+        self.model.ipsi.modalities = {"foo": Clinical(
+            specificity=rng.uniform(),
+            sensitivity=rng.uniform(),
+        )}
+        self.assertEqual(
+            self.model.ipsi.modalities["foo"].sensitivity,
+            self.model.contra.modalities["foo"].sensitivity,
+        )
+        self.assertEqual(
+            self.model.ipsi.modalities["foo"].specificity,
+            self.model.contra.modalities["foo"].specificity,
+        )
+
+    def test_asymmetric_model(self):
+        """Check if different graphs work for the ipsi and contra side."""
+        ipsi_graph = fixtures.get_graph("medium")
+        contra_graph = fixtures.get_graph("small")
+
+        model = models.Bilateral(
+            graph_dict=ipsi_graph,
+            contralateral_kwargs={"graph_dict": contra_graph},
+        )
+
+        self.assertEqual(
+            list(model.ipsi.graph.nodes.keys()),
+            [key[1] for key in ipsi_graph.keys()],
+        )
+        self.assertEqual(
+            list(model.contra.graph.nodes.keys()),
+            [key[1] for key in contra_graph.keys()],
+        )
+        self.assertEqual(
+            len(model.ipsi.get_params()),
+            sum(len(val) for val in ipsi_graph.values()),
+        )
+        self.assertEqual(
+            len(model.contra.get_params()),
+            sum(len(val) for val in contra_graph.values()),
+        )
+
+
+class ModalityDelegationTestCase(fixtures.BilateralModelMixin, unittest.TestCase):
+    """Make sure the modality is delegated from the ipsi side correctly."""
+
+    def setUp(self):
+        super().setUp()
+        self.model.modalities = fixtures.MODALITIES
+
+    def test_modality_access(self):
+        """Test that the modality can be accessed."""
+        self.assertEqual(
+            self.model.modalities["CT"].sensitivity,
+            self.model.ipsi.modalities["CT"].sensitivity,
+        )
+        self.assertEqual(
+            self.model.modalities["FNA"].specificity,
+            self.model.ipsi.modalities["FNA"].specificity,
+        )
+
+    def test_modality_delete(self):
+        """Test that the modality can be deleted."""
+        del self.model.modalities["CT"]
+        self.assertNotIn("CT", self.model.modalities)
+        self.assertNotIn("CT", self.model.ipsi.modalities)
+        self.assertNotIn("CT", self.model.contra.modalities)
+
+    def test_modality_update(self):
+        """Test that the modality can be updated."""
+        self.model.modalities["CT"].sensitivity = 0.8
+        self.assertEqual(
+            self.model.modalities["CT"].sensitivity,
+            self.model.ipsi.modalities["CT"].sensitivity,
+        )
+        self.assertEqual(
+            self.model.modalities["CT"].sensitivity,
+            self.model.contra.modalities["CT"].sensitivity,
+        )
+
+    def test_modality_reset(self):
+        """Test resetting the modalities also works."""
+        self.model.modalities = {"foo": Clinical(0.8, 0.9)}
+        self.assertEqual(
+            self.model.modalities["foo"].sensitivity,
+            self.model.ipsi.modalities["foo"].sensitivity,
+        )
+        self.assertEqual(
+            self.model.modalities["foo"].specificity,
+            self.model.contra.modalities["foo"].specificity,
+        )
+
+    def test_diag_time_dists_delegation(self):
+        """Test that the diagnose time distributions are delegated."""
+        self.assertTrue(np.allclose(
+            list(self.model.diag_time_dists["early"].distribution),
+            list(self.model.ipsi.diag_time_dists["early"].distribution),
+        ))
+        self.assertTrue(np.allclose(
+            list(self.model.diag_time_dists["late"].get_params()),
+            list(self.model.ipsi.diag_time_dists["late"].get_params()),
+        ))
+        self.assertTrue(np.allclose(
+            list(self.model.diag_time_dists["early"].distribution),
+            list(self.model.contra.diag_time_dists["early"].distribution),
+        ))
+        self.assertTrue(np.allclose(
+            list(self.model.diag_time_dists["late"].get_params()),
+            list(self.model.contra.diag_time_dists["late"].get_params()),
+        ))
+
+
+class ParameterAssignmentTestCase(fixtures.BilateralModelMixin, unittest.TestCase):
+    """Test the parameter assignment."""
+
+    def setUp(self):
+        self.model_kwargs = {
+            "is_symmetric": {
+                "tumor_spread": False,
+                "lnl_spread": False,
+                "modalities": True,
+            }
+        }
+        super().setUp()
+
+    def test_get_params_as_args(self):
+        """Test that the parameters can be retrieved."""
+        ipsi_args = self.model.ipsi.get_params()
+        contra_args = self.model.contra.get_params()
+        self.assertEqual(len(ipsi_args), len(contra_args))
+
+    def test_get_params_as_dict(self):
+        """Test that the parameters can be retrieved."""
+        ipsi_dict = self.model.ipsi.get_params(as_dict=True)
+        contra_dict = self.model.contra.get_params(as_dict=True)
+        self.assertEqual(ipsi_dict.keys(), contra_dict.keys())
+
+    def test_assign_params_as_args(self):
+        """Test that the parameters can be assigned."""
+        ipsi_args = self.rng.uniform(size=len(self.model.ipsi.get_params()))
+        contra_args = self.rng.uniform(size=len(self.model.contra.get_params()))
+        none_args = [None] * len(ipsi_args)
+
+        # Assigning only the ipsi side
+        self.model.assign_params(*ipsi_args, *none_args)
+        self.assertTrue(np.allclose(ipsi_args, list(self.model.ipsi.get_params())))
+        self.assertEqual(
+            list(self.model.ipsi.diag_time_dists["late"].get_params())[0],
+            list(self.model.contra.diag_time_dists["late"].get_params())[0],
+        )
+
+        # Assigning only the contra side
+        self.model.assign_params(*none_args, *contra_args)
+        self.assertTrue(np.allclose(contra_args, list(self.model.contra.get_params())))
+        self.assertEqual(
+            list(self.model.ipsi.diag_time_dists["late"].get_params())[0],
+            list(self.model.contra.diag_time_dists["late"].get_params())[0],
+        )
+
+        # Assigning both sides
+        self.model.assign_params(*ipsi_args, *contra_args)
+        self.assertTrue(np.allclose(ipsi_args[:-1], list(self.model.ipsi.get_params())[:-1]))
+        self.assertTrue(np.allclose(contra_args, list(self.model.contra.get_params())))
+        self.assertEqual(
+            list(self.model.ipsi.diag_time_dists["late"].get_params())[0],
+            list(self.model.contra.diag_time_dists["late"].get_params())[0],
+        )
+
+
+class LikelihoodTestCase(fixtures.BilateralModelMixin, unittest.TestCase):
+    """Check that the (log-)likelihood is computed correctly."""
+
+    def setUp(self):
+        super().setUp()
+        self.model.modalities = fixtures.MODALITIES
+        self.load_patient_data()
+
+    def test_compute_likelihood_twice(self):
+        """Test that the likelihood is computed correctly."""
+        first_llh = self.model.likelihood(log=True)
+        second_llh = self.model.likelihood(log=True)
+        self.assertEqual(first_llh, second_llh)
+
+
+class RiskTestCase(fixtures.BilateralModelMixin, unittest.TestCase):
+    """Check that the risk is computed correctly."""
+
+    def setUp(self):
+        super().setUp()
+        self.model.modalities = fixtures.MODALITIES
+
+    def create_random_diagnoses(self):
+        """Create a random diagnosis for each modality and LNL."""
+        diagnoses = {}
+
+        for side in ["ipsi", "contra"]:
+            diagnoses[side] = {}
+            side_model = getattr(self.model, side)
+            lnl_names = side_model.graph.lnls.keys()
+            for modality in side_model.modalities:
+                diagnoses[side][modality] = fixtures.create_random_pattern(lnl_names)
+
+        return diagnoses
+
+    def test_posterior_state_dist(self):
+        """Test that the posterior state distribution is computed correctly."""
+        num_states = len(self.model.ipsi.state_list)
+        random_parameters = self.create_random_params()
+        random_diagnoses = self.create_random_diagnoses()
+
+        posterior = self.model.comp_posterior_joint_state_dist(
+            given_param_kwargs=random_parameters,
+            given_diagnoses=random_diagnoses,
+        )
+        self.assertEqual(posterior.shape, (num_states, num_states))
+        self.assertEqual(posterior.dtype, float)
+        self.assertTrue(np.isclose(posterior.sum(), 1.))
+
+    def test_risk(self):
+        """Test that the risk is computed correctly."""
+        random_parameters = self.create_random_params()
+        random_diagnoses = self.create_random_diagnoses()
+        random_pattern = {
+            "ipsi": fixtures.create_random_pattern(self.model.ipsi.graph.lnls.keys()),
+            "contra": fixtures.create_random_pattern(self.model.contra.graph.lnls.keys()),
+        }
+        random_t_stage = self.rng.choice(["early", "late"])
+
+        risk = self.model.risk(
+            involvement=random_pattern,
+            given_param_kwargs=random_parameters,
+            given_diagnoses=random_diagnoses,
+            t_stage=random_t_stage,
+        )
+        self.assertLessEqual(risk, 1.)
+        self.assertGreaterEqual(risk, 0.)
