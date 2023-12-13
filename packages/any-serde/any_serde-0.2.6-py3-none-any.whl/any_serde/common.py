e8c706b9dacb56834de336f916d1ee1f722175c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 14:04:11 +0000

    Add package any-serde/0.2.6/any_serde-0.2.6-py3-none-any.whl

---
+++ packages/any-serde/any_serde-0.2.6-py3-none-any.whl/any_serde/common.py
@@ -0,0 +1,41 @@
+from typing import Union, TypeVar, Type, Dict, List
+
+
+JSON_PRIMITIVE_TYPES = [
+    str,
+    int,
+    float,
+    bool,
+    None,
+    type(None),
+]
+JSONPrimitive = Union[
+    str,
+    int,
+    float,
+    bool,
+    None,
+]
+JSON = Union[
+    JSONPrimitive,
+    Dict[str, "JSON"],
+    List["JSON"],
+]
+
+
+T_Any = TypeVar("T_Any")
+
+
+def resolve_newtypes(typ: Type[T_Any]) -> Type[T_Any]:
+    # https://github.com/python/mypy/issues/3325
+    while hasattr(typ, "__supertype__"):
+        typ = getattr(typ, "__supertype__")
+    return typ
+
+
+class InvalidDeserializationException(Exception):
+    """Indicates that the given data is not valid for the target type given."""
+
+
+class InvalidSerializationException(Exception):
+    """Indicates that the given variable could not be serialized."""
