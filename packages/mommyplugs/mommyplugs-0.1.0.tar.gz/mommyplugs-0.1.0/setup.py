bc970dfe24df1958eba4a833cb8a98b9fde1f54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 04:02:59 +0000

    Add package mommyplugs/0.1.0/mommyplugs-0.1.0.tar.gz

---
+++ packages/mommyplugs/mommyplugs-0.1.0.tar.gz/mommyplugs-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mommyplugs',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A collection of handy utility plugins',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourgithub/mommyplugs',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='progress bar utility',
+)
