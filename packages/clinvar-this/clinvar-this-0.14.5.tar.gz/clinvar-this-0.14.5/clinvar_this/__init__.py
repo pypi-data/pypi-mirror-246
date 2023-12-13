626bd017adf2470332f8ae251b1155b2282e53d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 17:06:21 +0000

    Add package clinvar-this/0.14.5/clinvar-this-0.14.5.tar.gz

---
+++ packages/clinvar-this/clinvar-this-0.14.5.tar.gz/clinvar-this-0.14.5/clinvar_this/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for ClinVar This!."""
+
+__author__ = """Manuel Holtgrewe"""
+__email__ = "manuel.holtgrewe@bih-charite.de"
+
+from clinvar_this import _version
+
+__version__ = _version.__version__
