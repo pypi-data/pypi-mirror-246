bec7250c01f4afd38e34e418ed90da8e8a2a4275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 03:03:30 +0000

    Add package netsys-client/0.44.0/netsys_client-0.44.0-py3-none-any.whl

---
+++ packages/netsys-client/netsys_client-0.44.0-py3-none-any.whl/netsys_client/gateway.py
@@ -0,0 +1,54 @@
+from . import ConfController, PrintController, Response, NFController
+import json
+
+class Gateway:
+
+    _erros = []
+
+    def __init__(self):
+
+        self._response = Response()
+
+    def change_config(self):
+        
+        self._message = {'token': self._token}
+        self._response.send_message(json.dumps(self._message))
+        self._message = self._response.get_message()
+        self._conf = ConfController(json.loads(self._message))
+        self._conf.execute()
+
+    def search_printers(self):
+
+        self._print = PrintController()
+        self._message = self._print.get_printer_list(self._token)
+        self._response.send_message(json.dumps(self._message))
+
+    def print_cupom_fiscal(self):
+
+        self._message = {'token': self._token}
+        self._response.send_message(json.dumps(self._message))
+        self._message = self._response.get_message()
+        self._print = PrintController()
+        self._print.print_data(json.loads(self._message))
+
+    def emitir_nota_fiscal(self):
+
+        input('teste1')
+
+        self._message = {'token': self._token}
+        input('teste2')
+        self._response.send_message(json.dumps(self._message))
+        input('teste3')
+        self._message = self._response.get_message()
+        input('teste4')
+        self._nf = NFController(json.loads(self._message))
+        input('teste5')
+        self._nf.execute()
+    
+    def set_erro(self, erro):
+
+        self._erros.append(erro)
+
+    def set_token(self, token):
+
+        self._token = token
