3797fdb291f5d71cdfbe8b4f2cda6bd1fda4092b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 03:03:24 +0000

    Add package pytest-timeassert-ethan/1.6/pytest-timeassert-ethan-1.6.tar.gz

---
+++ packages/pytest-timeassert-ethan/pytest-timeassert-ethan-1.6.tar.gz/pytest-timeassert-ethan-1.6/tests/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# Author : Ethan
+# Time : 2023/12/11 15:37
---
+++ packages/pytest-timeassert-ethan/pytest-timeassert-ethan-1.6.tar.gz/pytest-timeassert-ethan-1.6/tests/conftest.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Author : Ethan
+# Time : 2023/12/11 16:12
+
+'''pytester is needed for testing plgugins.'''
+
+
+pytest_plugins = ['pytester']
