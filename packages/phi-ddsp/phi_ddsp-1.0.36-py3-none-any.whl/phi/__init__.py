3a1c8647141d85df458f02b2edf62b033a1f1cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 16:03:57 +0000

    Add package phi-ddsp/1.0.36/phi_ddsp-1.0.36-py3-none-any.whl

---
+++ packages/phi-ddsp/phi_ddsp-1.0.36-py3-none-any.whl/phi/__init__.py
@@ -0,0 +1,22 @@
+"""
+phi
+
+A conditional timbral model based off of DDSP
+
+Author: Max Ardito 
+"""
+
+__version__ = "1.0.36"
+
+# phi/__init__.py
+
+# Import the functions you want to expose
+from .preprocess import preprocess 
+from .train import train 
+from .export import export 
+
+# Define __all__ to specify what's included with "from phi import *"
+__all__ = ['preprocess', 'train', 'export']
+
+
+
