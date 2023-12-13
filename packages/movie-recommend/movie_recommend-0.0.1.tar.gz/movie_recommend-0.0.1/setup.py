2bf777844ea7df5d66d9e4267d325a2cb4e654ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-13 16:03:39 +0000

    Add package movie-recommend/0.0.1/movie_recommend-0.0.1.tar.gz

---
+++ packages/movie-recommend/movie_recommend-0.0.1.tar.gz/movie_recommend-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    author='Group_7',
+    description='Movie recommendation',
+    name='movie_recommend',
+    version='0.0.1',
+    packages=find_packages(),
+    
+)
