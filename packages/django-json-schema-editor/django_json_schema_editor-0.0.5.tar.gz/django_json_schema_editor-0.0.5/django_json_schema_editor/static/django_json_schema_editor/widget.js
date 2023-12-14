e467f764b4ae4c629874f501fc4079f8e3c6a2bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 05:04:32 +0000

    Add package django-json-schema-editor/0.0.5/django_json_schema_editor-0.0.5.tar.gz

---
+++ packages/django-json-schema-editor/django_json_schema_editor-0.0.5.tar.gz/django_json_schema_editor-0.0.5/django_json_schema_editor/static/django_json_schema_editor/widget.js
@@ -0,0 +1,37 @@
+/* global django, JSONEditor */
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .querySelectorAll(
+      ".inline-related:not(.empty-form) .django_json_schema_editor",
+    )
+    .forEach((el) => {
+      initEditor(el)
+    })
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+  django.jQuery(document).on("formset:added", (event) => {
+    event.target
+      .querySelectorAll(".django_json_schema_editor")
+      .forEach((el) => {
+        initEditor(el)
+      })
+  })
+})
+
+const initEditor = (el) => {
+  const input = el.querySelector("textarea")
+  const config = JSON.parse(el.dataset.editorConfig)
+  const editor = new JSONEditor(el, config)
+
+  editor.on("ready", () => {
+    const data = input.value ? JSON.parse(input.value) : null
+    if (data) {
+      editor.setValue(data)
+    }
+  })
+
+  editor.on("change", () => {
+    input.value = JSON.stringify(editor.getValue())
+  })
+}
