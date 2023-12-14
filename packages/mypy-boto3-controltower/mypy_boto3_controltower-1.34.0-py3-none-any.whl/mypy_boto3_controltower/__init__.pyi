820ab11dda311a59d64efe1714d78f8d992c9624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 07:04:02 +0000

    Add package mypy-boto3-controltower/1.34.0/mypy_boto3_controltower-1.34.0-py3-none-any.whl

---
+++ packages/mypy-boto3-controltower/mypy_boto3_controltower-1.34.0-py3-none-any.whl/mypy_boto3_controltower/__init__.pyi
@@ -0,0 +1,33 @@
+"""
+Main interface for controltower service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_controltower import (
+        Client,
+        ControlTowerClient,
+        ListEnabledControlsPaginator,
+        ListLandingZonesPaginator,
+    )
+
+    session = Session()
+    client: ControlTowerClient = session.client("controltower")
+
+    list_enabled_controls_paginator: ListEnabledControlsPaginator = client.get_paginator("list_enabled_controls")
+    list_landing_zones_paginator: ListLandingZonesPaginator = client.get_paginator("list_landing_zones")
+    ```
+"""
+
+from .client import ControlTowerClient
+from .paginator import ListEnabledControlsPaginator, ListLandingZonesPaginator
+
+Client = ControlTowerClient
+
+__all__ = (
+    "Client",
+    "ControlTowerClient",
+    "ListEnabledControlsPaginator",
+    "ListLandingZonesPaginator",
+)
