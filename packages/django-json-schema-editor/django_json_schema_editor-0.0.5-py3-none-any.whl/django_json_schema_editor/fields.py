7692919484dba4ac431794e2572843535013d683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 05:04:32 +0000

    Add package django-json-schema-editor/0.0.5/django_json_schema_editor-0.0.5-py3-none-any.whl

---
+++ packages/django-json-schema-editor/django_json_schema_editor-0.0.5-py3-none-any.whl/django_json_schema_editor/fields.py
@@ -0,0 +1,19 @@
+from functools import partial
+
+from django.db import models
+
+from django_json_schema_editor.forms import JSONEditorField
+
+
+class JSONField(models.JSONField):
+    def __init__(self, *args, **kwargs):
+        self._schema = kwargs.pop("schema", None)
+        super().__init__(*args, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        return name, "django.db.models.JSONField", args, kwargs
+
+    def formfield(self, **kwargs):
+        kwargs.setdefault("form_class", partial(JSONEditorField, schema=self._schema))
+        return super().formfield(**kwargs)
