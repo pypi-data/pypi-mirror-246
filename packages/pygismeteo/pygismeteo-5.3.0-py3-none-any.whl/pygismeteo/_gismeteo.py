4f57581ad3b5bf1d5584274f4fc922b59c18e3a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 04:04:03 +0000

    Add package pygismeteo/5.3.0/pygismeteo-5.3.0-py3-none-any.whl

---
+++ packages/pygismeteo/pygismeteo-5.3.0-py3-none-any.whl/pygismeteo/_gismeteo.py
@@ -0,0 +1,104 @@
+from __future__ import annotations
+
+from typing import Optional
+
+from pygismeteo_base.types import Lang
+from pygismeteo_base.validators import Settings
+from requests import Session
+
+from ._current import Current
+from ._http import RequestsClient
+from ._search import Search
+from ._step_n import Step3, Step6, Step24
+
+
+class Gismeteo:
+    """Обёртка для Gismeteo API."""
+
+    __slots__ = (
+        "_settings",
+        "_session",
+        "_current",
+        "_step3",
+        "_step6",
+        "_step24",
+        "_search",
+    )
+
+    def __init__(
+        self,
+        *,
+        lang: Optional[Lang] = None,
+        token: str,
+        session: Optional[Session] = None,
+    ) -> None:
+        """Обёртка для Gismeteo API.
+
+        Args:
+            lang:
+                Язык. По умолчанию "ru".
+            token:
+                X-Gismeteo-Token.
+                Запросить можно по электронной почте b2b@gismeteo.ru.
+            session:
+                Экземпляр requests.Session.
+                По умолчанию для каждого запроса создаётся новый экземпляр.
+        """
+        self._settings = Settings(lang=lang, token=token)
+        self._session = RequestsClient(session, self._settings)
+        self._current = Current(self._session)
+        self._step3 = Step3(self._session)
+        self._step6 = Step6(self._session)
+        self._step24 = Step24(self._session)
+        self._search = Search(self._session)
+
+    @property
+    def session(self) -> Optional[Session]:
+        return self._session.session
+
+    @session.setter
+    def session(self, session: Optional[Session]) -> None:
+        self._session.session = session
+
+    @property
+    def lang(self) -> Optional[Lang]:
+        """Язык."""
+        return self._settings.lang
+
+    @lang.setter
+    def lang(self, lang: Optional[Lang]) -> None:
+        self._settings.lang = lang
+
+    @property
+    def token(self) -> str:
+        """X-Gismeteo-Token."""
+        return self._settings.token
+
+    @token.setter
+    def token(self, token: str) -> None:
+        self._settings.token = token
+
+    @property
+    def current(self) -> Current:
+        """Текущая погода."""
+        return self._current
+
+    @property
+    def step3(self) -> Step3:
+        """Погода с шагом 3 часа."""
+        return self._step3
+
+    @property
+    def step6(self) -> Step6:
+        """Погода с шагом 6 часов."""
+        return self._step6
+
+    @property
+    def step24(self) -> Step24:
+        """Погода с шагом 24 часа."""
+        return self._step24
+
+    @property
+    def search(self) -> Search:
+        """Поиск."""
+        return self._search
---
+++ packages/pygismeteo/pygismeteo-5.3.0-py3-none-any.whl/pygismeteo/types.py
@@ -0,0 +1,28 @@
+"""Может пригодиться для типизации кода, использующего библиотеку."""
+
+from __future__ import annotations
+
+from pygismeteo_base.types import (
+    Lang,
+    SearchLimit,
+    Step3Days,
+    Step6Days,
+    Step24Days,
+)
+
+from ._current import Current
+from ._search import Search
+from ._step_n import Step3, Step6, Step24
+
+__all__ = (
+    "Current",
+    "Lang",
+    "Search",
+    "SearchLimit",
+    "Step24",
+    "Step24Days",
+    "Step3",
+    "Step3Days",
+    "Step6",
+    "Step6Days",
+)
