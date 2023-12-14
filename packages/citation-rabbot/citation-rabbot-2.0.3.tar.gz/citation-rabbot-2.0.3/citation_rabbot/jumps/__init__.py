a6098cb2ee03085a90a25eefb1ad5a491f727261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 15:04:44 +0000

    Add package citation-rabbot/2.0.3/citation-rabbot-2.0.3.tar.gz

---
+++ packages/citation-rabbot/citation-rabbot-2.0.3.tar.gz/citation-rabbot-2.0.3/citation_rabbot/jumps/__init__.py
@@ -0,0 +1,2 @@
+from .papers import paper_authors_jump, author_papers_jump, citations_jump, references_jump
+from .search import search_by_title_jump
---
+++ packages/citation-rabbot/citation-rabbot-2.0.3.tar.gz/citation-rabbot-2.0.3/citation_rabbot/jumps/search.py
@@ -0,0 +1,17 @@
+from argparse import ArgumentParser
+from typing import Tuple, Dict, List
+from .papers import papers_results2message
+from .args import add_arguments_papers, parse_args_papers
+
+
+def search_by_title_args2querys(args: object) -> List[Tuple[str, Dict]]:
+    where, orderby, limits, values = parse_args_papers(args)
+    return [(
+        f"MATCH (c:Publication)-[:CITE]->(p:Publication)-[:PUBLISH]->(j:Journal) WHERE {where} "
+        f"RETURN p, j, COUNT(c) AS citation ORDER BY {orderby} LIMIT {limits}",
+        values
+    )]
+
+
+search_by_title_jump = ("search_by_title", add_arguments_papers(ArgumentParser()), search_by_title_args2querys,
+                        papers_results2message, "Search papers by keywords in title")
