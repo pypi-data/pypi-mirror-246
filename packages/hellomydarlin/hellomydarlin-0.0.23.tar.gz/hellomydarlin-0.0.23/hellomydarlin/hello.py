e18ba88e672e945b36d8e5f243007f42df26738d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 15:04:50 +0000

    Add package hellomydarlin/0.0.23/hellomydarlin-0.0.23.tar.gz

---
+++ packages/hellomydarlin/hellomydarlin-0.0.23.tar.gz/hellomydarlin-0.0.23/hellomydarlin/hello.py
@@ -0,0 +1,7 @@
+import random
+
+def sayhello():
+    print("Hello, World!")
+
+def pickNum(num):
+    print(random.randint(1, num))
---
+++ packages/hellomydarlin/hellomydarlin-0.0.23.tar.gz/hellomydarlin-0.0.23/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+
+VERSION = '0.0.23'
+DESCRIPTION = 'Says Hello'
+# Setting up
+setup(
+    name="hellomydarlin",
+    version=VERSION,
+    author="Stephen Burwell",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
