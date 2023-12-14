82fd99314f0b59e3527204398642d5920c67803c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 15:05:45 +0000

    Add package vessels/0.0.6/vessels-0.0.6.tar.gz

---
+++ packages/vessels/vessels-0.0.6.tar.gz/vessels-0.0.6/places/structures/vessels/proxies/HA/install/Fedora/__init__.py
@@ -0,0 +1,11 @@
+
+'''
+	import vessels.proxies.HA.install.Fedora as install_HA_proxy
+	install_HA_proxy.charismatically ()
+'''
+
+def charismatically ():
+	lyrics = "dnf install haproxy"
+	
+	import os
+	os.system (lyrics)
