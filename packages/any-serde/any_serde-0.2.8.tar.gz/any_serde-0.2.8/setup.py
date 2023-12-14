55f7507baac9fb882b6cce8ffb695ea5ed7df940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 15:04:50 +0000

    Add package any-serde/0.2.8/any_serde-0.2.8.tar.gz

---
+++ packages/any-serde/any_serde-0.2.8.tar.gz/any_serde-0.2.8/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="any_serde",
+    version="0.2.8",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    package_data={
+        "": ["*py.typed"],
+    },
+)
