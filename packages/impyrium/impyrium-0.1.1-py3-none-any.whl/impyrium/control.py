91843abcc7f9b42c07b000ddf82adf75ec2e4685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 07:04:01 +0000

    Add package impyrium/0.1.1/impyrium-0.1.1-py3-none-any.whl

---
+++ packages/impyrium/impyrium-0.1.1-py3-none-any.whl/impyrium/control.py
@@ -0,0 +1,371 @@
+from .aitpi.src import aitpi
+from .aitpi.src.aitpi import router
+
+from .thread_safe_queue import ThreadSafeQueue
+
+from . import pyqt_file
+
+from . import device_thread
+from enum import Enum
+
+class ControlEvents(Enum):
+    VALUE_SET       = "VALUE_SET"
+    BUTTON_PRESS    = "BUTTON_PRESS"
+    BUTTON_RELEASE  = "BUTTON_RELEASE"
+
+class RangeValue():
+    def __init__(self, minimum, maximum, increment, default=None):
+        if default is None:
+            default = minimum
+        self.min = minimum
+        self.max = maximum
+        self.inc = increment
+        res = (self.max - self.min) / self.inc
+
+        self.max = self.generateValidValue(self.max)
+        self.value = self.generateValidValue(default)
+
+
+    def generateValidValue(self, value):
+        if value < self.min:
+            value = self.min
+        if value > self.max:
+            value = self.max
+
+        res = (value - self.min) / self.inc
+        if (float(int(res)) != res):
+            value = self.min + int(res) * self.inc 
+        return value
+
+    def sub(self):
+        self.value = self.generateValidValue(self.value - self.inc)
+
+    def add(self):
+        self.value = self.generateValidValue(self.value + self.inc)
+
+    def left(self):
+        return self.sub()
+
+    def right(self):
+        return self.add()
+
+    def setValue(self, value):
+        self.value = self.generateValidValue(value)
+
+    def getValue(self):
+        return self.value
+
+controls_ = {}
+newDeviceFun_ = None
+signal_ = None
+
+class Control():
+    def __init__(self, category, name, sendFun, deviceAutoReserve=False, enabled=True):
+        self.name = name
+        self.sendFun = sendFun
+        self.category = category
+        self.deviceAutoReserve = deviceAutoReserve
+        self.hasReleased = False
+        self.data = {}
+        self.inputType = "button"
+        self.enabled = enabled
+
+    def getValue(self):
+        pass
+
+    def disable(self):
+        self.enabled = False
+
+    def enable(self):
+        self.enabled = True
+
+    def consume(self, msg):
+        if self.enabled:
+            self.handleAitpi(msg)
+
+    def handleGuiEvent(self, event, devList):
+        if self.enabled:
+            self.sendFun(self, event, devList)
+
+    def handleAitpi(self, msg):
+        if (msg.name == self.name):
+            e = ControlEvents.BUTTON_PRESS
+            if msg.event == aitpi.BUTTON_RELEASE:
+                e = ControlEvents.BUTTON_RELEASE
+            self.sendFun(self, e, DeviceType.getControlDevList(self))
+
+class ControlButton(Control):
+    pass #Base Control() is a button
+
+class ControlDial(Control):
+    pass #TODO:
+
+class ControlFile(Control):
+    fileQueue : ThreadSafeQueue = None
+    allFiles = "All Files (*)"
+
+    def __init__(self, category, name, sendFun, deviceAutoReserve=False, enabled=True, directory=""):
+        super().__init__(category, name, sendFun, deviceAutoReserve, enabled)
+        self.file = ""
+        self.dir = directory
+
+    def getValue(self):
+        return self.file
+
+    def runCallback(self):
+        self.file = pyqt_file.getFile(ControlFile.allFiles, self.dir)
+        if self.file is not None and self.file != "":
+            self.sendFun(self, ControlEvents.VALUE_SET, DeviceType.getControlDevList(self))
+
+    def requestFile(self):
+        print("Requesting file?")
+        ControlFile.fileQueue.put({
+            "fun": self.runCallback,
+            "file_types": ControlFile.allFiles,
+        })
+
+    def handleGuiEvent(self, event, devList):
+        if event == ControlEvents.BUTTON_PRESS:
+            return
+        self.requestFile()
+
+    def handleAitpi(self, msg):
+        if msg.event == aitpi.BUTTON_PRESS:
+            return
+        if (msg.name == self.name):
+            self.requestFile()
+
+class ControlString(Control):
+    pass #TODO:
+
+class ControlDate(Control):
+    pass #TODO:
+
+class ControlEnum(Control):
+    pass #TODO:
+
+class ControlSlider(Control):
+    def __init__(self, category, name, sendFun, sliderRange : RangeValue, deviceAutoReserve=False) -> None:
+        super().__init__(category, name, sendFun, deviceAutoReserve)
+        self.range = sliderRange
+        self.inputType = "encoder"
+
+    def convertSliderValue(self, value):
+        value = (self.range.inc * value) + self.range.min
+        return value
+
+    def generateSliderValues(self):
+        distance = abs(self.range.max - self.range.min)
+        counts = abs(distance / self.range.inc)
+        #       min    max       increment
+        return (0,     int(counts),   1)
+
+    def setValueFromSlider(self, value):
+        self.range.setValue(self.convertSliderValue(value))
+
+    def setValue(self, value):
+        self.range.setValue(value)
+
+    def getValue(self):
+        return self.range.getValue()
+
+    def handleAitpi(self, msg):
+        if (msg.name == self.name):
+            e = ControlEvents.VALUE_SET
+            if msg.event == aitpi.ENCODER_LEFT:
+                self.range.left()
+            elif msg.event == aitpi.ENCODER_RIGHT:
+                self.range.right()
+            else:
+                print("Error----")
+                raise Exception(f"Invalid aitpit command {msg.event}")
+
+            self.sendFun(self, e, DeviceType.getControlDevList(self))
+
+# Simple helper class that defines a devices unique id, and stores reservation state
+class Device():
+    def __init__(self, uid, deviceType):
+        self.uid = uid
+        if type(deviceType) == str:
+            deviceType = DeviceType._deviceTypes[deviceType]
+        if type(deviceType) != DeviceType:
+            raise Exception("deviceType needs to be a DeviceType(), or the name string")
+        self.deviceType = deviceType
+        self.reserveTask = None
+        self.reserveTime = 0.0
+
+    def __str__(self):
+        return f"<{self.uid}>"
+
+    def __eq__(self, other):
+        if type(other) != Device:
+            return False
+        return self.uid == other.uid
+
+    def __hash__(self):
+        return self.uid.__hash__()
+
+    def isReserved(self):
+        return self.deviceType.isDevReserved(self)
+
+def registerNewDeviceFun(fun):
+    global newDeviceFun_
+    newDeviceFun_ = fun
+    global signal_
+    pass
+
+def registerFileQueue(fun):
+    ControlFile.fileQueue = fun
+
+def registerDeviceType(devType):
+    DeviceType._deviceTypes[devType.name] = devType
+    devType.scheduleDetection()
+
+def removeReserved(device):
+    for key in DeviceType._deviceTypes:
+        t = DeviceType._deviceTypes[key]
+        if device.uid in t.reservedDevices:
+            t.removeReserved(device)
+
+# We allow the users to define devices types so that different types of devices can work
+class DeviceType():
+    _deviceTypes = {}
+
+    def __init__(self, name, controlCategories = [], detector=None, pollRate=1, reserveDeviceFun=None, releaseDeviceFun=None, autoReservationTimeout=None, reserveCheck=None):
+        self.name = name
+        self.detector = detector
+        self.pollRate = pollRate
+        self.controlCategories = set(controlCategories)
+        self.reserveCheck = reserveCheck
+        self.reserveDeviceFun = reserveDeviceFun
+        self.releaseDeviceFun = releaseDeviceFun
+        self.autoReservationTimeout = autoReservationTimeout
+        self.ownsDevice = False
+        self.reservedDevices = set()
+        self.devices = set()
+
+    def hasCategory(self, category):
+        return category in self.controlCategories
+
+    def canReserve(self):
+        return self.reserveDeviceFun is not None
+
+    def getControlCategories(self):
+        return list(self.controlCategories)
+
+    def reserveAllDevices(self, autoReserve=False):
+        if not self.canReserve():
+            return
+        for dev in list(self.devices):
+            self.reserveDevice(dev, autoReserve=autoReserve)
+
+    def isDevReserved(self, device):
+        return device in self.reservedDevices
+
+    def getUnreservedDevices(self):
+        if self.canReserve():
+            return self.devices.difference(self.reservedDevices)
+        return set()
+
+    def getReservedDevices(self):
+        if self.canReserve():
+            return self.reservedDevices
+        return self.devices
+
+    def sendUpdateSignal(self):
+        global newDeviceFun_
+        if newDeviceFun_ is not None:
+            # TODO: This is really jank
+            newDeviceFun_.objectNameChanged.emit("")
+
+    def detect(self):
+        global signal_
+        devices = self.detector()
+        newDevices = set()
+        for device in devices:
+            if type(device) != Device:
+                raise Exception("All detected devices need to be Device()")
+            newDevices.add(device)
+        if (self.devices != newDevices):
+            self.devices = newDevices
+            self.checkReservations()
+            self.sendUpdateSignal()
+        self.scheduleDetection()
+
+    def scheduleDetection(self):
+        if self.detector is not None:
+            device_thread.scheduleItem(self.pollRate, self.detect)
+
+    def scheduleAutoTimeout(self, device):
+        if device.reserveTask is not None:
+            device_thread.cancel(device.reserveTask)
+            device.reserveTask = None
+        if self.autoReservationTimeout is not None and self.releaseDeviceFun is not None:
+            device.reserveTask = device_thread.scheduleItem(self.autoReservationTimeout, lambda: self.releaseDevice(device))
+
+    def checkReservations(self):
+        self.reservedDevices.difference_update(self.reservedDevices - self.devices)
+
+    def releaseDevice(self, device):
+        if (self.releaseDeviceFun is not None):
+            device.reserveTask = None
+            if (self.reserveCheck is not None and not self.reserveCheck(device)):
+                # We know the device has already been released
+                return
+            self.releaseDeviceFun(device)
+            if self.canReserve() and device in self.reservedDevices:
+                self.reservedDevices.remove(device)
+                self.sendUpdateSignal()
+
+    def reserveDevice(self, device, autoReserve=False):
+        if (self.reserveDeviceFun is not None):
+            self.reserveDeviceFun(device)
+            self.reservedDevices.add(device)
+            self.sendUpdateSignal()
+            if autoReserve:
+                self.scheduleAutoTimeout(device)
+
+    @staticmethod
+    def getAllDeviceTypes(category):
+        ret = []
+        for key in DeviceType._deviceTypes.keys():
+            t = DeviceType._deviceTypes[key]
+            if t.hasCategory(category):
+                ret.append(t)
+        return ret
+
+    @staticmethod
+    def getControlDevList(ctrl, shouldAutoReserve=True):
+        devices = set()
+        for t in DeviceType.getAllDeviceTypes(ctrl.category):
+            if ctrl.deviceAutoReserve and shouldAutoReserve:
+                t.reserveAllDevices(autoReserve=True)
+            devices.update(t.getReservedDevices())
+        return devices
+
+def getControls():
+    global controls_
+    return controls_
+
+def init():
+    # We use a None registry to add controls
+    aitpi.addRegistry(None)
+
+def addToAitpi(control):
+    aitpi.addCommandToRegistry(None, control.name, control.category, control.inputType)
+    router.addConsumer([control.category], control)
+
+def registerControl(control):
+    global controls_
+    if (control.category not in controls_):
+        controls_[control.category] = []
+    controls_[control.category].append(control)
+    addToAitpi(control)
+
+def getControlsForDevice(device : Device):
+    global controls_
+    ret = {}
+    for cat in device.deviceType.getControlCategories():
+        ret[cat] = list(controls_[cat])
+
+    return ret
---
+++ packages/impyrium/impyrium-0.1.1-py3-none-any.whl/impyrium/main_menu.py
@@ -0,0 +1,351 @@
+import sys
+import time
+
+from .aitpi.src.aitpi import router
+from .aitpi.src import aitpi
+
+from .thread_safe_queue import ThreadSafeQueue
+from .aitpi_widget import Aitpi
+from . import device_thread
+from . import control
+from .worker_thread import WorkerThread
+import os
+
+import typing
+
+from PyQt6 import QtGui
+from PyQt6.QtCore import Qt, pyqtBoundSignal, pyqtSignal, pyqtSlot, QTimer
+from PyQt6.QtWidgets import (
+    QScrollArea,
+    QFileDialog,
+    QApplication,
+    QCheckBox,
+    QComboBox,
+    QDateEdit,
+    QDateTimeEdit,
+    QDial,
+    QDoubleSpinBox,
+    QFontComboBox,
+    QLabel,
+    QLCDNumber,
+    QLineEdit,
+    QMainWindow,
+    QProgressBar,
+    QPushButton,
+    QRadioButton,
+    QSlider,
+    QSpinBox,
+    QTimeEdit,
+    QVBoxLayout,
+    QWidget,
+    QTabWidget,
+    QHBoxLayout,
+)
+
+def getScriptPath():
+    return os.path.dirname(os.path.realpath(__file__)).replace(os.path.basename(__file__), "")
+
+def printAllOfType(item, t):
+    for d in dir(item):
+        if (type(item.__getattribute__(d)) == t):
+            print(d)
+
+class ControlsScrollView(QWidget):
+    def __init__(self, category, autoReserve):
+        super(ControlsScrollView, self).__init__()
+        layout = QVBoxLayout(self)
+        self.autoReserve = autoReserve
+        layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+        controls = control.getControls()
+        self.worker = WorkerThread()
+        self.worker.start()
+        self.count = 0
+        if category in controls:
+            for c in controls[category]:
+                mod = self.getObjectMod(c)
+                if mod is None:
+                    print(f"Mod is None for {c.name}, cannot do anything with this, please fix")
+                    continue
+                for item in mod:
+                    layout.addWidget(item)
+                    if not c.enabled:
+                        item.hide()
+                    else:
+                        self.count += 1
+        else:
+            print("Could not find any controls for ", category)
+
+    def generateButtonCallbackFun(self, ctrl, event):
+        def fun():
+            if ctrl.enabled:
+                ctrl.handleGuiEvent(event, control.DeviceType.getControlDevList(ctrl, self.autoReserve))
+        return fun
+
+    def generateSliderCallbackFun(self, ctrl):
+        def fun(value):
+            if ctrl.enabled:
+                def item():
+                    ctrl.setValueFromSlider(value)
+                    ctrl.handleGuiEvent(control.ControlEvents.VALUE_SET, control.DeviceType.getControlDevList(ctrl, self.autoReserve))
+
+                if 'event' in ctrl.data and ctrl.data['event'] is not None:
+                    self.worker.removeItem(ctrl.data['event'])
+                ctrl.data['event'] = self.worker.scheduleItem(0.5, item)
+        return fun
+
+    def getObjectMod(self, ctrl):
+        if type(ctrl) == control.ControlButton or type(ctrl) == control.ControlFile:
+            button = QPushButton()
+            button.setMinimumHeight(25)
+            button.setText(ctrl.name)
+            button.pressed.connect(self.generateButtonCallbackFun(ctrl, control.ControlEvents.BUTTON_PRESS))
+            button.released.connect(self.generateButtonCallbackFun(ctrl, control.ControlEvents.BUTTON_RELEASE))
+            return [button]
+        if type(ctrl) == control.ControlSlider:
+            ret = QSlider(Qt.Orientation.Horizontal)
+            res = ctrl.generateSliderValues()
+            ret.setMinimum(res[0])
+            ret.setMaximum(res[1])
+            ret.setMinimumHeight(25)
+            label = QLabel(ctrl.name)
+            ret.valueChanged.connect(self.generateSliderCallbackFun(ctrl))
+            label.setBuddy(ret)
+            return [label, ret]
+        return None
+
+class ControlsTypeSection(QWidget):
+    def __init__(self, category, autoReserve, parent: QWidget = None):
+        super().__init__(parent)
+        layout = QVBoxLayout(self)
+        layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+        button = QPushButton(self)
+        button.setText(category + " ↓")
+        button.clicked.connect(self.buttonPressed)
+        layout.addWidget(button)
+        self.controlsView = ControlsScrollView(category, autoReserve)
+        if self.controlsView.count > 0:
+            self.showing = True
+            self.controlsView.show()
+        else:
+            self.showing = False
+            self.controlsView.hide()
+
+        layout.addWidget(self.controlsView)
+
+    def buttonPressed(self):
+        self.showing = not self.showing
+        if (self.showing):
+            self.controlsView.show()
+        else:
+            self.controlsView.hide()
+
+class ItemScrollView(QScrollArea):
+    def __init__(self, items, parent: QWidget = None) -> None:
+        super(ItemScrollView, self).__init__(parent)
+        widget = QWidget()
+        self.mainLayout = QVBoxLayout(widget)
+        self.mainLayout.setAlignment(Qt.AlignmentFlag.AlignTop)
+        for item in items:
+            self.mainLayout.addWidget(item)
+        self.setWidget(widget)
+        self.setWidgetResizable(True)
+
+    def addItem(self, item):
+        self.mainLayout.addWidget(item)
+
+    def removeItem(self, item):
+        self.mainLayout.removeWidget(item)
+
+class DeviceList(QScrollArea):
+    def __init__(self, parent: QWidget = None, selectDeviceFun = None) -> None:
+        super(DeviceList, self).__init__(parent)
+        self.widg = QWidget()
+        self.box = QVBoxLayout(self.widg)
+        self.box.setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.setWidget(self.widg)
+        self.setWidgetResizable(True)
+        self.objectNameChanged.connect(self.newDevices)
+        self.widgetList = []
+        self.selectedDevice = None
+        self.selectedDeviceWidget = None
+        self.selectDeviceFun = selectDeviceFun
+
+    def selectDevice(self, device, widget):
+        if self.selectDeviceFun is not None:
+            print("Selecting device")
+            if self.selectedDevice != device and device is not None:
+                widget.setStyleSheet("background-color: red")
+                widget.update()
+            else:
+                device = None
+                widget = None
+            if self.selectedDeviceWidget is not None:
+                self.selectedDeviceWidget.setStyleSheet("")
+                self.selectedDeviceWidget.update()
+            self.selectDeviceFun(device)
+            self.selectedDevice = device
+            self.selectedDeviceWidget = widget
+
+    def addWidgetToLayout(self, widget):
+        self.box.addWidget(widget)
+        self.widgetList.append(widget)
+
+    def clearWidgets(self):
+        for w in self.widgetList:
+            w.setStyleSheet("")
+            for child in w.children():
+                w.setStyleSheet("")
+                child.deleteLater()
+            self.box.removeWidget(w)
+        self.widgetList.clear()
+
+    def removeWidget(self, w):
+        self.box.removeWidget(w)
+        print(self.widgetList, w)
+        self.widgetList.remove(w)
+
+    def generateReservationHandleFun(self, device, t):
+        def fun():
+            t.reserveDevice(device)
+        return fun
+
+    def generateReleaseHandleFun(self, device, t, w):
+        def fun():
+            if device == self.selectedDevice:
+                self.selectDevice(None, None)
+            t.releaseDevice(device)
+        return fun
+
+    def generateSelectDeviceFun(self, device, widget):
+        def fun():
+            if self.selectDeviceFun is not None:
+                self.selectDevice(device, widget)
+        return fun
+
+    def newDevices(self, devTypes):
+        # We simply override the argument here
+        devTypes = control.DeviceType._deviceTypes
+        self.clearWidgets()
+        for t in devTypes.keys():
+            unreserved = devTypes[t].getUnreservedDevices()
+            if len(unreserved) > 0:
+                detectedLabel = QLabel(self)
+                detectedLabel.setText(f"{t} detected:")
+                self.addWidgetToLayout(detectedLabel)
+            for dev in unreserved:
+                button = QPushButton(self)
+                button.clicked.connect(self.generateReservationHandleFun(dev, devTypes[t]))
+                button.setText(str(dev.uid))
+                self.addWidgetToLayout(button)
+            reservedLabel = QLabel(self)
+            reserved = devTypes[t].getReservedDevices()
+            if len(reserved) > 0:
+                reservedLabel.setText(f"{t} reserved:")
+                self.addWidgetToLayout(reservedLabel)
+            for dev in reserved:
+                miniWidget = QWidget(self)
+                button = QPushButton(miniWidget)
+                miniLayout = QHBoxLayout(miniWidget)
+                miniWidget.setLayout(miniLayout)
+
+                button.clicked.connect(self.generateReleaseHandleFun(dev, devTypes[t], miniWidget))
+                button.setText(str(dev.uid))
+                miniLayout.addWidget(button)
+
+                if self.selectDeviceFun is not None:
+                    deviceButton = QPushButton(miniWidget)
+                    deviceButton.clicked.connect(self.generateSelectDeviceFun(dev, miniWidget))
+                    deviceButton.setText("Select")
+                    miniLayout.addWidget(deviceButton)
+                self.addWidgetToLayout(miniWidget)
+        self.update()
+
+class Selectable(QWidget):
+    def __init__(self, title, items, onSelectFun, parent: QWidget = None) -> None:
+        super().__init__(parent)
+        layout = QVBoxLayout(self)
+        label = QLabel(title)
+        combo = QComboBox()
+        combo.addItems(items)
+        label.setBuddy(combo)
+        combo.currentIndexChanged.connect(onSelectFun)
+        layout.addWidget(label)
+        layout.addWidget(combo)
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    ThisisANewThing: typing.ClassVar[pyqtBoundSignal]
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Impyrium")
+        self.setMinimumSize(800, 500)
+        self.fileCallback = None
+
+        # None registry is the control box
+        commands = aitpi.getCommandsByRegistry(None)
+        categories = set()
+        for c in commands:
+            categories.add(c['id'])
+
+        view2 = ItemScrollView([ControlsTypeSection(cat, True) for cat in categories])
+        self.currentControlList = ItemScrollView([], self)
+        mainWidget = QWidget()
+        mainLayout = QHBoxLayout()
+        tabwidget = QTabWidget()
+
+        self.setWindowIcon(QtGui.QIcon(f"{getScriptPath()}/../../graphics/imperium.jpg"))
+
+        tabwidget.addTab(self.currentControlList, "Device Controls")
+        tabwidget.addTab(view2, "Global Controls")
+        tabwidget.addTab(Aitpi(self), "Shortcuts")
+        devList = DeviceList(self, self.selectDevice)
+        mainLayout.addWidget(devList)
+        mainLayout.addWidget(tabwidget)
+
+        self.selectedDevControls = []
+
+        mainWidget.setLayout(mainLayout)
+        device_thread.worker_
+        control.registerNewDeviceFun(devList)
+
+
+        self.timer=QTimer()
+        self.timer.timeout.connect(self.queueTimer)
+        self.timer.setInterval(100)
+        self.timer.start()
+        self.queue = ThreadSafeQueue()
+        control.registerFileQueue(self.queue)
+
+        # Set the central widget of the Window. Widget will expand
+        # to take up all the space in the window by default.
+        self.setCentralWidget(mainWidget)
+
+        self.isLinux = sys.platform.startswith('linux')
+
+    def queueTimer(self):
+        value = self.queue.pop()
+        while (value != None):
+            value['fun']()
+            value = self.queue.pop()
+
+    def selectDevice(self, dev):
+        for w in self.selectedDevControls:
+            self.currentControlList.removeItem(w)
+        self.selectedDevControls.clear()
+        if dev is not None:
+            categories = dev.deviceType.getControlCategories()
+            for cat in categories:
+                self.selectedDevControls.append(ControlsTypeSection(cat, False))
+            for w in self.selectedDevControls:
+                self.currentControlList.addItem(w)
+        self.currentControlList.update()
+
+    def keyPressEvent(self, event):
+        if self.isLinux:
+            aitpi.pyqt6KeyPressEvent(event)
+
+    def keyReleaseEvent(self, event):
+        if self.isLinux:
+            aitpi.pyqt6KeyReleaseEvent(event)
+
+
