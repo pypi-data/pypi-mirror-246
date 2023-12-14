ad5a8229ab350a304bc78bb74a02d7e77e5d12be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 04:04:02 +0000

    Add package aioinject/0.16.1/aioinject-0.16.1.tar.gz

---
+++ packages/aioinject/aioinject-0.16.1.tar.gz/aioinject-0.16.1/tests/context/test_context.py
@@ -0,0 +1,114 @@
+from collections.abc import Generator
+from typing import Annotated, Any
+
+import anyio
+import pytest
+
+from aioinject import Callable, Provider, Singleton, providers
+from aioinject.containers import Container
+from aioinject.markers import Inject
+
+
+class _Session:
+    pass
+
+
+class _Repository:
+    def __init__(self, session: Annotated[_Session, Inject]) -> None:
+        self.session = session
+
+
+class _Service:
+    def __init__(self, repository: Annotated[_Repository, Inject]) -> None:
+        self.repository = repository
+
+
+@pytest.fixture
+def container() -> Container:
+    container = Container()
+    container.register(providers.Callable(_Session))
+    container.register(providers.Callable(_Repository))
+    container.register(providers.Callable(_Service))
+    return container
+
+
+def test_can_instantiate_context(container: Container) -> None:
+    assert container.context()
+
+
+def test_can_retrieve_service(container: Container) -> None:
+    with container.sync_context() as ctx:
+        service = ctx.resolve(_Service)
+        assert isinstance(service, _Service)
+        assert isinstance(service.repository, _Repository)
+        assert isinstance(service.repository.session, _Session)
+
+
+def test_uses_cache(container: Container) -> None:
+    with container.sync_context() as ctx:
+        service = ctx.resolve(_Service)
+        a, b, c = service, service.repository, service.repository.session
+
+        service = ctx.resolve(_Service)
+        assert a is service
+        assert b is service.repository
+        assert c is service.repository.session
+
+
+def test_does_not_preserve_cache_if_recreated(container: Container) -> None:
+    with container.sync_context() as ctx:
+        service = ctx.resolve(_Service)
+
+    with container.sync_context() as ctx:
+        assert ctx.resolve(_Service) is not service
+
+
+@pytest.mark.anyio
+async def test_provide_async() -> None:
+    class Test:
+        pass
+
+    container = Container()
+    container.register(Callable(Test))
+    async with container.context() as ctx:
+        instance = await ctx.resolve(Test)
+        assert isinstance(instance, Test)
+
+
+class _AwaitableCls:
+    def __init__(self) -> None:
+        self.awaited = False
+
+    def __await__(self) -> Generator[Any, None, None]:
+        self.awaited = True
+        return anyio.sleep(0).__await__()
+
+
+async def _async_awaitable() -> _AwaitableCls:
+    return _AwaitableCls()
+
+
+def _sync_awaitable() -> _AwaitableCls:
+    return _AwaitableCls()
+
+
+@pytest.mark.anyio
+@pytest.mark.parametrize(
+    "provider",
+    [
+        Callable(_async_awaitable),  # type: ignore[arg-type]
+        Callable(_sync_awaitable),  # type: ignore[arg-type]
+        Singleton(_async_awaitable),  # type: ignore[arg-type]
+        Singleton(_sync_awaitable),  # type: ignore[arg-type]
+    ],
+)
+async def test_should_not_execute_awaitable_classes(
+    provider: Provider[_AwaitableCls],
+) -> None:
+    container = Container()
+    container.register(provider)
+
+    async with container.context() as ctx:
+        resolved = await ctx.resolve(_AwaitableCls)
+        assert isinstance(resolved, _AwaitableCls)
+        assert not resolved.awaited
