05e44b9fa841c28a594101d60c794c33d2a6416d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 15:04:34 +0000

    Add package spaceone-monitoring/2.0.dev7/spaceone-monitoring-2.0.dev7.tar.gz

---
+++ packages/spaceone-monitoring/spaceone-monitoring-2.0.dev7.tar.gz/spaceone-monitoring-2.0.dev7/spaceone/monitoring/conf/default_escalation_policy.py
@@ -0,0 +1,13 @@
+DEFAULT_ESCALATION_POLICY = {
+    'name': 'Default',
+    'is_default': True,
+    'rules': [
+        {
+            'notification_level': 'ALL',
+            'escalate_minutes': 0
+        }
+    ],
+    'repeat_count': 0,
+    'finish_condition': 'ACKNOWLEDGED',
+    'scope': 'DOMAIN'
+}
---
+++ packages/spaceone-monitoring/spaceone-monitoring-2.0.dev7.tar.gz/spaceone-monitoring-2.0.dev7/spaceone/monitoring/conf/global_conf.py
@@ -0,0 +1,80 @@
+WEBHOOK_DOMAIN = ''
+CONSOLE_DOMAIN = ''
+
+# Service Description
+TITLE = 'Documentation for Monitoring Webhook'
+DESCRIPTION = ''
+
+DATABASE_AUTO_CREATE_INDEX = True
+DATABASES = {
+    'default': {
+        'db': 'monitoring',
+        'host': 'localhost',
+        'port': 27017,
+        'username': '',
+        'password': ''
+    }
+}
+
+CACHES = {
+    'default': {},
+    'local': {
+        'backend': 'spaceone.core.cache.local_cache.LocalCache',
+        'max_size': 128,
+        'ttl': 300
+    }
+}
+
+HANDLERS = {
+}
+
+CONNECTORS = {
+    'SpaceConnector': {
+        'backend': 'spaceone.core.connector.space_connector.SpaceConnector',
+        'endpoints': {
+            'identity': 'grpc://identity:50051',
+            'inventory': 'grpc://inventory:50051',
+            'plugin': 'grpc://plugin:50051',
+            'repository': 'grpc://repository:50051',
+            'secret': 'grpc://secret:50051',
+            'notification': 'grpc://notification:50051'
+        }
+    },
+    'DataSourcePluginConnector': {
+    },
+    'WebhookPluginConnector': {
+    },
+}
+
+# Scheduler Settings
+QUEUES = {}
+SCHEDULERS = {}
+WORKERS = {}
+TOKEN = ""
+TOKEN_INFO = {}
+
+# Job Settings
+JOB_TIMEOUT = 600
+
+# Event Settings
+SAME_EVENT_TIME = 600
+
+INSTALLED_DATA_SOURCE_PLUGINS = [
+    # {
+    #     'name': '',
+    #     'plugin_info': {
+    #         'plugin_id': '',
+    #         'version': '',
+    #         'options': {},
+    #         'secret_data': {},
+    #         'schema': '',
+    #         'upgrade_mode': ''
+    #     },
+    #     'tags':{
+    #         'description': ''
+    #     }
+    # }
+]
+
+MAX_CONCURRENT_WORKER = 10
+MAX_REQUEST_LIMIT = 200
