86ec619dcff228087307c9bc20fff738ce0ef70e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 15:04:19 +0000

    Add package dbt-tidb/1.6.4/dbt-tidb-1.6.4.tar.gz

---
+++ packages/dbt-tidb/dbt-tidb-1.6.4.tar.gz/dbt-tidb-1.6.4/dbt/include/tidb/macros/materializations/incremental/incremental.sql
@@ -0,0 +1,65 @@
+{% materialization incremental, adapter='tidb' %}
+
+  {% set unique_key = config.get('unique_key') %}
+
+  {% set target_relation = this.incorporate(type='table') %}
+  {% set existing_relation = load_relation(this) %}
+  {% set tmp_relation = make_temp_relation(this) %}
+
+   -- grab current tables grants config for comparision later on
+  {% set grant_config = config.get('grants') %}
+
+  {{ run_hooks(pre_hooks, inside_transaction=False) }}
+
+  -- `BEGIN` happens here:
+  {{ run_hooks(pre_hooks, inside_transaction=True) }}
+
+  {% set to_drop = [] %}
+  {% if existing_relation is none %}
+      {% set build_sql = create_table_as(False, target_relation, sql) %}
+  {% elif existing_relation.is_view or should_full_refresh() %}
+      {#-- Make sure the backup doesn't exist so we don't encounter issues with the rename below #}
+      {% set backup_identifier = existing_relation.identifier ~ "__dbt_backup" %}
+      {% set backup_relation = existing_relation.incorporate(path={"identifier": backup_identifier}) %}
+      {% do adapter.drop_relation(backup_relation) %}
+
+      {% do adapter.rename_relation(target_relation, backup_relation) %}
+      {% set build_sql = create_table_as(False, target_relation, sql) %}
+      {% do to_drop.append(backup_relation) %}
+  {% else %}
+      {% set tmp_relation = make_temp_relation(target_relation) %}
+      {% do run_query(create_table_as(True, tmp_relation, sql)) %}
+      {% do adapter.expand_target_column_types(
+             from_relation=tmp_relation,
+             to_relation=target_relation) %}
+     {% set dest_columns = adapter.get_columns_in_relation(existing_relation) %}
+     {#-- Get the incremental_strategy, the macro to use for the strategy, and build the sql --#}
+     {% set incremental_strategy = config.get('incremental_strategy') or 'default' %}
+     {% set incremental_predicates = config.get('incremental_predicates', none) %}
+     {% set strategy_sql_macro_func = adapter.get_incremental_strategy_macro(context, incremental_strategy) %}
+     {% set strategy_arg_dict = ({'target_relation': target_relation, 'temp_relation': tmp_relation, 'unique_key': unique_key, 'dest_columns': dest_columns, 'predicates': incremental_predicates }) %}
+      {% set build_sql = strategy_sql_macro_func(strategy_arg_dict) %}
+  {% endif %}
+
+  {% call statement("main") %}
+      {{ build_sql }}
+  {% endcall %}
+
+  {% set should_revoke = should_revoke(existing_relation, full_refresh_mode) %}
+  {% do apply_grants(target_relation, grant_config, should_revoke=should_revoke) %}
+  {% do persist_docs(target_relation, model) %}
+
+  {{ run_hooks(post_hooks, inside_transaction=True) }}
+
+  -- `COMMIT` happens here
+  {% do adapter.commit() %}
+
+  {% for rel in to_drop %}
+      {% do adapter.drop_relation(rel) %}
+  {% endfor %}
+
+  {{ run_hooks(post_hooks, inside_transaction=False) }}
+
+  {{ return({'relations': [target_relation]}) }}
+
+{%- endmaterialization %}
---
+++ packages/dbt-tidb/dbt-tidb-1.6.4.tar.gz/dbt-tidb-1.6.4/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if sys.version_info < (3, 8):
+    print("Error: dbt-tidb does not support this version of Python.")
+    print("Please install Python 3.8 or higher.")
+    sys.exit(1)
+
+# require version of setuptools that supports find_namespace_packages
+from setuptools import setup
+
+try:
+    from setuptools import find_namespace_packages
+except ImportError:
+    # the user has a downlevel version of setuptools.
+    print("Error: dbt requires setuptools v40.1.0 or higher.")
+    print(
+        'Please upgrade setuptools with "pip install --upgrade setuptools" '
+        "and try again"
+    )
+    sys.exit(1)
+
+# pull long description from README
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md")) as f:
+    long_description = f.read()
+
+package_name = "dbt-tidb"
+package_version = "1.6.4"
+dbt_core_version = "1.6.4"
+description = """The TiDB adapter plugin for dbt"""
+
+setup(
+    name=package_name,
+    version=package_version,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="daemonxiao",
+    author_email="qiang.wu@pingcap.com",
+    url="https://github.com/pingcap/dbt-tidb.git",
+    packages=find_namespace_packages(include=["dbt", "dbt.*"]),
+    include_package_data=True,
+    install_requires=[
+        "dbt-core~={}".format(dbt_core_version),
+        "mysql-connector-python>=8.0.0,<8.2",
+    ],
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.8",
+)
