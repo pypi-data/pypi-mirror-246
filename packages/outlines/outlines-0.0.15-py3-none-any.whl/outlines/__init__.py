7a3da16f06ff83d194fdccaddb7e6d6e958bc203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 04:03:58 +0000

    Add package outlines/0.0.15/outlines-0.0.15-py3-none-any.whl

---
+++ packages/outlines/outlines-0.0.15-py3-none-any.whl/outlines/__init__.py
@@ -0,0 +1,15 @@
+"""Outlines is a Generative Model Programming Framework."""
+import outlines.generate
+import outlines.models
+import outlines.text.generate
+from outlines.base import vectorize
+from outlines.caching import clear_cache, disable_cache, get_cache
+from outlines.prompts import prompt
+
+__all__ = [
+    "clear_cache",
+    "disable_cache",
+    "get_cache",
+    "prompt",
+    "vectorize",
+]
