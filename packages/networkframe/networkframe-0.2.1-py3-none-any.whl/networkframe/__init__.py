b5a7c9cdb813e364442b83cd266c32b994b66f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 14:03:40 +0000

    Add package networkframe/0.2.1/networkframe-0.2.1-py3-none-any.whl

---
+++ packages/networkframe/networkframe-0.2.1-py3-none-any.whl/networkframe/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for networkframe."""
+
+__author__ = """Ben Pedigo"""
+__email__ = "ben.pedigo@alleninstitute.org"
+__version__ = "0.2.1"
+
+
+__all__ = ["test"]
---
+++ packages/networkframe/networkframe-0.2.1-py3-none-any.whl/networkframe/test/test.py
@@ -0,0 +1,14 @@
+"""module"""
+
+
+def my_func(a: int, b: int) -> int:
+    """Add two numbers.
+
+    Args:
+        a (int): First number.
+        b (int): Second number.
+
+    Returns:
+        int: Sum of the two numbers.
+    """
+    return a + b
