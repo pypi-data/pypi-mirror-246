6a4bf6c9c25e7a2e233f039d26b0f3fd5401bb53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 04:03:55 +0000

    Add package fts-rtmidi/0.0.1/fts-rtmidi-0.0.1.tar.gz

---
+++ packages/fts-rtmidi/fts-rtmidi-0.0.1.tar.gz/fts-rtmidi-0.0.1/setup.py
@@ -0,0 +1,25 @@
+"""Created using tutorial: https://www.youtube.com/watch?v=tEFkHEKypLI&ab_channel=NeuralNine"""
+
+from setuptools import setup, find_packages
+import codecs
+import os
+
+root = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name         = 'fts-rtmidi',
+    version      = '0.0.1',
+    author       = 'Ferdinand Oliver M Tonby-Strandborg',
+    author_email = 'ferdinand.tonby.strandborg@gmail.com',
+    description  = 'Simple class for MIDI control',
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.12',
+    install_requires = [
+        'python-rtmidi>=1.5.8',
+    ],
+    packages = find_packages(include=['src/*']),
+)
