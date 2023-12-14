63b8eb76d194c65e83e66618e383cae39fea6ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 03:04:04 +0000

    Add package StenLib/0.3.4/StenLib-0.3.4.tar.gz

---
+++ packages/StenLib/StenLib-0.3.4.tar.gz/StenLib-0.3.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+# TODO: can we replace setuptools with something built in?
+
+setup(
+    name="StenLib",
+    version="0.3.4",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license=open("LICENSE.md").read(),
+    url="https://github.com/Structura-Engineering/StenLib",
+    project_urls={
+        "Bug Tracker": "https://github.com/Structura-Engineering/StenLib/issues"
+    },
+    install_requires=open("requirements.txt").read().splitlines(),
+    packages=find_packages(),
+    python_requires=">=3.12.0",
+    package_data={
+        "*": ["*.py", "data/*.json", "*.pyi"],
+    },
+    zip_safe=False,
+)
