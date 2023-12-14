48d2c35fc2dec7be820c4b125ba01d4c82ff3fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 03:06:45 +0000

    Add package cadwyn/3.0.2/cadwyn-3.0.2-py3-none-any.whl

---
+++ packages/cadwyn/cadwyn-3.0.2-py3-none-any.whl/cadwyn/exceptions.py
@@ -0,0 +1,35 @@
+from fastapi.routing import APIRoute
+
+
+class CadwynError(Exception):
+    pass
+
+
+class LintingError(CadwynError):
+    pass
+
+
+class CodeGenerationError(CadwynError):
+    pass
+
+
+class InvalidGenerationInstructionError(CodeGenerationError):
+    pass
+
+
+class RouterGenerationError(CadwynError):
+    pass
+
+
+class RouteAlreadyExistsError(RouterGenerationError):
+    def __init__(self, *routes: APIRoute):
+        self.routes = routes
+        super().__init__(f"The following routes are duplicates of each other: {routes}")
+
+
+class CadwynStructureError(CadwynError):
+    pass
+
+
+class ModuleIsNotVersionedError(ValueError):
+    pass
