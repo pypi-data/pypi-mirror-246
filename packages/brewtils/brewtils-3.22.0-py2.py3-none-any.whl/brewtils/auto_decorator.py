2179d58598345801e43e68bd1decc6fc90217c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 06:04:25 +0000

    Add package brewtils/3.22.0/brewtils-3.22.0-py2.py3-none-any.whl

---
+++ packages/brewtils/brewtils-3.22.0-py2.py3-none-any.whl/brewtils/auto_decorator.py
@@ -0,0 +1,33 @@
+import inspect
+
+from brewtils.models import Command, Parameter
+
+
+class AutoDecorator:
+    def updateClientClass(self, client, name=None, version=None):
+        if name:
+            client._bg_name = name
+        else:
+            client._bg_name = getattr(client, "__name__", client.__class__.__name__)
+
+        if version:
+            client._bg_version = version
+        else:
+            client._bg_version = getattr(client, "__version__", "0.0.0")
+        client._bg_commands = []
+        client._current_request = None
+
+        self.addFunctions(client)
+
+        return client
+
+    def addFunctions(self, client):
+        for func in dir(client):
+            if callable(getattr(client, func)):
+                if not func.startswith("_"):
+                    _wrapped = getattr(client, func)
+
+                    # decorators.py will handle all of the markings
+                    _wrapped._command = Command()
+
+        return client
