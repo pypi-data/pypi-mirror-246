1f7effd6a6fed979abde6527647bd484c2391afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 14:03:40 +0000

    Add package networkframe/0.2.1/networkframe-0.2.1.tar.gz

---
+++ packages/networkframe/networkframe-0.2.1.tar.gz/networkframe-0.2.1/tests/test_networkframe.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+"""Tests for `networkframe` package."""
+
+from networkframe.test import my_func
+
+
+def test_my_func():
+    """Test the my_func function."""
+    assert my_func(1, 2) == 3
