82d63218165132b74fed38d5a82bf8bfcdf0aa15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 04:04:08 +0000

    Add package fusionX/0.0.2/fusionX-0.0.2.tar.gz

---
+++ packages/fusionX/fusionX-0.0.2.tar.gz/fusionX-0.0.2/setup.py
@@ -0,0 +1,38 @@
+
+
+from setuptools import setup, find_packages
+
+
+install_deps = ['numpy>=1.20.0', 'opencv-python>= 4.8.1.78']
+VERSION = 'V0.0.2'
+DESCRIPTION = 'fusionX'
+
+# Setting up
+setup(
+    name="fusionX",
+    version=VERSION,
+    author="FusionX",
+    author_email="<bar.bendavid@weiamann.ac.il>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description="""
+    This is a detailed description of your package.
+
+    :Author: Barben 
+    :Email: your.email@example.com
+    """
+,
+ 
+    packages=find_packages(),
+    install_requires=install_deps,
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
+
