526c8bae7d8c5a33350b9e4a293672c77f5b7540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 15:05:07 +0000

    Add package sysprl412/0.1/sysprl412-0.1.tar.gz

---
+++ packages/sysprl412/sysprl412-0.1.tar.gz/sysprl412-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sysprl412',
+    version='0.1',
+    description="Consume API",
+    author="n4ss4u",
+    packages=["sysprl412", "sysprl412"],
+    install_requires=[
+        "requests==2.31.0"
+    ],
+)
+
