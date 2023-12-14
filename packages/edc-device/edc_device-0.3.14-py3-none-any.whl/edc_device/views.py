f7a3c9cb159d15d02e053017a7f356a5580d4627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 15:04:44 +0000

    Add package edc-device/0.3.14/edc_device-0.3.14-py3-none-any.whl

---
+++ packages/edc-device/edc_device-0.3.14-py3-none-any.whl/edc_device/views.py
@@ -0,0 +1,20 @@
+from django.apps import apps as django_apps
+from django.views.generic.base import TemplateView
+from edc_dashboard.utils import get_bootstrap_version
+from edc_dashboard.view_mixins import EdcViewMixin
+from edc_navbar import NavbarViewMixin
+
+from .view_mixins import EdcDeviceViewMixin
+
+
+class HomeView(EdcViewMixin, NavbarViewMixin, EdcDeviceViewMixin, TemplateView):
+    template_name = f"edc_device/bootstrap{get_bootstrap_version()}/home.html"
+    navbar_name = "edc_device"
+    navbar_selected_item = "device"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        app_config = django_apps.get_app_config("edc_device")
+        project_name = context.get("project_name")
+        context.update({"project_name": f"{project_name}: {app_config.verbose_name}"})
+        return context
