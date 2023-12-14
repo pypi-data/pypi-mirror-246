c3c030426b6181e3960ae4e7e278e260de8cb41e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 16:04:32 +0000

    Add package chintan-sample-api-client/1.0.2/chintan_sample_api_client-1.0.2-py3-none-any.whl

---
+++ packages/chintan-sample-api-client/chintan_sample_api_client-1.0.2-py3-none-any.whl/test.py
@@ -0,0 +1,18 @@
+import logging
+
+from sample_api_client.api.default import get_list
+from sample_api_client.client import Client
+from sample_api_client.types import Response
+
+
+def client_test():
+    client = Client(base_url="http://localhost")
+    with client as client:
+        response: Response = get_list.sync_detailed(client=client)
+        logging.info("response = {}".format(response))
+
+
+if __name__ == '__main__':
+    client_test()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
