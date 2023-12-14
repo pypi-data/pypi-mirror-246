a5e6f4569262eeb1ab7bc7a88a5b97a29960795a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 06:04:31 +0000

    Add package cachier/2.2.2/cachier-2.2.2-py2.py3-none-any.whl

---
+++ packages/cachier/cachier-2.2.2-py2.py3-none-any.whl/cachier/__init__.py
@@ -0,0 +1,12 @@
+from .core import (
+    cachier,
+    set_default_params,
+    get_default_params,
+    enable_caching,
+    disable_caching,
+)
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+# flake8: noqa
