89ab3ede7145b4aaba37ebc66c032340da07c4ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 03:04:25 +0000

    Add package ttxt/0.0.1/ttxt-0.0.1.tar.gz

---
+++ packages/ttxt/ttxt-0.0.1.tar.gz/ttxt-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ttxt',
+    version='0.0.1',
+    description='TT exchange interfacing library',
+    author='sus',
+    packages=find_packages(),
+)
