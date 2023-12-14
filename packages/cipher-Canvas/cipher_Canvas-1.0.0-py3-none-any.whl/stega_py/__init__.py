508520247e973683b04a48f623318f9df8df4f3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 06:04:25 +0000

    Add package cipher-Canvas/1.0.0/cipher_Canvas-1.0.0-py3-none-any.whl

---
+++ packages/cipher-Canvas/cipher_Canvas-1.0.0-py3-none-any.whl/stega_py/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+
+__version__ = "1.0.0"
+__author__ = "Mani"
+__description__ = "A package for embedding and extracting text within images using markers."
+__license__ = "MIT"
+__url__ = "https://github.com/xmannii/cipher_Canvas-Python-Text-Embedding-in-Images"
+
+from .module import embed_text, extract_text
