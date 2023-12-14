2dd472bb4d07d92c80daf8f01d6f57d56189792e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 03:04:58 +0000

    Add package nagra-network-paloalto-utils/0.1.7/nagra_network_paloalto_utils-0.1.7.tar.gz

---
+++ packages/nagra-network-paloalto-utils/nagra_network_paloalto_utils-0.1.7.tar.gz/nagra_network_paloalto_utils-0.1.7/nagra_network_paloalto_utils/commands/panorama/__init__.py
@@ -0,0 +1,24 @@
+import click
+
+from nagra_network_paloalto_utils.utils.common.utils import clean_url_host
+
+from .objects import inject
+from .panorama import cmd_commit, cmd_lock, cmd_unlock
+
+
+@click.group()
+@click.option("--url", envvar="PANOS_HOSTNAME", help="", required=True)
+@click.option("--api-key", envvar="PANOS_API_KEY", help="", required=True)
+@click.pass_obj
+def panorama(obj, url, api_key):
+    url, host, port = clean_url_host(url.strip())
+    obj.URL = url
+    obj.HOSTNAME = host
+    obj.API_KEY = api_key.strip()
+
+
+# panorama.add_command(objects)
+inject(panorama)
+panorama.add_command(cmd_lock)
+panorama.add_command(cmd_unlock)
+panorama.add_command(cmd_commit)
