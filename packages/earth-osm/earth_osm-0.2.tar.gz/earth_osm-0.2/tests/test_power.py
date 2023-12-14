1fbc30b84501739654e9d77282f285bb60970d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 05:05:02 +0000

    Add package earth-osm/0.2/earth_osm-0.2.tar.gz

---
+++ packages/earth-osm/earth_osm-0.2.tar.gz/earth_osm-0.2/tests/test_power.py
@@ -0,0 +1,16 @@
+from earth_osm.eo import save_osm_data
+
+
+primary_name = "power"
+
+# test low resource region (benin)
+# test high resource region (germany)
+
+# test low resource feature (cable)
+# test high resource feature (substation)
+
+# test aggregation by region (benin, germany)
+# test aggregation by feature (tower, line)
+# test no aggregation
+ 
+ 
