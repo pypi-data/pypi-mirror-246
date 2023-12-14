13a493c84fb55d19157082ef17a895f5b7098859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 07:04:03 +0000

    Add package mypy-boto3-mobile/1.34.0/mypy_boto3_mobile-1.34.0-py3-none-any.whl

---
+++ packages/mypy-boto3-mobile/mypy_boto3_mobile-1.34.0-py3-none-any.whl/mypy_boto3_mobile/paginator.py
@@ -0,0 +1,71 @@
+"""
+Type annotations for mobile service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_mobile/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_mobile.client import MobileClient
+    from mypy_boto3_mobile.paginator import (
+        ListBundlesPaginator,
+        ListProjectsPaginator,
+    )
+
+    session = Session()
+    client: MobileClient = session.client("mobile")
+
+    list_bundles_paginator: ListBundlesPaginator = client.get_paginator("list_bundles")
+    list_projects_paginator: ListProjectsPaginator = client.get_paginator("list_projects")
+    ```
+"""
+
+from typing import Generic, Iterator, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .type_defs import ListBundlesResultTypeDef, ListProjectsResultTypeDef, PaginatorConfigTypeDef
+
+__all__ = ("ListBundlesPaginator", "ListProjectsPaginator")
+
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListBundlesPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mobile.html#Mobile.Paginator.ListBundles)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_mobile/paginators/#listbundlespaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListBundlesResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mobile.html#Mobile.Paginator.ListBundles.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_mobile/paginators/#listbundlespaginator)
+        """
+
+
+class ListProjectsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mobile.html#Mobile.Paginator.ListProjects)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_mobile/paginators/#listprojectspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListProjectsResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mobile.html#Mobile.Paginator.ListProjects.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_mobile/paginators/#listprojectspaginator)
+        """
