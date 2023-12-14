6f181d3c2d3bcb6bfe87fb0fc33fa071a84f5bec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 03:03:54 +0000

    Add package Ais-cryoET/0.0.23/Ais-cryoET-0.0.23.tar.gz

---
+++ packages/Ais-cryoET/Ais-cryoET-0.0.23.tar.gz/Ais-cryoET-0.0.23/Ais/core/debug_startup.py
@@ -0,0 +1,8 @@
+import Ais.core.config as cfg
+from Ais.core.segmentation_editor import Segmentation, SegmentationEditor
+
+cfg.segmentation_editor.import_dataset("C:/Users/mgflast/Desktop/DELETE.scns")
+
+ds = cfg.se_active_frame
+SegmentationEditor.OVERLAY_ALPHA = 0.0
+ds.features.append(Segmentation(cfg.se_active_frame, "Debug feature"))
