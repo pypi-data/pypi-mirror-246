c27819c0a323239f6424f6081c53bf580f471f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 16:03:59 +0000

    Add package whisper-mps/0.0.3/whisper_mps-0.0.3-py3-none-any.whl

---
+++ packages/whisper-mps/whisper_mps-0.0.3-py3-none-any.whl/whisper_mps/cli.py
@@ -0,0 +1,30 @@
+from . import whisper
+import argparse
+from rich.progress import Progress, TimeElapsedColumn, BarColumn, TextColumn
+import json
+
+parser = argparse.ArgumentParser(description="Automatic Speech Recognition")
+parser.add_argument(
+    "--file-name",
+    required=True,
+    type=str,
+    help="Path to the audio file to be transcribed.",
+)
+
+def main():
+    args = parser.parse_args()
+    print('Default with small size model')
+    with Progress(
+        TextColumn("🤗 [progress.description]"),
+        BarColumn(style="yellow1", pulse_style="white"),
+        TimeElapsedColumn(),
+    ) as progress:
+        progress.add_task("[yellow]Transcribing...", total=None)
+        # text = whisper.transcribe(speech_file)["text"] ## only for debug
+        text = whisper.transcribe(args.file_name)
+        print(text)
+        with open("output.json", "w", encoding="utf8") as fp:
+            json.dump(text, fp, ensure_ascii=False)
+        print(
+            f"Voila!✨ Your file has been transcribed go check it out over here 👉 output.json"
+        )
