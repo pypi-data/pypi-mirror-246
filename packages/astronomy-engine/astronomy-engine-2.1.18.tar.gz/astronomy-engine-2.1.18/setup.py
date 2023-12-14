e2676d1b352e80bf32389162a1d27508ae046867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 07:04:04 +0000

    Add package astronomy-engine/2.1.18/astronomy-engine-2.1.18.tar.gz

---
+++ packages/astronomy-engine/astronomy-engine-2.1.18.tar.gz/astronomy-engine-2.1.18/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+def _LoadFile(filename):
+    with open(filename) as infile:
+        return infile.read()
+
+setup(
+    name='astronomy-engine',
+    version='2.1.18',
+    description='Astronomy calculation for Sun, Moon, and planets.',
+    long_description=_LoadFile('README.md'),
+    long_description_content_type='text/markdown',
+    author='Donald Cross',
+    author_email='cosinekitty@gmail.com',
+    url='https://github.com/cosinekitty/astronomy',
+    license=_LoadFile('LICENSE'),
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7"
+    ],
+    packages = ['astronomy']
+)
