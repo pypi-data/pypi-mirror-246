e957da8953269f742b28b98a2b4b28ef37fbde4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 03:04:03 +0000

    Add package jwtauthenticatortest1/1.0.0/jwtauthenticatortest1-1.0.0.tar.gz

---
+++ packages/jwtauthenticatortest1/jwtauthenticatortest1-1.0.0.tar.gz/jwtauthenticatortest1-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='jwtauthenticatortest1',
+    version='1.0.0',
+    license='Apache 2.0',
+    packages=['jwtauthenticatortest1'],
+    install_requires=[
+        'jupyterhub',
+        'pyjwt',
+    ]
+)
