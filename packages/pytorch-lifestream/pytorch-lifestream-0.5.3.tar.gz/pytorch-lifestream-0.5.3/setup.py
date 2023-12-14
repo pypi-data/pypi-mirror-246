2cda27923eda10997dd61c46489271baa9965690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 14:03:59 +0000

    Add package pytorch-lifestream/0.5.3/pytorch-lifestream-0.5.3.tar.gz

---
+++ packages/pytorch-lifestream/pytorch-lifestream-0.5.3.tar.gz/pytorch-lifestream-0.5.3/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name='pytorch-lifestream',
+    version='0.5.3',
+    author='',
+    author_email='',
+    description='Lifestream data analysis with PyTorch',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'pytorch-lightning==1.6.*',
+        'torch==1.12.*',
+        'numpy==1.23.*',
+        'torchmetrics==0.9.*',
+        'omegaconf',
+    ],
+)
