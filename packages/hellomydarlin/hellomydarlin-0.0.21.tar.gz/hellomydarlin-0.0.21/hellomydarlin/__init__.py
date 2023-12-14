5a7ceb458b08b515e8a46084ca9756c088aaf899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 14:04:03 +0000

    Add package hellomydarlin/0.0.21/hellomydarlin-0.0.21.tar.gz

---
+++ packages/hellomydarlin/hellomydarlin-0.0.21.tar.gz/hellomydarlin-0.0.21/hellomydarlin/__init__.py
@@ -0,0 +1,4 @@
+import hellomydarlin
+
+
+hellomydarlin.sayhello()
---
+++ packages/hellomydarlin/hellomydarlin-0.0.21.tar.gz/hellomydarlin-0.0.21/hellomydarlin/hello.py
@@ -0,0 +1,7 @@
+import random
+
+def sayhello():
+    print("Hello, World!")
+
+def pickNum(num):
+    print(random.randint(0, num+1))
