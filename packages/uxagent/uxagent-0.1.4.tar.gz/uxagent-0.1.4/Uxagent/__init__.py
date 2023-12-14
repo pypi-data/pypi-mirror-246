a808ddc2417810605d45ddd3af09881792019b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 03:04:04 +0000

    Add package uxagent/0.1.4/uxagent-0.1.4.tar.gz

---
+++ packages/uxagent/uxagent-0.1.4.tar.gz/uxagent-0.1.4/Uxagent/__init__.py
@@ -0,0 +1,2 @@
+from .uxagent import *
+from .filters import *
---
+++ packages/uxagent/uxagent-0.1.4.tar.gz/uxagent-0.1.4/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as o:
+    long_description = o.read()
+
+DATA01 = "clintonabrahamc@gmail.com"
+
+DATA02 = ['Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules']
+
+setup(
+    name='uxagent',
+    license='MIT',
+    zip_safe=False,
+    version='0.1.4',
+    classifiers=DATA02,
+    author_email=DATA01,
+    python_requires='~=3.7',
+    packages=find_packages(),
+    author='Clinton-Abraham',
+    description='Random user agent',
+    long_description=long_description,
+    package_data={'Uxagent': ['data/*.zip']},
+    keywords=['python', 'useragent', 'telegram'],
+    long_description_content_type="text/markdown",
+    url='https://github.com/Clinton-Abraham/USER-X-AGENT',)
