b4c4a0a8bb2be74b93c2833e269f0b1a7a95a878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 03:05:13 +0000

    Add package fugit/0.0.3/fugit-0.0.3-py3-none-any.whl

---
+++ packages/fugit/fugit-0.0.3-py3-none-any.whl/fugit/__init__.py
@@ -0,0 +1,5 @@
+from .core import diff
+
+__version__ = "0.0.3"
+
+__all__ = ["diff"]
---
+++ packages/fugit/fugit-0.0.3-py3-none-any.whl/fugit/interfaces/display.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+
+__all__ = ["DisplayConfig"]
+
+
+class DisplayConfig(BaseModel):
+    """Put any display settings here"""
