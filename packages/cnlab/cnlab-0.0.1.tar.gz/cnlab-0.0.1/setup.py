a9472aa30989a3faa30e3e46f6329d1ee4e626a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 05:04:45 +0000

    Add package cnlab/0.0.1/cnlab-0.0.1.tar.gz

---
+++ packages/cnlab/cnlab-0.0.1.tar.gz/cnlab-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+name = 'cnlab',
+version = '0.0.1',
+author = 'Oh Please!,',
+author_email = 'amazinggorillaxo@gmail.com',
+description = 'For creating nothing',
+classifiers =[
+    'Programming Language :: Python :: 3',
+    'Operating System :: OS Independent'
+],
+packages = ['cnlab']
+)
