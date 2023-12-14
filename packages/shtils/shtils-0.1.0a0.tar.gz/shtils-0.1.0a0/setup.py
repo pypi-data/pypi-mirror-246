6c5b1fe70ee4bfdc3c3b42ce5cb8e78e0d3e4a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 06:04:04 +0000

    Add package shtils/0.1.0a0/shtils-0.1.0a0.tar.gz

---
+++ packages/shtils/shtils-0.1.0a0.tar.gz/shtils-0.1.0a0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.0-alpha'
+DESCRIPTION = 'A collection of OS shell utils'
+LONG_DESCRIPTION = 'A collection of OS shell utils that allows a developer to implement often-used shell commands (like "ls" or "clear") in both Windows and Unix terms.'
+
+# Setting up
+setup(
+    name="shtils",
+    version=VERSION,
+    author="alpenstorm (Mihai Negrean)",
+    author_email="<mihai@negrean.net>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    keywords=['python', 'os', 'shell', 'utils'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
---
+++ packages/shtils/shtils-0.1.0a0.tar.gz/shtils-0.1.0a0/shtils/shtils.py
@@ -0,0 +1,15 @@
+def clear():
+    import os
+    try:
+        os.system('cls')
+    except:
+        os.system('clear')
+
+def ls():
+    import os
+    try:
+        os.system('dir')
+    except:
+        os.system('ls')
+
+
