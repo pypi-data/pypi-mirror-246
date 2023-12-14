72ab4250f5587f48f579e504bdbe8377f1044413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 06:04:09 +0000

    Add package mypy-boto3-inspector2/1.34.0/mypy-boto3-inspector2-1.34.0.tar.gz

---
+++ packages/mypy-boto3-inspector2/mypy-boto3-inspector2-1.34.0.tar.gz/mypy-boto3-inspector2-1.34.0/mypy_boto3_inspector2/paginator.pyi
@@ -0,0 +1,248 @@
+"""
+Type annotations for inspector2 service client paginators.
+
+[Open documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/)
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+
+    from mypy_boto3_inspector2.client import Inspector2Client
+    from mypy_boto3_inspector2.paginator import (
+        ListAccountPermissionsPaginator,
+        ListCoveragePaginator,
+        ListCoverageStatisticsPaginator,
+        ListDelegatedAdminAccountsPaginator,
+        ListFiltersPaginator,
+        ListFindingAggregationsPaginator,
+        ListFindingsPaginator,
+        ListMembersPaginator,
+        ListUsageTotalsPaginator,
+        SearchVulnerabilitiesPaginator,
+    )
+
+    session = Session()
+    client: Inspector2Client = session.client("inspector2")
+
+    list_account_permissions_paginator: ListAccountPermissionsPaginator = client.get_paginator("list_account_permissions")
+    list_coverage_paginator: ListCoveragePaginator = client.get_paginator("list_coverage")
+    list_coverage_statistics_paginator: ListCoverageStatisticsPaginator = client.get_paginator("list_coverage_statistics")
+    list_delegated_admin_accounts_paginator: ListDelegatedAdminAccountsPaginator = client.get_paginator("list_delegated_admin_accounts")
+    list_filters_paginator: ListFiltersPaginator = client.get_paginator("list_filters")
+    list_finding_aggregations_paginator: ListFindingAggregationsPaginator = client.get_paginator("list_finding_aggregations")
+    list_findings_paginator: ListFindingsPaginator = client.get_paginator("list_findings")
+    list_members_paginator: ListMembersPaginator = client.get_paginator("list_members")
+    list_usage_totals_paginator: ListUsageTotalsPaginator = client.get_paginator("list_usage_totals")
+    search_vulnerabilities_paginator: SearchVulnerabilitiesPaginator = client.get_paginator("search_vulnerabilities")
+    ```
+"""
+
+from typing import Generic, Iterator, Sequence, TypeVar
+
+from botocore.paginate import PageIterator, Paginator
+
+from .literals import AggregationTypeType, FilterActionType, GroupKeyType, ServiceType
+from .type_defs import (
+    AggregationRequestTypeDef,
+    CoverageFilterCriteriaTypeDef,
+    FilterCriteriaPaginatorTypeDef,
+    ListAccountPermissionsResponseTypeDef,
+    ListCoverageResponseTypeDef,
+    ListCoverageStatisticsResponseTypeDef,
+    ListDelegatedAdminAccountsResponseTypeDef,
+    ListFiltersResponsePaginatorTypeDef,
+    ListFindingAggregationsResponseTypeDef,
+    ListFindingsResponseTypeDef,
+    ListMembersResponseTypeDef,
+    ListUsageTotalsResponseTypeDef,
+    PaginatorConfigTypeDef,
+    SearchVulnerabilitiesFilterCriteriaTypeDef,
+    SearchVulnerabilitiesResponseTypeDef,
+    SortCriteriaTypeDef,
+    StringFilterTypeDef,
+)
+
+__all__ = (
+    "ListAccountPermissionsPaginator",
+    "ListCoveragePaginator",
+    "ListCoverageStatisticsPaginator",
+    "ListDelegatedAdminAccountsPaginator",
+    "ListFiltersPaginator",
+    "ListFindingAggregationsPaginator",
+    "ListFindingsPaginator",
+    "ListMembersPaginator",
+    "ListUsageTotalsPaginator",
+    "SearchVulnerabilitiesPaginator",
+)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+class ListAccountPermissionsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.ListAccountPermissions)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#listaccountpermissionspaginator)
+    """
+
+    def paginate(
+        self, *, service: ServiceType = ..., PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListAccountPermissionsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.ListAccountPermissions.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#listaccountpermissionspaginator)
+        """
+
+class ListCoveragePaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.ListCoverage)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#listcoveragepaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        filterCriteria: CoverageFilterCriteriaTypeDef = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListCoverageResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.ListCoverage.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#listcoveragepaginator)
+        """
+
+class ListCoverageStatisticsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.ListCoverageStatistics)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#listcoveragestatisticspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        filterCriteria: CoverageFilterCriteriaTypeDef = ...,
+        groupBy: GroupKeyType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListCoverageStatisticsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.ListCoverageStatistics.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#listcoveragestatisticspaginator)
+        """
+
+class ListDelegatedAdminAccountsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.ListDelegatedAdminAccounts)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#listdelegatedadminaccountspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListDelegatedAdminAccountsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.ListDelegatedAdminAccounts.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#listdelegatedadminaccountspaginator)
+        """
+
+class ListFiltersPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.ListFilters)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#listfilterspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        action: FilterActionType = ...,
+        arns: Sequence[str] = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListFiltersResponsePaginatorTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.ListFilters.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#listfilterspaginator)
+        """
+
+class ListFindingAggregationsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.ListFindingAggregations)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#listfindingaggregationspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        aggregationType: AggregationTypeType,
+        accountIds: Sequence[StringFilterTypeDef] = ...,
+        aggregationRequest: AggregationRequestTypeDef = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListFindingAggregationsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.ListFindingAggregations.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#listfindingaggregationspaginator)
+        """
+
+class ListFindingsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.ListFindings)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#listfindingspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        filterCriteria: FilterCriteriaPaginatorTypeDef = ...,
+        sortCriteria: SortCriteriaTypeDef = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListFindingsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.ListFindings.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#listfindingspaginator)
+        """
+
+class ListMembersPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.ListMembers)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#listmemberspaginator)
+    """
+
+    def paginate(
+        self, *, onlyAssociated: bool = ..., PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListMembersResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.ListMembers.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#listmemberspaginator)
+        """
+
+class ListUsageTotalsPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.ListUsageTotals)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#listusagetotalspaginator)
+    """
+
+    def paginate(
+        self, *, accountIds: Sequence[str] = ..., PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[ListUsageTotalsResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.ListUsageTotals.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#listusagetotalspaginator)
+        """
+
+class SearchVulnerabilitiesPaginator(Paginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.SearchVulnerabilities)
+    [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#searchvulnerabilitiespaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        filterCriteria: SearchVulnerabilitiesFilterCriteriaTypeDef,
+        PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> _PageIterator[SearchVulnerabilitiesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/inspector2.html#Inspector2.Paginator.SearchVulnerabilities.paginate)
+        [Show boto3-stubs documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_inspector2/paginators/#searchvulnerabilitiespaginator)
+        """
