6cfb6958e182c15233c900fba5c3168576f77012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 15:04:58 +0000

    Add package skymap-job-processing/0.1.0/skymap_job_processing-0.1.0.tar.gz

---
+++ packages/skymap-job-processing/skymap_job_processing-0.1.0.tar.gz/skymap_job_processing-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='skymap_job_processing',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+)
