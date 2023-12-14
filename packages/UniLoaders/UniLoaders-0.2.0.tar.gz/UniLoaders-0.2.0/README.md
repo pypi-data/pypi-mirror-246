8ab5cb52eaaeacb1810a6f8c399bbdc23ecbf855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 16:04:00 +0000

    Add package UniLoaders/0.2.0/UniLoaders-0.2.0.tar.gz

---
+++ packages/UniLoaders/UniLoaders-0.2.0.tar.gz/UniLoaders-0.2.0/README.md
@@ -0,0 +1,14 @@
+#UniLoaders
+
+
+**UniLoaders** is a langchain based script that aims to solve any type of data ingestion within a single package. The formats currently supported are the following:
+1. URLs
+2. Microsoft Word Documents
+3. Microsoft Powerpoints
+4. PDFs
+5. JPGs
+6. PNGs
+
+# One Install
+
+```pip install UniLoaders```
---
+++ packages/UniLoaders/UniLoaders-0.2.0.tar.gz/UniLoaders-0.2.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='UniLoaders',
+    version='0.2.0',
+    description='Loading everything from URLs to powerpoints',
+    author='Enzo Bonelli', 
+    long_description=long_description,
+    long_description_content_type="text/markdown", 
+    packages=find_packages(),
+    install_requires=[
+        "langchain",
+        "unstructured[docx]",
+        "docx2txt",
+        "unstructured",
+        "pdfminer.six",
+        "pdf2image",
+        "boto3"
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    license='Apache-2.0', 
+    python_requires='>=3.6',
+)
