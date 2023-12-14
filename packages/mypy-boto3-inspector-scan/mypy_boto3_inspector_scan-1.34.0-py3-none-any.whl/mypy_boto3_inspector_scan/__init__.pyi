02356057cbab85b04baefa83ade0272815af40e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 06:04:06 +0000

    Add package mypy-boto3-inspector-scan/1.34.0/mypy_boto3_inspector_scan-1.34.0-py3-none-any.whl

---
+++ packages/mypy-boto3-inspector-scan/mypy_boto3_inspector_scan-1.34.0-py3-none-any.whl/mypy_boto3_inspector_scan/__init__.pyi
@@ -0,0 +1,22 @@
+"""
+Main interface for inspector-scan service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_inspector_scan import (
+        Client,
+        inspectorscanClient,
+    )
+
+    session = Session()
+    client: inspectorscanClient = session.client("inspector-scan")
+    ```
+"""
+
+from .client import inspectorscanClient
+
+Client = inspectorscanClient
+
+__all__ = ("Client", "inspectorscanClient")
