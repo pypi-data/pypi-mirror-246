e5da32b8230381f2ccfd487455e8969b53e23a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 03:07:05 +0000

    Add package sequana-pipetools/0.16.6/sequana_pipetools-0.16.6-py3-none-any.whl

---
+++ packages/sequana-pipetools/sequana_pipetools-0.16.6-py3-none-any.whl/sequana_pipetools/__init__.py
@@ -0,0 +1,25 @@
+import pkg_resources
+
+try:
+    version = pkg_resources.require("sequana_pipetools")[0].version
+except pkg_resources.DistributionNotFound:  # pragma: no cover
+    version = ">=0.2.0"
+
+
+from easydev.logging_tools import Logging
+
+logger = Logging("sequana_pipetools", "WARNING")
+# To keep the inheritance/propagation of levels. Logging from easydev will do
+# the formatting only.
+import colorlog
+
+logger = colorlog.getLogger(logger.name)
+
+from .misc import url2hash
+from .sequana_manager import SequanaManager  # , get_pipeline_location
+from .snaketools import (
+    Pipeline,
+    PipelineManager,
+    PipelineManagerDirectory,
+    SequanaConfig,
+)
