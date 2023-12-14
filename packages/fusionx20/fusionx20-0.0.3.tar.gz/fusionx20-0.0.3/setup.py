da2f000c6a74bd7dcdf8beef6d9e3a9bf42e4f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 04:03:49 +0000

    Add package fusionx20/0.0.3/fusionx20-0.0.3.tar.gz

---
+++ packages/fusionx20/fusionx20-0.0.3.tar.gz/fusionx20-0.0.3/setup.py
@@ -0,0 +1,38 @@
+
+
+from setuptools import setup, find_packages
+
+
+install_deps = ['numpy>=1.20.0']
+VERSION = '0.0.3'
+DESCRIPTION = 'fusionx20'
+
+# Setting up
+setup(
+    name="fusionx20",
+    version=VERSION,
+    author="FusionX",
+    author_email="<bar.bendavid@weiamann.ac.il>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description="""
+    This is a detailed description of your package.
+
+    :Author: Barben 
+    :Email: your.email@example.com
+    """
+,
+ 
+    packages=find_packages(),
+    install_requires=install_deps,
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
+
