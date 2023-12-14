d6e450016324e96e26e3b783e92dcfddc25e9143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 07:04:03 +0000

    Add package factor-pricing-model-risk-model/2023.8.0/factor_pricing_model_risk_model-2023.8.0-py3-none-any.whl

---
+++ packages/factor-pricing-model-risk-model/factor_pricing_model_risk_model-2023.8.0-py3-none-any.whl/fpm_risk_model/regressor/wls.py
@@ -0,0 +1,101 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from numpy import ndarray
+
+from ..engine import LinAlgEngine
+
+linalg = LinAlgEngine()
+
+
+@dataclass
+class RegressionResult:
+    """
+    Regression result.
+
+    Assume that
+
+      y = X @ beta + alpha
+
+    where
+
+      X is a matrix in shape (m, n)
+      y is a matrix in shape (m, k)
+
+    Then
+
+      beta is a matrix in shape (n, k)
+      alpha is a matrix in shape (m, k)
+
+    Parameters
+    ----------
+    alpha: Optional[ndarray]
+      The error term of the regression.
+    beta: Optional[ndarray]
+      The fitted linear variables of the regression.
+    """
+
+    alpha: Optional[ndarray] = None
+    beta: Optional[ndarray] = None
+
+
+class WLS:
+    """
+    Weighted least squares problem solver.
+
+    The solver is to run regression with weighted least squares
+    objective.
+    """
+
+    def __init__(self, executor: str = "closed"):
+        """
+        Construct
+
+        Parameters
+        ----------
+        executor : str
+          Executor name. Default is "closed", deriving from closed formula.
+        """
+        self._executor = executor
+
+    def fit(self, X: ndarray, y: ndarray, weights: Optional[ndarray] = None):
+        """
+        Fit the coefficients by the data X and y.
+
+        Parameters
+        ----------
+        X: ndarray
+          Training data in dimension (m, n).
+        y: ndarray
+          Target values in dimension (m, k).
+        weights: Optional[ndarray]
+          Weightings in regressiond data. The dimension should be
+          same as y.
+        """
+        if self._executor == "closed":
+            return self._close_fit(X=X, y=y, weights=weights)
+
+        raise ValueError(f"Executor {self._executor} is not supported")
+
+    @staticmethod
+    def _close_fit(X: ndarray, y: ndarray, weights: Optional[ndarray] = None):
+        """
+        Fit the coefficients with closed formula.
+
+        coefficients = (X^T @ W @ X)^{-1} @ X^T @ W @ y
+        """
+        if isinstance(weights, ndarray):
+            if len(weights.shape) == 1 and weights.shape[0] == y.shape[0]:
+                weights = weights**0.5
+                X_t_w = X.T * weights * weights.T
+                beta = linalg.pinv(X_t_w @ X) @ X_t_w @ y
+            else:
+                raise ValueError(
+                    f"Dimension of y {y.shape} does not align with weights "
+                    f"{weights.shape}"
+                )
+        else:
+            beta = linalg.pinv(X.T @ X) @ X.T @ y
+
+        alpha = y - X @ beta
+        return RegressionResult(alpha=alpha, beta=beta)
---
+++ packages/factor-pricing-model-risk-model/factor_pricing_model_risk_model-2023.8.0-py3-none-any.whl/fpm_risk_model/rolling_factor_risk_model.py
@@ -0,0 +1,210 @@
+import json
+from datetime import datetime
+from multiprocessing import cpu_count
+from multiprocessing.pool import ThreadPool
+from os import makedirs
+from os.path import join
+from typing import Dict, Optional
+
+from pandas import DataFrame, Timestamp
+
+from .factor_risk_model import FactorRiskModel
+from .risk_model import RiskModel
+from .rolling_risk_model import RollingRiskModel
+
+
+class RollingFactorRiskModel(RollingRiskModel):
+    """
+    Rolling factor risk model.
+
+    Rolling factor risk model is a subclass of rolling risk model,
+    while it supports transforming each risk model given the instrument
+    returns.
+    """
+
+    def __init__(
+        self,
+        model: Optional[RiskModel] = None,
+        window: Optional[int] = None,
+        show_progress: Optional[bool] = False,
+        values: Optional[Dict[datetime, RiskModel]] = None,
+    ):
+        """
+        Constructor.
+
+        Parameters
+        ----------
+        model: Optional[RiskModel]
+            Risk model object to fit in rolling basis.
+
+        window: Optional[int]
+            Number of rolling windows to use from the returns.
+            Must be provided in fitting the model.
+
+        show_progress: Optional[bool]
+            Indicate to show progress bar in running.
+
+        values: Optional[Dict[datetime, RiskModel]]
+            Rolling risk models values.
+        """
+        super().__init__(
+            model=model,
+            window=window,
+            show_progress=show_progress,
+            values=values,
+        )
+
+    def transform(
+        self,
+        y: DataFrame,
+        validity: Optional[DataFrame] = None,
+        regressor: Optional[object] = None,
+        start_date: Optional[Timestamp] = None,
+    ) -> object:
+        """
+        Transform the rolling factor risk model.
+
+        The method is used to transform the rolling factor risk model by
+        passing another set of returns. Most of the time, the
+        factor risk model is fitted by the estimation universe,
+        and then transformed by the model universe.
+
+        Parameters
+        ----------
+        y : DataFrame
+            The instrument returns of which its index and columns
+            are the date / time and return values.
+
+        validity: DataFrame
+            The instrument validity on the date.
+
+        regressor : object, default=None
+            Regressor to transform the input y into factor exposures.
+            If None, the regressor is set to the default WLS.
+
+        Returns
+        -------
+        object
+            The transformed rolling factor risk model.
+        """
+        if not isinstance(y, DataFrame):
+            raise TypeError(
+                "Only DataFrame type is supported, but not " f"{y.__class__.__name__}"
+            )
+
+        if not self._config.window:
+            raise ValueError(
+                f"Rolling timeframe must be specified, but not {self._config.window}"
+            )
+
+        values = {}
+        iterator = self.keys()
+        if self._config.show_progress:
+            from tqdm import tqdm
+
+            iterator = tqdm(iterator, leave=False)
+
+        for index in iterator:
+            if start_date is not None and start_date > index:
+                continue
+            y_end_index = y.index.get_loc(index)
+            y_start_index = y_end_index - self._config.window
+            if y_start_index < 0:
+                raise ValueError(
+                    "Input data does not have sufficient history for " f"index {index}"
+                )
+
+            y_input = y.iloc[y_start_index : y_end_index + 1]
+            if validity is not None:
+                validity_input = validity.loc[index]
+                y_input = y_input.loc[:, validity_input]
+
+            # Skip if the number of sample size is zero
+            if y_input.shape[1] == 0:
+                continue
+
+            risk_model = self.get(index)
+            values[index] = risk_model.transform(
+                y=y_input.fillna(0.0),
+                regressor=regressor,
+            )
+
+        self._values = values
+        return self
+
+    def write_directory(
+        self, path: str, format: str = "parquet", workers: int = cpu_count(), **kwargs
+    ):
+        """
+        Write to a directory.
+
+        Parameters
+        ----------
+        path: str
+            Directory path to write to.
+
+        format: str
+            Supported formats. Default is "parquet". Options
+            are "csv", "parquet" and "hdf".
+
+        workers: int
+            Number of workers to use for parallel write operations.
+            Default is the number of CPUs provided.
+
+        **kwargs: dict
+            Optional keyword arguments for the write operation.
+        """
+
+        def _frm_write_directory(item):
+            key, frm = item
+            if isinstance(key, Timestamp):
+                key = key.isoformat()
+            key_path = join(path, key)
+            makedirs(key_path, exist_ok=True)
+            frm.write_directory(key_path, format=format, **kwargs)
+            return key
+
+        with ThreadPool(processes=workers) as pool:
+            keys = pool.map(_frm_write_directory, self._values.items())
+
+        with open(join(path, "metadata.json"), mode="w+") as fp:
+            json.dump({"directories": keys, "parameters": self.asdict()}, fp)
+
+    @classmethod
+    def read_directory(
+        cls, path: str, format: str = "parquet", workers: int = cpu_count(), **kwargs
+    ):
+        """
+        Read a model from directory.
+
+        Parameters
+        ----------
+        path: str
+            Directory path to read from.
+
+        format: str
+            Supported formats. Default is "parquet". Options
+            are "csv", "parquet" and "hdf".
+
+        workers: int
+            Number of workers to use for parallel read operations.
+            Default is the number of CPUs provided.
+
+        **kwargs: dict
+            Optional keyword arguments for the read operation.
+        """
+
+        def _frm_read_directory(key):
+            key_path = join(path, key)
+            value = FactorRiskModel.read_directory(key_path, format=format, **kwargs)
+            return Timestamp(key), value
+
+        with open(join(path, "metadata.json")) as fp:
+            metadata = json.load(fp)
+            directories = metadata["directories"]
+            metadata = metadata["parameters"]
+
+        with ThreadPool(processes=workers) as pool:
+            values = pool.map(_frm_read_directory, directories)
+
+        return cls(values=dict(values), **metadata)
