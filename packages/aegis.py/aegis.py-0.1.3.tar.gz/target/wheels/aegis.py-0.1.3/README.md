11058dc5abb3587cbf23b324bd3dfe37b580767c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 15:04:35 +0000

    Add package aegis.py/0.1.3/aegis.py-0.1.3.tar.gz

---
+++ packages/aegis.py/aegis.py-0.1.3.tar.gz/target/wheels/aegis.py-0.1.3/README.md
@@ -0,0 +1,10 @@
+# aegis.py
+Fast versions of the AEGIS authenticated encryption algorithm.
+
+## features
+- Extremely fast hardware-accelerated encryption
+- Ensures authenticity (that your encrypted message isn't tampered with)
+- 128 and 256-bit tags
+
+## benchmarks
+todo
---
+++ packages/aegis.py/aegis.py-0.1.3.tar.gz/target/wheels/aegis.py-0.1.3/src/backends/fallback.rs
@@ -0,0 +1,36 @@
+use softaes::SoftAesFast;
+use core::ops::{BitAnd, BitXor};
+
+pub(crate) struct Block(softaes::Block);
+
+impl Block {
+    pub fn load(input: &[u8]) -> Self {
+        Block(softaes::Block::from_slice(input))
+    }
+    
+    pub fn store(&self) -> [u8; 16] {
+        self.0.to_bytes()
+    }
+
+    pub fn enc(&self, other: Block) {
+        SoftAesFast.block_encrypt(&self.0, &other.0)
+    }
+}
+
+impl BitXor for Block {
+    type Output = Block;
+
+    #[inline(always)]
+    fn bitxor(self, rhs: Self) -> Self::Output {
+        Block(unsafe { self.0 ^ other.0 })
+    }
+}
+
+impl BitAnd for Block {
+    type Output = Block;
+
+    #[inline(always)]
+    fn bitand(self, rhs: Self) -> Self::Output {
+        Block(unsafe { self.0 & other.0 })
+    }
+}
