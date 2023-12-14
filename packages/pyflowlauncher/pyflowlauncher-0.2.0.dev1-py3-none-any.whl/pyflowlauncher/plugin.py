0786ff83a6d8495ebf4746ab3737a079eb6fd92d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 15:04:21 +0000

    Add package pyflowlauncher/0.2.0.dev1/pyflowlauncher-0.2.0.dev1-py3-none-any.whl

---
+++ packages/pyflowlauncher/pyflowlauncher-0.2.0.dev1-py3-none-any.whl/pyflowlauncher/plugin.py
@@ -0,0 +1,56 @@
+from __future__ import annotations
+
+from functools import wraps
+from typing import Any, Callable, Iterable, Union
+
+from pyflowlauncher.shared import logger
+
+from .event import EventHandler
+from .jsonrpc import JsonRPCClient
+from .result import JsonRPCAction, ResultResponse
+
+Method = Callable[..., Union[ResultResponse, JsonRPCAction]]
+
+
+class Plugin:
+
+    def __init__(self, methods: list[Method] | None = None) -> None:
+        self._logger = logger(self)
+        self._client = JsonRPCClient()
+        self._event_handler = EventHandler()
+        self._settings: dict[str, Any] = {}
+        if methods:
+            self.add_methods(methods)
+
+    def add_method(self, method: Method) -> None:
+        """Add a method to the event handler."""
+        name = getattr(method, '__name__', method.__class__.__name__).lower()
+        self._logger.debug(f"Adding method: '{name}'")
+        self._event_handler.add_method(method, name=name)
+
+    def add_methods(self, methods: Iterable[Method]) -> None:
+        self._event_handler.add_methods(methods)
+
+    def on_method(self, method: Method) -> Method:
+        @wraps(method)
+        def wrapper(*args, **kwargs):
+            return method(*args, **kwargs)
+        self._event_handler.add_method(wrapper)
+        return wrapper
+
+    @property
+    def settings(self) -> dict:
+        if self._settings is None:
+            self._settings = {}
+        self._settings = self._client.recieve().get('settings', {})
+        return self._settings
+
+    def run(self) -> None:
+        request = self._client.recieve()
+        method = request.get('method')
+        parameters = request.get('parameters', [])
+        feedback = self._event_handler(method, *parameters)
+        # Inject settings if changed
+        if 'result' in feedback and self._settings is not None:
+            feedback['SettingsChange'] = self.settings
+        self._client.send(feedback)
