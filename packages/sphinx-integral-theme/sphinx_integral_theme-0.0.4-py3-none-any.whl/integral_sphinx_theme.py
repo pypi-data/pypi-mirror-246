0a93cef5725282d1dc663b376f4c94e556f2df14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 06:04:09 +0000

    Add package sphinx-integral-theme/0.0.4/sphinx_integral_theme-0.0.4-py3-none-any.whl

---
+++ packages/sphinx-integral-theme/sphinx_integral_theme-0.0.4-py3-none-any.whl/integral_sphinx_theme.py
@@ -0,0 +1,9 @@
+import os
+
+__version_info__ = (0, 0, 4)
+__version__ = "0.0.4"
+
+def setup(app):
+    app.require_sphinx("1.6")
+    theme_path = os.path.abspath(os.path.dirname(__file__))
+    app.add_html_theme("integral_sphinx_theme", theme_path)
