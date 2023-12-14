68b785144bd0ab2513e54c1fc00be8bace01dd42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 03:06:57 +0000

    Add package a3m/0.7.5/a3m-0.7.5-py3-none-any.whl

---
+++ packages/a3m/a3m-0.7.5-py3-none-any.whl/a3m/bag.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from bagit import Bag, BagError
+
+
+def is_bag(path):
+    """Determine whether the directory contains a BagIt package.
+
+    The constructor of ``Bag`` is fast enough but we may prefer to optimize
+    later.
+    """
+    if isinstance(path, Path):
+        path = str(path)
+    try:
+        Bag(path)
+    except BagError:
+        return False
+    return True
