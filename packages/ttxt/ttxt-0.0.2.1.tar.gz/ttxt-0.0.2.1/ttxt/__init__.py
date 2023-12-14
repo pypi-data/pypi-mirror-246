2fdbf85df88adf95b3fe18ed8206e99eee609bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 03:04:47 +0000

    Add package ttxt/0.0.2.1/ttxt-0.0.2.1.tar.gz

---
+++ packages/ttxt/ttxt-0.0.2.1.tar.gz/ttxt-0.0.2.1/ttxt/__init__.py
@@ -0,0 +1,14 @@
+# from ttxt.base import baseFuturesExchange
+from ttxt.exchanges.gateFutures import gateFutures
+from ttxt.exchanges.bybitFutures import bybitFutures
+
+exchanges = [
+    "gateFutures",
+    "bybitFutures"
+]
+
+# base = [
+#     "baseFuturesExchange"
+# ]
+
+_all__ =  exchanges
