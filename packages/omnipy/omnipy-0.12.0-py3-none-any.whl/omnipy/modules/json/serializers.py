0564bc5ebf5c8af34912f28d6fe1b8c4b9ccd530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 07:04:02 +0000

    Add package omnipy/0.12.0/omnipy-0.12.0-py3-none-any.whl

---
+++ packages/omnipy/omnipy-0.12.0-py3-none-any.whl/omnipy/modules/json/serializers.py
@@ -0,0 +1,48 @@
+from typing import IO, Type
+
+from omnipy.data.dataset import Dataset
+from omnipy.data.serializer import TarFileSerializer
+
+from .datasets import JsonBaseDataset, JsonDataset
+from .models import JsonModel
+
+
+class JsonDatasetToTarFileSerializer(TarFileSerializer):
+    """"""
+    @classmethod
+    def is_dataset_directly_supported(cls, dataset: Dataset) -> bool:
+        return isinstance(dataset, JsonBaseDataset)
+
+    @classmethod
+    def get_dataset_cls_for_new(cls) -> Type[Dataset]:
+        return JsonDataset
+
+    @classmethod
+    def get_output_file_suffix(cls) -> str:
+        return 'json'
+
+    @classmethod
+    def serialize(cls, json_dataset: JsonBaseDataset) -> bytes | memoryview:
+        def json_encode_func(json_data: JsonModel) -> bytes:
+            return json_data.json(indent=2).encode('utf8')
+
+        return cls.create_tarfile_from_dataset(json_dataset, data_encode_func=json_encode_func)
+
+    @classmethod
+    def deserialize(cls, tarfile_bytes: bytes) -> JsonDataset:
+        json_dataset = JsonDataset()
+
+        def json_decode_func(file_stream: IO[bytes]) -> str:
+            return file_stream.read().decode('utf8')
+
+        def json_dictify_object(obj_type: str, obj_val: str) -> dict[str, str]:
+            return {f'{obj_type}': f'{obj_val}'}
+
+        cls.create_dataset_from_tarfile(
+            json_dataset,
+            tarfile_bytes,
+            data_decode_func=json_decode_func,
+            dictify_object_func=json_dictify_object,
+            import_method='from_json')
+
+        return json_dataset
