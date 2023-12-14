2eaac4d71daab365fbcc9cb927cbd70baa9c4820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 03:04:59 +0000

    Add package PyFa-tool/0.0.1/pyfa_tool-0.0.1.tar.gz

---
+++ packages/PyFa-tool/pyfa_tool-0.0.1.tar.gz/pyfa_tool-0.0.1/pyfa_tool/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 23 17:20:08 2023
+
+@author: thoverga
+"""
+
+# from .lib_functions import setup_shell_command, FA_to_Xarray, get_fields
+
+
+__version__ = "0.0.1"
+
+
+from .lib_functions import get_fields, FA_to_Xarray, setup_shell_command
