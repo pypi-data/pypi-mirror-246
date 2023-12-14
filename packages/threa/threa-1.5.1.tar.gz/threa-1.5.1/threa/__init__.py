d7ec5bbd1179e1540ccfaac7289e871d6a9979ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 05:04:26 +0000

    Add package threa/1.5.1/threa-1.5.1.tar.gz

---
+++ packages/threa/threa-1.5.1.tar.gz/threa-1.5.1/threa/__init__.py
@@ -0,0 +1,13 @@
+from .collection import HasRunnables, Runnables, ThreadQueue
+from .runnable import Event, Runnable
+from .thread import HasThread, IsThread
+
+__all__ = (
+    'Event',
+    'HasRunnables',
+    'HasThread',
+    'IsThread',
+    'Runnable',
+    'Runnables',
+    'ThreadQueue',
+)
