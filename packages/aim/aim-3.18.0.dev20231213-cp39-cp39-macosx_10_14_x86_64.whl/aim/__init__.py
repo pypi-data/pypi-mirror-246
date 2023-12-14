82400ee86635d4249008fd56d60870b35af10626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 06:04:15 +0000

    Add package aim/3.18.0.dev20231213/aim-3.18.0.dev20231213-cp39-cp39-macosx_10_14_x86_64.whl

---
+++ packages/aim/aim-3.18.0.dev20231213-cp39-cp39-macosx_10_14_x86_64.whl/aim/__init__.py
@@ -0,0 +1,9 @@
+import aimrocks
+from aim.sdk import *
+from aim.ext.notebook.notebook import load_ipython_extension
+from aim.cli.manager.manager import run_process
+
+from aim.utils.deprecation import python_version_deprecation_check, sqlalchemy_version_check
+
+python_version_deprecation_check()
+sqlalchemy_version_check()
