76ee4b256a6f15832d303998e0df858edae03388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 04:04:00 +0000

    Add package CryptoLyzer/0.12.1/CryptoLyzer-0.12.1-py3-none-any.whl

---
+++ packages/CryptoLyzer/CryptoLyzer-0.12.1-py3-none-any.whl/cryptolyzer/ssh/analyzer.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from cryptoparser.ssh.version import SshProtocolVersion, SshVersion
+
+from cryptolyzer.common.analyzer import ProtocolHandlerSshBase, ProtocolHandlerSshExactVersion
+
+from cryptolyzer.ssh.all import AnalyzerAll
+from cryptolyzer.ssh.ciphers import AnalyzerCiphers
+from cryptolyzer.ssh.dhparams import AnalyzerDHParams
+from cryptolyzer.ssh.pubkeys import AnalyzerPublicKeys
+from cryptolyzer.ssh.versions import AnalyzerVersions
+
+
+class ProtocolHandlerSsh2(ProtocolHandlerSshExactVersion):
+    @classmethod
+    def get_analyzers(cls):
+        return (
+            AnalyzerCiphers,
+            AnalyzerDHParams,
+            AnalyzerPublicKeys,
+        )
+
+    @classmethod
+    def get_protocol_version(cls):
+        return SshProtocolVersion(SshVersion.SSH2)  # pragma: no cover
+
+
+class ProtocolHandlerSshVersionIndependent(ProtocolHandlerSshBase):
+    @classmethod
+    def get_analyzers(cls):
+        return (
+            AnalyzerVersions,
+            AnalyzerAll,
+        )
+
+    @classmethod
+    def get_protocol(cls):
+        return 'ssh'
