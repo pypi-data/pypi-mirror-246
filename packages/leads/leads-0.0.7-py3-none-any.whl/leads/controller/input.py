419ea29bf76cff9eb13c92609a384e5cdb3c78a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 15:04:23 +0000

    Add package leads/0.0.7/leads-0.0.7-py3-none-any.whl

---
+++ packages/leads/leads-0.0.7-py3-none-any.whl/leads/controller/input.py
@@ -0,0 +1,24 @@
+from abc import abstractmethod as _abstractmethod, ABCMeta as _ABCMeta
+from typing import TypeVar as _TypeVar, Generic as _Generic
+
+from leads.controller.device import Device
+
+T = _TypeVar("T")
+
+
+class Input(Device, _Generic[T], metaclass=_ABCMeta):
+    @_abstractmethod
+    def read(self) -> T:
+        raise NotImplementedError
+
+
+class Sensor(Input[T], metaclass=_ABCMeta):
+    pass
+
+
+class WheelSpeedSensor(Sensor[float], metaclass=_ABCMeta):
+    pass
+
+
+class Camera(Sensor[bytes], metaclass=_ABCMeta):
+    pass
