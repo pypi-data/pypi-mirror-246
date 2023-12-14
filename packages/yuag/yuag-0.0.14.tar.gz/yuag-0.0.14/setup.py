97fff023fc74c71e9c82562aa5eec8b855087d0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 04:04:08 +0000

    Add package yuag/0.0.14/yuag-0.0.14.tar.gz

---
+++ packages/yuag/yuag-0.0.14.tar.gz/yuag-0.0.14/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yuag',
+    version='0.0.14',
+    packages=find_packages(),
+    install_requires=[
+        # قائمة بالاعتماديات اللازمة لتشغيل المكتبة
+        "mouse",
+        "keyboard",
+        "requests",
+        "time",
+        "googletrans",
+        "langdetect",
+        "os",
+        "json"
+    ],
+)
