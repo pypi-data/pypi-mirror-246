624665b867f45a0e77e698cf7cd2c73a55cc4a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 17:03:53 +0000

    Add package pyjeb/0.1.1/pyjeb-0.1.1.tar.gz

---
+++ packages/pyjeb/pyjeb-0.1.1.tar.gz/pyjeb-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup (
+    name = "pyjeb",
+    long_description_content_type="text/x-rst",
+    long_description="A package to help on configuration file",
+    version = "0.1.1",
+    packages = [
+        "src"
+    ]
+)
