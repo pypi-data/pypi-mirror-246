c35290a5e8af5319c8c2a90380268a974f4c38fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 15:04:19 +0000

    Add package atools_whisper/0.8.0/atools_whisper-0.8.0.tar.gz

---
+++ packages/atools_whisper/atools_whisper-0.8.0.tar.gz/atools_whisper-0.8.0/src/atools_whisper/cli.py
@@ -0,0 +1,33 @@
+import whisper
+import sys
+import argparse
+from rich.progress import Progress, TimeElapsedColumn, BarColumn, TextColumn
+import json
+
+parser = argparse.ArgumentParser(description="Automatic Speech Recognition")
+parser.add_argument(
+    "--file-name",
+    required=True,
+    type=str,
+    help="Path or URL to the audio file to be transcribed.",
+)
+
+def main():
+    args = parser.parse_args()
+
+    print(args)
+
+    with Progress(
+        TextColumn("🤗 [progress.description]"),
+        BarColumn(style="yellow1", pulse_style="white"),
+        TimeElapsedColumn(),
+    ) as progress:
+        progress.add_task("[yellow]Transcribing...", total=None)
+        # text = whisper.transcribe(speech_file)["text"] ## only for debug
+        text = whisper.transcribe(args.file_name)
+        print(text)
+        with open("out.json", "w", encoding="utf8") as fp:
+            json.dump(text, fp, ensure_ascii=False)
+        print(
+            f"Voila!✨ Your file has been transcribed go check it out over here 👉 out.json"
+        )
