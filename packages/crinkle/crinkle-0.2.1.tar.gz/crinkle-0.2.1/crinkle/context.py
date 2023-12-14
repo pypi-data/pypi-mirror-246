01b67a3ce1839ab38dccbb0308ea89f3be7a7304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 15:05:09 +0000

    Add package crinkle/0.2.1/crinkle-0.2.1.tar.gz

---
+++ packages/crinkle/crinkle-0.2.1.tar.gz/crinkle-0.2.1/crinkle/context.py
@@ -0,0 +1,16 @@
+from typing import Generic, Optional, TypeVar
+
+from pydantic import BaseModel, ConfigDict
+
+T = TypeVar('T')
+D = TypeVar('D')
+
+
+class Context(BaseModel, Generic[T, D]):
+    state: T
+    additional_data: Optional[D]
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+    def __init__(self, initial_state: T, additional_data: Optional[D]):
+        super().__init__(state=initial_state, additional_data=additional_data)
