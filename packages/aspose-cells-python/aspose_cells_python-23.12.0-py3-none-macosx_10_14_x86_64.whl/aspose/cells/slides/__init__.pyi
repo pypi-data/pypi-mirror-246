662d0effcbc09332a61d301087eeab89a36b0340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 03:04:14 +0000

    Add package aspose-cells-python/23.12.0/aspose_cells_python-23.12.0-py3-none-macosx_10_14_x86_64.whl

---
+++ packages/aspose-cells-python/aspose_cells_python-23.12.0-py3-none-macosx_10_14_x86_64.whl/aspose/cells/slides/__init__.pyi
@@ -0,0 +1,49 @@
+from typing import List, Optional, Dict, Iterable
+import aspose.pycore
+import aspose.pydrawing
+import aspose.cells
+import aspose.cells.charts
+import aspose.cells.digitalsignatures
+import aspose.cells.drawing
+import aspose.cells.drawing.activexcontrols
+import aspose.cells.drawing.equations
+import aspose.cells.drawing.texts
+import aspose.cells.externalconnections
+import aspose.cells.json
+import aspose.cells.markup
+import aspose.cells.metadata
+import aspose.cells.numbers
+import aspose.cells.ods
+import aspose.cells.pivot
+import aspose.cells.properties
+import aspose.cells.querytables
+import aspose.cells.rendering
+import aspose.cells.rendering.pdfsecurity
+import aspose.cells.revisions
+import aspose.cells.saving
+import aspose.cells.settings
+import aspose.cells.slicers
+import aspose.cells.slides
+import aspose.cells.tables
+import aspose.cells.timelines
+import aspose.cells.utility
+import aspose.cells.vba
+import aspose.cells.webextensions
+
+class AdjustFontSizeForRowType:
+    '''Represents what kind of rows should be ajusted.'''
+    
+    @classmethod
+    @property
+    def NONE(cls) -> AdjustFontSizeForRowType:
+        '''No adjsut.'''
+        ...
+    
+    @classmethod
+    @property
+    def EMPTY_ROWS(cls) -> AdjustFontSizeForRowType:
+        '''If the row is empty, change font size to fit row height.'''
+        ...
+    
+    ...
+
