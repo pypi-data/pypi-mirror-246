6bf62ebbf6b09b1a332b9eea714dc00070c888fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 15:04:27 +0000

    Add package llm-anyscale-endpoints/0.4/llm-anyscale-endpoints-0.4.tar.gz

---
+++ packages/llm-anyscale-endpoints/llm-anyscale-endpoints-0.4.tar.gz/llm-anyscale-endpoints-0.4/llm_anyscale_endpoints.py
@@ -0,0 +1,36 @@
+import llm
+from llm.default_plugins.openai_models import Chat
+
+MODELS = (
+    "meta-llama/Llama-2-7b-chat-hf",
+    "meta-llama/Llama-2-13b-chat-hf",
+    "meta-llama/Llama-2-70b-chat-hf",
+    "codellama/CodeLlama-34b-Instruct-hf",
+    "mistralai/Mistral-7B-Instruct-v0.1",
+    "mistralai/Mixtral-8x7B-Instruct-v0.1",
+    "Open-Orca/Mistral-7B-OpenOrca",
+    "HuggingFaceH4/zephyr-7b-beta",
+)
+
+
+class AnyscaleEndpointChat(Chat):
+    needs_key = "anyscale-endpoints"
+
+    def __str__(self):
+        return "AnyscaleEndpoints: {}".format(self.model_id)
+
+
+@llm.hookimpl
+def register_models(register):
+    # Only do this if the key is set
+    key = llm.get_key("", "anyscale-endpoints", "LLM_ANYSCALE_ENDPOINTS_KEY")
+    if not key:
+        return
+    for model_id in MODELS:
+        register(
+            AnyscaleEndpointChat(
+                model_id=model_id,
+                model_name=model_id,
+                api_base="https://api.endpoints.anyscale.com/v1",
+            )
+        )
