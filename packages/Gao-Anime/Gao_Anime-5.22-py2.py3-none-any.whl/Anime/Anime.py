843c08217a1b55d31c853b8c323aad55b1fc772f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 15:05:12 +0000

    Add package Gao-Anime/5.22/Gao_Anime-5.22-py2.py3-none-any.whl

---
+++ packages/Gao-Anime/Gao_Anime-5.22-py2.py3-none-any.whl/Anime/Anime.py
@@ -0,0 +1,26 @@
+import cv2
+import paddlehub as hub
+
+# 根据自己喜好选择风格：
+## 今敏:'animegan_v2_paprika_98'
+## 新海诚:'animegan_v2_shinkai_53'
+## 宫崎骏:'animegan_v2_hayao_99'
+
+def apply_style_transfer(input_image_path, output_image_path, model_name="animegan_v2_hayao_99", use_gpu=False):
+    # 加载 PaddleHub 模型
+    model = hub.Module(name=model_name, use_gpu=use_gpu)
+
+    # 读取输入图像
+    input_image = cv2.imread(input_image_path)
+    if model_name=='animegan_v2_hayao_99':
+        print(f'正在进行宫崎骏风格的转化,是否正在使用GPU：{use_gpu}')
+    if model_name=='animegan_v2_shinkai_53':
+        print(f'正在进行新海诚风格的转化,是否正在使用GPU：{use_gpu}')
+    if model_name=='animegan_v2_paprika_98':
+        print(f'正在进行今敏风格的转化,是否正在使用GPU：{use_gpu}')
+    # 应用图像样式迁移
+    stylized_image = model.style_transfer(images=[input_image])[0]
+
+    # 保存样式化图像
+    cv2.imwrite(output_image_path, stylized_image)
+    print(f'转换完成！！！！路径：{output_image_path}')
