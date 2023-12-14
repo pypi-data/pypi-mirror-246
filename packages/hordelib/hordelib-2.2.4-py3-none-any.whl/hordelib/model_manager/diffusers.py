cd9197308de0f24ca20c3024fddf8a23738025a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 14:04:32 +0000

    Add package hordelib/2.2.4/hordelib-2.2.4-py3-none-any.whl

---
+++ packages/hordelib/hordelib-2.2.4-py3-none-any.whl/hordelib/model_manager/diffusers.py
@@ -0,0 +1,12 @@
+from hordelib.consts import MODEL_CATEGORY_NAMES
+from hordelib.model_manager.base import BaseModelManager
+
+
+class DiffusersModelManager(BaseModelManager):
+    def __init__(self, download_reference=False):
+        raise NotImplementedError("Diffusers are not yet supported")
+
+        super().__init__(
+            model_category_name=MODEL_CATEGORY_NAMES.diffusers,
+            download_reference=download_reference,
+        )
