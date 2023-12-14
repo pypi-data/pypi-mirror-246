cf389963b63a90b6005aa6f7ee4ffcf64b4801ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 04:04:09 +0000

    Add package NEMO-user-details/1.7.1/NEMO_user_details-1.7.1-py3-none-any.whl

---
+++ packages/NEMO-user-details/NEMO_user_details-1.7.1-py3-none-any.whl/NEMO_user_details/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class NEMOUserDetailsConfig(AppConfig):
+    name = "NEMO_user_details"
+    verbose_name = "NEMO User details"
+    default_auto_field = "django.db.models.AutoField"
