40e9f3faca23f697e083e775280f51f9bf528284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 06:04:27 +0000

    Add package metient/1.1.0.dev2/metient-1.1.0.dev2.tar.gz

---
+++ packages/metient/metient-1.1.0.dev2.tar.gz/metient-1.1.0.dev2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+    print(requirements)
+
+setup(name='metient', version='1.1.0.dev2', url="https://github.com/divyakoyy/metient.git", packages=find_packages(), install_requires=requirements,)
