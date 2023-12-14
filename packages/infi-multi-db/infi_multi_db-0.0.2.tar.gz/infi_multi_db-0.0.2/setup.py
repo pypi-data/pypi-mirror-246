3103e9a9a32bd6d25a69c6c5a16b9df72a6f6181
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 17:03:53 +0000

    Add package infi-multi-db/0.0.2/infi_multi_db-0.0.2.tar.gz

---
+++ packages/infi-multi-db/infi_multi_db-0.0.2.tar.gz/infi_multi_db-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="infi_multi_db",
+    packages=find_packages(),
+    version="0.0.2",
+    description="Package for using multi DBs for Infinity team",
+    author="Infinity Team",
+    install_requires=[
+        "psycopg2",
+        # Add other dependencies as needed in the future
+    ],
+    license="MIT",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
