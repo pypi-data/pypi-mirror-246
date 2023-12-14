311e65f3305df56dfdb638c0ab293157607d4bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 05:04:49 +0000

    Add package kraken-init/0.0.8/kraken-init-0.0.8.tar.gz

---
+++ packages/kraken-init/kraken-init-0.0.8.tar.gz/kraken-init-0.0.8/kraken_init/helpers/template_requirements.py
@@ -0,0 +1,17 @@
+
+
+def get_filename(name):
+
+    return f'requirements.txt'
+
+
+def get_content(name=None):
+    """
+    """
+    content = f'''
+requests
+kraken-thing
+flask
+    
+    '''
+    return content
