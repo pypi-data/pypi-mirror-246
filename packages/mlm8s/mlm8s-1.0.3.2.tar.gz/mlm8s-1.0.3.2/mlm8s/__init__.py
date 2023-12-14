39c35a93da50885703315744d523253fd322f683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 03:06:51 +0000

    Add package mlm8s/1.0.3.2/mlm8s-1.0.3.2.tar.gz

---
+++ packages/mlm8s/mlm8s-1.0.3.2.tar.gz/mlm8s-1.0.3.2/mlm8s/__init__.py
@@ -0,0 +1,20 @@
+from .helpers import *
+from .paths import *
+from .plot import *
+from .generator import *
+from .connect import *
+from .hypermodel import *
+from .training import *
+from .text import *
+
+from .chaos import *
+from .custom_metrices import *
+from .lattice import *
+from .numeric import *
+from .rounding_loss import *
+from .spiral import *
+from .voltera import *
+from .sparse import *
+from .triangle import *
+from .polynom import *
+
