8e038ef531729258e6ac3426d4fc6dfae80a4de4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 15:05:19 +0000

    Add package Gao-Anime/5.20/Gao_Anime-5.20.tar.gz

---
+++ packages/Gao-Anime/Gao_Anime-5.20.tar.gz/Gao_Anime-5.20/setup.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pipenv install twine --dev
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.  1》》》
+# 写入自己的内容 start——1
+NAME = 'Gao_Anime'  # 为项目取名，这个名字就是 pip install xxx的xxx
+DESCRIPTION = 'AI Head'
+URL = 'https://github.com/CuteCar384/Gao_Anime'
+EMAIL = '1664625666@qq.com'
+AUTHOR = 'Huang'
+REQUIRES_PYTHON = '>=3.8'
+VERSION = '5.20'  # 为项目指定目前的版本号
+# end——1
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    # 'requests', 'maya', 'records',
+]
+
+# What packages are optional?
+EXTRAS = {
+    # 'fancy feature': ['django'],
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+# 写入自己的内容 start——2
+# Where the magic happens:
+setup(
+    name="Gao_Anime",  # 和前边的保持一致
+    version=about['__version__'],
+    description="AI Head",
+    long_description=open('README.md', 'r', encoding='utf-8').read(),  # 默认是readme文件。
+    long_description_content_type='text/markdown',
+    author="Huang",
+    author_email="1664625666@qq.com",
+    python_requires=">=3.8",
+    url="https://github.com/CuteCar384/Gao_Anime",
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+
+    # end——2
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    # REQUIRED 是项目依赖的库
+    install_requires=[],
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
