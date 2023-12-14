1cb0cf3634004ed7e2ae69d23d720ba1014987b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 03:04:11 +0000

    Add package jwtauthenticatortest1/1.0.1/jwtauthenticatortest1-1.0.1-py3-none-any.whl

---
+++ packages/jwtauthenticatortest1/jwtauthenticatortest1-1.0.1-py3-none-any.whl/jwtauthenticatortest1/__init__.py
@@ -0,0 +1,8 @@
+from .jwtauthenticatortest1 import (
+    JSONWebTokenAuthenticator,
+    JSONWebTokenLocalAuthenticator,
+    JSONWebTokenLoginHandler
+)
+
+
+__all__ = ['JSONWebTokenAuthenticator', 'JSONWebTokenLocalAuthenticator', 'JSONWebTokenLoginHandler']
