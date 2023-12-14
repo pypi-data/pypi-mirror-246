4ef946e44ae17c12d46eb487592d7794e97d5f18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 16:04:12 +0000

    Add package reportzz-ai/0.1.15/reportzz_ai-0.1.15.tar.gz

---
+++ packages/reportzz-ai/reportzz_ai-0.1.15.tar.gz/reportzz_ai-0.1.15/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="reportzz_ai",
+    version="0.1.15",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    license="MIT",
+    description="Reportzz is a Python project that is designed to generate reports.",
+    long_description=open("README.md").read(),
+    install_requires=[
+        "pytest",
+        "Jinja2"
+    ],
+    url="https://github.com/zzirakadze/reportzz",
+    author="Zura Zirakadze",
+    author_email="zirakadzez@gmail.com",
+    include_package_data=True,
+    entry_points={
+        "pytest11": [
+            "name_of_plugin = data_collection.conftest",
+        ],
+    },
+)
