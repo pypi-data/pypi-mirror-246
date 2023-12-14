824951bb8c9016c0fb80c50fbfced0f7692b8d22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 06:04:18 +0000

    Add package shtils/0.0.1/shtils-0.0.1.tar.gz

---
+++ packages/shtils/shtils-0.0.1.tar.gz/shtils-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A collection of OS shell utils'
+LONG_DESCRIPTION = 'A collection of OS shell utils that allows a developer to implement often-used shell commands (like "ls" or "clear") in both Windows and Unix terms.'
+
+# Setting up
+setup(
+    name="shtils",
+    version=VERSION,
+    author="alpenstorm (Mihai Negrean)",
+    author_email="<mihai@negrean.net>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['os'],
+    keywords=['python', 'os', 'shell', 'utils'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
