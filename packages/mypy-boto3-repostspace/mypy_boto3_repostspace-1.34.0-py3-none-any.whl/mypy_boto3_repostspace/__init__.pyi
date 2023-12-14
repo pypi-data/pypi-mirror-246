2c3411819ddea2c702a13e2f3edbf44c09b871fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-14 07:04:04 +0000

    Add package mypy-boto3-repostspace/1.34.0/mypy_boto3_repostspace-1.34.0-py3-none-any.whl

---
+++ packages/mypy-boto3-repostspace/mypy_boto3_repostspace-1.34.0-py3-none-any.whl/mypy_boto3_repostspace/__init__.pyi
@@ -0,0 +1,26 @@
+"""
+Main interface for repostspace service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_repostspace import (
+        Client,
+        ListSpacesPaginator,
+        rePostPrivateClient,
+    )
+
+    session = Session()
+    client: rePostPrivateClient = session.client("repostspace")
+
+    list_spaces_paginator: ListSpacesPaginator = client.get_paginator("list_spaces")
+    ```
+"""
+
+from .client import rePostPrivateClient
+from .paginator import ListSpacesPaginator
+
+Client = rePostPrivateClient
+
+__all__ = ("Client", "ListSpacesPaginator", "rePostPrivateClient")
