9c85bb1b63e3c3073cac31508e9534771db08cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 05:04:09 +0000

    Add package pwsh-utils/0.1.6/pwsh_utils-0.1.6-py3-none-any.whl

---
+++ packages/pwsh-utils/pwsh_utils-0.1.6-py3-none-any.whl/main/main.py
@@ -0,0 +1,10 @@
+from pwsh_utils import __version__
+
+
+def run():
+    print('main.main.py')
+    print(f'version:{__version__}')
+
+
+if __name__ == '__main__':
+    run()
---
+++ packages/pwsh-utils/pwsh_utils-0.1.6-py3-none-any.whl/main/os.py
@@ -0,0 +1,9 @@
+from pwsh_utils.os import os
+
+
+def run():
+    os()
+
+
+if __name__ == '__main__':
+    os()
