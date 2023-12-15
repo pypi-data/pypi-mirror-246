d2a5e79d42b3eee14cc5ffd78b06e9029ea80363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 03:05:08 +0000

    Add package fusionX/0.0.4/fusionX-0.0.4.tar.gz

---
+++ packages/fusionX/fusionX-0.0.4.tar.gz/fusionX-0.0.4/setup.py
@@ -0,0 +1,38 @@
+
+
+from setuptools import setup, find_packages
+
+
+install_deps = ['numpy>=1.20.0', 'opencv-python>= 4.8.1.78', 'pickle5>=0.0.12']
+VERSION = 'V0.0.4'
+DESCRIPTION = 'fusionX'
+
+# Setting up
+setup(
+    name="fusionX",
+    version=VERSION,
+    author="Bar Ben David, Suman Khan",
+    author_email="<FusionX_pipeline@hotmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description="""
+    A deep learning pipeline for cell-cell fusion analysis
+
+    Authors: Bar Ben David , Suman Khan
+    Email: FusionX_pipeline@hotmail.com
+    """
+,
+ 
+    packages=find_packages(),
+    install_requires=install_deps,
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
+
