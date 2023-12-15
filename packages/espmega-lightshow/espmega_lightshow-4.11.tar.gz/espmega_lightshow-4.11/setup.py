5e38865340c1b5cb28851719f81709b26c9c7ba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 05:04:13 +0000

    Add package espmega-lightshow/4.11/espmega_lightshow-4.11.tar.gz

---
+++ packages/espmega-lightshow/espmega_lightshow-4.11.tar.gz/espmega_lightshow-4.11/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='espmega_lightshow',
+    version='4.11',
+    license='Apache 2.0',
+    author="Siwat Sirichai",
+    author_email='siwat@siwatinc.com',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={
+        '': ['*.png', '*.ico'],
+    },
+    include_package_data=True,
+    url='https://github.com/SiwatINC/espmega-lightshow',
+    keywords='light mqtt espmega',
+    install_requires=[
+          'espmega',
+          'pillow'
+      ],
+    extra_require={
+        'homeassistant_driver': ['homeassistant_api']
+    }
+)
