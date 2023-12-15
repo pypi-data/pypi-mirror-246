d1e18b94ceeb30a0f0d6a800ef2665fdea175d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 04:04:19 +0000

    Add package CveattLib/0.0.1/CveattLib-0.0.1.tar.gz

---
+++ packages/CveattLib/CveattLib-0.0.1.tar.gz/CveattLib-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+# def readme():
+#   with open('README.md', 'r') as f:
+#     return f.read()
+
+setup(
+  name='CveattLib',
+  version='0.0.1',
+  author='cveatt',
+  description='Gaussian and Binomial distributions',
+  url='https://github.com/cveatt/demo',
+  packages=['CveattLib'],
+  author_email='Anastasiia.Belik@aug-global.com',
+  
+  zip_safe=False
+)
