1da80377604f1d4a2ffc50aa00bd57d3804c5239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 15:04:28 +0000

    Add package cflare-ddns/0.1.2/cflare-ddns-0.1.2.tar.gz

---
+++ packages/cflare-ddns/cflare-ddns-0.1.2.tar.gz/cflare-ddns-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cflare-ddns",
+    version="0.1.2",
+    author="Ian Dela Cruz",
+    author_email="iandc76@gmail.com",
+    url="https://github.com/ianpogi5/cflare-ddns",
+    packages=find_packages(),
+    install_requires=["requests>=2.31.0"],
+    entry_points={
+        "console_scripts": [
+            "cflare-ddns=main:main",
+        ],
+    },
+)
