25174e51b9324b46f288b5ecbabaec7c5ff923e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 16:04:48 +0000

    Add package CRFActions/3.0.1/CRFActions-3.0.1.tar.gz

---
+++ packages/CRFActions/CRFActions-3.0.1.tar.gz/CRFActions-3.0.1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  8 14:55:19 2023
+
+@author: QianYang
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="CRFActions",
+    version="3.0.1",
+    packages=find_packages(),
+)
