5ca83b775de864bef5cd276438ea1ab6c6c36497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 05:04:18 +0000

    Add package cast-ai-se-cli/0.0.6/cast-ai-se-cli-0.0.6.tar.gz

---
+++ packages/cast-ai-se-cli/cast-ai-se-cli-0.0.6.tar.gz/cast-ai-se-cli-0.0.6/cli/constants.py
@@ -0,0 +1,9 @@
+DEMO_OFF_CRONJOB = "hibernate-pause"
+# DEMO_ON_CRONJOB = "hibernate-resume"
+
+# DEBUG LOGS
+LOG_DIR = "logs"
+LOG_FILE = "all_logs.log"
+
+CONFIG_PATH = "config.json"
+SUPPORTED_COMMANDS = ["demo", "audit"]
