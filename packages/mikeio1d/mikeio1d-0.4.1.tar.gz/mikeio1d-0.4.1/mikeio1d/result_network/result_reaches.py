2312a9d68153077f82f47e137c8c7626550d4bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 05:04:23 +0000

    Add package mikeio1d/0.4.1/mikeio1d-0.4.1.tar.gz

---
+++ packages/mikeio1d/mikeio1d-0.4.1.tar.gz/mikeio1d-0.4.1/mikeio1d/result_network/result_reaches.py
@@ -0,0 +1,74 @@
+from .result_locations import ResultLocations
+from .result_reach import ResultReach
+from .various import make_proper_variable_name
+from ..dotnet import pythonnet_implementation as impl
+
+
+class ResultReaches(ResultLocations):
+    """
+    Class for wrapping ResultData reaches.
+
+    By itself it is also a dict, which contains
+    mapping between reach name and IRes1DReach object
+    or a list of IRes1DReach objects.
+
+
+    Parameters
+    ----------
+    res1d : Res1D
+        Res1D object the reaches belong to.
+
+    Attributes
+    ----------
+    reach_label : str
+        A label, which is appended if the reach name starts
+        with a number. The value used is reach_label = 'r_'
+    result_reach_map : dict
+        Dictionary from reach name to a list of ResultReach objects.
+        This is needed, because the reach name is not necessarily unique and
+        several reaches could have the same name.
+    """
+
+    def __init__(self, res1d):
+        ResultLocations.__init__(self, res1d)
+        self.reach_label = "r_"
+        self.result_reach_map = {}
+
+        res1d.result_network.reaches = self
+        self.set_reaches()
+        self.set_quantity_collections()
+
+    def set_reaches(self):
+        """
+        Set attributes to the current ResultReaches object based
+        on the reach name.
+        """
+        for reach in self.data.Reaches:
+            reach = impl(reach)
+            result_reach = self.get_or_create_result_reach(reach)
+            result_reach_attribute_string = make_proper_variable_name(reach.Name, self.reach_label)
+            setattr(self, result_reach_attribute_string, result_reach)
+
+    def set_quantity_collections(self):
+        ResultLocations.set_quantity_collections(self)
+        for reach_name in self:
+            result_reach = self[reach_name]
+            ResultLocations.set_quantity_collections(result_reach)
+
+    def get_or_create_result_reach(self, reach):
+        """
+        Create or get already existing ResultReach object.
+        There potentially could be just a single ResultReach object,
+        for many IRes1DReach object, which have the same name.
+
+        Also update self's dict entry from reach name
+        to a ResultReach object.
+        """
+        if reach.Name in self:
+            result_reach = self[reach.Name]
+            result_reach.add_res1d_reach(reach)
+            return result_reach
+
+        result_reach = ResultReach([reach], self.res1d)
+        self[reach.Name] = result_reach
+        return result_reach
---
+++ packages/mikeio1d/mikeio1d-0.4.1.tar.gz/mikeio1d-0.4.1/setup.py
@@ -0,0 +1,69 @@
+import setuptools
+
+# Try to download NuGet packages if the download script is present
+try:
+    from scripts.nuget_retriever import NuGetRetriever
+
+    NuGetRetriever.install()
+except Exception as e:
+    print("NuGetRetriever failed with an ERROR:")
+    print(e)
+    print("NuGetRetriever did not succeed: packages are not installed.")
+
+# Try to build DHI.Mike1D.MikeIO C# project
+try:
+    from scripts.util_builder import UtilBuilder
+
+    UtilBuilder.build_and_install()
+except Exception as e:
+    print("UtilBuilder failed with an ERROR:")
+    print(e)
+    print("UtilBuilder did not succeed: packages are not installed.")
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mikeio1d",
+    version="0.4.1",
+    install_requires=[
+        'pythonnet<=2.5.2; python_version < "3.7.0"',
+        'pythonnet>=3.0.0a2; python_version >= "3.7.0"',
+        "numpy",
+        "pandas",
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "black",
+            "sphinx",
+            "sphinx-rtd-theme",
+            "matplotlib",
+            "jupyterlab",
+        ],
+        "test": ["pytest", "matplotlib", "pyarrow"],
+    },
+    author="Gediminas Kirsanskas",
+    author_email="geki@dhigroup.com",
+    description="A package that uses the DHI MIKE1D .NET libraries to read res1d and xns11 files.",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DHI/mikeio1d",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Scientific/Engineering",
+    ],
+)
