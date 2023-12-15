2d88455585a86c8173f8c8b658ae98a4310df971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 06:05:12 +0000

    Add package webint-mentions/0.0.16/webint_mentions-0.0.16.tar.gz

---
+++ packages/webint-mentions/webint_mentions-0.0.16.tar.gz/webint_mentions-0.0.16/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['webint_mentions',
+ 'webint_mentions.templates',
+ 'webint_mentions.templates.received',
+ 'webint_mentions.templates.sent']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['webint>=0.0']
+
+entry_points = \
+{'webapps': ['mentions = webint_mentions:app']}
+
+setup_kwargs = {
+    'name': 'webint-mentions',
+    'version': '0.0.16',
+    'description': 'manage mentions on your website',
+    'long_description': 'None',
+    'author': 'Angelo Gladding',
+    'author_email': 'angelo@ragt.ag',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<3.11',
+}
+
+
+setup(**setup_kwargs)
