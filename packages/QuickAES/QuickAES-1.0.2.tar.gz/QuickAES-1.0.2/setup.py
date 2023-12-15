d2fad47e3eb38fea892a55e8bc5e2b48dc3279bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 14:04:32 +0000

    Add package QuickAES/1.0.2/QuickAES-1.0.2.tar.gz

---
+++ packages/QuickAES/QuickAES-1.0.2.tar.gz/QuickAES-1.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='QuickAES',
+    version='1.0.2',
+    description='Quick Encrypt and Decrypt Text',
+    url='',
+    author='CarbonBlackz',
+    author_email='carbonblack.ns@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='encrypt',
+    packages=find_packages(),
+    install_requires=['pycryptodome']
+)
