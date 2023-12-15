e63d930adb533f6d777e8a9c4310ebb599ac11c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 16:04:39 +0000

    Add package yamlify/1.4.0/yamlify-1.4.0.tar.gz

---
+++ packages/yamlify/yamlify-1.4.0.tar.gz/yamlify-1.4.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="yamlify",
+    version="1.4.0",
+    description="YAMLify is a versatile and user-friendly tool that facilitates seamless conversion between YAML and JSON data formats. It offers a comprehensive set of features that cater to diverse needs, making it an invaluable asset for developers, data analysts, and anyone working with these data formats.",
+    long_description=open("README.md").read(),
+    url="https://github.com/aliftech/YAMLify",
+    author="Wahyu Krisna Aji",
+    author_email="wahyukrisnaaji32@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3"
+    ],
+    keywords=["yamlify", "json_to_yaml", "yaml_to_json"],
+)
