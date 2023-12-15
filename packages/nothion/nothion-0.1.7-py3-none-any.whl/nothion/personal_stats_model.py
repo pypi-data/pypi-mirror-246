bb67ccef467a197647d1763097942d128b9e9b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 15:04:07 +0000

    Add package nothion/0.1.7/nothion-0.1.7-py3-none-any.whl

---
+++ packages/nothion/nothion-0.1.7-py3-none-any.whl/nothion/personal_stats_model.py
@@ -0,0 +1,19 @@
+from attrs import define
+
+
+@define
+class PersonalStats:
+    """Represents a personal stats row in Notion.
+
+    Attributes:
+        date: The date of the stats in format YYYY-MM-DD.
+        work_time: Personal work time.
+        leisure_time: Personal leisure time.
+        focus_time: Personal focus time.
+        weight: Personal weight.
+    """
+    date: str
+    work_time: float
+    leisure_time: float
+    focus_time: float
+    weight: float = 0.0
