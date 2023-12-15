10112656250dca76a000b57dce090123cc21ba05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 16:05:40 +0000

    Add package robot-description-builder/0.0.3/robot_description_builder-0.0.3-cp39-none-win_amd64.whl

---
+++ packages/robot-description-builder/robot_description_builder-0.0.3-cp39-none-win_amd64.whl/robot_description_builder/exceptions.py
@@ -0,0 +1,9 @@
+from ._internal import (AttachChainError, GroupIDError, RebuildBranchError,
+                        XMLError)
+
+__all__ = [
+    "AttachChainError",
+    "GroupIDError",
+    "RebuildBranchError",
+    "XMLError",
+]
