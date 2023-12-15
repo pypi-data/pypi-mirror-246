03cef0293192d9a0a65fdd19d08032ce1157e250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 04:05:01 +0000

    Add package test-tele/0.0.3.7/test_tele-0.0.3.7-py3-none-any.whl

---
+++ packages/test-tele/test_tele-0.0.3.7-py3-none-any.whl/test_tele/const.py
@@ -0,0 +1,20 @@
+"""Declare all global constants."""
+
+COMMANDS = {
+    "start": "Restart bot",
+    "get": "Forward any posts from public channel",
+    "help": "Learn usage",
+}
+
+REGISTER_COMMANDS = True
+
+KEEP_LAST_MANY = 10000
+
+CONFIG_FILE_NAME = "tgcf.config.json"
+CONFIG_ENV_VAR_NAME = "TGCF_CONFIG"
+
+MONGO_DB_NAME = "tgcf-config"
+MONGO_COL_NAME = "tgcf-instance-0"
+
+BOT_CONFIG_FILE_NAME = "bot.config.json"
+BOT_CONFIG_ENV_VAR_NAME = "TGCF_BOT_CONFIG"
