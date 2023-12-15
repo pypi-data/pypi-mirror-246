5ba85e719079e35dba41c0df579900fda188c943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 06:05:09 +0000

    Add package cronjob-scanner/0.5/cronjob_scanner-0.5.tar.gz

---
+++ packages/cronjob-scanner/cronjob_scanner-0.5.tar.gz/cronjob_scanner-0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cronjob_scanner',
+    version='0.5',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'cronjob_scanner=cronjob_scanner.cron_collector:main'
+        ]
+    }
+)
