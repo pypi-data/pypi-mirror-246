a91b3298b06bfb26dbd669fd3c867186d5ff9b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 05:04:18 +0000

    Add package deephaven-plugin-plotly-express/0.2.0/deephaven_plugin_plotly_express-0.2.0-py3-none-any.whl

---
+++ packages/deephaven-plugin-plotly-express/deephaven_plugin_plotly_express-0.2.0-py3-none-any.whl/js/plotly-express/dist/PlotlyExpressChartPanel.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+import { type WidgetPanelProps } from '@deephaven/plugin';
+export declare function PlotlyExpressChartPanel(props: WidgetPanelProps): JSX.Element;
+export default PlotlyExpressChartPanel;
+//# sourceMappingURL=PlotlyExpressChartPanel.d.ts.map
