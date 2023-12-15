a53f2bcd2e5c9c14c356f626d1f48a79819e5156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 14:04:34 +0000

    Add package mockTestAPI/0.0.1/mockTestAPI-0.0.1.tar.gz

---
+++ packages/mockTestAPI/mockTestAPI-0.0.1.tar.gz/mockTestAPI-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open('README.md', 'r') as arq: 
+    readme = arq.read()
+
+setup(
+    name='mockTestAPI',
+    version='0.0.1',
+    license='MIT License',
+    author='Victor Augusto do Carmo',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='Victoraugustodocarmo32@gmail.com',
+    keywords='Dados mocks',
+    description='Geração de dados falsos com Json',
+    packages=['mockTest'],  
+    install_requires=[
+        'pydantic',
+        'faker',
+    ],
+)
