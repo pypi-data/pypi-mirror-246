6ebadc4951423c652f55afd346e2a106b18ba984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 16:05:13 +0000

    Add package shubhamsareliya/0.1.0/shubhamsareliya-0.1.0.tar.gz

---
+++ packages/shubhamsareliya/shubhamsareliya-0.1.0.tar.gz/shubhamsareliya-0.1.0/README.md
@@ -0,0 +1,3 @@
+Hello
+2124: Shubham Sareliya
+This is a Testing / Training Project
---
+++ packages/shubhamsareliya/shubhamsareliya-0.1.0.tar.gz/shubhamsareliya-0.1.0/shubhamsareliya.py
@@ -0,0 +1,17 @@
+"""
+This module contains a function that adds two numbers.
+"""
+
+
+def add_numbers(a, b):
+    """
+    Adds two numbers.
+
+    Parameters:
+        a (int): The first number.
+        b (int): The second number.
+
+    Returns:
+        int: The sum of the two numbers.
+    """
+    return a + b
