17953378f88f7d7efdf89bd5ce57e510b6340d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 06:05:41 +0000

    Add package edc-export/0.3.41/edc-export-0.3.41.tar.gz

---
+++ packages/edc-export/edc-export-0.3.41.tar.gz/edc-export-0.3.41/edc_export/views/home_view.py
@@ -0,0 +1,14 @@
+from django.views.generic import TemplateView
+from edc_dashboard.utils import get_bootstrap_version
+from edc_dashboard.view_mixins import EdcViewMixin
+from edc_navbar import NavbarViewMixin
+
+
+class HomeView(EdcViewMixin, NavbarViewMixin, TemplateView):
+    template_name = f"edc_export/bootstrap{get_bootstrap_version()}/home.html"
+    navbar_name = "edc_export"
+    navbar_selected_item = "export"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
