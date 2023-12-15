adf2a0ef450122a2730ff8bf9b10f3c8cd9acc10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 04:05:11 +0000

    Add package FGL/0.0.1rc21.post1/fgl-0.0.1rc21.post1.tar.gz

---
+++ packages/FGL/fgl-0.0.1rc21.post1.tar.gz/fgl-0.0.1rc21.post1/src/FGL/matching/__src/wrappers.py
@@ -0,0 +1,18 @@
+from .. import __lib
+import networkx
+import scipy
+# This file contains python wrappers for our C functions.
+# The whole purpose of that is to make it easier for
+# auto-completions to know our function definitions.
+
+# __lib is the compiled library containing our c functions.
+
+def max_cardinality_matching(rows, cols, matching):
+    return __lib.match_wrapper(rows, cols, matching)
+
+def max_cardinality_matching(G: networkx.Graph):
+    sparse = networkx.to_scipy_sparse_array(G,format="csr")
+    rows=sparse.indptr.tolist()
+    cols=sparse.indices.tolist()
+    matching = []
+    return __lib.match_wrapper(rows, cols, matching)
