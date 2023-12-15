b5d63e97086676920c457cac7910f37bf35d9d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 04:04:19 +0000

    Add package dipy/1.8.0/dipy-1.8.0-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/dipy/dipy-1.8.0-cp39-cp39-macosx_11_0_arm64.whl/dipy/version.py
@@ -0,0 +1,12 @@
+# THIS FILE IS GENERATED DURING THE DIPY BUILD
+# See tools/version_utils.py for details
+
+short_version = '1.8.0'
+version = '1.8.0'
+full_version = '1.8.0'
+git_revision = '493a9d9'
+commit_count = '1239'
+release = True
+
+if not release:
+    version = full_version
