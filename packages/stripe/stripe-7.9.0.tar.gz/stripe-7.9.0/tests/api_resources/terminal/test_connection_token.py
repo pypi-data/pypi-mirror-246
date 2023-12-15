6bbb872e65e77fa3751840e917f50cbf07cb34da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 14:04:04 +0000

    Add package stripe/7.9.0/stripe-7.9.0.tar.gz

---
+++ packages/stripe/stripe-7.9.0.tar.gz/stripe-7.9.0/tests/api_resources/terminal/test_connection_token.py
@@ -0,0 +1,11 @@
+import stripe
+
+
+TEST_RESOURCE_ID = "rdr_123"
+
+
+class TestConnectionToken(object):
+    def test_is_creatable(self, request_mock):
+        resource = stripe.terminal.ConnectionToken.create()
+        request_mock.assert_requested("post", "/v1/terminal/connection_tokens")
+        assert isinstance(resource, stripe.terminal.ConnectionToken)
---
+++ packages/stripe/stripe-7.9.0.tar.gz/stripe-7.9.0/tests/api_resources/test_setup_attempt.py
@@ -0,0 +1,9 @@
+import stripe
+
+
+class TestSetupAttempt(object):
+    def test_is_listable(self, request_mock):
+        resources = stripe.SetupAttempt.list(setup_intent="seti_123")
+        request_mock.assert_requested("get", "/v1/setup_attempts")
+        assert isinstance(resources.data, list)
+        assert isinstance(resources.data[0], stripe.SetupAttempt)
