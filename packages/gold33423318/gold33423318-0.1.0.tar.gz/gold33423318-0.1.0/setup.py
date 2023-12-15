6c9f11cb6bb2591d876f8503bc2785ba3a8b8ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 16:04:39 +0000

    Add package gold33423318/0.1.0/gold33423318-0.1.0.tar.gz

---
+++ packages/gold33423318/gold33423318-0.1.0.tar.gz/gold33423318-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='gold33423318',
+    version='0.1.0',
+    py_modules=['gold33423318'],
+    author='Nur Ridwan',
+    author_email='nurridwan270305@email.com',
+    description='UAS Praktek DASPRO',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/nrrdwan/33423318' ,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
