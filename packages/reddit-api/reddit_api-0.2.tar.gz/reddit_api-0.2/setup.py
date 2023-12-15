96c07e892566069c88df8625fa9234333c7d7976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 04:04:52 +0000

    Add package reddit-api/0.2/reddit_api-0.2.tar.gz

---
+++ packages/reddit-api/reddit_api-0.2.tar.gz/reddit_api-0.2/setup.py
@@ -0,0 +1,41 @@
+"""
+reddit-api
+"""
+
+from setuptools import setup, find_packages
+
+LONG_DESCRIPTION = 'reddit-api is a open source package that manipulate RedditAPI output and use it in ETL tasks'
+
+
+setup(
+    name='reddit_api',
+    version='0.2',
+    packages=find_packages(where='src'),
+    description='Reddit API for Airflow',
+    long_description=LONG_DESCRIPTION,
+    author='Unknwon',
+    install_requires=[
+        'praw',
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-cov',
+            'flake8',
+            'black',
+            'isort',
+            'mypy',
+            'pylint',
+            'pre-commit',
+            'tox',
+            'twine',
+            'wheel',
+        ]
+    },
+    package_dir={"": "src"},
+    entry_points={
+        'console_scripts': [
+            'airflow_reddit=airflow_reddit.cli:main',
+        ],
+    },
+)
