64cbc6ed412fe201da0e8da27f1b6f349750a7e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 06:05:29 +0000

    Add package luis-v-subtitler/0.0.2/luis_v_subtitler-0.0.2-py3-none-any.whl

---
+++ packages/luis-v-subtitler/luis_v_subtitler-0.0.2-py3-none-any.whl/luis_v_subtitler/utils.py
@@ -0,0 +1,19 @@
+"""Utilities for the Luis V subtitler package"""
+
+
+def basic_test(
+    argument_1: int = 1,
+    argument_2: int = 2,
+):
+    """A simple test function
+
+    Parameters
+    ----------
+    argument_1 : int, optional
+        _description_, by default 1
+    argument_2 : int, optional
+        _description_, by default 2
+    """
+
+    print(argument_1, argument_2)
+    return
