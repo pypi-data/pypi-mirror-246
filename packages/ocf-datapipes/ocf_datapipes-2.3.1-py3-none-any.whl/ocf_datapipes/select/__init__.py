9028164f451c8233d9302c8eba60169d6e412dd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 04:04:14 +0000

    Add package ocf-datapipes/2.3.1/ocf_datapipes-2.3.1-py3-none-any.whl

---
+++ packages/ocf-datapipes/ocf_datapipes-2.3.1-py3-none-any.whl/ocf_datapipes/select/__init__.py
@@ -0,0 +1,28 @@
+"""Selection datapipes"""
+from .drop_national_gsp import DropGSPIterDataPipe as DropGSP
+from .drop_pv_sys_generating_overnight import (
+    DropPvSysGeneratingOvernightIterDataPipe as DropPvSysGeneratingOvernight,
+)
+from .drop_pv_sys_with_only_nan_in_a_day import (
+    DropPVSystemsWithOnlyNanInADayIterDataPipe as DropPVSystemsWithOnlyNanInADay,
+)
+from .location_picker import LocationPickerIterDataPipe as LocationPicker
+from .number_of_location import NumberOfLocationsrIterDataPipe as NumberOfLocations
+from .offset_t0 import OffsetT0IterDataPipe as OffsetT0
+from .select_channels import SelectChannelsIterDataPipe as SelectChannels
+from .select_id import SelectIDIterDataPipe as SelectID
+from .select_live_t0_time import SelectLiveT0TimeIterDataPipe as SelectLiveT0Time
+from .select_live_time_slice import SelectLiveTimeSliceIterDataPipe as SelectLiveTimeSlice
+from .select_loc_and_t0 import LocationT0PickerIterDataPipe as LocationT0Picker
+from .select_overlapping_time_slices import (
+    SelectOverlappingTimeSliceIterDataPipe as SelectOverlappingTimeSlice,
+)
+from .select_spatial_slice import SelectSpatialSliceMetersIterDataPipe as SelectSpatialSliceMeters
+from .select_spatial_slice import SelectSpatialSlicePixelsIterDataPipe as SelectSpatialSlicePixels
+from .select_t0_time import SelectT0TimeIterDataPipe as SelectT0Time
+from .select_time_periods import SelectTimePeriodsIterDataPipe as SelectTimePeriods
+from .select_time_slice import SelectTimeSliceIterDataPipe as SelectTimeSlice
+from .select_train_test import SelectTrainTestTimePeriodsIterDataPipe as SelectTrainTestTimePeriod
+from .trim_dates_with_insufficent_data import (
+    TrimDatesWithInsufficentDataIterDataPipe as TrimDatesWithInsufficentData,
+)
