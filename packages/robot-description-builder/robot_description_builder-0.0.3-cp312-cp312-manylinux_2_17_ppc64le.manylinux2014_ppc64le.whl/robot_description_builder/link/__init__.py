f8fba2c1066c677de3cd2bc5f5e59ac30c803c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 16:05:36 +0000

    Add package robot-description-builder/0.0.3/robot_description_builder-0.0.3-cp312-cp312-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl

---
+++ packages/robot-description-builder/robot_description_builder-0.0.3-cp312-cp312-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl/robot_description_builder/link/__init__.py
@@ -0,0 +1,12 @@
+from .._internal import Inertial, Link, LinkBuilder, LinkBuilderChain
+from . import collision, geometry, visual
+
+__all__ = [
+    "Inertial",
+    "Link",
+    "LinkBuilder",
+    "LinkBuilderChain",
+    "collision",
+    "geometry",
+    "visual",
+]
