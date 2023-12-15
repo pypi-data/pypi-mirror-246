de868727132c9608db4722afe9532954347d5452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 04:04:59 +0000

    Add package MVDataProcessing/0.1.1/MVDataProcessing-0.1.1.tar.gz

---
+++ packages/MVDataProcessing/MVDataProcessing-0.1.1.tar.gz/MVDataProcessing-0.1.1/tests/test_lib.py
@@ -0,0 +1,23 @@
+"""
+Created on Mon Apr 18 06:45:44 2022
+
+@author: Bacalhau
+
+"""
+
+
+import pandas
+import numpy
+from datetime import datetime
+import matplotlib.pyplot
+import time
+import FinishedFunctions as f_remove
+
+
+
+if __name__ == "__main__":
+    
+
+    f_remove.ShowExampleSimpleProcess(plot=False)
+    #f_remove.ShowExampleNSSCProcess(plot=False)
+    
