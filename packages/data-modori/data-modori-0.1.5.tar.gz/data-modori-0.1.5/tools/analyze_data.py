5a5ad2ca0c2bbff0c6091811826dd7dff39b5064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 05:04:15 +0000

    Add package data-modori/0.1.5/data-modori-0.1.5.tar.gz

---
+++ packages/data-modori/data-modori-0.1.5.tar.gz/data-modori-0.1.5/tools/analyze_data.py
@@ -0,0 +1,12 @@
+from loguru import logger
+
+from data_modori.core import Analyser
+
+@logger.catch
+def main():
+    analyser = Analyser()
+    analyser.run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/data-modori/data-modori-0.1.5.tar.gz/data-modori-0.1.5/tools/process_data.py
@@ -0,0 +1,18 @@
+from loguru import logger
+
+from data_modori.config import init_configs
+from data_modori.core import Executor
+
+@logger.catch
+def main():
+    cfg = init_configs()
+    if cfg.executor_type == 'default':
+        executor = Executor(cfg)
+    elif cfg.executor_type == 'ray':
+        from data_modori.core.ray_executor import RayExecutor
+        executor = RayExecutor(cfg)
+    executor.run()
+
+
+if __name__ == '__main__':
+    main()
