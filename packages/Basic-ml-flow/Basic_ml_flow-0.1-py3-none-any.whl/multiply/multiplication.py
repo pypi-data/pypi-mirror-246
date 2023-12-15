c1ca03187fc950b631e68122e18e7d0397834e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 04:04:24 +0000

    Add package Basic-ml-flow/0.1/Basic_ml_flow-0.1-py3-none-any.whl

---
+++ packages/Basic-ml-flow/Basic_ml_flow-0.1-py3-none-any.whl/multiply/multiplication.py
@@ -0,0 +1,41 @@
+import mlflow
+from sklearn import datasets
+from  sklearn.model_selection import  train_test_split
+class Multiplication:
+    """
+    Instantiate a multiplication operation.
+    Numbers will be multiplied by the given multiplier.
+
+    :param multiplier: The multiplier.
+    :type multiplier: int
+    """
+
+    def __init__(self, multiplier):
+        self.multiplier = multiplier
+
+    def multiply(self, number):
+        """
+        Multiply a given number by the multiplier.
+
+        :param number: The number to multiply.
+        :type number: int
+
+        :return: The result of the multiplication.
+        :rtype: int
+        """
+
+        mlflow.start_run()
+        mlflow.log_param("my", "param")
+        mlflow.log_metric("score", 100)
+        #dataset = datasets.load_iris()
+        #print(dataset)
+        mlflow.end_run()
+
+        return number * self.multiplier
+
+
+# Instantiate a Multiplication object
+multiplication = Multiplication(2)
+
+# Call the multiply method
+print(multiplication.multiply(5))
