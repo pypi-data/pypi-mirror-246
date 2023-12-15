7163e77011617971359fa41cb7d377dcdbaf70bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 15:04:11 +0000

    Add package sample-test-package/1.5.0/sample_test_package-1.5.0.tar.gz

---
+++ packages/sample-test-package/sample_test_package-1.5.0.tar.gz/sample_test_package-1.5.0/multidb_admin/router.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class MultiDBRouter:
+    keep_models_in_default = ["Session", "ContentType", "User", "AdminLog"]
+
+    def get_db(self, model, **kwargs):
+
+        if hasattr(self, "request"):
+            if model.__name__ not in self.keep_models_in_default:
+                return self.request.session.get("DB", "default")
+
+        return "default"
+
+
+    def db_for_read(self, model, **hints):
+        return self.get_db(model, **hints)
+
+    def db_for_write(self, model, **hints):
+        return self.get_db(model)
+
+    def allow_relation(self, *args, **kwargs):
+        return True
+
+    def allow_syncdb(self, *args, **kwargs):
+        return True
+
+    def allow_migrate(self, *args, **kwargs):
+        return True
