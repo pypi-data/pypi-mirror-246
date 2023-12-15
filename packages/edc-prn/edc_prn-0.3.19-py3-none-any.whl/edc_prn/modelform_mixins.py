0e1b524bc7df8c63ee98b9ae78d523fb03267a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 15:04:04 +0000

    Add package edc-prn/0.3.19/edc_prn-0.3.19-py3-none-any.whl

---
+++ packages/edc-prn/edc_prn-0.3.19-py3-none-any.whl/edc_prn/modelform_mixins.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from edc_crf.crf_form_validator_mixins import BaseFormValidatorMixin
+
+
+class PrnFormValidatorMixin(BaseFormValidatorMixin):
+    """to be declared with PRN FormValidators."""
+
+    pass
