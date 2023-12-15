aaaa8e455f834b8cb429b81f76ec4e9d897800fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 03:05:08 +0000

    Add package pibe/0.20/pibe-0.20.tar.gz

---
+++ packages/pibe/pibe-0.20.tar.gz/pibe-0.20/pibe_ext/validator.py
@@ -0,0 +1,37 @@
+import funcy as fn
+from webob import exc
+from json.decoder import JSONDecodeError
+import cerberus
+from box import Box
+
+from .http import _raise_exc, not_acceptable
+
+__all__ = ("validate", )
+
+
+@fn.decorator
+def validate(
+    call,
+    schema,
+    data_source="json_body",
+    exception_class=exc.HTTPBadRequest,
+    **kwargs):
+
+    if data_source == "json_body":
+        try:
+            data = dict(call.req.json)
+        except JSONDecodeError:
+            not_acceptable(error="Invalid JSON Request")
+
+    elif data_source == "params":
+        data = dict(call.req.params)
+    else:
+        raise KeyError("unknown data source")
+
+    v = cerberus.Validator(schema, **kwargs)
+    if not v.validate(data):
+        _raise_exc(exception_class, errors=v.errors)
+
+    call.req.data = Box(v.document)
+
+    return call()
