57857edc16a59905316bf5385f6556bad7aa8609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 06:05:29 +0000

    Add package RockyRoad/0.0.341/RockyRoad-0.0.341-py3-none-any.whl

---
+++ packages/RockyRoad/RockyRoad-0.0.341-py3-none-any.whl/rockyroad/modules/tco_calculator.py
@@ -0,0 +1,89 @@
+from .module_imports import *
+
+
+@headers({"Ocp-Apim-Subscription-Key": key})
+class _TCO(Consumer):
+    """Inteface to TCO resource for the RockyRoad API."""
+
+    def __init__(self, Resource, *args, **kw):
+        self._base_url = Resource._base_url
+        super().__init__(base_url=Resource._base_url, *args, **kw)
+
+    def maintenance(self):
+        return self._Maintenance(self)
+
+    @headers({"Ocp-Apim-Subscription-Key": key})
+    class _Maintenance(Consumer):
+        """Inteface to TCO resource for the RockyRoad API."""
+
+        def __init__(self, Resource, *args, **kw):
+            self._base_url = Resource._base_url
+            super().__init__(base_url=Resource._base_url, *args, **kw)
+
+        def parts(self):
+            return self._Parts(self)
+
+        @returns.json
+        @http_get("calculators/tco/maintenance")
+        def list(
+            self,
+        ):
+            """This call will return list of TCO Maintenances."""
+
+        @returns.json
+        @http_get("calculators/tco/maintenance/{uid}")
+        def get(self, uid: str):
+            """This call will return the specified TCO Maintenance."""
+
+        @delete("calculators/tco/maintenance/{uid}")
+        def delete(self, uid: str):
+            """This call will delete the TCO Maintenance."""
+
+        @returns.json
+        @json
+        @post("calculators/tco/maintenance")
+        def insert(self, tco_part: Body):
+            """This call will create the TCO Maintenance."""
+
+        @json
+        @patch("calculators/tco/maintenance/{uid}")
+        def update(self, uid: str, tco_part: Body):
+            """This call will update the TCO Maintenance."""
+
+        @headers({"Ocp-Apim-Subscription-Key": key})
+        class _Parts(Consumer):
+            """Inteface to TCO resource for the RockyRoad API."""
+
+            def __init__(self, Resource, *args, **kw):
+                self._base_url = Resource._base_url
+                super().__init__(base_url=Resource._base_url, *args, **kw)
+
+            def parts(self):
+                return self.TCO(self)
+
+            @returns.json
+            @http_get("calculators/tco/maintenance/parts")
+            def list(
+                self,
+            ):
+                """This call will return list of TCO Parts."""
+
+            @returns.json
+            @http_get("calculators/tco/maintenance/parts/{uid}")
+            def get(self, uid: str):
+                """This call will return the specified TCO Part."""
+
+            @delete("calculators/tco/maintenance/parts/{uid}")
+            def delete(self, uid: str):
+                """This call will delete the TCO Part."""
+
+            @returns.json
+            @json
+            @post("calculators/tco/maintenance/parts")
+            def insert(self, tco_part: Body):
+                """This call will create the TCO Part."""
+
+            @json
+            @patch("calculators/tco/maintenance/parts/{uid}")
+            def update(self, uid: str, tco_part: Body):
+                """This call will update the TCO Part."""
