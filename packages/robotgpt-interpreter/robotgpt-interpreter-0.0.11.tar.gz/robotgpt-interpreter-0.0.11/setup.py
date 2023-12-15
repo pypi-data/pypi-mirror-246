7d523577f80f846d994406141991d2af2a038b5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 15:04:00 +0000

    Add package robotgpt-interpreter/0.0.11/robotgpt-interpreter-0.0.11.tar.gz

---
+++ packages/robotgpt-interpreter/robotgpt-interpreter-0.0.11.tar.gz/robotgpt-interpreter-0.0.11/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import setuptools
+from setuptools import setup,find_packages
+with open("README1.md", "r", encoding="UTF-8") as fh:
+    long_description = fh.read()
+setup(
+    name='robotgpt-interpreter',
+    version='0.0.11',
+    author='blaze.zhang',
+    author_email='blaze.zhang@cloudminds.com',
+    url='https://src.cloudminds.com/ai-api/robotgpt-interpreter',
+    description=u'robotgpt-interpreter',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    data_files=[
+    ('bin',  ['bin/interpreter'])
+    ],
+    scripts=['bin/interpreter']
+)
