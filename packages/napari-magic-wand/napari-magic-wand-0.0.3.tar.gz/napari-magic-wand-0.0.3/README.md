0304c709ff783bc2209174d0924be4dac373ac74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 04:04:51 +0000

    Add package napari-magic-wand/0.0.3/napari-magic-wand-0.0.3.tar.gz

---
+++ packages/napari-magic-wand/napari-magic-wand-0.0.3.tar.gz/napari-magic-wand-0.0.3/README.md
@@ -0,0 +1,72 @@
+# 🪄 napari-magic-wand
+
+Object annotation in Napari using *magic wands* (shortest path algorithms). This plugin supports annotation in 2D (grayscale and RGB), 2D+t (grayscale, frame by frame) and 3D images (slice by slice). It can be used to annotate **paths** or the contour of solid objects.
+
+The plugin provides two annotation functions:
+
+- The **Magic wand** tool is based on the [PyIFT](https://github.com/PyIFT/pyift) library. Use it to trace annotations that follow the **intensity gradients** in the image.
+- The **Brightest path** tool is based on the [Brightest Path Lib](https://github.com/mapmanager/brightest-path-lib) library. Use it to trace annotations that follow **the brightest (or darkest) path** between two points.
+
+<p align="center">
+    <img src="https://github.com/MalloryWittwer/napari-magic-wand/blob/main/assets/screenshot.gif" height="400">
+</p>
+
+**Related plugins**
+
+Take a look at these related plugins that offer similar functionality and slightly different user interactions.
+
+- [grabber-ift](https://www.napari-hub.org/plugins/grabber-ift) which is based on *pyift*.
+- [napari-tracing](https://github.com/mapmanager/napari-tracing) which is based on *brightest-path-lib*.
+
+## Installation
+
+You can install `napari-magic-wand` via [pip]:
+
+    pip install napari-magic-wand
+
+## Usage
+
+- Select the tool of your choice from the `Plugins` menu of Napari.
+- Open an image to annotate (2D, 2D+t, or 3D).
+- Click on the button "Start live wire". A new `Labels` layer *Live wire (current edit)* should appear.
+- Click on the image to annotate paths interactively.
+- Double-click to confirm an annotation and move to the next.
+
+**Options and parameters**
+- *Close and fill objects*: You can fill (or not) the inside of the annotated object. Do not tick this option if you are annotating **paths** (e.g. filament-like strucutres).
+- *Auto-increment label index*: Tick this option to increment the label index every time a new object is completed (e.g. if you are annotating multiple objects).
+- *Sigma*: Higher values of sigma increase the "stickiness" of the object boundaries to the magic wand.
+- *Black ridges*: Tick this checkbox if you are annotating dark paths on a bright background.
+
+## Contributing
+
+Contributions are very welcome. Please get in touch if you'd like to be involved in improving or extending the package.
+
+## License
+
+Distributed under the terms of the [BSD-3] license,
+"napari-magic-wand" is free and open source software
+
+## Issues
+
+If you encounter any problems, please file an issue along with a detailed description.
+
+----------------------------------
+
+This [napari] plugin was generated with [Cookiecutter] using [@napari]'s [cookiecutter-napari-plugin] template.
+
+[napari]: https://github.com/napari/napari
+[Cookiecutter]: https://github.com/audreyr/cookiecutter
+[@napari]: https://github.com/napari
+[MIT]: http://opensource.org/licenses/MIT
+[BSD-3]: http://opensource.org/licenses/BSD-3-Clause
+[GNU GPL v3.0]: http://www.gnu.org/licenses/gpl-3.0.txt
+[GNU LGPL v3.0]: http://www.gnu.org/licenses/lgpl-3.0.txt
+[Apache Software License 2.0]: http://www.apache.org/licenses/LICENSE-2.0
+[Mozilla Public License 2.0]: https://www.mozilla.org/media/MPL/2.0/index.txt
+[cookiecutter-napari-plugin]: https://github.com/napari/cookiecutter-napari-plugin
+
+[napari]: https://github.com/napari/napari
+[tox]: https://tox.readthedocs.io/en/latest/
+[pip]: https://pypi.org/project/pip/
+[PyPI]: https://pypi.org/
---
+++ packages/napari-magic-wand/napari-magic-wand-0.0.3.tar.gz/napari-magic-wand-0.0.3/src/napari_magic_wand/_widget_brightest_path_lib.py
@@ -0,0 +1,274 @@
+import napari
+import napari.layers
+from PyQt5.QtCore import Qt
+from qtpy.QtWidgets import (
+    QWidget, 
+    QComboBox, 
+    QSizePolicy, 
+    QLabel, 
+    QGridLayout, 
+    QPushButton,
+    QCheckBox
+)
+
+from brightest_path_lib.algorithm import AStarSearch
+
+import numpy as np
+
+
+class BrightestPathWidget(QWidget):
+    def __init__(self, napari_viewer):
+        super().__init__()
+
+        self.viewer = napari_viewer
+        self.viewer.text_overlay.text = 'Double-click to confirm the selection.'
+
+        # Initial state
+        self.image_layer = None
+        self.labels_layer = None
+        self.result_layer = None
+        self.is_active = False
+        self.current_start_point = None
+
+        # Layout
+        grid_layout = QGridLayout()
+        grid_layout.setAlignment(Qt.AlignTop)
+        self.setLayout(grid_layout)
+
+        # Image
+        self.cb_image = QComboBox()
+        self.cb_image.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        grid_layout.addWidget(QLabel("Image", self), 0, 0)
+        grid_layout.addWidget(self.cb_image, 0, 1)
+
+        # Result
+        self.cb_result = QComboBox()
+        self.cb_result.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        grid_layout.addWidget(QLabel("Labels (result)", self), 1, 0)
+        grid_layout.addWidget(self.cb_result, 1, 1)
+
+        # Increment label index
+        grid_layout.addWidget(QLabel("Auto-increment label index", self), 2, 0)
+        self.check_label_increment = QCheckBox()
+        self.check_label_increment.setChecked(False)
+        grid_layout.addWidget(self.check_label_increment, 2, 1)
+
+        # Black ridges
+        grid_layout.addWidget(QLabel("Black ridges", self), 3, 0)
+        self.check_black_ridges = QCheckBox()
+        self.check_black_ridges.setChecked(False)
+        grid_layout.addWidget(self.check_black_ridges, 3, 1)
+
+        # Push button
+        self.btn = QPushButton('Start live wire', self)
+        self.btn.clicked.connect(self._on_button_push)
+        grid_layout.addWidget(self.btn, 4, 0, 1, 2)
+
+        # Setup layer callbacks
+        self.viewer.layers.events.inserted.connect(
+            lambda e: e.value.events.name.connect(self._on_layer_change)
+        )
+        self.viewer.layers.events.inserted.connect(self._on_layer_change)
+        self.viewer.layers.events.removed.connect(self._on_layer_change)
+        self.viewer.layers.events.removed.connect(self._on_image_layer_removed)
+        self._on_layer_change(None)
+
+        # import skimage.data; self.viewer.add_image(skimage.data.coins())
+        # import skimage.data; self.viewer.add_image(skimage.data.brain())
+
+    @property
+    def image_data(self):
+        """The image data, adjusted to handle the RGB case."""
+        if self.image_layer is None:
+            return
+        
+        if self.image_layer.data is None:
+            return
+        
+        if self.image_layer.data.ndim == 2:
+            return self.image_layer.data
+        
+        elif self.image_layer.data.ndim == 3:
+            if self.image_layer.rgb is True:
+                return np.mean(self.image_layer.data, axis=2)
+            else:
+                return self.image_layer.data
+    
+    @property
+    def is_in_3d_view(self):
+        return self.viewer.dims.ndisplay == 3
+
+    @property
+    def dims_displayed(self):
+        return list(self.viewer.dims.displayed)
+    
+    @property
+    def ndim(self):        
+        if self.image_data is None:
+            return
+        
+        if self.image_layer.rgb is True:
+            return 2
+        else:
+            return self.image_layer.data.ndim
+    
+    @property
+    def axes(self):
+        if self.is_in_3d_view:
+            return
+        
+        axes = self.dims_displayed
+        if self.ndim == 3:
+            axes.insert(0, list(set([0, 1, 2]) - set(self.dims_displayed))[0])
+        
+        return axes
+    
+    @property
+    def current_step(self):
+        """Current step, adjusted based on the layer transpose state."""
+        return np.array(self.viewer.dims.current_step)[self.axes][0]
+    
+    @property
+    def selected_label(self):
+        if self.labels_layer is None:
+            return
+        
+        return self.labels_layer.selected_label
+    
+    @selected_label.setter
+    def selected_label(self, selected_label):
+        if self.labels_layer is None:
+            return
+        
+        self.labels_layer.selected_label = selected_label
+    
+    @property
+    def image_data_slice(self):
+        """The currently visible 2D slice if the image is 3D, otherwise the image itself (if 2D)."""      
+        if self.image_data is None:
+            return
+        
+        if self.ndim == 2:
+            return self.image_data
+        
+        elif self.ndim == 3:
+            return self.image_data.transpose(self.axes)[self.current_step]
+    
+    def _on_image_layer_removed(self, e):
+        if self.image_layer == e.value:
+            self.is_active = False
+            self._handle_inactive()
+
+    def _on_layer_change(self, e):
+        self.cb_image.clear()
+        for x in self.viewer.layers:
+            if isinstance(x, napari.layers.Image):
+                if x.data.ndim in [2, 3]:
+                    self.cb_image.addItem(x.name, x.data)
+
+        self.cb_result.clear()
+        for x in self.viewer.layers:
+            if isinstance(x, napari.layers.Labels):
+                self.cb_result.addItem(x.name, x.data)
+
+    def _on_button_push(self):
+        if (self.is_in_3d_view) | (self.cb_image.currentText() == ''):
+            return
+        
+        self.image_layer = self.viewer.layers[self.cb_image.currentText()]
+        
+        if self.cb_result.currentText() == '':
+            self.result_layer = self.viewer.add_labels(np.zeros_like(self.image_data, dtype=np.int_), name='Live wire')
+        else:
+            self.result_layer = self.viewer.layers[self.cb_result.currentText()]
+        
+        self.is_active = not self.is_active
+
+        if self.is_active:
+            self._handle_active()
+        else:
+            self._handle_inactive()
+        
+    def _handle_active(self):
+        self.labels_layer = self.viewer.add_labels(np.zeros_like(self.image_data, dtype=np.int_), name='Live wire (current edit)')
+        self.labels_layer.mouse_drag_callbacks.append(self._on_mouse_click)
+        self.labels_layer.mouse_double_click_callbacks.append(self._on_press_finish_key)
+
+        # Bind keys
+        self.viewer.bind_key('Escape', self._on_escape, overwrite=True)
+
+        self.viewer.cursor.events.position.connect(self._on_cursor_move)
+        self.btn.setText('Stop live wire')
+        self.viewer.text_overlay.visible = True
+
+    def _handle_inactive(self):
+        if self._on_mouse_click in self.labels_layer.mouse_drag_callbacks:
+            self.labels_layer.mouse_drag_callbacks.remove(self._on_mouse_click)
+        
+        if self._on_press_finish_key in self.labels_layer.mouse_double_click_callbacks:
+            self.labels_layer.mouse_double_click_callbacks.remove(self._on_press_finish_key)
+
+        for idx, layer in enumerate(self.viewer.layers):
+            if layer.name == 'Live wire (current edit)':
+                self.viewer.layers.pop(idx)
+
+        # Remove the viewer key bindings
+        if 'Escape' in self.viewer.keymap:
+            self.viewer.keymap.pop('Escape')
+        
+        self.viewer.cursor.events.position.disconnect(self._on_cursor_move)
+        self.btn.setText('Start live wire')
+        self.viewer.text_overlay.visible = False
+
+    def _on_press_finish_key(self, source_layer, e):
+        if (self.is_in_3d_view) | (self.labels_layer is None):
+            return
+        
+        if self.check_label_increment.isChecked():
+            self.selected_label += 1
+
+        self.current_start_point = None
+
+    def _on_cursor_move(self, event):
+        if (self.is_in_3d_view) | (self.current_start_point is None):
+            return
+        
+        goal_point = self._event_position(event.value)
+
+        rx, ry = self.image_data_slice.shape
+        if not (0 < goal_point[0] < rx) & (0 < goal_point[1] < ry):
+            return
+
+        result = AStarSearch(
+            (-self.image_data_slice if self.check_black_ridges.isChecked() else self.image_data_slice),
+            self.current_start_point,
+            goal_point
+        ).search()
+        
+        wire_mask = np.zeros(self.image_data_slice.shape, dtype=np.uint8)
+        for (x, y) in result:
+            wire_mask[x, y] = self.selected_label
+
+        if self.ndim == 2:
+            self.labels_layer.data = wire_mask
+        elif self.ndim == 3:
+            self.labels_layer.data.transpose(self.axes)[self.current_step] = wire_mask
+
+        self.labels_layer.refresh()
+        
+    def _on_mouse_click(self, source_layer=None, event=None):
+        self.current_start_point = self._event_position(event.position)
+        self.result_layer.data[self.labels_layer.data == self.selected_label] = self.selected_label
+        self.result_layer.refresh()
+
+    def _on_escape(self, e):
+        self.labels_layer.data *= 0
+        self.current_start_point = None
+
+    def _event_position(self, event_position):
+        if self.ndim == 2:
+            xpos, ypos = np.array(event_position).astype(int)
+        elif self.ndim == 3:
+            _, xpos, ypos = np.array(event_position).astype(int)[self.axes]
+
+        return np.array([xpos, ypos])
