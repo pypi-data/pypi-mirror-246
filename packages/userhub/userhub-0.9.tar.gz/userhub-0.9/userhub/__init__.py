9222e3d9cb31426841a3876e99788ac83871dde0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 03:05:05 +0000

    Add package userhub/0.9/userhub-0.9.tar.gz

---
+++ packages/userhub/userhub-0.9.tar.gz/userhub-0.9/userhub/__init__.py
@@ -0,0 +1,15 @@
+"""
+Initializing the Python package
+"""
+
+from .main import detect_type, auth, token
+
+
+__version__ = '0.9'
+
+__all__ = (
+    '__version__',
+    'detect_type',
+    'auth',
+    'token',
+)
