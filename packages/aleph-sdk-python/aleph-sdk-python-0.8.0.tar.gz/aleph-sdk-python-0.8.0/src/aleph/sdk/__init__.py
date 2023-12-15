b588bb99b706dac5459523e09427f23f4ccf1b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 04:04:22 +0000

    Add package aleph-sdk-python/0.8.0/aleph-sdk-python-0.8.0.tar.gz

---
+++ packages/aleph-sdk-python/aleph-sdk-python-0.8.0.tar.gz/aleph-sdk-python-0.8.0/src/aleph/sdk/__init__.py
@@ -0,0 +1,14 @@
+from pkg_resources import DistributionNotFound, get_distribution
+
+from aleph.sdk.client import AlephHttpClient, AuthenticatedAlephHttpClient
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = "aleph-sdk-python"
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:
+    __version__ = "unknown"
+finally:
+    del get_distribution, DistributionNotFound
+
+__all__ = ["AlephHttpClient", "AuthenticatedAlephHttpClient"]
