6177e2c818a73ef4f41214c947d063579042c326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 15:04:28 +0000

    Add package tastymap/0.4.0/tastymap-0.4.0.tar.gz

---
+++ packages/tastymap/tastymap-0.4.0.tar.gz/tastymap-0.4.0/tastymap/__init__.py
@@ -0,0 +1,9 @@
+"""Color palettes for your palate"""
+
+from .core import cook_tmap, pair_tbar
+from .models import TastyBar, TastyMap
+from .ui import TastyKitchen
+
+__version__ = "0.4.0"
+
+__all__ = ["cook_tmap", "pair_tbar", "TastyMap", "TastyBar", "TastyKitchen"]
