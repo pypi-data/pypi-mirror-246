fa3fe07ad251486a0f545be664b8b36a906f2dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 04:04:15 +0000

    Add package rstats/0.3.0/rstats-0.3.0-py3-none-any.whl

---
+++ packages/rstats/rstats-0.3.0-py3-none-any.whl/rstats/__init__.py
@@ -0,0 +1,14 @@
+from .bench import bench
+from .stats import BaseStatsCollector, DeepCounter, MovingAverage, NestedDict, format_count, format_file_size, format_time
+
+__all__ = [
+    "BaseStatsCollector",
+    "NestedDict",
+    "bench",
+    "MovingAverage",
+    "MovingAverage",
+    "DeepCounter",
+    "format_time",
+    "format_file_size",
+    "format_count",
+]
