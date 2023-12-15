f39805fd85a9f5ab81a659cbb5cfe5c9c0aa3c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 04:04:46 +0000

    Add package wagtail-admin-interface/0.0.5/wagtail_admin_interface-0.0.5-py3-none-any.whl

---
+++ packages/wagtail-admin-interface/wagtail_admin_interface-0.0.5-py3-none-any.whl/wagtail_admin_interface/wagtail_hooks.py
@@ -0,0 +1,59 @@
+# Python Standard Library
+import json
+
+# Django
+from django.conf import settings
+from django.contrib.auth.models import Permission
+from django.utils.safestring import mark_safe
+
+# Wagtail
+from wagtail import hooks
+from wagtail.models import Page
+
+
+@hooks.register('construct_main_menu')
+def construct_main_menu(request, menu_items):
+    if not hasattr(settings, 'WAGTAIL_ADMIN_INTERFACE'):
+        return
+
+    # Hide main menu items that are not whitelisted
+    if 'HIDDEN_MAIN_MENU_ITEMS' in settings.WAGTAIL_ADMIN_INTERFACE \
+            and settings.WAGTAIL_ADMIN_INTERFACE['HIDDEN_MAIN_MENU_ITEMS'] is not None:
+        menu_items[:] = [menu_item for menu_item in menu_items if menu_item.name not in settings.WAGTAIL_ADMIN_INTERFACE['HIDDEN_MAIN_MENU_ITEMS']]
+
+
+@hooks.register('construct_settings_menu')
+def construct_settings_menu(request, menu_items):
+    if not hasattr(settings, 'WAGTAIL_ADMIN_INTERFACE'):
+        return
+
+    # Hide settings menu items that are not whitelisted
+    if 'HIDDEN_SETTINGS_MENU_ITEMS' in settings.WAGTAIL_ADMIN_INTERFACE \
+            and settings.WAGTAIL_ADMIN_INTERFACE['HIDDEN_SETTINGS_MENU_ITEMS'] is not None:
+        menu_items[:] = [menu_item for menu_item in menu_items if menu_item.name not in settings.WAGTAIL_ADMIN_INTERFACE['HIDDEN_SETTINGS_MENU_ITEMS']]
+
+
+@hooks.register('insert_global_admin_js')
+def insert_global_admin_js():
+    data = {}
+
+    if not hasattr(settings, 'WAGTAIL_ADMIN_INTERFACE'):
+        return
+
+    # Hide the root page in the crumb trail
+    if 'HIDE_ROOT_PAGE_IN_CRUMB_TRAIL' in settings.WAGTAIL_ADMIN_INTERFACE \
+            and settings.WAGTAIL_ADMIN_INTERFACE['HIDE_ROOT_PAGE_IN_CRUMB_TRAIL']:
+        data['HIDE_ROOT_PAGE_IN_CRUMB_TRAIL'] = True
+
+    # Hide group object permisions that are not whitelisted
+    if 'HIDDEN_GROUP_OBJECT_PERMISSIONS' in settings.WAGTAIL_ADMIN_INTERFACE \
+            and settings.WAGTAIL_ADMIN_INTERFACE['HIDDEN_GROUP_OBJECT_PERMISSIONS'] is not None:
+        permissions = Permission.objects.filter(content_type__model__in=settings.WAGTAIL_ADMIN_INTERFACE['HIDDEN_GROUP_OBJECT_PERMISSIONS'])
+        data['HIDDEN_GROUP_OBJECT_PERMISSIONS'] = [permission.id for permission in permissions]
+
+    # Hide the editor tab bar if there is only a single tab
+    if 'HIDE_EDITOR_TAB_BAR_IF_SINGLE_TAB' in settings.WAGTAIL_ADMIN_INTERFACE \
+            and settings.WAGTAIL_ADMIN_INTERFACE['HIDE_EDITOR_TAB_BAR_IF_SINGLE_TAB']:
+        data['HIDE_EDITOR_TAB_BAR_IF_SINGLE_TAB'] = True
+
+    return mark_safe('<script>const WagtailAdminInterface = ' + json.dumps(data)  + ';</script>')
