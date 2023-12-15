d55ad3370de2772d5b1a431109a1a2b8ecafed84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 04:05:09 +0000

    Add package mns-common/1.0.1/mns-common-1.0.1.tar.gz

---
+++ packages/mns-common/mns-common-1.0.1.tar.gz/mns-common-1.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='mns-common',
+    version='1.0.1',
+    packages=["mns-common", "mns-common.api", "mns-common.db", 'mns-common.utils'],
+    install_requires=[],  # 如果有依赖项，可以在这里列出
+)
