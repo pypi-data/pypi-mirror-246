e75ccfcb7b087748bd39eaffd750fd030c8a9379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 14:04:57 +0000

    Add package feu/0.0.2/feu-0.0.2-py3-none-any.whl

---
+++ packages/feu/feu-0.0.2-py3-none-any.whl/feu/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+__all__ = [
+    "compare_version",
+    "get_package_version",
+    "is_module_available",
+    "is_package_available",
+]
+
+from feu.imports import is_module_available, is_package_available
+from feu.version import compare_version, get_package_version
