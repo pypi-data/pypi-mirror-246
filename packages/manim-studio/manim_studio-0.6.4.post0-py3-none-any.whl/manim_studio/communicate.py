36e8660fc44d3b6d5d8dcc9bb01d55109968edfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 06:05:42 +0000

    Add package manim-studio/0.6.4.post0/manim_studio-0.6.4.post0-py3-none-any.whl

---
+++ packages/manim-studio/manim_studio-0.6.4.post0-py3-none-any.whl/manim_studio/communicate.py
@@ -0,0 +1,23 @@
+from PyQt6.QtCore import pyqtSignal, QObject
+from manim import Mobject
+import numpy as np
+
+
+class Communicate(QObject):
+    """A class to communicate using signals and slots."""
+
+    update_scene = pyqtSignal(str)
+    update_image = pyqtSignal(np.ndarray)
+    show_in_status_bar = pyqtSignal(str)
+    print_gui = pyqtSignal(str)
+    save_to_python = pyqtSignal()
+    save_state = pyqtSignal()
+    undo_state = pyqtSignal(bool)
+    redo_state = pyqtSignal()
+    update_state = pyqtSignal()
+    add_value_tracker = pyqtSignal(str, Mobject)
+    save_mobject = pyqtSignal()
+    load_mobject = pyqtSignal()
+    enable_interact = pyqtSignal(bool)
+    add_to_interactive_mobjects = pyqtSignal(str)
+    remove_from_interactive_mobjects = pyqtSignal(str)
