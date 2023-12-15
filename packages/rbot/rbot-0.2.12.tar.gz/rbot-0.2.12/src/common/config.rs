1bcb40f4da1e0fe056a541949a0d3a88033cf1a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 04:04:32 +0000

    Add package rbot/0.2.12/rbot-0.2.12.tar.gz

---
+++ packages/rbot/rbot-0.2.12.tar.gz/rbot-0.2.12/src/common/config.rs
@@ -0,0 +1,75 @@
+use pyo3::{pyclass, pymethods};
+use rust_decimal::Decimal;
+use rust_decimal_macros::dec;
+use serde_derive::{Serialize, Deserialize};
+
+
+#[pyclass]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum FeeType {
+    Home,
+    Foreign,
+    Both,
+}
+#[pyclass]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum PriceType {
+    Home,
+    Foreign,
+    Both,
+}
+
+#[pyclass]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct MarketConfig {
+    pub price_unit: Decimal,
+    pub price_scale: u32,
+
+    pub size_unit: Decimal,
+    pub size_scale: u32,
+
+    pub maker_fee: Decimal,
+    pub taker_fee: Decimal,
+
+    pub price_type: PriceType,
+    pub fee_type: FeeType,
+
+    pub home_currency: String,
+    pub foreign_currency: String,
+
+    pub market_order_price_slip: Decimal,
+
+    pub board_depth: u32
+}
+
+
+impl MarketConfig {
+    pub fn new(
+        home_currency: &str,
+        foreign_currency: &str,
+        price_scale: u32,
+        size_scale: u32,
+    ) -> Self {
+        Self {
+            price_unit: Decimal::new(1, price_scale),
+            price_scale,
+            size_unit: Decimal::new(1, size_scale),
+            size_scale,
+            maker_fee: dec![0.0], // dec![0.00_015],  // 0.015%
+            taker_fee: dec![0.0], // dec![0.00_015],  // 0.015%
+            price_type: PriceType::Foreign,
+            fee_type: FeeType::Home,
+            home_currency: home_currency.to_string(),
+            foreign_currency: foreign_currency.to_string(),
+            market_order_price_slip: dec![0.0],
+            board_depth: 1000,
+        }
+    }
+}
+
+#[pymethods]
+impl MarketConfig {
+    pub fn symbol(&self) -> String {
+        return format!("{}{}", self.foreign_currency, self.home_currency);
+    }
+}
