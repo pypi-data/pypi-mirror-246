6b55a8169c551b5eb1f7a15e95a98626fdee32e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 16:04:48 +0000

    Add package gold-ratio/0.1/gold_ratio-0.1.tar.gz

---
+++ packages/gold-ratio/gold_ratio-0.1.tar.gz/gold_ratio-0.1/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='gold_ratio',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Tidak ada dependensi eksternal
+    ],
+)
