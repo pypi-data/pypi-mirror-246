1c6784cb1a697f625fb643272350c2f472046859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 15:03:59 +0000

    Add package SmartShopper/0.1.0/SmartShopper-0.1.0.tar.gz

---
+++ packages/SmartShopper/SmartShopper-0.1.0.tar.gz/SmartShopper-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SmartShopper',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        "setuptools"
+    ],
+    author="Ben Stoller, Shengyao Li",
+    description="A tool for creating and managing grocery shopping lists",
+    long_description="Please refer to the README.md file for details.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Bengician/DATA533-Project.git",
+    project_urls={
+        "Homepage": "https://github.com/Bengician/DATA533-Project.git"
+    },
+)
