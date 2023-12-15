4558a25c3699e3dcef1e312280bb8b273149fea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 04:05:04 +0000

    Add package fractopo/0.6.0/fractopo-0.6.0-py3-none-any.whl

---
+++ packages/fractopo/fractopo-0.6.0-py3-none-any.whl/fractopo/__init__.py
@@ -0,0 +1,25 @@
+"""
+fractopo.
+
+Fracture Network Analysis
+"""
+import logging
+import os
+
+# Make GeoPandas use shapely 2.0 instead of pygeos
+os.environ["USE_PYGEOS"] = "0"
+
+
+from fractopo.analysis.multi_network import MultiNetwork  # noqa: E402,C0413
+from fractopo.analysis.network import Network  # noqa: E402,C0413
+from fractopo.tval.trace_validation import Validation  # noqa: E402,C0413
+
+log = logging.getLogger(__name__)
+
+__version__ = "0.6.0"
+
+
+log.info(
+    "Main imports available from fractopo/__init__.py:"
+    f" {Network, Validation, MultiNetwork}"
+)
