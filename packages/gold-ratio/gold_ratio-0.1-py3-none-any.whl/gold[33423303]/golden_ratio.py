fec9377b3b212913eaea8bc4f42c1086c0affe13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 16:04:48 +0000

    Add package gold-ratio/0.1/gold_ratio-0.1-py3-none-any.whl

---
+++ packages/gold-ratio/gold_ratio-0.1-py3-none-any.whl/gold[33423303]/golden_ratio.py
@@ -0,0 +1,11 @@
+# gold_ratio.py
+
+def calculate_golden_ratio():
+    """
+    Calculate the golden ratio.
+
+    Returns:
+    float: The golden ratio value.
+    """
+    golden_ratio = (1 + 5 ** 0.5) / 2
+    return golden_ratio
