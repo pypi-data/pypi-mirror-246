de8e0004cec4b93ea4c43c75927e1952828b7e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 04:04:32 +0000

    Add package battleships-game-pkg-oj263/0.0.1/battleships-game-pkg-oj263-0.0.1.tar.gz

---
+++ packages/battleships-game-pkg-oj263/battleships-game-pkg-oj263-0.0.1.tar.gz/battleships-game-pkg-oj263-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="battleships-game-pkg-oj263",
+    version="0.0.1",
+    author="OJ",
+    author_email="oj263@exeter.ac.uk",
+    description="A battleships game",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+)
