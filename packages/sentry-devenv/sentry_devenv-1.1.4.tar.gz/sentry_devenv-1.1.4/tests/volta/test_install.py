a1920cf65997b96fdb371ba4f6ecd5711bbd644c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 05:04:20 +0000

    Add package sentry-devenv/1.1.4/sentry_devenv-1.1.4.tar.gz

---
+++ packages/sentry-devenv/sentry_devenv-1.1.4.tar.gz/sentry_devenv-1.1.4/tests/volta/test_install.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+import os
+from unittest.mock import call
+from unittest.mock import patch
+
+from devenv.lib.volta import _version
+from devenv.lib.volta import install
+
+
+def test_install_already_installed(tmp_path: str) -> None:
+    with patch.multiple(
+        "devenv.lib.volta", root=tmp_path, VOLTA_HOME=f"{tmp_path}/volta"
+    ), patch(
+        "devenv.lib.volta.which",
+        side_effect=[f"{tmp_path}/bin/volta", f"{tmp_path}/volta/bin/node"],
+    ) as mock_which:
+        install()
+        assert mock_which.call_args_list == [
+            call("volta", path=f"{tmp_path}/bin"),
+            call("node", path=f"{tmp_path}/volta/bin"),
+        ]
+
+
+def test_install(tmp_path: str) -> None:
+    with patch.multiple(
+        "devenv.lib.volta", root=tmp_path, VOLTA_HOME=f"{tmp_path}/volta"
+    ):
+        with patch("devenv.lib.volta.which", side_effect=[None, None]), patch(
+            "devenv.lib.volta.install_volta"
+        ) as mock_install_volta, patch(
+            "devenv.lib.volta.proc.run",
+            side_effect=[None, _version],  # volta-migrate  # volta -v
+        ) as mock_proc_run, patch(
+            "devenv.lib.volta.os.path.exists", return_value=True
+        ) as mock_path_exists:
+            install()
+
+            mock_install_volta.assert_called_once_with(f"{tmp_path}/bin")
+            mock_proc_run.assert_has_calls(
+                [
+                    call((f"{tmp_path}/bin/volta-migrate",)),
+                    call((f"{tmp_path}/bin/volta", "-v"), stdout=True),
+                ]
+            )
+            mock_path_exists.assert_called_once_with(
+                f"{tmp_path}/volta/bin/node"
+            )
+            assert os.path.exists(f"{tmp_path}/volta/bin/node")
