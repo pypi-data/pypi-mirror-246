a33c4eb4f0bf25589ac2c1239e0e53703f9ea0e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 05:04:16 +0000

    Add package OptiFlowPy/1.0.3/OptiFlowPy-1.0.3.tar.gz

---
+++ packages/OptiFlowPy/OptiFlowPy-1.0.3.tar.gz/OptiFlowPy-1.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="OptiFlowPy",
+    version="1.0.3",
+    url="https://github.com/LightAndy/OptiFlowPy",
+    license="MIT",
+    author="Light Andy",
+    author_email="andy197197@example.com",
+    description="An advanced batch image compressor.",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=["imageio", "numpy", "Pillow", "PyQt5", "tk", "pytest", "wheel"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
