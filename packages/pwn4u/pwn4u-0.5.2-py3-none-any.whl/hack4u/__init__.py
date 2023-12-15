7e9cd1ff44a5f89ab062ff1ceb324f71eaca846e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 14:04:40 +0000

    Add package pwn4u/0.5.2/pwn4u-0.5.2-py3-none-any.whl

---
+++ packages/pwn4u/pwn4u-0.5.2-py3-none-any.whl/hack4u/__init__.py
@@ -0,0 +1,2 @@
+from .courses import *# Del modulo courses (courses.py) importa todo (funciones, clases, etc)
+from .utils import *# Del modulo utils (utils.py) importa todo (funciones, clases, etc)
