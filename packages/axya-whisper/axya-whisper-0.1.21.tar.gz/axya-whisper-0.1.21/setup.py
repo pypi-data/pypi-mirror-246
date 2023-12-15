c8813acd5cb12318dd2b95b279a82fdd82a274c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 04:04:52 +0000

    Add package axya-whisper/0.1.21/axya-whisper-0.1.21.tar.gz

---
+++ packages/axya-whisper/axya-whisper-0.1.21.tar.gz/axya-whisper-0.1.21/setup.py
@@ -0,0 +1,22 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='axya-whisper',
+    version='0.1.21',
+    packages=find_packages(),
+    package_data={'': ['assets/axya.ico']},
+    install_requires=[
+        'pywin32',
+        'keyring',
+        'requests',
+        'pystray'
+    ],
+    entry_points={
+        'console_scripts': [
+            'axya-whisper-windows-service=axya_whisper.__main__:main',
+        ],
+    },
+    python_requires='>=3.6',
+)
