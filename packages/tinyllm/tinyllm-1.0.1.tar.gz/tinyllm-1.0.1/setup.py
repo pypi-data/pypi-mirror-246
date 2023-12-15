881a10f572ba2285ed696b072ff6b92f716ec7c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 06:05:19 +0000

    Add package tinyllm/1.0.1/tinyllm-1.0.1.tar.gz

---
+++ packages/tinyllm/tinyllm-1.0.1.tar.gz/tinyllm-1.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(name='tinyllm',
+      version='1.0.1',
+      description='Development and management infrastructure for LLM applications',
+      packages=find_packages(),
+      install_requires=[
+          'click',
+          'tenacity',
+          'langchain',
+          'langfuse',
+          'litellm',
+          'openai',
+          'pathspec',
+          'pgvector',
+          'psutil',
+          'psycopg2-binary',
+          'pydantic==1.10.7',  # Compatible with langchain + langfuse
+          'pytest',
+          'pyyaml',
+          'sentence_transformers',
+          'sqlalchemy',
+          'tiktoken',
+          'typing-extensions==4.5.0',  # This fixes the pydantic/subclass typing bug
+          'uuid',
+          'fuzzywuzzy'
+      ],
+      author='Othmane Zoheir',
+      author_email='zozoheir@umich.edu',
+      url='')
