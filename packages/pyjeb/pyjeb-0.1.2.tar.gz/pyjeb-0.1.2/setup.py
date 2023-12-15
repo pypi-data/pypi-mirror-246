cfd2c478cf7a480c3e093794843960e2ddc72e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 03:04:33 +0000

    Add package pyjeb/0.1.2/pyjeb-0.1.2.tar.gz

---
+++ packages/pyjeb/pyjeb-0.1.2.tar.gz/pyjeb-0.1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup (
+    name = "pyjeb",
+    long_description_content_type="text/x-rst",
+    long_description="A package to help on configuration file",
+    version = "0.1.2",
+    packages = find_packages(),
+)
