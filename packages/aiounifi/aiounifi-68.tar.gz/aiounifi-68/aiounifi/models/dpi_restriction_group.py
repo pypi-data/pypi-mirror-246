2109c8f3df8fb8ac84fdf4c1b22a1273c56d7ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 05:04:15 +0000

    Add package aiounifi/68/aiounifi-68.tar.gz

---
+++ packages/aiounifi/aiounifi-68.tar.gz/aiounifi-68/aiounifi/models/dpi_restriction_group.py
@@ -0,0 +1,67 @@
+"""DPI Restrictions as part of a UniFi network."""
+
+from dataclasses import dataclass
+from typing import NotRequired, Self, TypedDict
+
+from .api import ApiItem, ApiRequest
+
+
+@dataclass
+class DpiRestrictionGroupListRequest(ApiRequest):
+    """Request object for DPI restriction group list."""
+
+    @classmethod
+    def create(cls) -> Self:
+        """Create DPI restriction group list request."""
+        return cls(method="get", path="/rest/dpigroup")
+
+
+class TypedDPIRestrictionGroup(TypedDict):
+    """DPI restriction group type definition."""
+
+    _id: str
+    attr_no_delete: NotRequired[bool]
+    attr_hidden_id: str
+    name: str
+    site_id: str
+    dpiapp_ids: NotRequired[list[str]]
+
+
+class DPIRestrictionGroup(ApiItem):
+    """Represents a DPI Group configuration."""
+
+    raw: TypedDPIRestrictionGroup
+
+    @property
+    def id(self) -> str:
+        """DPI group ID."""
+        return self.raw["_id"]
+
+    @property
+    def attr_no_delete(self) -> bool | None:
+        """Can be deleted."""
+        if "attr_no_delete" in self.raw:
+            return self.raw["attr_no_delete"]
+        return None
+
+    @property
+    def attr_hidden_id(self) -> str:
+        """Attr hidden ID."""
+        return self.raw.get("attr_hidden_id", "")
+
+    @property
+    def name(self) -> str:
+        """DPI group name."""
+        return self.raw["name"]
+
+    @property
+    def site_id(self) -> str:
+        """Site ID."""
+        return self.raw["site_id"]
+
+    @property
+    def dpiapp_ids(self) -> list[str] | None:
+        """DPI app IDs belonging to group."""
+        if "dpiapp_ids" in self.raw:
+            return self.raw["dpiapp_ids"]
+        return None
---
+++ packages/aiounifi/aiounifi-68.tar.gz/aiounifi-68/aiounifi/models/port.py
@@ -0,0 +1,124 @@
+"""Device port implementation."""
+
+from dataclasses import dataclass
+
+from .api import ApiItem
+from .device import TypedDevicePortTable
+
+
+@dataclass
+class Port2:
+    """Network port representation."""
+
+    ifname: str | None
+    media: str | None
+    name: str
+    port_idx: int | None
+    poe_class: str | None
+    poe_enable: bool | None
+    poe_mode: str | None
+    poe_power: str | None
+    poe_voltage: str | None
+    portconf_id: str | None
+    port_poe: bool | None
+    up: bool | None
+
+    @classmethod
+    def from_dict(cls, data: TypedDevicePortTable) -> "Port2":
+        """Create data container instance from dict."""
+        return cls(
+            ifname=data.get("ifname"),  # Port index as name used by USG
+            media=data.get("media"),  # Media port is connected to
+            name=data["name"],  # Port name
+            port_idx=data.get("port_idx"),  # Port index
+            poe_class=data.get("poe_class"),  # Port PoE class
+            poe_enable=data.get("poe_enable"),  # Is PoE supported/requested by client
+            poe_mode=data.get("poe_mode"),  # auto, pasv24, passthrough, off or None
+            poe_power=data.get("poe_power"),  # PoE power usage
+            poe_voltage=data.get("poe_voltage"),  # PoE voltage usage
+            portconf_id=data.get("portconf_id"),  # Port configuration ID
+            port_poe=data.get("port_poe"),  # Is PoE used
+            up=data.get("up"),  # Is port up
+        )
+
+
+class Port(ApiItem):
+    """Represents a network port."""
+
+    raw: TypedDevicePortTable
+
+    @property
+    def ifname(self) -> str | None:
+        """Port name used by USG."""
+        return self.raw.get("ifname")
+
+    @property
+    def media(self) -> str | None:
+        """Media port is connected to."""
+        return self.raw.get("media")
+
+    @property
+    def name(self) -> str:
+        """Port name."""
+        return self.raw["name"]
+
+    @property
+    def port_idx(self) -> int | None:
+        """Port index."""
+        return self.raw.get("port_idx")
+
+    @property
+    def poe_caps(self) -> int | None:
+        """Port PoE capabilities.
+
+        0 - no caps
+        3 - auto (PoE/PoE+)
+        35 - auto (Poe/PoE+/PoE++)
+        7 - 24V passive
+        8 - Passthrough
+        """
+        return self.raw.get("poe_caps")
+
+    @property
+    def poe_class(self) -> str | None:
+        """Port PoE class."""
+        return self.raw.get("poe_class")
+
+    @property
+    def poe_enable(self) -> bool | None:
+        """Is PoE supported/requested by client."""
+        return self.raw.get("poe_enable")
+
+    @property
+    def poe_mode(self) -> str | None:
+        """Is PoE auto, pasv24, passthrough, off or None."""
+        return self.raw.get("poe_mode")
+
+    @property
+    def poe_power(self) -> str | None:
+        """Is PoE power usage."""
+        return self.raw.get("poe_power")
+
+    @property
+    def poe_voltage(self) -> str | None:
+        """Is PoE voltage usage."""
+        return self.raw.get("poe_voltage")
+
+    @property
+    def portconf_id(self) -> str | None:
+        """Port configuration ID."""
+        return self.raw.get("portconf_id")
+
+    @property
+    def port_poe(self) -> bool | None:
+        """Is PoE used."""
+        return self.raw.get("port_poe")
+
+    @property
+    def up(self) -> bool | None:
+        """Is port up."""
+        return self.raw.get("up")
+
+    def __repr__(self) -> str:
+        """Return the representation."""
+        return f"<{self.name}: Poe {self.poe_enable}>"
