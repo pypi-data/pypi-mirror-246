476737d5bacf96b5676a8f51f8b205e89b441762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 14:04:40 +0000

    Add package tinyllm/1.0.6/tinyllm-1.0.6-py3-none-any.whl

---
+++ packages/tinyllm/tinyllm-1.0.6-py3-none-any.whl/tests/test_memory.py
@@ -0,0 +1,27 @@
+import unittest
+
+from tinyllm.functions.memory.memory import Memory
+from tinyllm.state import States
+from tests.base import AsyncioTestCase
+
+
+class TestOpenAIMemory(AsyncioTestCase):
+
+    def test_openai_memory(self):
+        memory = Memory(name="Memory test")
+
+        # User message
+        msg = {
+            'content': 'Hi agent, how are you?',
+            'role': 'user'
+        }
+
+        result = self.loop.run_until_complete(memory(message=msg))
+
+        self.assertEqual(memory.state, States.COMPLETE)
+        self.assertEqual(len(memory.memories), 1)
+        self.assertEqual(memory.memories[0], {'role': 'user', 'content': 'Hi agent, how are you?'})
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/tinyllm/tinyllm-1.0.6-py3-none-any.whl/tinyllm/functions/memory/memory.py
@@ -0,0 +1,34 @@
+from tinyllm.function import Function
+from tinyllm.functions.util.helpers import count_tokens
+from tinyllm.validator import Validator
+
+
+class MemoryOutputValidator(Validator):
+    memories: list
+
+class MemoryInputValidator(Validator):
+    message: dict
+
+class Memory(Function):
+    def __init__(self,
+                 **kwargs):
+        super().__init__(
+            output_validator=MemoryOutputValidator,
+            **kwargs
+        )
+        self.memories = []
+
+    async def run(self, **kwargs):
+        self.memories.append(kwargs['message'])
+        return {'memories': self.memories}
+
+    @property
+    def size(self):
+        return count_tokens(self.memories,
+                            header='',
+                            ignore_keys=[])
+
+    def get_memories(self,
+                     method='list'):
+        if method == 'list':
+            return self.memories
