7be05bf06d9b978f31f6ec419495210dd7c8c4c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 15:04:21 +0000

    Add package biopipen/0.22.2/biopipen-0.22.2.tar.gz

---
+++ packages/biopipen/biopipen-0.22.2.tar.gz/biopipen-0.22.2/biopipen/scripts/scrna/ExprImpution-rmagic.R
@@ -0,0 +1,29 @@
+
+tryCatch({
+    # in order to load Rmagic
+    workdir = {{job.outdir | r}}
+    conda_prefix = Sys.getenv("CONDA_PREFIX")
+    setwd(workdir)
+    file.symlink(conda_prefix, "miniconda3")
+}, error=function(e) {})
+
+python = {{envs.rmagic_args.python | r}}
+Sys.setenv(RETICULATE_PYTHON = Sys.which(python))
+# reticulate::use_python(python)
+
+library(Rmagic)
+library(Seurat)
+
+infile = {{in.infile | r}}
+outfile = {{out.outfile | r}}
+
+sobj = readRDS(infile)
+DefaultAssay(sobj) <- "RNA"
+
+sobj = magic(sobj)
+sobj = RenameAssays(sobj, RNA = "UNIMPUTED_RNA", MAGIC_RNA = "RNA")
+
+DefaultAssay(sobj) <- "RNA"
+
+attr(sobj, "impute") = "rmagic"
+saveRDS(sobj, outfile)
