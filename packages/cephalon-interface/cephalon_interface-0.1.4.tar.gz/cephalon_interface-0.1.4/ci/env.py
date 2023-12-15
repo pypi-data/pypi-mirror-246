917a1518f7dcc85c5282daaf81cd33805d8dfe06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 14:04:32 +0000

    Add package cephalon-interface/0.1.4/cephalon_interface-0.1.4.tar.gz

---
+++ packages/cephalon-interface/cephalon_interface-0.1.4.tar.gz/cephalon_interface-0.1.4/ci/env.py
@@ -0,0 +1,30 @@
+from PIL import Image
+from pathlib import Path
+from importlib import metadata
+
+PACKAGE_NAME: str = "cephalon-interface"
+PACKAGE_NAME_TITLE: str = " ".join(PACKAGE_NAME.split("-")).title()
+PACKAGE_VERSION: str = metadata.version(PACKAGE_NAME)
+
+PATH_HOME = Path.home()
+PATH_CACHE = PATH_HOME / ".ci"
+PATH_CACHE.mkdir(exist_ok=True)
+PATH_TOKEN = PATH_CACHE / "token.toml"
+PATH_PACKAGE = Path(__file__).parent
+PATH_INCLUDE = PATH_PACKAGE / ".include"
+PATH_LOGO = PATH_INCLUDE / "logo.png"
+PATH_THEME = PATH_INCLUDE / "theme.css"
+PATH_APP_ENTRY = PATH_PACKAGE / "gui" / "app.py"
+
+
+CFG_PAGE_ICON = "🧠"
+CFG_LAYOUT = "wide"
+CFG_INITIAL_SIDEBAR_STATE = "expanded"
+
+
+OBJ_LOGO: Image = Image.open(PATH_LOGO)
+
+# todo: replace with pyeio
+with open(PATH_THEME) as theme_file:
+    OBJ_THEME: str = theme_file.read()
+theme_file.close()
