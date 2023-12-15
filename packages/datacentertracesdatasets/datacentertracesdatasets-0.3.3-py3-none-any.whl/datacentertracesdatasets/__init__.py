18824d396642ad8d4553ecb0d9e8188e36b653f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 03:05:21 +0000

    Add package datacentertracesdatasets/0.3.3/datacentertracesdatasets-0.3.3-py3-none-any.whl

---
+++ packages/datacentertracesdatasets/datacentertracesdatasets-0.3.3-py3-none-any.whl/datacentertracesdatasets/__init__.py
@@ -0,0 +1,8 @@
+"""
+pyexample.
+An example python library.
+"""
+
+__version__ = "0.1.1.2"
+__author__ = 'Alejandro Fernández-Montes González and Damián Fernández Cerero'
+__credits__ = 'Universidad de Sevilla'
