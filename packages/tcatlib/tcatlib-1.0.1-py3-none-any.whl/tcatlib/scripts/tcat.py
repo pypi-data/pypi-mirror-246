d25f0c3d2f1a9c2666f7ed7f3a977f49b2569d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 05:04:28 +0000

    Add package tcatlib/1.0.1/tcatlib-1.0.1-py3-none-any.whl

---
+++ packages/tcatlib/tcatlib-1.0.1-py3-none-any.whl/tcatlib/scripts/tcat.py
@@ -0,0 +1,21 @@
+"""Entrypoint into tcat utility."""
+import sys
+import fileinput
+import argparse
+
+from tcatlib.printing import print_table
+
+def main_program():
+    parser = argparse.ArgumentParser(
+        prog='tcat',
+        description='For peeking at tabular data in the terminal. Accepts a file or standard input.',
+    )
+    parser.add_argument('filename', help='Tab-separated or comma-separated table file.', nargs='?')
+    _ = parser.parse_args()
+    if sys.stdin.isatty() and len(sys.argv) <= 1:
+        sys.exit(0)
+    lines = list(fileinput.input())
+    try:
+        print_table(lines)
+    except BrokenPipeError:
+        pass
