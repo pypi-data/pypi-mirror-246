87064153d4f1e27194a9624587895100e38c25ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 15:04:28 +0000

    Add package pytest-homeassistant-custom-component/0.13.85/pytest-homeassistant-custom-component-0.13.85.tar.gz

---
+++ packages/pytest-homeassistant-custom-component/pytest-homeassistant-custom-component-0.13.85.tar.gz/pytest-homeassistant-custom-component-0.13.85/src/pytest_homeassistant_custom_component/const.py
@@ -0,0 +1,18 @@
+"""
+Constants used by Home Assistant components.
+
+This file is originally from homeassistant/core and modified by pytest-homeassistant-custom-component.
+"""
+from __future__ import annotations
+
+from enum import StrEnum
+from typing import Final
+
+APPLICATION_NAME: Final = "HomeAssistant"
+MAJOR_VERSION: Final = 2023
+MINOR_VERSION: Final = 12
+PATCH_VERSION: Final = "3"
+__short_version__: Final = f"{MAJOR_VERSION}.{MINOR_VERSION}"
+__version__: Final = f"{__short_version__}.{PATCH_VERSION}"
+REQUIRED_PYTHON_VER: Final[tuple[int, int, int]] = (3, 11, 0)
+REQUIRED_NEXT_PYTHON_VER: Final[tuple[int, int, int]] = (3, 11, 0)
