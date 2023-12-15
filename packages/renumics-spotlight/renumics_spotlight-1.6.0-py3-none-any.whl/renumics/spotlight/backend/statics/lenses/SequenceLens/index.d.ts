f98699c9f916ddd7fc4268fa487e4f2caac7aa43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 03:05:54 +0000

    Add package renumics-spotlight/1.6.0/renumics_spotlight-1.6.0-py3-none-any.whl

---
+++ packages/renumics-spotlight/renumics_spotlight-1.6.0-py3-none-any.whl/renumics/spotlight/backend/statics/lenses/SequenceLens/index.d.ts
@@ -0,0 +1,3 @@
+import type { Lens, Vec2 } from '../../types';
+declare const SequenceView: Lens<Vec2[]>;
+export default SequenceView;
---
+++ packages/renumics-spotlight/renumics_spotlight-1.6.0-py3-none-any.whl/renumics/spotlight/backend/statics/lenses/index.d.ts
@@ -0,0 +1,1 @@
+export declare const ALL_LENSES: (import("../types").Lens | import("../types").Lens<string> | import("../types").Lens<import("../types").Vec2[]>)[];
