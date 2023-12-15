7bf11e866716c8def850616c9d9ae40d65379477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 03:05:46 +0000

    Add package pycalib-simple/2023.12.14.1/pycalib-simple-2023.12.14.1.tar.gz

---
+++ packages/pycalib-simple/pycalib-simple-2023.12.14.1.tar.gz/pycalib-simple-2023.12.14.1/pycalib/__init__.py
@@ -0,0 +1,10 @@
+__all__ = ["ba", "bal",  "calib", "plot", "util", "sphere"]
+__version__ = '2023.12.14.1'
+
+from .ba import *
+from .bal import *
+from .calib import *
+from .plot import *
+from .util import *
+from .sphere import *
+
