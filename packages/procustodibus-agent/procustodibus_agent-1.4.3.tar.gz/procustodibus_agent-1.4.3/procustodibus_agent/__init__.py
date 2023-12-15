b51d1000439de3513087963e57939263dfe45a3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 14:04:13 +0000

    Add package procustodibus-agent/1.4.3/procustodibus_agent-1.4.3.tar.gz

---
+++ packages/procustodibus-agent/procustodibus_agent-1.4.3.tar.gz/procustodibus_agent-1.4.3/procustodibus_agent/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Pro Custodibus Agent."""
+
+__version__ = "1.4.3"
+
+APP_URL = "https://pro.custodib.us"
+DEFAULT_API_URL = "https://api.custodib.us"
+DOCS_URL = "https://docs.procustodibus.com"
