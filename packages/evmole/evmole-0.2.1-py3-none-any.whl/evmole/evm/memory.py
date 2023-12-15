4b79e0ef682847a5c659bcfd4fadd2c8e6119134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 03:05:37 +0000

    Add package evmole/0.2.1/evmole-0.2.1-py3-none-any.whl

---
+++ packages/evmole/evmole-0.2.1-py3-none-any.whl/evmole/evm/memory.py
@@ -0,0 +1,25 @@
+class Memory:
+    def __init__(self):
+        self._seq = 0
+        self._data: list[tuple[int, int, bytes]] = []
+
+    def __str__(self):
+        r = f'{len(self._data)} elems:\n'
+        return r + '\n'.join(f'  - {off},{seq}: {val.hex()} | {type(val)}' for off, seq, val in self._data)
+
+    def store(self, offset: int, value: bytes):
+        self._data.append((offset, self._seq, value))
+        self._seq += 1
+
+    def load(self, offset: int) -> tuple[bytes, set[bytes]]:
+        self._data = sorted(self._data)
+        res: list[tuple[int, bytes, bytes | None]] = [(-1, b'\x00', None)] * 32
+        for i in range(offset, offset + 32):
+            idx = i - offset
+            for off, seq, val in self._data:
+                if seq > res[idx][0] and i >= off and i < off + len(val):
+                    res[idx] = (seq, val[i - off : i - off + 1], val)
+
+        ret = b''.join(v[1] for v in res)
+        used = set(v[2] for v in res if v[2] is not None)
+        return ret, used
