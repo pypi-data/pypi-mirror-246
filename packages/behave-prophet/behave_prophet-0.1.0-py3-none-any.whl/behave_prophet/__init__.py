b4998080368d4523d6de242a59b5187c1b079f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-15 16:05:14 +0000

    Add package behave-prophet/0.1.0/behave_prophet-0.1.0-py3-none-any.whl

---
+++ packages/behave-prophet/behave_prophet-0.1.0-py3-none-any.whl/behave_prophet/__init__.py
@@ -0,0 +1,9 @@
+"""
+This is the main module of flair - a behavior testing package for Prophet 
+time series forecasting. It contains the main classes of flair:
+
+Classes:
+--------
+- `ProphetEvaluator`: The main class of flair. It simulates, 
+                      evaluates, and visualizes the results of the behavior testing.
+"""
