a84d751e0080d9dbb88b2f0beefd098d69c319fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 03:06:00 +0000

    Add package django-relay-endpoint/1.2.0/django_relay_endpoint-1.2.0.tar.gz

---
+++ packages/django-relay-endpoint/django_relay_endpoint-1.2.0.tar.gz/django_relay_endpoint-1.2.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django_relay_endpoint",
+    version='1.2.0',
+    license="MIT",
+    author="Gevorg Hakobyan",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type='text/markdown',
+
+    install_requires=[
+        "graphene-django>=3.1.5",
+        "graphene-file-upload>=1.3.0",
+        "django_filter>=23.3.0",
+    ],
+
+)
