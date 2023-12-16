2e55122c8e988914aed919d2d631670d43be8345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 05:05:16 +0000

    Add package python-muckrock/0.1.3/python_muckrock-0.1.3-py2.py3-none-any.whl

---
+++ packages/python-muckrock/python_muckrock-0.1.3-py2.py3-none-any.whl/muckrock/decorators.py
@@ -0,0 +1,21 @@
+"""Custom decorators."""
+from functools import wraps
+
+from .exceptions import CredentialsMissingError
+
+
+def credentials_required(method_func):
+    """Return a decorator for methods that checks that the client has credentials.
+
+    Throws a CredentialsMissingError when they are absent.
+    """
+
+    def _checkcredentials(self, *args, **kwargs):
+        if self.username and self.password:
+            return method_func(self, *args, **kwargs)
+        else:
+            raise CredentialsMissingError(
+                "This is a private method. You must provide a username and password."
+            )
+
+    return wraps(method_func)(_checkcredentials)
