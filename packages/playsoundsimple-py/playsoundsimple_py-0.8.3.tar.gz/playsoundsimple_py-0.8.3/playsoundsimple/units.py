b86c57af89f6ba8fa048f0658793be1fc2a80ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 05:04:44 +0000

    Add package playsoundsimple-py/0.8.3/playsoundsimple_py-0.8.3.tar.gz

---
+++ packages/playsoundsimple-py/playsoundsimple_py-0.8.3.tar.gz/playsoundsimple_py-0.8.3/playsoundsimple/units.py
@@ -0,0 +1,6 @@
+import os
+
+# ! Paths
+LOCAL_DIRPATH = os.path.dirname(__file__)
+DATA_DIRPATH = os.path.join(LOCAL_DIRPATH, "data")
+DEFAULT_SOUND_FONTS_PATH = os.path.join(DATA_DIRPATH, "default.sf2")
