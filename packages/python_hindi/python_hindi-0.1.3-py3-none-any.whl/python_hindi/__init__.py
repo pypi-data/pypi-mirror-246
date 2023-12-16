859f7589b179292c616fbbe815c12f57511bf28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 15:05:26 +0000

    Add package python_hindi/0.1.3/python_hindi-0.1.3-py3-none-any.whl

---
+++ packages/python_hindi/python_hindi-0.1.3-py3-none-any.whl/python_hindi/__init__.py
@@ -0,0 +1,4 @@
+from .hook import create_hook, set_lang
+from .ipython import load_ipython_extension, unload_ipython_extension
+
+__all__ = ["create_hook", "set_lang", "load_ipython_extension", "unload_ipython_extension"]
