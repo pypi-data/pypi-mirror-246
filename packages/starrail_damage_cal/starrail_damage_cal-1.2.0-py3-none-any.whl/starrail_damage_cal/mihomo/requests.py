c4ce7ccb9d6be26cec224648d903637bb32e9d06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 15:04:55 +0000

    Add package starrail_damage_cal/1.2.0/starrail_damage_cal-1.2.0-py3-none-any.whl

---
+++ packages/starrail_damage_cal/starrail_damage_cal-1.2.0-py3-none-any.whl/starrail_damage_cal/mihomo/requests.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+from httpx import AsyncClient
+from msgspec import convert
+
+from starrail_damage_cal.mihomo.models import MihomoData
+
+_HEADER = {"User-Agent": "StarRailDamageCal/"}
+
+
+async def get_char_card_info(
+    uid: str,
+    save_path: Path | None = None,
+) -> MihomoData:
+    async with AsyncClient(
+        base_url="http://api.mihomo.me",
+        headers=_HEADER,
+        timeout=30,
+    ) as client:
+        req = await client.get(f"/sr_info/{uid}")
+        if save_path:
+            path = save_path / str(uid)
+            path.mkdir(parents=True, exist_ok=True)
+            with Path.open(path / f"{uid!s}.json", "w") as file:
+                file.write(req.text)
+        return convert(req.json(), type=MihomoData)
