44cfa38e41608eb4b4a5014fb5fefaa1ed628be4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 15:04:44 +0000

    Add package SimuBox/0.1.16/simubox-0.1.16.tar.gz

---
+++ packages/SimuBox/simubox-0.1.16.tar.gz/simubox-0.1.16/SimuBox/Web/web_unit.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import streamlit as st
+
+def check_state(parent_path: Path, **kwargs):
+
+    if "save_auto" not in st.session_state:
+        st.session_state.save_auto = kwargs.get("save_auto", False)
+
+    if "cache_dir" not in st.session_state:
+        st.session_state.cache_dir = parent_path / kwargs.get("cache", "cache")
+
+        if not st.session_state.cache_dir.exists():
+            st.session_state.cache_dir.mkdir()
+
+    if "dpi" not in st.session_state:
+        st.session_state.dpi = kwargs.get("dpi", 150)
