20971a2526779025db510497a62eec2cbf3c8ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 05:05:16 +0000

    Add package ChemoBiolysis/0.0.1/ChemoBiolysis-0.0.1.tar.gz

---
+++ packages/ChemoBiolysis/ChemoBiolysis-0.0.1.tar.gz/ChemoBiolysis-0.0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='ChemoBiolysis',
+    version='0.0.1',
+    description='This Python package, ChemoBiolysis',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='https://github.com/ahmed1212212/PubChemPyTools.git',  # Replace with your actual repository URL
+    author='Ahmed Alhilal',
+    author_email='aalhilal@udel.edu',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='Cheminformatics',
+    packages=find_packages(),
+    install_requires=[
+        'rdkit',
+        'Pillow',
+        'ipython',
+        'mordred',
+        'pandas',
+        'statsmodels',
+        'matplotlib',  # Add the comma here
+        'lazypredict',
+        'rdkit',
+        'mordred',
+        'pubsams'
+    ]
+)
