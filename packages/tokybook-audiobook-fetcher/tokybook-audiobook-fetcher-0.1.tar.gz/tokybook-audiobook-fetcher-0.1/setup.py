05ceccca38ef44f5785a5cece033b2c4d481ed42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 05:04:48 +0000

    Add package tokybook-audiobook-fetcher/0.1/tokybook-audiobook-fetcher-0.1.tar.gz

---
+++ packages/tokybook-audiobook-fetcher/tokybook-audiobook-fetcher-0.1.tar.gz/tokybook-audiobook-fetcher-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tokybook-audiobook-fetcher',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'beautifulsoup4 == 4.12.2',
+        'requests == 2.31.0',
+        'tqdm == 4.66.1',
+        'urllib3 == 2.1.0'
+    ],
+    entry_points={
+        'console_scripts': [
+            'tokybook-audiobook-fetcher = tokybook_audiobook_fetcher.main:main_function',
+        ],
+    },
+)
