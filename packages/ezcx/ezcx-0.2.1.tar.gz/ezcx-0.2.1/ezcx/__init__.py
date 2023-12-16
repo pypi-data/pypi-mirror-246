63f7a63930b5c2b9d6d6c6a0f40b2a877132f21b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 06:04:36 +0000

    Add package ezcx/0.2.1/ezcx-0.2.1.tar.gz

---
+++ packages/ezcx/ezcx-0.2.1.tar.gz/ezcx-0.2.1/ezcx/__init__.py
@@ -0,0 +1,6 @@
+from ezcx.server import Server
+from ezcx.server import TagRouter
+from ezcx.server import PathRouter
+
+from ezcx.webhooks import WebhookRequest
+from ezcx.webhooks import WebhookResponse
