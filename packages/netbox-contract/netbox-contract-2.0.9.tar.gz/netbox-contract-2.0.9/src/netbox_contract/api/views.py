1affa4445bd1d5580955c73606d455e6783a3045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 06:04:59 +0000

    Add package netbox-contract/2.0.9/netbox-contract-2.0.9.tar.gz

---
+++ packages/netbox-contract/netbox-contract-2.0.9.tar.gz/netbox-contract-2.0.9/src/netbox_contract/api/views.py
@@ -0,0 +1,30 @@
+from netbox.api.viewsets import NetBoxModelViewSet
+
+from .. import filtersets, models
+from .serializers import (
+    ContractAssignementSerializer,
+    ContractSerializer,
+    InvoiceSerializer,
+    ServiceProviderSerializer,
+)
+
+
+class ContractViewSet(NetBoxModelViewSet):
+    queryset = models.Contract.objects.prefetch_related('parent', 'circuit', 'tags')
+    serializer_class = ContractSerializer
+
+
+class InvoiceViewSet(NetBoxModelViewSet):
+    queryset = models.Invoice.objects.prefetch_related('contracts', 'tags')
+    serializer_class = InvoiceSerializer
+    filterset_class = filtersets.InvoiceFilterSet
+
+
+class ServiceProviderViewSet(NetBoxModelViewSet):
+    queryset = models.ServiceProvider.objects.prefetch_related('tags')
+    serializer_class = ServiceProviderSerializer
+
+
+class ContractAssignementViewSet(NetBoxModelViewSet):
+    queryset = models.ContractAssignement.objects.prefetch_related('contract', 'tags')
+    serializer_class = ContractAssignementSerializer
