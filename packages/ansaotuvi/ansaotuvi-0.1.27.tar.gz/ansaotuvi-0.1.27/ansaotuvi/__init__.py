a35a8f3e330c033367c1cd9fdd8170c8f57794b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 15:05:36 +0000

    Add package ansaotuvi/0.1.27/ansaotuvi-0.1.27.tar.gz

---
+++ packages/ansaotuvi/ansaotuvi-0.1.27.tar.gz/ansaotuvi-0.1.27/ansaotuvi/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__title__ = 'ansaotuvi'
+__version__ = '0.1.1'
+__author__ = 'hieu.do'
+__author_email__ = 'hieu.do@ursa.vn'
+__license__ = 'MIT License'
