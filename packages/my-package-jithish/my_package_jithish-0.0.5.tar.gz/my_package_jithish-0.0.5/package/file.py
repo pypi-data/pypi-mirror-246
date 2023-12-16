48cc765de2f3678cbe9501f89fd5ce773e370026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 16:04:52 +0000

    Add package my-package-jithish/0.0.5/my_package_jithish-0.0.5.tar.gz

---
+++ packages/my-package-jithish/my_package_jithish-0.0.5.tar.gz/my_package_jithish-0.0.5/package/file.py
@@ -0,0 +1,4 @@
+import numpy as np
+CONST = "hiii!!!"
+MY_ARRAY = np.array([1, 2, 3, 4, 5])
+print(MY_ARRAY)
---
+++ packages/my-package-jithish/my_package_jithish-0.0.5.tar.gz/my_package_jithish-0.0.5/package/my_folder/my_nested_file.py
@@ -0,0 +1,6 @@
+# import sys
+# sys.path.append('/home/jithish/Documents/package/')
+# # print(sys.path)
+
+from package.file import CONST
+print(CONST)
