4b3bf172ada446a50f03823bffe9899a537ad3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 15:04:51 +0000

    Add package dagster-insights/0.0.12/dagster_insights-0.0.12-py3-none-any.whl

---
+++ packages/dagster-insights/dagster_insights-0.0.12-py3-none-any.whl/dagster_insights_prerelease/__init__.py
@@ -0,0 +1,10 @@
+import sys
+import logging
+import dagster_cloud
+from . import dagster_insights_patched
+
+def patch_dagster_insights():
+    logging.warning("Patching dagster_cloud.dagster_insights with pre-releases fixes.")
+    sys.modules['dagster_cloud.dagster_insights'] = dagster_insights_patched
+
+patch_dagster_insights()
