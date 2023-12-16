1c55656b81ee3444531a9db3883ffba52a72e28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 15:05:10 +0000

    Add package ast-grep-cli/0.15.0/ast_grep_cli-0.15.0.tar.gz

---
+++ packages/ast-grep-cli/ast_grep_cli-0.15.0.tar.gz/ast_grep_cli-0.15.0/crates/cli/tests/scan_test.rs
@@ -0,0 +1,72 @@
+mod common;
+
+use anyhow::Result;
+use assert_cmd::Command;
+use ast_grep::main_with_args;
+use common::create_test_files;
+use predicates::prelude::*;
+use predicates::str::contains;
+use tempfile::TempDir;
+
+const CONFIG: &str = "
+ruleDirs:
+- rules
+testConfigs:
+- testDir: rule-tests
+";
+const RULE1: &str = "
+id: on-rule
+message: test rule
+severity: warning
+language: TypeScript
+rule:
+  pattern: Some($A)
+";
+
+const RULE2: &str = "
+id: off-rule
+severity: off
+language: TypeScript
+rule:
+  pattern: Some($A)
+";
+
+fn setup() -> Result<TempDir> {
+  let dir = create_test_files([
+    ("sgconfig.yml", CONFIG),
+    ("rules/on-rule.yml", RULE1),
+    ("rules/off-rule.yml", RULE2),
+    ("test.ts", "Some(123)"),
+  ])?;
+  assert!(dir.path().join("sgconfig.yml").exists());
+  Ok(dir)
+}
+
+fn sg(s: &str) -> Result<()> {
+  let args = s.split(' ').map(String::from);
+  main_with_args(args)
+}
+
+#[test]
+fn test_sg_scan() -> Result<()> {
+  let dir = setup()?;
+  let config = dir.path().join("sgconfig.yml");
+  let ret = sg(&format!("sg scan -c {}", config.display()));
+  assert!(ret.is_ok());
+  drop(dir);
+  Ok(())
+}
+
+#[test]
+fn test_sg_rule_off() -> Result<()> {
+  let dir = setup()?;
+  Command::cargo_bin("sg")?
+    .current_dir(dir.path())
+    .args(["scan"])
+    .assert()
+    .success()
+    .stdout(contains("on-rule"))
+    .stdout(contains("off-rule").not());
+  drop(dir);
+  Ok(())
+}
