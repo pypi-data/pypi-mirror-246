bbce612e53de8031503d8732eea7d3f9e4c9c824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 06:04:38 +0000

    Add package pii-names/0.0.0/pii-names-0.0.0.tar.gz

---
+++ packages/pii-names/pii-names-0.0.0.tar.gz/pii-names-0.0.0/README.md
@@ -0,0 +1,3 @@
+# pii-names
+
+This is a Python package to manage dictionaries of person names
---
+++ packages/pii-names/pii-names-0.0.0.tar.gz/pii-names-0.0.0/setup.py
@@ -0,0 +1,101 @@
+"""
+Create pii-names as a Python package
+"""
+
+import io
+import sys
+import re
+
+from setuptools import setup, find_packages
+
+from src.pii_names import VERSION
+
+PKGNAME = "pii-names"
+GITHUB_URL = "https://github.com/piisa/" + PKGNAME
+
+# --------------------------------------------------------------------
+
+PYTHON_VERSION = (3, 8)
+
+if sys.version_info < PYTHON_VERSION:
+    sys.exit(
+        "**** Sorry, {} {} needs at least Python {}".format(
+            PKGNAME, VERSION, ".".join(map(str, PYTHON_VERSION))
+        )
+    )
+
+
+def requirements(filename="requirements.txt"):
+    """Read the requirements file"""
+    with io.open(filename, "r") as f:
+        return [line.strip() for line in f if line and line[0] != "#"]
+
+
+def long_description():
+    """
+    Take the README and remove markdown hyperlinks
+    """
+    with open("README.md", "rt", encoding="utf-8") as f:
+        desc = f.read()
+        # action icons
+        desc = re.sub(r"^ \[ ! \[ [^\]]+ \] \( [^\)]+ \) \] \( [^\)]+ \)\n", "",
+                      desc, flags=re.X | re.M)
+        # References
+        desc = re.sub(r"^\[ [^\]]+ \]: \s+ \S.*\n", "", desc, flags=re.X | re.M)
+        # Links
+        return re.sub(r"\[ ([^\]]+) \] (?: \( [^\)]+ \) )?", r"\1", desc,
+                      flags=re.X)
+
+
+# --------------------------------------------------------------------
+
+
+setup_args = dict(
+    # Metadata
+    name=PKGNAME,
+    version=VERSION,
+    author="Paulo Villegas",
+    author_email="paulo.vllgs@gmail.com",
+    description="Management of name dictionaries for PII processing",
+    long_description_content_type="text/markdown",
+    long_description=long_description(),
+    license="Apache",
+    url=GITHUB_URL,
+    download_url=GITHUB_URL + "/tarball/v" + VERSION,
+    # Locate packages
+    packages=find_packages("src"),  # [ PKGNAME ],
+    package_dir={"": "src"},
+    # Requirements
+    python_requires=">=" + ".".join(map(str, PYTHON_VERSION)),
+    # Optional requirements
+    extras_require={
+        "test": ["pytest", "nose", "coverage"],
+    },
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "pii-names-info = pii_names.app.info:main",
+            "pii-names-db = pii_names.app.namedb:main",
+            "pii-names-build = pii_names.app.build:main",
+        ],
+        #"pii_extract.plugins": "piisa-detectors-names = pii_names.plugin_loader:PiiExtractPluginLoader"
+    },
+    include_package_data=True,
+    package_data={"": ["pii_names/resources/*.json"]},
+    # Post-install hooks
+    cmdclass={},
+    keywords=["PIISA, PII"],
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    ],
+)
+
+if __name__ == "__main__":
+    # Add requirements
+    setup_args["install_requires"] = requirements()
+    # Setup
+    setup(**setup_args)
