9e555f8ec90f8cd27d7ab8ba810c75603235f49a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 14:04:58 +0000

    Add package hnn-utils/0.1.25/hnn_utils-0.1.25.tar.gz

---
+++ packages/hnn-utils/hnn_utils-0.1.25.tar.gz/hnn_utils-0.1.25/hnn_utils/nn/__init__.py
@@ -0,0 +1,8 @@
+from hnn_utils.nn.RotaryEmbedding import XPOS
+from hnn_utils.nn.Transformer import (
+    RotaryDecoder,
+    RotaryDecoderLayer,
+    RotaryEncoder,
+    RotaryEncoderLayer,
+    RotaryMultiheadAttention,
+)
