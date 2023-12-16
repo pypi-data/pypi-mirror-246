1692832d710bcd5f4a307abe49754d44edc82778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 16:04:54 +0000

    Add package async-sender/3.0.0/async_sender-3.0.0.tar.gz

---
+++ packages/async-sender/async_sender-3.0.0.tar.gz/async_sender-3.0.0/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import os
+
+from async_sender import __version__
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+except IOError:
+    README = ''
+
+
+REQUIRED = [
+    'aiosmtplib==3.0.1'
+]
+
+setup(
+    name='async_sender',
+    version=__version__,
+    description="AsyncSender is a tiny module for SMTP mail sending, Inspired by Sender.",
+    long_description=README,
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='email, sender, smtp, asyncio',
+    author='Bakhtiyor Ruziev',
+    author_email='rbakhtiyor+github@gmail.com',
+    url='https://github.com/theruziev/async_sender',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIRED,
+)
