20ab7b7251eca5062e512aeccb14f7d18c4baf89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 15:05:07 +0000

    Add package proto-clip-toolkit/0.1.3/proto_clip_toolkit-0.1.3.tar.gz

---
+++ packages/proto-clip-toolkit/proto_clip_toolkit-0.1.3.tar.gz/proto_clip_toolkit-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import setuptools
+from setuptools import setup
+
+required = Path("requirements.txt").read_text(encoding="utf-8").split("\n")
+
+setup(name='proto_clip_toolkit', 
+      description="A simple toolkit from Proto-CLIP demo that provies speech recognition, part-of-speech tagging and realworld robot demo APIs.",
+      author="IRVL-UTD: Intelligent Robotics and Vision Lab at the University of Texas at Dallas",
+      author_email="irvl.utd@gmail.com",
+      version='0.1.3',
+      url="https://github.com/IRVLUTD/Proto-CLIP",
+      license="MIT",
+      packages=setuptools.find_packages(),
+      install_requires=required,
+      include_package_data=True,
+      python_requires=">=3.7"
+)
