be632062de23ec50122b2ff008af690ae33b19f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 03:06:33 +0000

    Add package caosdb/0.13.2/caosdb-0.13.2.tar.gz

---
+++ packages/caosdb/caosdb-0.13.2.tar.gz/caosdb-0.13.2/src/linkahead/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM linkahead SETUP.PY
+#
+short_version = '0.13.2'
+version = '0.13.2'
+full_version = '0.13.2'
+git_revision = '694100f6aef5433df559ac6bd2851faf18359e74'
+release = True
+
+if not release:
+    version = full_version
