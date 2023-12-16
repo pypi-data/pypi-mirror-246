a5d437716b8ad083c5ca80c3af8ee43c033f8e76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 15:05:07 +0000

    Add package Python-Algorithm-pyal/1.1.6/Python_Algorithm_pyal-1.1.6-py3-none-any.whl

---
+++ packages/Python-Algorithm-pyal/Python_Algorithm_pyal-1.1.6-py3-none-any.whl/pyal/list/linked_list_TEST.py
@@ -0,0 +1,59 @@
+from pyal.list.linked_list import LinkedList, ListNode
+
+
+def test_list():
+  list = LinkedList()
+  list.push_front(1)
+  assert list.begin().get() == 1
+
+  list.push_back(2)
+  assert list.rbegin().get() == 2
+
+  assert list.size() == 2
+  assert list.to_list() == [1, 2]
+
+  list.push_back(3)
+  print("list:", list.to_list())
+  assert list.get_element(-1) == 3
+  assert list.get(1).get() == 2
+
+  list.clear()
+  assert list.to_list() == []
+
+  assert list.index(3) is None
+
+  list.push_back(1)
+  list.push_back(3)
+  list.push_back(5)
+  assert list.index(4) is None
+  assert list.index(5).prev().prev().get() == 1
+
+
+def test_insertion():
+  list = LinkedList()
+  list.push_back(1)
+  list.push_back(2)
+  list.push_back(3)
+  list.push_back(4)
+  list.insert_element(list.begin(), 0)
+  list.insert(list.rbegin(), ListNode(100))
+
+  iter = list.begin()
+  iter = iter.next()
+  list.remove(iter)
+
+  data = []
+  iter = list.begin()
+  while iter is not list.end():
+    data.append(iter.get())
+    iter = iter.next()
+
+  assert data == [0, 2, 3, 100, 4]
+
+  data = []
+  iter = list.rbegin()
+  while iter is not list.rend():
+    data.append(iter.get())
+    iter = iter.prev()
+
+  assert data == [0, 2, 3, 100, 4][::-1]
