8bfd22a0e6e6fa48b9131f07b1f86845edd0ec9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 03:05:15 +0000

    Add package ovos-core/0.0.8a57/ovos_core-0.0.8a57-py3-none-any.whl

---
+++ packages/ovos-core/ovos_core-0.0.8a57-py3-none-any.whl/mycroft/audio/interface.py
@@ -0,0 +1,3 @@
+# backwards compat imports
+# TODO - import the OCP compat audio service instead of classic
+from ovos_utils.skills.audioservice import ClassicAudioServiceInterface as AudioService, ensure_uri
---
+++ packages/ovos-core/ovos_core-0.0.8a57-py3-none-any.whl/mycroft/skills/audioservice.py
@@ -0,0 +1,2 @@
+# this was moved in order to allow importing it without dragging skills service dependencies
+from ovos_utils.skills.audioservice import ClassicAudioServiceInterface as AudioService
