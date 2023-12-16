432eb97d30a8d120ccf8d001c02e8afe3feb93e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 03:06:20 +0000

    Add package drlab-minilib/0.3.1/drlab_minilib-0.3.1.tar.gz

---
+++ packages/drlab-minilib/drlab_minilib-0.3.1.tar.gz/drlab_minilib-0.3.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='drlab_minilib',
+    version='0.3.1',
+    license='MIT',
+    author="Rafal Labedzki",
+    author_email='rlabed@sgh.waw.pl',
+    packages=find_packages(),
+    # package_dir={'drlab_minilib'},
+    install_requires=[ 
+          'pandas',
+          'matplotlib'
+      ],
+
+)
