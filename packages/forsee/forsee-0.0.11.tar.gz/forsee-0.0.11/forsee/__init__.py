beaa6c58e2f0500fcb71ac4f66db25e25e472695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 03:06:07 +0000

    Add package forsee/0.0.11/forsee-0.0.11.tar.gz

---
+++ packages/forsee/forsee-0.0.11.tar.gz/forsee-0.0.11/forsee/__init__.py
@@ -0,0 +1,12 @@
+# __init__.py
+
+# This file can be empty, or you can include initialization code here.
+
+
+
+from forsee_functions import *
+from args_handler import *
+
+
+# You can also define variables or functions that should be available at the package level.
+# These will be accessible when someone imports your package.
