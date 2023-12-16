b40d0790353046d2085f6896d169b99ba94fb625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 03:05:48 +0000

    Add package example-pkg-adam/0.2/example_pkg_adam-0.2.tar.gz

---
+++ packages/example-pkg-adam/example_pkg_adam-0.2.tar.gz/example_pkg_adam-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+name="example_pkg_adam",
+version="0.2",
+author="adam",
+author_email="adamzagnoune2023@gmail.com",
+description="Csv Json Txt",
+packages=find_packages()
+)
+
+
