792f948964911598292d3726d420945d16a53e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 04:05:12 +0000

    Add package gdal2numpy/0.0.257/gdal2numpy-0.0.257.tar.gz

---
+++ packages/gdal2numpy/gdal2numpy-0.0.257.tar.gz/gdal2numpy-0.0.257/src/gdal2numpy/module_http.py
@@ -0,0 +1,45 @@
+import socket
+import requests
+
+
+def hostname():
+    """
+    hostname
+    """
+    return socket.gethostname()
+
+
+def local_ip():
+    """
+    get_ip -
+    https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(("10.254.254.254", 1))
+        res = s.getsockname()[0]
+    except socket.error:
+        res = "127.0.0.1"
+    finally:
+        s.close()
+    return res
+
+
+def whatsmyip():
+    """
+    whatsmyip
+    """
+    uris = ["https://checkip.amazonaws.com",
+            "https://ifconfig.co/ip", 
+            "https://ipinfo.io/ip", 
+            "https://icanhazip.com",
+            "https://api.ip.sb/ip", 
+            "https://api.ipify.org"]
+    for uri in uris:
+        try:
+            return requests.get(uri).text.strip()
+        except requests.exceptions.RequestException:
+            continue
+    return None
