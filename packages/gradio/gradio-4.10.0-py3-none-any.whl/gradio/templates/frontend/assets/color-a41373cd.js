4ddde1c330194b0837fa90f25a6f5029074a3ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 14:05:33 +0000

    Add package gradio/4.10.0/gradio-4.10.0-py3-none-any.whl

---
+++ packages/gradio/gradio-4.10.0-py3-none-any.whl/gradio/templates/frontend/assets/color-a41373cd.js
@@ -0,0 +1,8 @@
+import { o as ordered_colors } from './Button-2ad52d97.js';
+
+const get_next_color = (index) => {
+  return ordered_colors[index % ordered_colors.length];
+};
+
+export { get_next_color as g };
+//# sourceMappingURL=color-a41373cd.js.map
