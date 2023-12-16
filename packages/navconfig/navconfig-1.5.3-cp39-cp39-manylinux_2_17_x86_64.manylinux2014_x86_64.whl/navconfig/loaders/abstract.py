65c16c7b9236c89f0120dcafa0e698912237b846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 14:06:06 +0000

    Add package navconfig/1.5.3/navconfig-1.5.3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/navconfig/navconfig-1.5.3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/navconfig/loaders/abstract.py
@@ -0,0 +1,62 @@
+from typing import Union
+from abc import ABC, abstractmethod
+from pathlib import PurePath
+from io import StringIO
+from dotenv import dotenv_values, load_dotenv
+
+
+class BaseLoader(ABC):
+
+    def __init__(
+            self,
+            env_path: PurePath = None,
+            override: bool = False,
+            create: bool = True,
+            **kwargs
+        ) -> None:
+        self.override: bool = override
+        self.env_path = env_path
+        self.env_file = '.env'
+        self._kwargs = kwargs
+        self.downloadable: bool = False
+        if isinstance(self.env_path, PurePath):
+            if not env_path.exists():
+                if create:
+                    try:
+                        env_path.mkdir(parents=True, exist_ok=True)
+                    except IOError as ex:
+                        raise RuntimeError(
+                            f'{type(self).__name__}: IO error creating directory {env_path}: {ex}'
+                        ) from ex
+                raise FileExistsError(
+                    f'{type(self).__name__}: No Directory Path: {env_path}'
+                )
+
+    @abstractmethod
+    def load_environment(self):
+        pass
+
+    @abstractmethod
+    def save_environment(self):
+        pass
+
+
+    def load_from_file(self, path):
+        load_dotenv(
+            dotenv_path=path,
+            override=self.override
+        )
+
+    def load_from_stream(self, content: str):
+        load_dotenv(
+            stream=content,
+            override=self.override
+        )
+
+    def load_from_string(self, content: Union[str, dict]):
+        if isinstance(content, str):
+            filelike = StringIO(content)
+            filelike.seek(0)
+            dotenv_values(stream=filelike)
+        else:
+            return content
---
+++ packages/navconfig/navconfig-1.5.3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/navconfig/readers/memcache.py
@@ -0,0 +1,86 @@
+import os
+import logging
+import pylibmc
+from .abstract import AbstractReader
+
+class mcache(AbstractReader):
+    """
+    Basic Connector for Memcached.
+    Future-proof.
+    """
+    _args = {"tcp_nodelay": True, "ketama": True}
+
+    def __init__(self) -> None:
+        host = os.getenv('MEMCACHE_HOST', 'localhost')
+        port = int(os.getenv('MEMCACHE_PORT', '11211'))
+        try:
+            self._server = [f"{host}:{port}"]
+            self._memcached = pylibmc.Client(
+                self._server,
+                binary=True,
+                behaviors=self._args
+            )
+        except Exception as err:  # pylint: disable=W0703
+            self.enabled = False
+            logging.exception(err, stack_info=True)
+
+    def get(self, key, default=None):
+        try:
+            result = self._memcached.get(bytes(key, "utf-8"), default)
+            if result:
+                return result.decode("utf-8")
+            else:
+                return None
+        except Exception as err:
+            raise Exception(
+                f"Memcache Get Error: {err!s}"
+            ) from err
+
+    def exists(self, key: str) -> bool:
+        try:
+            result = self._memcached.get(bytes(key, "utf-8"))
+            if result:
+                return True
+            else:
+                return False
+        except Exception: #pylint: disable=W0703
+            return False
+
+    def set(self, key, value, timeout: int = None):
+        try:
+            if timeout:
+                return self._memcached.set(
+                    bytes(key, "utf-8"), bytes(value, "utf-8"), time=timeout
+                )
+            else:
+                return self._memcached.set(
+                    bytes(key, "utf-8"), bytes(value, "utf-8")
+                )
+        except Exception as err:
+            raise Exception(
+                f"Memcache Set Error: {err!s}"
+            ) from err
+
+    def multi_get(self, *keys):
+        try:
+            return  self._memcached.multi_get(
+                *[bytes(v, 'utf-8') for v in keys]
+            )
+        except Exception as err:
+            raise Exception(
+                f"Memcache Multi Error: {err!s}"
+            ) from err
+
+    def delete(self, key):
+        try:
+            self._memcached.delete(bytes(key, "utf-8"))
+        except Exception as err:
+            raise Exception(
+                f"Memcache Delete Error: {err!s}"
+            ) from err
+
+    def close(self):
+        try:
+            self._memcached.disconnect_all()
+        except Exception as err: # pylint: disable=W0703
+            logging.exception(err, stack_info=False)
