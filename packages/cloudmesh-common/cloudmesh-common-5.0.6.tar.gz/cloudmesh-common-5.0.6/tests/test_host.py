3ed6ac1484e280ad489d2e85d961503288f3b10a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 03:06:24 +0000

    Add package cloudmesh-common/5.0.6/cloudmesh-common-5.0.6.tar.gz

---
+++ packages/cloudmesh-common/cloudmesh-common-5.0.6.tar.gz/cloudmesh-common-5.0.6/tests/test_host.py
@@ -0,0 +1,22 @@
+###############################################################
+# pytest -v --capture=no  tests/test_host.py
+# pytest -v --capture=no  tests/test_host..py::Test_host.test_001
+# pytest -v tests/test_host.py
+###############################################################
+import getpass
+
+from cloudmesh.common.Shell import Shell
+from cloudmesh.common.util import HEADING
+import pytest
+# from cloudmesh.common.Host import Host
+
+
+@pytest.mark.incremental
+class Test_host(object):
+
+    def test_001_whoami(self):
+        HEADING()
+
+        result = Shell.run("whoami")
+        print(result)
+        assert getpass.getuser() in result
