21a56edf091140f7905a7c024838c0af290e8f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 03:05:18 +0000

    Add package pydotbot/0.17/pydotbot-0.17-cp312-cp312-macosx_10_16_x86_64.whl

---
+++ packages/pydotbot/pydotbot-0.17-cp312-cp312-macosx_10_16_x86_64.whl/dotbot/tests/test_main.py
@@ -0,0 +1,79 @@
+"""Test module for the main function."""
+
+from importlib.metadata import PackageNotFoundError
+from unittest.mock import patch
+
+import serial
+from click.testing import CliRunner
+
+from dotbot.main import main
+
+MAIN_HELP_EXPECTED = """Usage: main [OPTIONS]
+
+  DotBotController, universal SailBot and DotBot controller.
+
+Options:
+  -p, --port TEXT                 Linux users: path to port in '/dev' folder ;
+                                  Windows users: COM port. Defaults to
+                                  '/dev/ttyACM0'
+  -b, --baudrate INTEGER          Serial baudrate. Defaults to 1000000
+  -d, --dotbot-address TEXT       Address in hex of the DotBot to control.
+                                  Defaults to FFFFFFFFFFFFFFFF
+  -g, --gw-address TEXT           Gateway address in hex. Defaults to
+                                  0000000000000000
+  -s, --swarm-id TEXT             Swarm ID in hex. Defaults to 0000
+  -w, --webbrowser                Open a web browser automatically
+  -v, --verbose                   Run in verbose mode (all payloads received are
+                                  printed in terminal)
+  --log-level [debug|info|warning|error]
+                                  Logging level. Defaults to info
+  --log-output PATH               Filename where logs are redirected
+  --handshake                     Perform a basic handshake with the gateway
+                                  board on startup
+  --use-mqtt                      Connect to the MQTT broker configured in the
+                                  .env file
+  --help                          Show this message and exit.
+"""
+
+
+def test_main_help():
+    runner = CliRunner()
+    result = runner.invoke(main, ["--help"])
+    assert result.exit_code == 0
+    assert result.output == MAIN_HELP_EXPECTED
+
+
+@patch("dotbot.serial_interface.serial.Serial.open")
+@patch("dotbot.version")
+@patch("dotbot.controller.Controller.run")
+def test_main(run, version, _):
+    version.return_value = "test"
+    runner = CliRunner()
+    result = runner.invoke(main)
+    assert result.exit_code == 0
+    assert "Welcome to the DotBots controller (version: test)." in result.output
+    run.assert_called_once()
+
+    version.side_effect = PackageNotFoundError
+    result = runner.invoke(main)
+    assert result.exit_code == 0
+    assert "Welcome to the DotBots controller (version: unknown)." in result.output
+
+
+@patch("dotbot.serial_interface.serial.Serial.open")
+@patch("dotbot.controller.Controller.run")
+def test_main_interrupts(run, _):
+    runner = CliRunner()
+    run.side_effect = KeyboardInterrupt
+    result = runner.invoke(main)
+    assert result.exit_code == 0
+
+    runner = CliRunner()
+    run.side_effect = SystemExit
+    result = runner.invoke(main)
+    assert result.exit_code == 0
+
+    run.side_effect = serial.serialutil.SerialException("serial test error")
+    result = runner.invoke(main)
+    assert result.exit_code != 0
+    assert "Serial error: serial test error" in result.output
