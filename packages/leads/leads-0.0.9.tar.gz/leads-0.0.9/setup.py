d9e7ca0395a20a596864741a1e5f2fb71eb0f376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 05:04:42 +0000

    Add package leads/0.0.9/leads-0.0.9.tar.gz

---
+++ packages/leads/leads-0.0.9.tar.gz/leads-0.0.9/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+
+with open("./README.md", "r") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="leads",
+    version="0.0.9",
+    author="ProjectNeura",
+    author_email="central@projectneura.org",
+    description="Lightweight Embedded Assisted Driving System",
+    license='Apache License 2.0',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ProjectNeura/LEADS",
+    packages=find_packages(),
+    package_data={
+        "leads_dashboard": ["_font/OpenSans-Regular.ttf"]
+    },
+    include_package_data=True,
+    install_requires=["numpy"]
+)
