47edb69806249c09b4a3bce45a2e42c9d0269c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 14:05:35 +0000

    Add package example-pkg-Ilyas/0.1/example_pkg_Ilyas-0.1.tar.gz

---
+++ packages/example-pkg-Ilyas/example_pkg_Ilyas-0.1.tar.gz/example_pkg_Ilyas-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+name="example_pkg_Ilyas",
+version="0.1",
+author="Ilyas",
+author_email="ilyassbihaouargui@gmail.com",
+description="create txt and csv files",
+packages=find_packages()
+)
+
+
