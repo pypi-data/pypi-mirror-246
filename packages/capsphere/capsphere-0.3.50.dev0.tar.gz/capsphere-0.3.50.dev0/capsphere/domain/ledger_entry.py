942cf0fa37f3768d48aa6e45e1c44946ecbb7abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 15:05:41 +0000

    Add package capsphere/0.3.50.dev0/capsphere-0.3.50.dev0.tar.gz

---
+++ packages/capsphere/capsphere-0.3.50.dev0.tar.gz/capsphere-0.3.50.dev0/capsphere/domain/ledger_entry.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from pydantic.dataclasses import dataclass
+from decimal import Decimal
+
+
+@dataclass
+class LedgerEntry:
+    user_id: int = 0
+    ledgerable_id: int = 0
+    ledgerable_type: str = ''
+    ref_code: str = ''
+    reason: str = ''
+    credit: int = 0
+    debit: int = 0
+    amount: Decimal = Decimal(0.00)
+    balance: Decimal = Decimal(0.00)
+    id: Optional[int] = None
+    loan_payment_id: Optional[int] = None
+    due_date: Optional[str] = None
+    created_at: Optional[str] = None
+    updated_at: Optional[str] = None
+
+    @staticmethod
+    def from_dict(data: dict):
+        return LedgerEntry(**data)
