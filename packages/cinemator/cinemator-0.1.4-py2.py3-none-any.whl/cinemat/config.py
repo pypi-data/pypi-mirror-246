b7981133a8a0f32ae17f140a4a642881b1ed8f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 04:04:31 +0000

    Add package cinemator/0.1.4/cinemator-0.1.4-py2.py3-none-any.whl

---
+++ packages/cinemator/cinemator-0.1.4-py2.py3-none-any.whl/cinemat/config.py
@@ -0,0 +1,13 @@
+OMDB_URL = "http://www.omdbapi.com"
+
+
+OMDBAPI_KEY = "9b0776a2"
+
+message0 = "* This Command Line Tool is implemented to provide search functionality."
+message1 = "* For instance, search for all movies with the word Planet In the title."
+message2 = "* Show details of a specific movie."
+message3 = "* We wanted to implement ith an option to show Rotten Tomatoes ratings too."
+message4 = "* But during development we find out OMDB api is not providing that information any more"
+message5 = "* We have kept the implementation to update the tool with fandango api for that details"
+
+message = f"{message0}\n{message1}\n{message2}\n{message3}\n{message4}\n{message5}\n"
