02f3f9dea163df45aac71bdf753355514344b216
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 05:05:05 +0000

    Add package my-awesome-package1/0.5.0/my_awesome_package1-0.5.0.tar.gz

---
+++ packages/my-awesome-package1/my_awesome_package1-0.5.0.tar.gz/my_awesome_package1-0.5.0/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open("README.MD", "r") as f:
+  long_description = f.read()
+
+setup(name='my_awesome_package1',  # 包名
+      version='0.5.0',  # 版本号
+      description='A small example package',
+      long_description=long_description,
+      author='mike_talk',
+      author_email='762357658@qq.com',
+      url='',
+      install_requires=[],
+      license='MIT License',
+      packages=["my_awesome_package1"],
+      platforms=["all"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Software Development :: Libraries'
+      ],
+      )
