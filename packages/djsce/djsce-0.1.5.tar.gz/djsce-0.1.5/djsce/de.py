ebd31dde09a9b71a742d0da29c1d52e8ae50c3f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 04:04:56 +0000

    Add package djsce/0.1.5/djsce-0.1.5.tar.gz

---
+++ packages/djsce/djsce-0.1.5.tar.gz/djsce-0.1.5/djsce/de.py
@@ -0,0 +1,30 @@
+de_links={
+    "AND Gate Using NAND Gate":"https://www.tinkercad.com/things/8F7W50Z0yTO-and-gate-using-nand-gate",
+    "OR Gate Using NAND Gate":"https://www.tinkercad.com/things/5lyi8rMzksf-or-gate-using-nand-gate",
+    "XOR_USING_NAND":"https://www.tinkercad.com/things/3TNx0pZHdPt-xorusingnand",
+    "XNOR_USING_NAND":"https://www.tinkercad.com/things/cJKN1IYIoVr-xnorusingnand",
+    "XOR_USING_NOR":"https://www.tinkercad.com/things/d69gI2y9yuS-xorusingnor",
+    "XNOR_USING_NOR":"https://www.tinkercad.com/things/0a7TXdYq9Tg-xnorusingnor",
+    "XNOR_USING_NOR":"https://www.tinkercad.com/things/0a7TXdYq9Tg-xnorusingnor",
+    "4 BIT GRAY TO BINARY CONVERTER":"https://www.tinkercad.com/things/3BrGYS6cjz3-4-bit-gray-to-binary-converter",
+    "4 BIT BINARY TO GRAY CONVERTER":"https://www.tinkercad.com/things/ktV9eRmlc5e-4-bit-binary-to-gray-converter",
+    "3 BIT EVEN PARITY BIT GENERATOR":"https://www.tinkercad.com/things/jtyMEUXciyE-3-bit-even-parity-bit-generator",
+    "3 BIT EVEN PARITY BIT CHECKER":"https://www.tinkercad.com/things/f4rbqSUwPmT-3-bit-even-parity-bit-checker",
+    "3 BIT ODD PARITY BIT GENERATOR":"https://www.tinkercad.com/things/6zzUbjCsWXF-3-bit-odd-parity-bit-generator",
+    "HALF ADDER":"https://www.tinkercad.com/things/jeeuNaDV6tB-half-adder",
+    "HALF SUBTRACTOR":"https://www.tinkercad.com/things/h05KFjuGHFL-half-subtractor",
+    "FULL SUBTRACTOR USING HALF SUBTRACTORS":"https://www.tinkercad.com/things/jy1YLCuqpHr-full-subtractor-using-half-subtractors",
+    "FULL ADDER USING HALF ADDERS":"https://www.tinkercad.com/things/368teABr34B-full-adder-using-half-adders",
+    "ONE BIT MAGNITUDE COMPARATOR":"https://www.tinkercad.com/things/eikFRy4bpPj-one-bit-magnitude-comparator",
+    "TWO BIT MAGNITUDE COMPARATOR":"https://www.tinkercad.com/things/86QxTgSXj6O-two-bit-magnitude-comparator",
+    "SR FlipFlop":"https://www.tinkercad.com/things/e3zz4RTUJtD-sr-flipflop",
+    "D FlipFlop":"https://www.tinkercad.com/things/aXbDegTbh1l-d-flipflop",
+    "Async Down MOD 4 Counter":"https://www.tinkercad.com/things/83lk2cVtZrl-async-down-mod-4-counter",
+    "Async Up MOD 4 Counter":"https://www.tinkercad.com/things/cii697B9Xkk-async-up-mod-4-counter",
+    "Async Up MOD 8 Counter":"https://www.tinkercad.com/things/5rnZ6A0WVt7-async-up-mod-8-counter",
+    "Sync Up MOD 4 Counter":"https://www.tinkercad.com/things/8FJOFphSqdH-sync-up-mod-4-counter",
+    "Async Down Dual JK FF":"https://www.tinkercad.com/things/0EaYx0MlSUq-async-down-dual-jf-ff"
+}
+def de_():
+    for filename, content in de_links.items():
+        print(filename+" : "+content)
