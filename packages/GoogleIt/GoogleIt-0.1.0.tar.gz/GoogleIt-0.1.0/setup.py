ed4c56b6c05cb245886c06e82c2efb5ad352281c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 15:04:33 +0000

    Add package GoogleIt/0.1.0/GoogleIt-0.1.0.tar.gz

---
+++ packages/GoogleIt/GoogleIt-0.1.0.tar.gz/GoogleIt-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GoogleIt',
+    version='0.1.0',
+    author='William Renaldy A',
+    author_email='',
+    description='Python package to search Google powered by PALM 2',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
