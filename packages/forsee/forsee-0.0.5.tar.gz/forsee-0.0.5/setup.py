6d0baf45f4771f07745295eb8d65d2f22c50441d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 03:05:34 +0000

    Add package forsee/0.0.5/forsee-0.0.5.tar.gz

---
+++ packages/forsee/forsee-0.0.5.tar.gz/forsee-0.0.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='forsee',
+    version='0.0.5',
+    author='Nikos Tzekas',
+    author_email='nt@itec-audio.com',
+    description='Exports a compile_commands.json from a Crosscore Project',
+    packages=['forsee'],
+    install_requires=[
+        # List your dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'c4=forsee.forsee:main',  
+        ],
+    },
+)
