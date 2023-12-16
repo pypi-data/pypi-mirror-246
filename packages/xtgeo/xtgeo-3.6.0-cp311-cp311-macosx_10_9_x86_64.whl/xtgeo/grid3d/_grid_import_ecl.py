1ffba435b6fd6625e2800e73e28e944d1ccb64c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 03:04:50 +0000

    Add package xtgeo/3.6.0/xtgeo-3.6.0-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/xtgeo/xtgeo-3.6.0-cp311-cp311-macosx_10_9_x86_64.whl/xtgeo/grid3d/_grid_import_ecl.py
@@ -0,0 +1,92 @@
+"""Grid import functions for Eclipse, new approach (i.e. version 2)."""
+
+
+import xtgeo
+from xtgeo.common import null_logger
+from xtgeo.grid3d._egrid import EGrid, RockModel
+from xtgeo.grid3d._grdecl_grid import GrdeclGrid, GridRelative
+
+logger = null_logger(__name__)
+
+
+def import_ecl_egrid(
+    gfile,
+    relative_to=GridRelative.MAP,
+    fileformat="egrid",
+):
+    egrid = EGrid.from_file(gfile._file, fileformat=fileformat)
+
+    result = grid_from_ecl_grid(egrid, relative_to=relative_to)
+
+    if egrid.egrid_head.file_head.rock_model == RockModel.DUAL_POROSITY:
+        result["dualporo"] = True
+        result["dualperm"] = False
+    elif egrid.egrid_head.file_head.rock_model == RockModel.DUAL_PERMEABILITY:
+        result["dualporo"] = True
+        result["dualperm"] = True
+
+    return result
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Import eclipse run suite: EGRID + properties from INIT and UNRST
+# For the INIT and UNRST, props dates shall be selected
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def import_ecl_run(
+    groot, ecl_grid, initprops=None, restartprops=None, restartdates=None
+):
+    """Import combo ECL runs."""
+    ecl_init = groot + ".INIT"
+    ecl_rsta = groot + ".UNRST"
+
+    ecl_init = xtgeo._XTGeoFile(ecl_init)
+    ecl_rsta = xtgeo._XTGeoFile(ecl_rsta)
+
+    grdprops = xtgeo.grid3d.GridProperties()
+
+    # import the init properties unless list is empty
+    if initprops:
+        initprops = xtgeo.gridproperties_from_file(
+            ecl_init.name, names=initprops, fformat="init", dates=None, grid=ecl_grid
+        )
+        grdprops.append_props(initprops.props)
+
+    # import the restart properties for dates unless lists are empty
+    if restartprops and restartdates:
+        restartprops = xtgeo.gridproperties_from_file(
+            ecl_rsta.name,
+            names=restartprops,
+            fformat="unrst",
+            dates=restartdates,
+            grid=ecl_grid,
+        )
+        grdprops.append_props(restartprops.props)
+
+    ecl_grid.gridprops = grdprops
+
+
+def import_ecl_grdecl(gfile, relative_to=GridRelative.MAP):
+    """Import grdecl format."""
+
+    grdecl_grid = GrdeclGrid.from_file(gfile._file, fileformat="grdecl")
+    return grid_from_ecl_grid(grdecl_grid, relative_to=relative_to)
+
+
+def import_ecl_bgrdecl(gfile, relative_to=GridRelative.MAP):
+    """Import binary files with GRDECL layout."""
+
+    grdecl_grid = GrdeclGrid.from_file(gfile._file, fileformat="bgrdecl")
+    return grid_from_ecl_grid(grdecl_grid, relative_to=relative_to)
+
+
+def grid_from_ecl_grid(ecl_grid, relative_to=GridRelative.MAP):
+    result = {}
+    result["coordsv"] = ecl_grid.xtgeo_coord(relative_to=relative_to)
+    result["zcornsv"] = ecl_grid.xtgeo_zcorn(relative_to=relative_to)
+    result["actnumsv"] = ecl_grid.xtgeo_actnum()
+    if relative_to == GridRelative.MAP and ecl_grid.map_axis_units is not None:
+        result["units"] = ecl_grid.map_axis_units
+    else:
+        result["units"] = ecl_grid.grid_units
+
+    return result
---
+++ packages/xtgeo/xtgeo-3.6.0-cp311-cp311-macosx_10_9_x86_64.whl/xtgeo/grid3d/_gridprop_export.py
@@ -0,0 +1,137 @@
+"""GridProperty (not GridProperies) export functions."""
+import json
+import struct
+
+import numpy as np
+import resfo
+import roffio
+
+import xtgeo
+from xtgeo.common import null_logger
+
+from ._roff_parameter import RoffParameter
+
+logger = null_logger(__name__)
+
+
+def to_file(self, pfile, fformat="roff", name=None, append=False, dtype=None, fmt=None):
+    """Export the grid property to file."""
+    logger.info("Export property to file %s as %s", pfile, fformat)
+
+    fobj = xtgeo._XTGeoFile(pfile, mode="rb")
+    if not fobj.memstream:
+        fobj.check_folder(raiseerror=OSError)
+
+    if name is None:
+        name = self.name
+
+    if "roff" in fformat:
+        binary = True
+        if "asc" in fformat:
+            binary = False
+
+        if append:
+            logger.warning(
+                "Append is not implemented for roff format, defaulting to write."
+            )
+
+        export_roff(self, fobj.name, name, binary=binary)
+
+    elif fformat == "grdecl":
+        export_grdecl(
+            self, fobj.name, name, append=append, binary=False, dtype=dtype, fmt=fmt
+        )
+
+    elif fformat == "bgrdecl":
+        export_grdecl(self, fobj.name, name, append=append, binary=True, dtype=dtype)
+
+    elif fformat == "xtgcpprop":
+        export_xtgcpprop(self, fobj.name)
+
+    else:
+        raise ValueError(f"Cannot export, invalid fformat: {fformat}")
+
+
+# Export ascii or binary ROFF format
+
+
+def export_roff(self, pfile, name, binary=True):
+    logger.info("Export roff to %s", pfile)
+    roff_param = RoffParameter.from_xtgeo_grid_property(self)
+    roff_param.name = name
+    roff_format = roffio.Format.ASCII
+    if binary:
+        roff_format = roffio.Format.BINARY
+
+    roff_param.to_file(pfile, roff_format)
+
+
+def export_grdecl(self, pfile, name, append=False, binary=False, dtype=None, fmt=None):
+    """Export ascii or binary GRDECL"""
+    vals = self.values.ravel(order="F")
+    if np.ma.isMaskedArray(vals):
+        vals = np.ma.filled(vals, self.undef)
+
+    if binary:
+        mode = "wb"
+        if append:
+            mode = "ab"
+
+        with open(pfile, mode) as fh:
+            if dtype is not None:
+                resfo.write(fh, [(name.ljust(8), vals.astype(dtype))])
+            elif self.isdiscrete:
+                resfo.write(fh, [(name.ljust(8), vals.astype(np.int32))])
+            else:
+                resfo.write(fh, [(name.ljust(8), vals.astype(np.float32))])
+
+    else:
+        mode = "w"
+        if append:
+            mode = "a"
+        with open(pfile, mode) as fh:
+            fh.write(name)
+            fh.write("\n")
+            for i, v in enumerate(vals):
+                fh.write(" ")
+                if fmt:
+                    fh.write(fmt % v)
+                elif self.isdiscrete:
+                    fh.write(str(v))
+                else:
+                    fh.write(f"{v:3e}")
+                if i % 6 == 5:
+                    fh.write("\n")
+
+            fh.write(" /\n")
+
+
+def export_xtgcpprop(self, mfile):
+    """Export to experimental xtgcpproperty format, python version."""
+    logger.info("Export as xtgcpprop...")
+    self._metadata.required = self
+
+    magic = 1351
+    if self.isdiscrete:
+        magic = 1352
+
+    prevalues = (1, magic, 4, self.ncol, self.nrow, self.nlay)
+    mystruct = struct.Struct("= i i i q q q")
+    pre = mystruct.pack(*prevalues)
+
+    meta = self.metadata.get_metadata()
+
+    with open(mfile, "wb") as fout:
+        fout.write(pre)
+
+    with open(mfile, "ab") as fout:
+        vv = self.get_npvalues1d(fill_value=self.undef)
+        vv.astype(np.float32).tofile(fout)
+
+    with open(mfile, "ab") as fout:
+        fout.write("\nXTGMETA.v01\n".encode())
+
+    with open(mfile, "ab") as fout:
+        fout.write(json.dumps(meta).encode())
+
+    logger.info("Export as xtgcpprop... done")
