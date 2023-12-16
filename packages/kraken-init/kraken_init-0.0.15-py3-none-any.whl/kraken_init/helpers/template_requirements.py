d044ea9f47d70cc8ec616a9670a80778e1633b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 04:04:17 +0000

    Add package kraken-init/0.0.15/kraken_init-0.0.15-py3-none-any.whl

---
+++ packages/kraken-init/kraken_init-0.0.15-py3-none-any.whl/kraken_init/helpers/template_requirements.py
@@ -0,0 +1,19 @@
+
+
+def get_filename(name):
+
+    return f'requirements.txt'
+
+
+def get_content(name=None):
+    """
+    """
+    content = f'''
+requests
+kraken-thing
+flask
+gunicorn
+
+    
+    '''
+    return content
