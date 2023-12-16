681a0d378950496540c4a94dbc448a7f472186c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 15:05:35 +0000

    Add package pypox/1.3.0/pypox-1.3.0.tar.gz

---
+++ packages/pypox/pypox-1.3.0.tar.gz/pypox-1.3.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pypox",
+    version="1.3.0",
+    author="Karl Robeck Alferez",
+    author_email="karlalferezfx@gmail.com",
+    description="",
+    packages=find_packages(),
+    install_requires=["uvicorn[standard]", "fastapi"],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11.0",
+)
