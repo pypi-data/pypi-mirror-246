629df705bb34f4886772146b0f51c0eaae7e5c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 05:04:59 +0000

    Add package spandrel/0.1.4/spandrel-0.1.4-py3-none-any.whl

---
+++ packages/spandrel/spandrel-0.1.4-py3-none-any.whl/spandrel/__init__.py
@@ -0,0 +1,31 @@
+__version__ = "0.1.4"
+
+from .__helpers.canonicalize import canonicalize_state_dict
+from .__helpers.loader import ModelLoader
+from .__helpers.main_registry import MAIN_REGISTRY
+from .__helpers.model_descriptor import (
+    ImageModelDescriptor,
+    MaskedImageModelDescriptor,
+    ModelBase,
+    ModelDescriptor,
+    Purpose,
+    SizeRequirements,
+    StateDict,
+)
+from .__helpers.registry import ArchRegistry, ArchSupport, UnsupportedModelError
+
+__all__ = [
+    "ArchRegistry",
+    "ArchSupport",
+    "canonicalize_state_dict",
+    "ImageModelDescriptor",
+    "MAIN_REGISTRY",
+    "MaskedImageModelDescriptor",
+    "ModelBase",
+    "ModelDescriptor",
+    "ModelLoader",
+    "Purpose",
+    "SizeRequirements",
+    "StateDict",
+    "UnsupportedModelError",
+]
