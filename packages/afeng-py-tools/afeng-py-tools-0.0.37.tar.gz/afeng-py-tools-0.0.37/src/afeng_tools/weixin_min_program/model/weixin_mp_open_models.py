f5a9fe6a1429cdbb8f7b95cba531e5fc29d88718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 15:04:52 +0000

    Add package afeng-py-tools/0.0.37/afeng-py-tools-0.0.37.tar.gz

---
+++ packages/afeng-py-tools/afeng-py-tools-0.0.37.tar.gz/afeng-py-tools-0.0.37/src/afeng_tools/weixin_min_program/model/weixin_mp_open_models.py
@@ -0,0 +1,34 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class WxMpLoginInfo(BaseModel):
+    """微信小程序登录信息"""
+    # 小程序 appId
+    mp_app_id: Optional[str] = None
+    # 会话密钥
+    session_key: Optional[str] = None
+    # 用户在开放平台的唯一标识符，若当前小程序已绑定到微信开放平台账号下会返回，详见 UnionID 机制说明。
+    union_id: Optional[str] = None
+    # 用户唯一标识
+    open_id: Optional[str] = None
+
+
+class WxMpUserInfo(BaseModel):
+    """微信小程序用户信息"""
+    # 小程序 appId
+    mp_app_id: Optional[str] = None
+    # 会话密钥
+    session_key: Optional[str] = None
+    # 用户在开放平台的唯一标识符，若当前小程序已绑定到微信开放平台账号下会返回，详见 UnionID 机制说明。
+    union_id: Optional[str] = None
+    # 用户唯一标识
+    open_id: Optional[str] = None
+    nickName: Optional[str] = None
+    avatarUrl: Optional[str] = None
+    city: Optional[str] = None
+    country: Optional[str] = None
+    province: Optional[str] = None
+    gender: Optional[int] = None
+    is_demote: Optional[bool] = None
+    language: Optional[str] = None
