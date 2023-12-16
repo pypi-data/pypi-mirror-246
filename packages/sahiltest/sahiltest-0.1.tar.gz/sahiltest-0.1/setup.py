a09774d3dee859173d7278f284b731ac6eab22e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 04:05:00 +0000

    Add package sahiltest/0.1/sahiltest-0.1.tar.gz

---
+++ packages/sahiltest/sahiltest-0.1.tar.gz/sahiltest-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sahiltest',
+    version='0.1',
+    description='A sample Python package',
+    author='Sahil Kumar',
+    author_email='sahilkanger1999@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
