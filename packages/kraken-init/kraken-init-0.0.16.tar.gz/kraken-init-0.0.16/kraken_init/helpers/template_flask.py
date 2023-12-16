bc2de015cb5653b8efada12eba7898f3ac347c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 05:04:40 +0000

    Add package kraken-init/0.0.16/kraken-init-0.0.16.tar.gz

---
+++ packages/kraken-init/kraken-init-0.0.16.tar.gz/kraken-init-0.0.16/kraken_init/helpers/template_flask.py
@@ -0,0 +1,96 @@
+
+
+def get_filename(name):
+
+    return f'{name}/flask_routes.py'
+
+
+def get_content(name=None):
+    """
+    """
+
+    
+    class_name = name.replace('kraken_', '')
+
+    class_name = class_name.capitalize()
+    class_name_collection = class_name + 's'
+    
+
+    
+    content = f'''
+
+
+from flask import Flask
+from flask import request
+from flask import Response
+from flask import redirect
+from flask import url_for
+from flask import jsonify
+from {name}.helpers import json
+
+from {name} import {name} as m
+from {name}.Class_{name} import {class_name}
+from {name}.Class_{name}s import {class_name_collection}
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+
+# Initialize flask app
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='static',
+            template_folder='templates')
+app.secret_key = b'_5#mn"F4Q8znxec]/'
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main_get():
+
+    key = 'name'
+    value = request.args.get(key)
+
+    if value:
+        r = {class_name}()
+        records = r.autocomplete(key, '%' + str(value) + '%')
+        return jsonify(records)
+
+
+    content = "Api for {name}"
+    return Response(content)
+
+
+@app.route('/<key>/<value>', methods=['GET', 'POST'])
+def search_path_get(key, value):
+
+    r = {class_name}()
+    records = r.search(key, '%' + value + '%')
+    return jsonify(records)
+
+
+@app.route('/autocomplete', methods=['GET', 'POST'])
+def autocomplete_params_get():
+
+    key = 'name'
+    value = request.args.get(key)
+
+    r = {class_name}()
+    records = r.autocomplete(key, '%' + str(value) + '%')
+    return jsonify(records)
+
+
+
+@app.route('/autocomplete/<key>/<value>', methods=['GET', 'POST'])
+def autocomplete_path_get(key, value):
+
+    r = {class_name}()
+    records = r.autocomplete(key, '%' + value + '%')
+    return jsonify(records)
+
+
+def run_api():
+    app.run(host='0.0.0.0', debug=False)
+
+
+    '''
+    return content
---
+++ packages/kraken-init/kraken-init-0.0.16.tar.gz/kraken-init-0.0.16/kraken_init/helpers/template_setup_cfg.py
@@ -0,0 +1,16 @@
+
+
+def get_filename(name):
+
+    return f'setup.cfg'
+
+
+
+def get_content(name):
+    content = '''
+    # Inside of setup.cfg
+[metadata]
+description-file = README.md
+
+    '''
+    return content
