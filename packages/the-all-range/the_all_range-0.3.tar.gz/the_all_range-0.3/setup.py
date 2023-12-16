e2a16640db9494154337db0c1d0dcfd87990a29e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 14:05:43 +0000

    Add package the-all-range/0.3/the_all_range-0.3.tar.gz

---
+++ packages/the-all-range/the_all_range-0.3.tar.gz/the_all_range-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+name="the_all_range",
+version="0.3",
+author="Ilyas",
+author_email="ilyassbihaouargui@gmail.com",
+description="create txt and csv files",
+packages=find_packages()
+)
+
+
