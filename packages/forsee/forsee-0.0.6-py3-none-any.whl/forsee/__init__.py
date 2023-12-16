42ad86faa2e8939c96a9b1ebe2f1c7573519a3b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 03:06:14 +0000

    Add package forsee/0.0.6/forsee-0.0.6-py3-none-any.whl

---
+++ packages/forsee/forsee-0.0.6-py3-none-any.whl/forsee/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+
+# This file can be empty, or you can include initialization code here.
+from . import forsee
+from . import args_handler
+from . import forsee_functions
+
+# You can also define variables or functions that should be available at the package level.
+# These will be accessible when someone imports your package.
