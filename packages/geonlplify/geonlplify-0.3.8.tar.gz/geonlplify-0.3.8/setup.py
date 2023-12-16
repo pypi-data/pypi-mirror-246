0afb8a1df00a5629b5292525450de7ca9a66219c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 03:06:09 +0000

    Add package geonlplify/0.3.8/geonlplify-0.3.8.tar.gz

---
+++ packages/geonlplify/geonlplify-0.3.8.tar.gz/geonlplify-0.3.8/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="geonlplify",
+    version="0.3.8",
+    url="https://github.com/remydecoupes/GeoNLPlify",
+    author="Rémy Decoupes",
+    author_email="",
+    description="GeoNLPlify aims to make variations of an input sentence working on spatial information contained in words",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "pandas",
+        "requests",
+        'spacy>=2.3.0,<=2.3.999',
+        'importlib-metadata>=3.7.0,<=3.999',
+        'typing-extensions>=3.7.4,<=3.999',
+        'transformers>=3.4.0,<=4.15.999',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
