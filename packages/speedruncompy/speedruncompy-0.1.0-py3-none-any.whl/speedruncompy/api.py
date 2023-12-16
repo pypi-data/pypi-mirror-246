ee412f41065dc1657d8e33e1f5936a006068843a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 04:04:33 +0000

    Add package speedruncompy/0.1.0/speedruncompy-0.1.0-py3-none-any.whl

---
+++ packages/speedruncompy/speedruncompy-0.1.0-py3-none-any.whl/speedruncompy/api.py
@@ -0,0 +1,199 @@
+import base64, json
+from .exceptions import *
+import logging
+import asyncio, aiohttp
+from requests import Response, get, post
+from requests.cookies import RequestsCookieJar
+from time import sleep
+from typing import Awaitable, Callable, Any, Optional
+
+API_URI = "https://www.speedrun.com/api/v2/"
+LANG = "en"
+ACCEPT = "application/json"
+DEFAULT_USER_AGENT = "speedruncompy/"
+
+_log = logging.getLogger("speedruncompy")
+
+class SpeedrunComPy():
+    """Api class. Holds a unique PHPSESSID and user_agent, as well as its own logger."""
+    def __init__(self, user_agent = None) -> None:
+        self.cookie_jar = RequestsCookieJar()
+        self.user_agent = user_agent
+        if user_agent is None:
+            self._log = _log
+        else:
+            self._log = _log.getChild(user_agent)
+    
+    def do_get(self, endpoint: str, params: dict = {}):
+        _header = {"Accept-Language": LANG, "Accept": ACCEPT, "User-Agent": f"{DEFAULT_USER_AGENT}{self.user_agent}"}
+        # Params passed to the API by the site are json-base64 encoded, even though std params are supported.
+        # We will do the same in case param support is retracted.
+        paramsjson = bytes(json.dumps(params, separators=(",", ":")).strip(), "utf-8")
+        _r = base64.urlsafe_b64encode(paramsjson).replace(b"=", b"")
+        self._log.debug(f"GET {API_URI}{endpoint} w/ params {paramsjson}")
+        return get(url=f"{API_URI}{endpoint}", headers=_header, params={"_r": _r})
+
+    def do_post(self, endpoint:str, params: dict = {}, _setCookie=True):
+        _header = {"Accept-Language": LANG, "Accept": ACCEPT, "User-Agent": f"{DEFAULT_USER_AGENT}{self.user_agent}"}
+        self._log.debug(f"POST {API_URI}{endpoint} w/ params {params}")
+        response = post(url=f"{API_URI}{endpoint}", headers=_header, cookies=self.cookie_jar, json=params)
+        if _setCookie and response.cookies:
+            self.cookie_jar.update(response.cookies)
+        return response
+
+    async def do_async_get(self, endpoint: str, params: dict = {}) -> tuple[bytes, int]:
+        _header = {"Accept-Language": LANG, "Accept": ACCEPT, "User-Agent": f"{DEFAULT_USER_AGENT}{self.user_agent}"}
+        # Params passed to the API by the site are json-base64 encoded, even though std params are supported.
+        # We will do the same in case param support is retracted.
+        paramsjson = bytes(json.dumps(params, separators=(",", ":")).strip(), "utf-8")
+        _r = base64.urlsafe_b64encode(paramsjson).replace(b"=", b"").decode()
+        self._log.debug(f"async GET {API_URI}{endpoint} w/ params {paramsjson}")
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=f"{API_URI}{endpoint}", headers=_header, params={"_r": _r}) as response:
+                return (await response.read(), response.status)
+        
+    async def do_async_post(self, endpoint:str, params: dict = {}, _setCookie=True) -> tuple[bytes, int]:
+        _header = {"Accept-Language": LANG, "Accept": ACCEPT, "User-Agent": f"{DEFAULT_USER_AGENT}{self.user_agent}"}
+        self._log.debug(f"async POST {API_URI}{endpoint} w/ params {params}")
+        async with aiohttp.ClientSession() as session:
+            async with session.post(url=f"{API_URI}{endpoint}", headers=_header,
+                                    cookies=self.cookie_jar, json=params) as response:
+                if _setCookie and response.cookies:
+                    self.cookie_jar.update(response.cookies)
+                return (await response.read(), response.status) 
+
+_default = SpeedrunComPy()
+
+def set_PHPSESSID(phpsessionid):
+    _default.cookie_jar.update({"PHPSESSID": phpsessionid})
+
+class BaseRequest():
+    def __init__(self, method: Callable[[str, dict[str, Any]], Response], 
+                 async_method: Callable[[str, dict[str, Any]], Awaitable[tuple[bytes, int]]],
+                 endpoint, **params):
+        self.method = method
+        self.async_method = async_method
+        self.endpoint = endpoint
+        self.params = params
+    
+    def update_params(self, **kwargs):
+        """Updates parameters using values set in kwargs"""
+        self.params.update(kwargs)
+
+    def perform(self, retries=5, delay=1, **kwargs) -> dict:
+        self.response = self.method(self.endpoint, self.params | kwargs)
+
+        if (self.response.status_code >= 500 and self.response.status_code <= 599) or self.response.status_code == 408:
+            if retries > 0:
+                _log.error(f"SRC returned error {self.response.status_code} {self.response.content}. Retrying with delay {delay}:")
+                for attempt in range(0, retries+1):
+                    self.response = self.method(self.endpoint, self.params)
+                    if not (self.response.status_code >= 500 and self.response.status_code <= 599) or self.response.status_code == 408: 
+                        break
+                    _log.error(f"Retry {attempt} returned error {self.response.status_code} {self.response.content}")
+                    sleep(delay)                      
+                else:
+                    if self.response.status_code == 408: raise RequestTimeout(self)
+                    else: raise ServerException(self)
+
+        if self.response.status_code == 400: raise BadRequest(self)
+        if self.response.status_code == 401: raise Unauthorized(self)
+        if self.response.status_code == 403: raise Forbidden(self)
+        if self.response.status_code == 404: raise NotFound(self)
+        if self.response.status_code == 405: raise MethodNotAllowed(self)
+        if self.response.status_code == 408: raise RequestTimeout(self)
+        if self.response.status_code == 429: raise RateLimitExceeded(self)
+
+        if (self.response.status_code >= 500 and self.response.status_code <= 599): raise ServerException(self)
+
+        if self.response.status_code < 200 or self.response.status_code > 299:
+            _log.error(f"Unknown response error returned from SRC! {self.response.status_code} {self.response.content}")
+            raise APIException(self)
+
+        return json.loads(self.response.content)
+    
+    async def perform_async(self, retries=5, delay=1, **kwargs) -> dict:
+        self.response = await self.async_method(self.endpoint, self.params | kwargs)
+        content = self.response[0]
+        status = self.response[1]
+
+        if (status >= 500 and status <= 599) or status == 408:
+            if retries > 0:
+                _log.error(f"SRC returned error {status} {content}. Retrying with delay {delay}:")
+                for attempt in range(0, retries+1):
+                    self.response = await self.async_method(self.endpoint, self.params)
+                    status = self.response[0]
+                    content = self.response[1]
+                    if not (status >= 500 and status <= 599) or status == 408: 
+                        break
+                    _log.error(f"Retry {attempt} returned error {status} {self.response.content}")
+                    await asyncio.sleep(delay)
+                else:
+                    if status == 408: raise RequestTimeout(self)
+                    else: raise ServerException(self)
+        
+        if status == 400: raise BadRequest(self)
+        if status == 401: raise Unauthorized(self)
+        if status == 403: raise Forbidden(self)
+        if status == 404: raise NotFound(self)
+        if status == 405: raise MethodNotAllowed(self)
+        if status == 408: raise RequestTimeout(self)
+        if status == 429: raise RateLimitExceeded(self)
+
+        if (status >= 500 and status <= 599): raise ServerException(self)
+
+        if status < 200 or status > 299:
+            _log.error(f"Unknown response error returned from SRC! {status} {self.response[0]}")
+            raise APIException(self)
+
+        return json.loads(content.decode())
+
+class BasePaginatedRequest(BaseRequest):
+    def perform_all(self, retries=5, delay=1) -> dict:
+        """Get all pages and return a dict of {pageNo : pageData}. Subclasses may merge this into a combined result."""
+        return self._perform_all_raw(retries, delay)
+    
+    async def perform_all_async(self, retries=5, delay=1) -> dict:
+        """Get all pages and return a dict of {pageNo : pageData}. Subclasses may merge this into a combined result."""
+        return await self._perform_all_async_raw(retries, delay)
+    
+    def _perform_all_raw(self, retries=5, delay=1):
+        """Get all pages and return a dict of {pageNo : pageData}."""
+        self.pages = {}
+        self.params.update(page=1)
+        while True:
+            page = self.params["page"]
+            data = self._perform_page(retries=retries, delay=delay) # this post-increments page
+            self.pages[page] = data
+            if page >= data.get("pagination", {}).get("pages", 1): 
+                return self.pages
+    
+    async def _perform_all_async_raw(self, retries=5, delay=1):
+        self.pages = {}
+        self.pages[1] = await self.perform_async(retries, delay, page=1)
+        numpages = self.pages[1]["pagination"]["pages"]
+        if numpages > 1:
+            results = await asyncio.gather(*[self.perform_async(retries, delay, page=p) for p in range(2, numpages)])
+            self.pages.update({p + 2:result for p, result in enumerate(results)})
+        return self.pages
+    
+    def _perform_page(self, retries=5, delay=1) -> Optional[dict]:
+        """Get the current page & advance counter to next page. Returns None if beyond the final page."""
+        if "page" not in self.params: self.params["page"] = 1
+
+        data = self.perform(retries, delay)
+        if data["pagination"]["page"] != self.params["page"]:
+            return None # Gone past the last page, don't give result
+        
+        self.params.update(page=(self.params.get("page", 0) + 1)) # advance page
+        return data
+
+class GetRequest(BaseRequest):
+    def __init__(self, endpoint, _api:SpeedrunComPy=None, **params) -> None:
+        if _api is None: _api = _default
+        super().__init__(method=_api.do_get, async_method=_api.do_async_get, endpoint=endpoint, **params)
+
+class PostRequest(BaseRequest):
+    def __init__(self, endpoint, _api:SpeedrunComPy=None, **params) -> None:
+        if _api is None: _api = _default
+        super().__init__(method=_api.do_post, async_method=_api.do_async_post, endpoint=endpoint, **params)
---
+++ packages/speedruncompy/speedruncompy-0.1.0-py3-none-any.whl/speedruncompy/endpoints.py
@@ -0,0 +1,272 @@
+from .api import BasePaginatedRequest, GetRequest, PostRequest, SpeedrunComPy, _log
+from .exceptions import SrcpyException
+from .enums import *
+
+"""
+GET requests are all unauthed & do not require PHPSESSID.
+"""
+
+class GetGameLeaderboard2(GetRequest, BasePaginatedRequest):
+    """The default leaderboard view."""
+    def __init__(self, gameId: str, categoryId: str, _api: SpeedrunComPy = None, **params) -> None:
+        page = params.pop("page", None)
+        param_construct = {"params": {"gameId": gameId, "categoryId": categoryId}}
+        param_construct["params"].update(params)
+        super().__init__("GetGameLeaderboard2", _api=_api, page=page, **param_construct)
+
+    def _combine_results(self, pages: dict):
+        runList = []
+        for p in pages.values():
+            runList += p["runList"]
+        return {"runList": runList, "playerList": [player for player in pages[1]["playerList"]]}
+
+    def perform_all(self, retries=5, delay=1) -> dict:
+        """Returns a combined dict of all pages. `pagination` is removed."""
+        pages = super().perform_all(retries, delay)
+        return self._combine_results(pages)
+
+    async def perform_all_async(self, retries=5, delay=1) -> dict:
+        """Returns a combined dict of all pages. `pagination` is removed."""
+        pages = await super().perform_all_async(retries, delay)
+        return self._combine_results(pages)
+
+class GetGameLeaderboard(GetRequest, BasePaginatedRequest):
+    """WARN: This is NOT the view used by SRC! It may be removed at any time!
+
+    This view is included as it is special & returns a bunch of extra information that may be useful."""
+    def __init__(self, gameId: str, categoryId: str, _api: SpeedrunComPy = None, **params) -> None:
+        page = params.pop("page", None)
+        param_construct = {"params": {"gameId": gameId, "categoryId": categoryId}}
+        param_construct["params"].update(params)
+        super().__init__("GetGameLeaderboard", _api=_api, page=page, **param_construct)
+
+class GetGameData(GetRequest):
+    def __init__(self, gameId: str = None, gameUrl: str = None, **params) -> None:
+        if gameId is None and gameUrl is None: raise SrcpyException("GetGameData requires gameId or gameUrl")
+        super().__init__("GetGameData", gameId=gameId, gameUrl=gameUrl, **params)
+
+class GetGameRecordHistory(GetRequest):
+    """Get the record history of a category.
+    
+    #### Other parameters
+    - `values[]: list[variableId, valueIds[]]`
+    - `emulator: (bool?)` TODO: check
+    - `obsolete: bool`"""
+    def __init__(self, gameId: str, categoryId: str, _api: SpeedrunComPy = None, **params) -> None:
+        page = params.pop("page", None)
+        param_construct = {"params": {"gameId": gameId, "categoryId": categoryId}}
+        param_construct["params"].update(params)
+        super().__init__("GetGameRecordHistory", _api=_api, page=page, **param_construct)
+
+class GetSearch(GetRequest):
+    """Search for an object based on its name.
+
+    #### Other parameters:
+    - `limit: int` max. 500
+    - `includeGames: bool`
+    - `includeNews: bool`
+    - `includePages: bool`
+    - `includeSeries: bool`
+    - `includeUsers: bool`
+    """
+    def __init__(self, query: str, **params) -> None:
+        super().__init__("GetSearch", query=query, **params)
+
+class GetLatestLeaderboard(GetRequest):
+    def __init__(self, **params) -> None:
+        super().__init__("GetLatestLeaderboard", **params)
+
+class GetRun(GetRequest):
+    def __init__(self, runId: str, **params) -> None:
+        super().__init__("GetRun", runId=runId, **params)
+
+"""
+POST requests may require auth
+"""
+
+# Session
+class PutAuthLogin(PostRequest):
+    def __init__(self, name: str, password: str, token: str = None, **params) -> None:
+        super().__init__("PutAuthLogin", name=name, password=password, token=token, **params)
+
+class PutAuthLogout(PostRequest):
+    def __init__(self, **params) -> None:
+        super().__init__("PutAuthLogout", **params)
+
+class GetSession(PostRequest):
+    def __init__(self, **params) -> None:
+        super().__init__("GetSession", **params)
+
+class PutSessionPing(PostRequest):
+    def __init__(self, **params) -> None:
+        super().__init__("PutSessionPing", **params)
+
+# Supermod actions
+class GetAuditLogList(PostRequest):
+    def __init__(self, gameId: str = None, seriesId: str = None, eventType: eventType = eventType.NONE, page: int = 1, **params) -> None:
+        if gameId is None and seriesId is None: raise SrcpyException("GetAuditLogList requires gameId or seriesId")
+        super().__init__("GetAuditLogList", gameId=gameId, eventType=eventType, page=page, **params)
+
+# Mod actions
+class GetGameSettings(PostRequest):
+    def __init__(self, gameId: str, **params) -> None:
+        super().__init__("GetGameSettings", gameId=gameId, **params)
+
+class PutGameSettings(PostRequest):
+    def __init__(self, gameId: str, settings: dict, **params) -> None:
+        super().__init__("PutGameSettings", gameId=gameId, settings=settings, **params)
+
+# Run verification
+class GetModerationGames(PostRequest):
+    def __init__(self, **params) -> None:
+        super().__init__("GetModerationGames", **params)
+
+class GetModerationRuns(PostRequest, BasePaginatedRequest):
+    def __init__(self, gameId: str, limit: int = 100, page: int = 1, **params) -> None:
+        super().__init__("GetModerationRuns", gameId=gameId, limit=limit, page=page, **params)
+    
+    def _combine_results(self, pages: dict):
+        games = [pages[1]["games"][0]]
+        categories, levels, platforms, players, regions, runs, users, values, variables = ([] for i in range(9))
+        for page in pages.values():
+            for c in (c for c in page["categories"] if c not in categories): categories.append(c)
+            for l in (l for l in page["levels"] if l not in levels): levels.append(l)
+            for p in (p for p in page["platforms"] if p not in platforms): platforms.append(p)
+            for p in (p for p in page["players"] if p not in players): players.append(p)
+            for r in (r for r in page["regions"] if r not in regions): regions.append(r)
+            for u in (u for u in page["users"] if u not in users): users.append(u)
+            for v in (v for v in page["values"] if v not in values): values.append(v)
+            for v in (v for v in page["variables"] if v not in variables): variables.append(v)
+            runs += page["runs"]
+        return {"categories": categories, "games": games, "levels": levels, "platforms": platforms, "players": players,
+                "regions": regions, "runs": runs, "users": users, "values": values, "variables": variables}
+
+    def perform_all(self, retries=5, delay=1) -> dict:
+        """Returns a combined dict of all pages. `pagination` is removed."""
+        pages = super().perform_all(retries, delay)
+        return self._combine_results(pages)
+
+    async def perform_all_async(self, retries=5, delay=1) -> dict:
+        """Returns a combined dict of all pages. `pagination` is removed."""
+        pages = await super().perform_all_async(retries, delay)
+        return self._combine_results(pages)
+
+class PutRunAssignee(PostRequest):
+    def __init__(self, assigneeId: str, runId: str, **params) -> None:
+        super().__init__("PutRunAssignee", assigneeId=assigneeId, runId=runId, **params)
+
+class PutRunVerification(PostRequest):
+    def __init__(self, runId: str, verified: int, **params) -> None:
+        super().__init__("PutRunVerification", runId=runId, verified=verified, **params)
+
+# Run management
+class GetRunSettings(PostRequest):
+    def __init__(self, runId: str, **params) -> None:
+        super().__init__("GetRunSettings", runId=runId, **params)
+
+class PutRunSettings(PostRequest):
+    def __init__(self, csrfToken: str, settings: dict, **params) -> None:
+        """Sets a run's settings. Note that the runId is contained in `settings`."""
+        super().__init__("PutRunSettings", csrfToken=csrfToken, settings=settings, **params)
+
+# User inbox actions
+class GetConversations(PostRequest):
+    def __init__(self, **params) -> None:
+        super().__init__("GetConversations", **params)
+
+class GetConversationMessages(PostRequest):
+    def __init__(self, **params) -> None:
+        super().__init__("GetConversationMessages", **params)
+
+# User notifications
+class GetNotifications(PostRequest, BasePaginatedRequest):
+    def __init__(self, **params) -> None:
+        super().__init__("GetNotifications", **params)
+
+    def _combine_results(self, pages: dict):
+        notifications = []
+        for p in pages.values():
+            notifications += p["notifications"]
+        return {"unreadCount": pages[1]["unreadCount"], "notifications": notifications}
+
+    def perform_all(self, retries=5, delay=1) -> dict:
+        """Returns a combined dict of all pages. `pagination` is removed."""
+        pages = super().perform_all(retries, delay)
+        return self._combine_results(pages)
+
+    async def perform_all_async(self, retries=5, delay=1) -> dict:
+        """Returns a combined dict of all pages. `pagination` is removed."""
+        pages = await super().perform_all_async(retries, delay)
+        return self._combine_results(pages)
+
+# User settings
+class GetUserSettings(PostRequest):
+    """Gets a user's settings. Note that unless you are a site mod, you can only get your own settings."""
+    def __init__(self, userUrl: str, **params) -> None:
+        super().__init__("GetUserSettings", userUrl=userUrl, **params)
+
+class PutUserSettings(PostRequest):
+    def __init__(self, userUrl: str, settings: dict, **params) -> None:
+        super().__init__("PutUserSettings", userUrl=userUrl, settings=settings, **params)
+
+# Comment Actions
+class GetCommentList(PostRequest):
+    def __init__(self, itemId: str, itemType: int, **params) -> None:
+        super().__init__("GetCommentList", itemId=itemId, itemType=itemType, **params)
+
+class GetCommentable(PostRequest):
+    def __init__(self, itemId: str, itemType: int, **params) -> None:
+        super().__init__("GetCommentable", itemId=itemId, itemType=itemType, **params)
+
+class PutComment(PostRequest):
+    def __init__(self, itemId: str, itemType: int, text: str, **params) -> None:
+        super().__init__("PutComment", itemId=itemId, itemType=itemType, text=text, **params)
+
+#TODO: test params
+class PutCommentableSettings(PostRequest):
+    def __init__(self, itemId: str, itemType: int, **params) -> None:
+        super().__init__("PutCommentableSettings", itemId=itemId, itemType=itemType, **params)
+
+# Thread Actions
+class GetThread(PostRequest, BasePaginatedRequest):
+    def __init__(self, id: str, **params) -> None:
+        super().__init__("GetThread", id=id, **params)
+
+    def _combine_results(self, pages: dict):
+        commentList = []
+        for p in pages.values():
+            commentList += p["commentList"]
+        return {"thread": pages[1]["thread"], "commentList": commentList, "userList": pages[1]["userList"], "likeList": pages[1]["likeList"]}
+
+    def perform_all(self, retries=5, delay=1) -> dict:
+        """Returns a combined dict of all pages. `pagination` is removed."""
+        pages = super().perform_all(retries, delay)
+        return self._combine_results(pages)
+
+    async def perform_all_async(self, retries=5, delay=1) -> dict:
+        """Returns a combined dict of all pages. `pagination` is removed."""
+        pages = await super().perform_all_async(retries, delay)
+        return self._combine_results(pages)
+
+class GetThreadReadStatus(PostRequest):
+    def __init__(self, threadIds: list[str], **params) -> None:
+        super().__init__("GetThreadReadStatus", threadIds=threadIds, **params)
+
+class PutThreadRead(PostRequest):
+    def __init__(self, threadId: str, **params) -> None:
+        super().__init__("PutThreadRead", threadId=threadId, **params)
+
+# Forum actions
+class GetForumList(PostRequest):
+    def __init__(self, **params) -> None:
+        super().__init__("GetForumList", **params)
+
+class GetForumReadStatus(PostRequest):
+    def __init__(self, forumIds: list[str], **params) -> None:
+        super().__init__("GetForumReadStatus", forumIds=forumIds, **params)
+
+# Theme actions
+class GetThemeSettings(PostRequest):
+    def __init__(self, **params) -> None:
+        """Provide either userId or gameId"""
+        super().__init__("GetThemeSettings", **params)
