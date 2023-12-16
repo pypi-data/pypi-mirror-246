3a26e05faf4bc229b791571b1ca3f532398c005b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-16 03:04:53 +0000

    Add package example-pkg-Ilyas/0.2/example_pkg_Ilyas-0.2.tar.gz

---
+++ packages/example-pkg-Ilyas/example_pkg_Ilyas-0.2.tar.gz/example_pkg_Ilyas-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+name="example_pkg_Ilyas",
+version="0.2",
+author="Ilyas",
+author_email="ilyassbihaouargui@gmail.com",
+description="Csv Json Txt",
+packages=find_packages()
+)
+
+
