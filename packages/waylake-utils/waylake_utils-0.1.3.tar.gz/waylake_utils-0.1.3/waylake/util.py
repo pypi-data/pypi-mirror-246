3dd820375d010c4fa69fa1e561e046f6ebd6fbde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 15:05:22 +0000

    Add package waylake-utils/0.1.3/waylake_utils-0.1.3.tar.gz

---
+++ packages/waylake-utils/waylake_utils-0.1.3.tar.gz/waylake_utils-0.1.3/waylake/util.py
@@ -0,0 +1,29 @@
+import logging.handlers
+import os
+
+def setup_logger(name, caller_file, log_dir='logs'):
+    # Determine the absolute path to the log directory based on the caller's file location
+    base_path = os.path.dirname(os.path.abspath(caller_file))
+    log_path = os.path.join(base_path, log_dir)
+
+    # Create the log directory if it doesn't exist
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+
+    # Configure logging
+    logger = logging.getLogger(name)
+    formatter = logging.Formatter('[%(levelname)s|%(filename)s:%(lineno)s] %(asctime)s > %(message)s')
+
+    # File handler
+    fileHandler = logging.FileHandler(os.path.join(log_path, f'{name}.log'))
+    fileHandler.setFormatter(formatter)
+
+    # Stream handler (for console output)
+    streamHandler = logging.StreamHandler()
+    streamHandler.setFormatter(formatter)
+
+    logger.addHandler(fileHandler)
+    logger.addHandler(streamHandler)
+    logger.setLevel(logging.DEBUG)
+
+    return logger
