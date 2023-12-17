d8f36c6660fe9821761a4b96b16e878f61698de5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 04:04:33 +0000

    Add package tuchuang/4.0.0/tuchuang-4.0.0.tar.gz

---
+++ packages/tuchuang/tuchuang-4.0.0.tar.gz/tuchuang-4.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='tuchuang',
+    version='4.0.0',
+    description='一个免费而简单图床(喜欢就关注:python学霸微信公众号)',
+    author='Python学霸',
+    author_email='python@xueba.com',
+    packages=['tuchuang'],
+    install_requires=['requests'],
+)
