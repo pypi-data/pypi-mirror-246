9b323eaa2cf3bdf1590b0293b81cbbe223a961e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 03:07:17 +0000

    Add package HelloPypiApacph/0.1.0/HelloPypiApacph-0.1.0.tar.gz

---
+++ packages/HelloPypiApacph/HelloPypiApacph-0.1.0.tar.gz/HelloPypiApacph-0.1.0/HelloPypiApacph/__init__.py
@@ -0,0 +1,17 @@
+'''
+author: cph
+since:  20231215
+'''
+import calc
+import dt
+
+__version__ = "0.1.0"
+
+def print2():
+    print('Hello, PyPI...')
+    
+if __name__ == '__main__':
+    print2()
+    calc.printPi()
+    dt.printDT()
+    
---
+++ packages/HelloPypiApacph/HelloPypiApacph-0.1.0.tar.gz/HelloPypiApacph-0.1.0/HelloPypiApacph/calc.py
@@ -0,0 +1,11 @@
+'''
+author: cph
+since:  20231215
+'''
+import math
+
+def printPi():
+    print(f'PI = {math.pi}')
+    
+if __name__ == '__main__':
+    printPi()
