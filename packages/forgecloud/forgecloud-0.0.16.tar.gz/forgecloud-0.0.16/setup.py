864dd82e2c8b31024fb5ede4d36e0cd418f857a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 03:08:12 +0000

    Add package forgecloud/0.0.16/forgecloud-0.0.16.tar.gz

---
+++ packages/forgecloud/forgecloud-0.0.16.tar.gz/forgecloud-0.0.16/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='forgecloud',  
+    version='0.0.16', 
+    author='Niclas Stoltenberg',  
+    author_email='niclasjensen@example.com',  
+    description='A package to connect and manage Forge apps with Google Cloud Functions',  
+    long_description=open('README.md').read(),  
+    long_description_content_type='text/markdown',  
+    url='https://github.com/Ns85/forgecloud',  
+    packages=find_packages(), 
+    install_requires=[
+        'pydantic==1.10.13 ',
+        'firebase-functions',
+        'firebase_admin',
+        'aiohttp',
+        'asyncio',
+        'python-dotenv' 
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',  
+        'Intended Audience :: Developers',  
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',  
+        'Programming Language :: Python :: 3',  
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    keywords='forge google-cloud-functions integration',  # Keywords for your package
+    python_requires='>=3.7',  # Minimum version requirement of Python
+)
