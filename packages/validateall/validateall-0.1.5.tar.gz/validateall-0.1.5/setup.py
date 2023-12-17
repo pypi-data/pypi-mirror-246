65374af1cdf89949b6cb3dc9eeed22a0ef8e0920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 04:05:13 +0000

    Add package validateall/0.1.5/validateall-0.1.5.tar.gz

---
+++ packages/validateall/validateall-0.1.5.tar.gz/validateall-0.1.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='validateall',
+    version='0.1.5', 
+    packages=['validateall'],
+    install_requires=[
+        'phonenumbers>=8.13.26'
+
+    ],
+)
