9379cd890ef95ae81382f0ff5d2e4bb931272718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 04:05:14 +0000

    Add package extended-setup-tools/0.2.0/extended-setup-tools-0.2.0.tar.gz

---
+++ packages/extended-setup-tools/extended-setup-tools-0.2.0.tar.gz/extended-setup-tools-0.2.0/setup.py
@@ -0,0 +1,26 @@
+from extended_setup import *
+
+SingleScriptModuleSetup('extended_setup').setup \
+(
+    short_description = "Python tool for helping making shorter and smarter setup.py scripts",
+    category = 'tools',
+    min_python_version = '3.6',
+    classifiers =
+    [
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+)
