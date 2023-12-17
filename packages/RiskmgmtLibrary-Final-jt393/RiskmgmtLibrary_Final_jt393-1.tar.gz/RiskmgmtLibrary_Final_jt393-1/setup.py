157d494e570f854b4a620a263f12cb89b1393ac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 14:10:31 +0000

    Add package RiskmgmtLibrary-Final-jt393/1/RiskmgmtLibrary_Final_jt393-1.tar.gz

---
+++ packages/RiskmgmtLibrary-Final-jt393/RiskmgmtLibrary_Final_jt393-1.tar.gz/RiskmgmtLibrary_Final_jt393-1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RiskmgmtLibrary_Final_jt393',  # Change to your package name
+    version='1',
+    py_modules=['myfunctionjt'],  # Name of your Python file
+    author='Jingyu Tan',
+    author_email='t1169242950@gmail.com',
+    description='Risk management',
+)
