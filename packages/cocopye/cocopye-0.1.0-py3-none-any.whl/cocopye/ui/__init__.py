76c3b983d5a077aa2291102fa3e88563c8377b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 03:05:24 +0000

    Add package cocopye/0.1.0/cocopye-0.1.0-py3-none-any.whl

---
+++ packages/cocopye/cocopye-0.1.0-py3-none-any.whl/cocopye/ui/__init__.py
@@ -0,0 +1,10 @@
+"""
+This module contains the terminal user interface and the webserver (should we move the server to a higher level?).
+
+### Submodules
+
+- **terminal**: Terminal user interface
+- **graphical**: Graphical user interface
+- **external**: Functions for checking and downloading external dependencies (binaries and data)
+- **config**: Configuration file and command line arguments
+"""
