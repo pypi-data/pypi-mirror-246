4622eb9e8e3fe7366cd799cfc7e3f1a98b229e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 04:04:59 +0000

    Add package gammaspotter/0.2.4/gammaspotter-0.2.4.tar.gz

---
+++ packages/gammaspotter/gammaspotter-0.2.4.tar.gz/gammaspotter-0.2.4/README.md
@@ -0,0 +1,33 @@
+# gammaspotter
+[![documentation](https://img.shields.io/badge/docs-mkdocs%20material-blue.svg?style=flat)](https://tijnsc.github.io/gammaspotter/)
+[![pypi version](https://img.shields.io/pypi/v/gammaspotter.svg)](https://pypi.org/project/gammaspotter/)
+
+The [gammaspotter](https://github.com/tijnsc/gammaspotter/) application is designed to identify radioactive sources in gamma spectra.
+
+---
+
+**[Features](#features)** - **[Installation](#installation)**
+
+![gammaspotter_window](https://raw.githubusercontent.com/tijnsc/gammaspotter/main/.github/images/gammaspotter_window.png)
+
+## Features
+
+- **Measurement Calibration** The calibrate tab lets the user calibrate a gamma spectrum by comparing known energies to peak energies.
+
+- **Data Visualization:** The analyze tab offers an interactive visualization to help you gain insights into your data.
+
+- **Statistical Analysis:** You can perform statistical analysis on your calibrated data using built-in functions and algorithms. This includes peak detection, peak fitting and isotope match detection.
+
+- **Report Generation:** Once you have completed your analysis, the analyze tab enables you to generate reports summarizing your findings. You can access visualizations and statistical results.
+
+## Installation
+
+You can easily install `gammaspotter` using `pip`. First, ensure you have Python and pip installed. Then, open a terminal or command prompt and execute the following command:
+
+```bash
+pip install gammaspotter
+```
+
+This command will download and install the latest version of gammaspotter from [PyPI](https://pypi.org/).
+
+You can now start the application by running the `gammaspotter` command in a terminal.
---
+++ packages/gammaspotter/gammaspotter-0.2.4.tar.gz/gammaspotter-0.2.4/src/gammaspotter/match_features.py
@@ -0,0 +1,66 @@
+from math import sqrt
+from scipy.special import erfc
+import pandas as pd
+
+
+class MatchFeatures:
+    def __init__(self, data_peaks: pd.DataFrame, catalog_data: pd.DataFrame):
+        """Class for matching the found gamma peaks with the literature energies.
+
+        Args:
+            data_peaks (pd.DataFrame): DataFrame containing the found gamma peaks.
+            catalog_data (pd.DataFrame): DataFrame containing the literature energies.
+        """
+        self.data_peaks = data_peaks
+        self.catalog_isotopes = catalog_data.iloc[:, [0, 1]].values.tolist()
+
+    def match_isotopes(self) -> pd.DataFrame:
+        """Function for matching the found gamma peaks with the literature energies.
+
+        Returns:
+            pd.DataFrame: A sorted DataFrame of possible sources.
+        """
+        output_list = []
+        peak_nr = 1
+
+        # looping every known source by every peak for finding all the sigmas
+        for found_isotope in self.data_peaks.itertuples(index=False):
+            for catalog_isotope in self.catalog_isotopes:
+                literature_energy = catalog_isotope[0]
+                literature_isotope = catalog_isotope[1]
+                measured_energy = found_isotope[1]
+                std_meas = found_isotope[2]
+
+                z_score = (measured_energy - literature_energy) / std_meas
+                percentage_match = self.erfc_pecentage(abs(z_score))
+
+                if percentage_match > 0:
+                    output_list.append(
+                        [
+                            peak_nr,
+                            literature_isotope,
+                            percentage_match,
+                            literature_energy,
+                        ]
+                    )
+            peak_nr += 1
+
+        output_df = pd.DataFrame(
+            output_list, columns=["Peak Number", "Isotope", "Percentage", "Energy"]
+        )
+        sorted_df = output_df.sort_values(
+            by=["Peak Number", "Percentage"], ascending=[True, False]
+        )
+
+        return sorted_df
+
+    def erfc_pecentage(self, z_score):
+        """Function for calculating the percentage match between the measured and literature energy.
+
+        Args:
+            z_score (float): The z_score of the measured data compared to the literature value.
+
+        Returns:
+            float: The percentage match between the measured and literature energy.
+        """
+        return round(erfc(z_score / sqrt(2)) * 100, 2)
