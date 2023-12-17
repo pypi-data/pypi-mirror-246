fa669afb556ce345a7a4e25d16d325cb6c9193d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 03:05:18 +0000

    Add package PlutoniumMCPE/0.0.1.dev1/PlutoniumMCPE-0.0.1.dev1.tar.gz

---
+++ packages/PlutoniumMCPE/PlutoniumMCPE-0.0.1.dev1.tar.gz/PlutoniumMCPE-0.0.1.dev1/plutonium/bounty.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import pathlib
+
+class bounty:
+    def __init__(self) -> None:
+        self.url = {
+            'base': 'https://www.plutonium.best'
+        }
+    
+    def getHighest(self):
+        url = self.url['base']
+        
+        path = pathlib.Path(__file__).parent.resolve()
+        with open(f'{path}\\assets\\headers.txt', 'r') as file:
+            headers = json.load(file)
+        
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        if response.status_code != 200:
+            return {'code': response.status_code, 'data': None}
+        else:
+            name = soup.find('p', class_='wanted-name').get_text()
+            reward = soup.find('p', class_='wanted-reward').get_text()
+            
+            return {
+                'code': response.status_code,
+                'data': {
+                    'name': name,
+                    'reward': reward
+                }
+            }
---
+++ packages/PlutoniumMCPE/PlutoniumMCPE-0.0.1.dev1.tar.gz/PlutoniumMCPE-0.0.1.dev1/plutonium/practice.py
@@ -0,0 +1,147 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import pathlib
+
+class practice:
+    def __init__(self) -> None:
+        self.url = {
+            'base': 'https://www.plutonium.best',
+            'build': '/stats/practice/build',
+            'gapple': '/stats/practice/gapple',
+            'nodebuff': '/stats/practice/nodebuff',
+            'sumo': '/stats/practice/sumo'
+        }
+
+        
+    def build(self):
+        url = self.url['base'] + self.url['build']
+        
+        path = pathlib.Path(__file__).parent.resolve()
+        with open(f'{path}\\assets\\headers.txt', 'r') as file:
+            headers = json.load(file)
+        
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        if response.status_code != 200:
+            return {'code': response.status_code, 'data': None}
+        else:
+            topPractice = soup.find_all('tr')[1:]
+            
+            result = {
+                'code': response.status_code,
+                'data': {}
+            }
+            
+            for i in range(len(topPractice)):
+                playerTop = int(topPractice[i].find_all('td')[0].get_text())
+                
+                name = topPractice[i].find_all('td')[1].get_text()
+                
+                kills = topPractice[i].find_all('td')[2].get_text()
+                deaths = topPractice[i].find_all('td')[3].get_text()
+                ratio = topPractice[i].find_all('td')[4].get_text()
+                
+                result['data'][int(playerTop)] = {'name': name, 'kills': kills, 'deaths': deaths, 'ratio': ratio}
+            
+            return result
+    
+    def gapple(self):
+        url = self.url['base'] + self.url['gapple']
+        
+        path = pathlib.Path(__file__).parent.resolve()
+        with open(f'{path}\\assets\\headers.txt', 'r') as file:
+            headers = json.load(file)
+        
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        if response.status_code != 200:
+            return {'code': response.status_code, 'data': None}
+        else:
+            topPractice = soup.find_all('tr')[1:]
+            
+            result = {
+                'code': response.status_code,
+                'data': {}
+            }
+            
+            for i in range(len(topPractice)):
+                playerTop = int(topPractice[i].find_all('td')[0].get_text())
+                
+                name = topPractice[i].find_all('td')[1].get_text()
+                
+                kills = topPractice[i].find_all('td')[2].get_text()
+                deaths = topPractice[i].find_all('td')[3].get_text()
+                ratio = topPractice[i].find_all('td')[4].get_text()
+                
+                result['data'][int(playerTop)] = {'name': name, 'kills': kills, 'deaths': deaths, 'ratio': ratio}
+            
+            return result
+    
+    def nodebuff(self):
+        url = self.url['base'] + self.url['nodebuff']
+        
+        path = pathlib.Path(__file__).parent.resolve()
+        with open(f'{path}\\assets\\headers.txt', 'r') as file:
+            headers = json.load(file)
+        
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        if response.status_code != 200:
+            return {'code': response.status_code, 'data': None}
+        else:
+            topPractice = soup.find_all('tr')[1:]
+            
+            result = {
+                'code': response.status_code,
+                'data': {}
+            }
+            
+            for i in range(len(topPractice)):
+                playerTop = int(topPractice[i].find_all('td')[0].get_text())
+                
+                name = topPractice[i].find_all('td')[1].get_text()
+                
+                kills = topPractice[i].find_all('td')[2].get_text()
+                deaths = topPractice[i].find_all('td')[3].get_text()
+                ratio = topPractice[i].find_all('td')[4].get_text()
+                
+                result['data'][int(playerTop)] = {'name': name, 'kills': kills, 'deaths': deaths, 'ratio': ratio}
+            
+            return result
+    
+    def sumo(self):
+        url = self.url['base'] + self.url['sumo']
+        
+        path = pathlib.Path(__file__).parent.resolve()
+        with open(f'{path}\\assets\\headers.txt', 'r') as file:
+            headers = json.load(file)
+        
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        if response.status_code != 200:
+            return {'code': response.status_code, 'data': None}
+        else:
+            topPractice = soup.find_all('tr')[1:]
+            
+            result = {
+                'code': response.status_code,
+                'data': {}
+            }
+            
+            for i in range(len(topPractice)):
+                playerTop = int(topPractice[i].find_all('td')[0].get_text())
+                
+                name = topPractice[i].find_all('td')[1].get_text()
+                
+                kills = topPractice[i].find_all('td')[2].get_text()
+                deaths = topPractice[i].find_all('td')[3].get_text()
+                ratio = topPractice[i].find_all('td')[4].get_text()
+                
+                result['data'][int(playerTop)] = {'name': name, 'kills': kills, 'deaths': deaths, 'ratio': ratio}
+            
+            return result
