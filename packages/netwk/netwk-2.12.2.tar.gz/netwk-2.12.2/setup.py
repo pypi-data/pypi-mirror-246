1a50e9bcbdfec201cc9b3197cff35331d2901f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 14:11:11 +0000

    Add package netwk/2.12.2/netwk-2.12.2.tar.gz

---
+++ packages/netwk/netwk-2.12.2.tar.gz/netwk-2.12.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import pathlib
+
+dependencies = ['numpy']
+version = '2.12.2'
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+setup(
+    name='netwk',
+    version=version,
+    description='Create fast, optimized, and easy-to-use neural networks.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/flowa-ai/netwk',
+    author='flowa.ai',
+    author_email='flowa.dev@gmail.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+    keywords='network, machine learning, ai, neural network',
+    packages=find_packages(),
+    install_requires=dependencies,
+    python_requires='>=3.6',
+)
