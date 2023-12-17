d9344a537f38ef9260387f1bec917323076af762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 04:05:18 +0000

    Add package tuchuang/3.0.0/tuchuang-3.0.0.tar.gz

---
+++ packages/tuchuang/tuchuang-3.0.0.tar.gz/tuchuang-3.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='tuchuang',
+    version='3.0.0',
+    description='一个免费而简单图床',
+    author='Python学霸',
+    author_email='python@xueba.com',
+    packages=['tuchuang'],
+    install_requires=['requests', 'json'],
+)
