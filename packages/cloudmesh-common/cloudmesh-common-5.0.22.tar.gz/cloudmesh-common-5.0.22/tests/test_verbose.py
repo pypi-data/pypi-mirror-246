6a0107e18acac482e41491ab3876b106b81ecc88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 03:06:28 +0000

    Add package cloudmesh-common/5.0.22/cloudmesh-common-5.0.22.tar.gz

---
+++ packages/cloudmesh-common/cloudmesh-common-5.0.22.tar.gz/cloudmesh-common-5.0.22/tests/test_verbose.py
@@ -0,0 +1,91 @@
+###############################################################
+# pytest -v --capture=no  tests/test_verbose.py
+# pytest -v tests/test_verbose.py
+# pytest -v --capture=no  tests/test_verbose..py::Test_verbose.test_001
+###############################################################
+
+from cloudmesh.common.util import HEADING
+from cloudmesh.common.debug import VERBOSE
+import io
+import pytest
+from contextlib import redirect_stdout
+from cloudmesh.common.variables import Variables
+import os
+
+variables = Variables()
+
+old = variables["verbose"]
+
+@pytest.mark.incremental
+class Test_Verbose:
+
+    def setup_method(self):
+        global old
+        old = variables["verbose"]
+        print()
+        print ("VERBOSE <-", old)
+        variables["verbose"] = 10
+        print ("VERBOSE =", variables["verbose"])
+
+    def teardown_class(self):
+        global old
+        variables["verbose"] = old
+        print()
+        print ("VERBOSE ->", old)
+
+    def test_value(self):
+        os.system("cms debug on")
+        v = variables["verbose"]
+        print()
+        print ("Variables", v)
+        assert True
+
+    def test_VERBOSE(self):
+        HEADING()
+
+        help = "hallo"
+        with io.StringIO() as buf, redirect_stdout(buf):
+            VERBOSE(help)
+            output = buf.getvalue()
+        print (output)
+
+        assert "help" in output
+        assert "hallo" in output
+        assert "#" in output
+        
+    def test_not_VERBOSE(self):
+        HEADING()
+
+        variables["verbose"] = 0
+        help = "hallo"
+        with io.StringIO() as buf, redirect_stdout(buf):
+            VERBOSE(help)
+            output = buf.getvalue()
+        print (output)
+        variables["verbose"] = 10
+
+        assert "help" not in output
+        assert "hallo" not in output
+        assert "#" not in output
+
+    def test_6_print_VERBOSE(self):
+        HEADING()
+
+        help = "hallo"
+
+        for v in [0,1,2,3,4,5,6,7,8,9,10]:
+            print("TEST FOR VERBOSE", v)
+            with io.StringIO() as buf, redirect_stdout(buf):
+                variables["verbose"] = v
+                VERBOSE(help, verbose=6)
+                output = buf.getvalue()
+            print (output)
+
+            if v < 6:
+                assert "hallo" not in output
+                assert "#" not in output
+            else:
+                assert "hallo" in output
+                assert "#" in output
+
+        variables["verbose"] = old
