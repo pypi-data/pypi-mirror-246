0b8905d5da16afaa445177842b40245ef4e013a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 14:11:32 +0000

    Add package umap-project/1.12.0/umap_project-1.12.0.tar.gz

---
+++ packages/umap-project/umap_project-1.12.0.tar.gz/umap_project-1.12.0/umap/admin.py
@@ -0,0 +1,24 @@
+from django.contrib.gis import admin
+
+from .models import DataLayer, Licence, Map, Pictogram, TileLayer
+
+
+class TileLayerAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "rank",
+    )
+    list_editable = ("rank",)
+
+
+class MapAdmin(admin.GISModelAdmin):
+    search_fields = ("name",)
+    autocomplete_fields = ("owner", "editors")
+    list_filter = ("share_status",)
+
+
+admin.site.register(Map, MapAdmin)
+admin.site.register(DataLayer)
+admin.site.register(Pictogram)
+admin.site.register(TileLayer, TileLayerAdmin)
+admin.site.register(Licence)
