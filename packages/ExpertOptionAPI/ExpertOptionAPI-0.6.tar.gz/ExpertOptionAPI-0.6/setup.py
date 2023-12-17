1e78db487a6a5bcb3f470ce2dab0496084f45c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 04:05:04 +0000

    Add package ExpertOptionAPI/0.6/ExpertOptionAPI-0.6.tar.gz

---
+++ packages/ExpertOptionAPI/ExpertOptionAPI-0.6.tar.gz/ExpertOptionAPI-0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ExpertOptionAPI',
+    version='0.6',
+    packages=find_packages(where="./"),
+    package_dir={'': "./"},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        # list your package dependencies here
+        "websocket",
+        "simplejson",
+        "pandas"
+    ],
+)
