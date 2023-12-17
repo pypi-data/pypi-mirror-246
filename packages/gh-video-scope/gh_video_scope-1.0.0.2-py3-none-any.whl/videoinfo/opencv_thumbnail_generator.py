918e9949144f255d1b1acf2a9640340e768f1021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 14:10:58 +0000

    Add package gh-video-scope/1.0.0.2/gh_video_scope-1.0.0.2-py3-none-any.whl

---
+++ packages/gh-video-scope/gh_video_scope-1.0.0.2-py3-none-any.whl/videoinfo/opencv_thumbnail_generator.py
@@ -0,0 +1,10 @@
+from videoinfo.interfaces.i_thumbnail_generator import IThumbnailGenerator
+
+
+import cv2
+
+
+class OpenCVThumbnailGenerator(IThumbnailGenerator):
+    def generate_thumbnail(self, frame, factor):
+        size = frame.shape
+        return cv2.resize(frame, (int(size[1] * factor), int(size[0] * factor)))
