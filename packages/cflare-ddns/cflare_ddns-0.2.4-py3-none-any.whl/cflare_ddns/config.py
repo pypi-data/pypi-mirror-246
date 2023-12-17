227fc3ddfa847977f1ad570948acbcc7634f1107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 14:10:49 +0000

    Add package cflare-ddns/0.2.4/cflare_ddns-0.2.4-py3-none-any.whl

---
+++ packages/cflare-ddns/cflare_ddns-0.2.4-py3-none-any.whl/cflare_ddns/config.py
@@ -0,0 +1,26 @@
+import json
+from cflare_ddns import exceptions as ex
+
+
+def parse(filename):
+    # Load configuration from a JSON file
+    with open(filename, "r") as json_file:
+        config = json.load(json_file)
+
+    if "api_key" not in config:
+        raise ex.ConfigInvalidError("Config missing api_key")
+    if "email" not in config:
+        raise ex.ConfigInvalidError("Config missing email")
+    if "records" not in config:
+        raise ex.ConfigInvalidError("Config missing records")
+    if type(config["records"]) is not list:
+        raise ex.ConfigInvalidError("Invalid records")
+    for item in config["records"]:
+        if "zone_id" not in item:
+            raise ex.ConfigInvalidError("Config missing zone_id")
+        if "domain" not in item:
+            raise ex.ConfigInvalidError("Config missing domain")
+        if "record_name" not in item:
+            raise ex.ConfigInvalidError("Config missing record_name")
+
+    return config
