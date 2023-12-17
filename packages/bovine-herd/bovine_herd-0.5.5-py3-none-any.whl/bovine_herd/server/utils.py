a6218a4a0ec41d9cd470548726580470d500b02d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 04:05:07 +0000

    Add package bovine-herd/0.5.5/bovine_herd-0.5.5-py3-none-any.whl

---
+++ packages/bovine-herd/bovine_herd-0.5.5-py3-none-any.whl/bovine_herd/server/utils.py
@@ -0,0 +1,12 @@
+from urllib.parse import urljoin, urlparse
+
+
+def path_from_request(request) -> str:
+    """Given a request (from quart) determines the url it is coming from.
+    Main goal is to abstract away http vs https"""
+
+    url = request.url
+    if request.headers.get("X-Forwarded-Proto") == "https":
+        url = url.replace("http://", "https://")
+
+    return urljoin(url, urlparse(url).path)
