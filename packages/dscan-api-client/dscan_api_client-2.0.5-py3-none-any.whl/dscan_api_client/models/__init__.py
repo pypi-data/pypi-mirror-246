a50107924a1dbf9256f47878db7437c942f10c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 03:06:30 +0000

    Add package dscan-api-client/2.0.5/dscan_api_client-2.0.5-py3-none-any.whl

---
+++ packages/dscan-api-client/dscan_api_client-2.0.5-py3-none-any.whl/dscan_api_client/models/__init__.py
@@ -0,0 +1,51 @@
+""" Contains all the data models used in inputs/outputs """
+
+from .domain import Domain
+from .patched_program import PatchedProgram
+from .patched_scan import PatchedScan
+from .patched_scanner import PatchedScanner
+from .patched_subdomain import PatchedSubdomain
+from .patched_task import PatchedTask
+from .program import Program
+from .program_subdomain_check import ProgramSubdomainCheck
+from .program_subdomain_detail_save_check import ProgramSubdomainDetailSaveCheck
+from .scan import Scan
+from .scan_detailed import ScanDetailed
+from .scan_detailed_detailed import ScanDetailedDetailed
+from .scan_issue_inc import ScanIssueInc
+from .scan_status import ScanStatus
+from .scanner import Scanner
+from .scanner_type import ScannerType
+from .subdomain import Subdomain
+from .subdomain_detail import SubdomainDetail
+from .subdomain_with_detail import SubdomainWithDetail
+from .task import Task
+from .task_status import TaskStatus
+from .x_schema_retrieve_format import XSchemaRetrieveFormat
+from .x_schema_retrieve_response_200 import XSchemaRetrieveResponse200
+
+__all__ = (
+    "Domain",
+    "PatchedProgram",
+    "PatchedScan",
+    "PatchedScanner",
+    "PatchedSubdomain",
+    "PatchedTask",
+    "Program",
+    "ProgramSubdomainCheck",
+    "ProgramSubdomainDetailSaveCheck",
+    "Scan",
+    "ScanDetailed",
+    "ScanDetailedDetailed",
+    "ScanIssueInc",
+    "Scanner",
+    "ScannerType",
+    "ScanStatus",
+    "Subdomain",
+    "SubdomainDetail",
+    "SubdomainWithDetail",
+    "Task",
+    "TaskStatus",
+    "XSchemaRetrieveFormat",
+    "XSchemaRetrieveResponse200",
+)
