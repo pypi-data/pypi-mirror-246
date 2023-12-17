e606a225f728bfecab442e28c8155860580232ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 03:05:33 +0000

    Add package sqlglot-doris/1.0.9.0/sqlglot-doris-1.0.9.0.tar.gz

---
+++ packages/sqlglot-doris/sqlglot-doris-1.0.9.0.tar.gz/sqlglot-doris-1.0.9.0/setup.py
@@ -0,0 +1,46 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="sqlglot-doris",
+    description="An easily customizable SQL parser and transpiler",
+    version="1.0.9.0",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tobymao/sqlglot",
+    author="Toby Mao",
+    author_email="toby.mao@gmail.com",
+    license="MIT",
+    packages=find_packages(include=["sqlglot", "sqlglot.*"]),
+    package_data={"sqlglot": ["py.typed"]},
+    # use_scm_version={
+    #     "write_to": "sqlglot/_version.py",
+    #     "fallback_version": "0.0.0",
+    #     "local_scheme": "no-local-version",
+    # },
+    setup_requires=["setuptools_scm"],
+    python_requires=">=3.7",
+    extras_require={
+        "dev": [
+            "autoflake",
+            "black",
+            "duckdb>=0.6",
+            "isort",
+            "mypy>=0.990",
+            "pandas",
+            "pyspark",
+            "python-dateutil",
+            "pdoc",
+            "pre-commit",
+            "types-python-dateutil",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: SQL",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+)
