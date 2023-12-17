d02f33268689e6308c1693035413a8bc306dffc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 04:04:52 +0000

    Add package sequentium/0.0.1a0/sequentium-0.0.1a0.tar.gz

---
+++ packages/sequentium/sequentium-0.0.1a0.tar.gz/sequentium-0.0.1a0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_namespace_packages
+
+from sequence import __version__
+
+setup(
+  name='sequentium',
+  version=f'{__version__}',
+  license='MIT',
+  author='Vasco Schiavo',
+  author_email='vasco.schiavo@protonmail.com',
+  url='https://github.com/VascoSch92/sequentium',
+  description='A package to work with sequences',
+  packages=find_namespace_packages(include=['sequence*'], exclude=['tests']),
+  entry_points={
+        'console_scripts': ['sequence=sequence:main']
+    },
+  # download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords=['math', 'mathematics', 'sequence', 'fibonacci'],
+  classifiers=[
+    'Development Status :: 1 - Planning',
+    'Intended Audience :: Education',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
+    'Topic :: Education',
+    'Topic :: Scientific/Engineering :: Mathematics',
+  ],
+  python_requires ='>=3.9',
+)
+
