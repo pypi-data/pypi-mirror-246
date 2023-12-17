db2c226f1f81b1c185ea683e29ec77f73357a0de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 14:10:49 +0000

    Add package cflare-ddns/0.2.4/cflare-ddns-0.2.4.tar.gz

---
+++ packages/cflare-ddns/cflare-ddns-0.2.4.tar.gz/cflare-ddns-0.2.4/cflare_ddns/args.py
@@ -0,0 +1,62 @@
+import os
+from argparse import ArgumentParser
+from importlib.metadata import version
+
+DEFAULT_CONFIGS = [
+    "/etc/cflare-ddns.json",
+    "cflare-ddns.json",
+]
+
+# 15 minutes
+DEFAULT_INTERVAL = 60 * 15
+
+
+def check_config():
+    for conf in DEFAULT_CONFIGS:
+        if os.path.exists(conf):
+            return conf
+    return None
+
+
+def init():
+    args_parse = ArgumentParser()
+
+    args_parse.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version=version("cflare-ddns"),
+        help="App version",
+    )
+
+    existing_config = check_config()
+    default_config = os.environ.get(
+        "CF_DDNS_CONFIG",
+        default=existing_config,
+    )
+    args_parse.add_argument(
+        "-c",
+        "--config",
+        default=default_config,
+        dest="config_file",
+        type=str,
+        required=default_config is None,
+        help=(
+            "Path to your configuration file. "
+            f"Default paths: {', '.join(DEFAULT_CONFIGS)}"
+        ),
+    )
+
+    args_parse.add_argument(
+        "-i",
+        "--interval",
+        default=os.environ.get("CF_DDNS_INTERVAL"),
+        dest="interval",
+        type=int,
+        help=(
+            "Number of seconds between each sync. "
+            "This will make the program run forever."
+            "Hit Ctrl-C to stop."
+        ),
+    )
+    return args_parse
