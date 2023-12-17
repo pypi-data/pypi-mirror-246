d824f73f2cc9af3db8b5a273f21f5340aa14fd76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 04:05:32 +0000

    Add package lyg/3.8.29/lyg-3.8.29.tar.gz

---
+++ packages/lyg/lyg-3.8.29.tar.gz/lyg-3.8.29/setup.py
@@ -0,0 +1,23 @@
+import os
+import distro
+import shutil
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+setup(
+    name="lyg",
+    version="3.8.29",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows"
+    ],
+    author="LYG.AI",
+    author_email="team@lyg.ai",
+    url="https://lyg.ai",
+)
+
