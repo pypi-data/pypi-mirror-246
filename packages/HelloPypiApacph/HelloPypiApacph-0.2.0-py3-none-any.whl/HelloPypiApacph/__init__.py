84bf9240d4c8db676adfbd90f9808cbd020046ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 03:07:17 +0000

    Add package HelloPypiApacph/0.2.0/HelloPypiApacph-0.2.0-py3-none-any.whl

---
+++ packages/HelloPypiApacph/HelloPypiApacph-0.2.0-py3-none-any.whl/HelloPypiApacph/__init__.py
@@ -0,0 +1,17 @@
+'''
+author: cph
+since:  20231215
+'''
+import calc
+import dt
+
+__version__ = "0.2.0"
+
+def print2():
+    print('Hello, PyPI...')
+    
+if __name__ == '__main__':
+    print2()
+    calc.printPi()
+    dt.printDT()
+    
