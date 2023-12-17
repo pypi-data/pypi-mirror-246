5b76da9d5bb197d52e12a045959edba2ce2913dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 04:05:07 +0000

    Add package validateall/0.1.3/validateall-0.1.3.tar.gz

---
+++ packages/validateall/validateall-0.1.3.tar.gz/validateall-0.1.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='validateall',
+    version='0.1.3', 
+    packages=['validateall'],
+    install_requires=[],
+)
---
+++ packages/validateall/validateall-0.1.3.tar.gz/validateall-0.1.3/validateall/validate_number.py
@@ -0,0 +1,14 @@
+import re
+
+def validate_number(phone_number):   
+    phone_pattern = re.compile(r'^\+998\d{9}$')
+    is_valid = bool(re.match(phone_pattern, phone_number))
+
+    if is_valid:
+        print(f"The phone number {phone_number} is in the Uzbek format.")
+    else:
+        print(f"The phone number {phone_number} is not in the Uzbek format.")
+
+    return is_valid
+
+
