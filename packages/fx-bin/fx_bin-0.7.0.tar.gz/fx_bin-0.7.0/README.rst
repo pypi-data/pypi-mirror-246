803ab2beaa81153590b0bca1b3cd23d7d85e8d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 03:07:18 +0000

    Add package fx-bin/0.7.0/fx_bin-0.7.0.tar.gz

---
+++ packages/fx-bin/fx_bin-0.7.0.tar.gz/fx_bin-0.7.0/README.rst
@@ -0,0 +1,18 @@
+=============
+FX bin
+=============
+
+
+A common bin collection for my own usage
+
+
+
+Features
+--------
+
+* size
+
+Install
+-------
+
+    pip install --upgrade fx-bin
---
+++ packages/fx-bin/fx_bin-0.7.0.tar.gz/fx_bin-0.7.0/fx_bin/find_files.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import click
+
+
+def find_files(keyword):
+    cwd = os.getcwd()
+    for root, dirs, files in os.walk(cwd):
+        for o in [dirs+files]:
+            if keyword in o:
+                print(os.path.join(root, o))
+
+
+@click.command()
+@click.argument('keyword')
+def main(keyword):
+    if not keyword:
+        click.echo("Please type text to search. For example: fx_ff bar")
+    find_files(keyword)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
