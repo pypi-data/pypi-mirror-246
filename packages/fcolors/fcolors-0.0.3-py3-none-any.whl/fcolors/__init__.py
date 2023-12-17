59fbd1ad76d9191bd5835586f2c01f9cc8510639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 04:04:55 +0000

    Add package fcolors/0.0.3/fcolors-0.0.3-py3-none-any.whl

---
+++ packages/fcolors/fcolors-0.0.3-py3-none-any.whl/fcolors/__init__.py
@@ -0,0 +1,25 @@
+# from fcolors import fg
+# from fcolors import bg
+# from fcolors import bold
+# from fcolors import uline
+# from fcolors import fg_hi
+# from fcolors import bg_hi
+# from fcolors import all_hi
+# from fcolors import bold_hi
+# from fcolors import reset
+# from fcolors import Color
+# from fcolors import esc
+
+# __all__ = [
+#     'fg',
+#     'bg',
+#     'bold',
+#     'uline',
+#     'fg_hi',
+#     'bg_hi',
+#     'fb_hi',
+#     'reset',
+#     'Color',
+#     'esc',
+# ]
+
