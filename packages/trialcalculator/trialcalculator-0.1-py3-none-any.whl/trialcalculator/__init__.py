6fecf7615024749d6e18e3d8a4b0990f394342c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 03:05:39 +0000

    Add package trialcalculator/0.1/trialcalculator-0.1-py3-none-any.whl

---
+++ packages/trialcalculator/trialcalculator-0.1-py3-none-any.whl/trialcalculator/__init__.py
@@ -0,0 +1,52 @@
+"""Calculator
+"""
+
+
+def add_numbers(a,b):
+    """
+    Adds two numbers together and returns the result.
+
+    Parameters:
+        a (int): The first number to be added.
+        b (int): The second number to be added.
+
+    Returns:
+        int: The sum of the two numbers.
+    """
+    return a+b
+
+def sub_numbers(a,b):
+    """
+    Compute the subtraction of two numbers.
+
+    Parameters:
+        a (int): The first number.
+        b (int): The second number.
+
+    Returns:
+        int: The difference between `a` and `b`.
+    """
+    return a-b
+
+def multiply_numbers(a,b):
+    """
+    Multiplies two numbers and returns the result.
+
+    Parameters:
+        a (int): The first number.
+        b (int): The second number.
+
+    Returns:
+        int: The product of the two numbers.
+    """
+    return a*b
+
+def div_numbers(a,b):
+    """
+    Divide two numbers and return the result.
+
+    :param a: The first number to be divided.
+    :param b: The second number to divide by.
+    :return: The result of dividing `a` by `b`.
+    """
+    return a/b
