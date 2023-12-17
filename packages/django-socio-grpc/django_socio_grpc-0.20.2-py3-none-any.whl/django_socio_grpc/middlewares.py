1163f1e2046672574e74a49e2b505bafd908a5ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 04:05:00 +0000

    Add package django-socio-grpc/0.20.2/django_socio_grpc-0.20.2-py3-none-any.whl

---
+++ packages/django-socio-grpc/django_socio_grpc-0.20.2-py3-none-any.whl/django_socio_grpc/middlewares.py
@@ -0,0 +1,134 @@
+""" https://docs.djangoproject.com/fr/4.1/topics/http/middleware
+For now only the __call__ method of a middleware is supported
+
+To use async middlewares, you need to use the correct decorator
+Here we use the sync_and_async_middleware decorators to declare these
+middlewares as sync and async compatible
+"""
+
+import asyncio
+import logging
+from typing import Callable
+
+from asgiref.sync import async_to_sync, sync_to_async
+from django import db
+from django.utils import translation
+from django.utils.decorators import sync_and_async_middleware
+from django.utils.translation import get_language_from_request
+
+from django_socio_grpc.services.servicer_proxy import GRPCRequestContainer
+from django_socio_grpc.settings import grpc_settings
+from django_socio_grpc.utils.utils import safe_async_response
+
+logger = logging.getLogger("django_socio_grpc.middlewares")
+
+
+def _close_old_connections():
+    for conn in db.connections.all():
+        if conn.connection is None:
+            continue
+        if conn.get_autocommit():
+            conn.close_if_unusable_or_obsolete()
+
+
+@sync_and_async_middleware
+def close_old_connections_middleware(get_response: Callable):
+    if asyncio.iscoroutinefunction(get_response):
+
+        async def middleware(request: GRPCRequestContainer):
+            db.reset_queries()
+            await sync_to_async(_close_old_connections)()
+            try:
+                # INFO - L.G. - 03/01/2023 - We need to use safe_async_response here
+                # because get_response might return a generator
+                return await safe_async_response(get_response, request)
+            finally:
+                await sync_to_async(_close_old_connections)()
+
+    else:
+
+        def middleware(request: GRPCRequestContainer):
+            db.reset_queries()
+            _close_old_connections()
+            try:
+                return get_response(request)
+            finally:
+                _close_old_connections()
+
+    return middleware
+
+
+def _log_requests(request: GRPCRequestContainer):
+    if (
+        f"{request.service.__class__.__name__}.{request.action}"
+        not in grpc_settings.IGNORE_LOG_FOR_ACTION
+    ):
+        logger.info(
+            f"Receive action {request.action} on service {request.service.__class__.__name__}",
+        )
+
+
+@sync_and_async_middleware
+def log_requests_middleware(get_response: Callable):
+    if asyncio.iscoroutinefunction(get_response):
+
+        async def middleware(request: GRPCRequestContainer):
+            _log_requests(request)
+            return await safe_async_response(get_response, request)
+
+    else:
+
+        def middleware(request: GRPCRequestContainer):
+            _log_requests(request)
+            return get_response(request)
+
+    return middleware
+
+
+@sync_and_async_middleware
+def locale_middleware(get_response: Callable):
+    if asyncio.iscoroutinefunction(get_response):
+
+        async def middleware(request: GRPCRequestContainer):
+            language = get_language_from_request(request.context)
+            translation.activate(language)
+            return await safe_async_response(get_response, request)
+
+    else:
+
+        def middleware(request: GRPCRequestContainer):
+            language = get_language_from_request(request.context)
+            translation.activate(language)
+            return get_response(request)
+
+    return middleware
+
+
+@sync_and_async_middleware
+def auth_without_session_middleware(get_response: Callable):
+    """
+    This middleware is here to replace Django default Authentication Middleware as it look for the user session created by the login django method.
+    This middleware is to use when using other Authenifciation pattern such as Token one.
+
+    This middleware call the perform_authentication of the service. It should be placed before any other middleware that need context.user.
+    If this middleware is not installed the authentication will still perform but after the execution of all the middleware.
+    """
+    if asyncio.iscoroutinefunction(get_response):
+
+        async def middleware(request: GRPCRequestContainer):
+            if asyncio.iscoroutinefunction(request.service.perform_authentication):
+                await request.service.perform_authentication()
+            else:
+                await sync_to_async(request.service.perform_authentication)()
+            return await safe_async_response(get_response, request)
+
+    else:
+
+        def middleware(request: GRPCRequestContainer):
+            if asyncio.iscoroutinefunction(request.service.perform_authentication):
+                async_to_sync(request.service.perform_authentication)()
+            else:
+                request.service.perform_authentication()
+            return get_response(request)
+
+    return middleware
