56d277e9c21f429938d548afa8d7d6076df32d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 14:10:29 +0000

    Add package pypimaker/1.0.5/pypimaker-1.0.5-py3-none-any.whl

---
+++ packages/pypimaker/pypimaker-1.0.5-py3-none-any.whl/pypimaker/reset.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+
+from pypimaker import strings
+from pypimaker.ui.DialogWindow import ActionDialogWindow
+
+
+def reset(topLevelDirectory):
+    ActionDialogWindow("Are you sure?", strings.RESET_SELECTION_TEXT,
+                       negativeAction=exit, mainWindow=True)
+
+    filesToRemove = ["LICENSE", "README.md",
+                     "requirements.txt", "setup.py", ".gitignore"]
+    foldersToRemove = ["build", "dist"]
+    for item in os.listdir(topLevelDirectory):
+        if item.endswith("egg-info"):
+            foldersToRemove.append(item)
+
+    for file in filesToRemove:
+        commandArray = ["rm", topLevelDirectory + "/" + file]
+        subprocess.run(commandArray, capture_output=True)
+
+    for folder in foldersToRemove:
+        commandArray = ["rm", "-r", topLevelDirectory + "/" + folder]
+        subprocess.run(commandArray, capture_output=True)
