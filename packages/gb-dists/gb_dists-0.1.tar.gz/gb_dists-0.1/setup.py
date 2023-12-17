78d1a0151582c40f0ddb2b58c115b03cd3e9340f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 14:11:17 +0000

    Add package gb-dists/0.1/gb_dists-0.1.tar.gz

---
+++ packages/gb-dists/gb_dists-0.1.tar.gz/gb_dists-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gb_dists',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gb_dists'],
+      author = 'Yakup Akkaya',
+      zip_safe=False)
