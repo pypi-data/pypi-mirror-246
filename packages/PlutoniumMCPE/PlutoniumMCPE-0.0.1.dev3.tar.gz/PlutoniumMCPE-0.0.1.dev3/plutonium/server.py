1264ce9d4c2eae8e0af2a5e806100a07bacbd64f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 03:06:16 +0000

    Add package PlutoniumMCPE/0.0.1.dev3/PlutoniumMCPE-0.0.1.dev3.tar.gz

---
+++ packages/PlutoniumMCPE/PlutoniumMCPE-0.0.1.dev3.tar.gz/PlutoniumMCPE-0.0.1.dev3/plutonium/server.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import pathlib
+
+from plutonium.assets.headers import headers as h
+
+class server:
+    def __init__(self) -> None:
+        self.url = {
+            'base': 'https://www.plutonium.best'
+        }
+    
+    def info(self):
+        url = self.url['base']
+        
+        path = pathlib.Path(__file__).parent.resolve()
+        headers = h().variable
+        
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        if response.status_code != 200:
+            return {'code': response.status_code, 'data': None}
+        else:
+            serverStats = soup.find_all('p', class_='server-stats')
+            
+            onlinePlayers = serverStats[0].find('span').get_text()
+            registeredPlayers = serverStats[1].find('span').get_text().replace(" ", "")
+            
+            return {
+                'code': response.status_code,
+                'data': {
+                    'onlinePlayers': int(onlinePlayers),
+                    'registeredPlayers': int(registeredPlayers)
+                }
+            }
