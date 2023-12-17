a024fc8b0012fc51be0ba129feeba04d495b6761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 04:04:48 +0000

    Add package fluxy/0.1.3/fluxy-0.1.3.tar.gz

---
+++ packages/fluxy/fluxy-0.1.3.tar.gz/fluxy-0.1.3/fluxy/validate.py
@@ -0,0 +1,25 @@
+import re
+
+class Validator:
+    @staticmethod
+    async def validate_number(phone_number):
+        phone_pattern = re.compile(r'^\+998\d{9}$')
+        is_valid = bool(re.match(phone_pattern, phone_number))
+
+        if is_valid:
+            print(f"Ваш номер {phone_number} соответствует узбекскому формату. ")
+            return True
+        else:
+            print(f"Ваш номер {phone_number} не соответствует узбекскому формату.")
+            return False
+
+    @staticmethod
+    async def validate_passport_number(passport_number):
+        passport_pattern = re.compile(r'^[A-Z]{2}\d{7}$')
+
+        if re.match(passport_pattern, passport_number):
+            print(f"Номер узбекского паспорта {passport_number} соответствует шаблону.")
+            return True
+        else:
+            print(f"Номер узбекского паспорта {passport_number} не соответствует шаблону.")
+            return False
---
+++ packages/fluxy/fluxy-0.1.3.tar.gz/fluxy-0.1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='fluxy',
+    version='0.1.3', 
+    packages=['fluxy'],
+    install_requires=[
+    
+
+    ],
+)
