7ce074a46be627f7cf3804ddbdb6f855fe9bc63a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 14:10:56 +0000

    Add package pygpt-net/2.0.33/pygpt_net-2.0.33-py3-none-any.whl

---
+++ packages/pygpt-net/pygpt_net-2.0.33-py3-none-any.whl/pygpt_net/core/plugin/base_plugin.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2023.12.14 15:00:00                  #
+# ================================================== #
+
+class BasePlugin:
+    def __init__(self):
+        self.id = ""
+        self.name = ""
+        self.type = []  # audio.input, audio.output
+        self.description = ""
+        self.options = {}
+        self.initial_options = {}
+        self.window = None
+        self.enabled = False
+        self.order = 0
+
+    def setup(self):
+        """
+        Returns available config options
+
+        :return: config options
+        """
+        return self.options
+
+    def add_option(self, name, type, value=None, label="", description="",
+                   tooltip=None, min=None, max=None, multiplier=1, step=1, slider=False,
+                   keys=None, advanced=False, secret=False, persist=False):
+        """
+        Adds option
+
+        :param name: Option name (ID, key)
+        :param type: Option type (text, textarea, bool, int, float, dict)
+        :param value: Option value
+        :param label: Option label
+        :param description: Option description
+        :param tooltip: Option tooltip
+        :param min: Option min value
+        :param max: Option max value
+        :param multiplier: Option float value multiplier
+        :param step: Option step value (for slider)
+        :param slider: Option slider (True/False)
+        :param keys: Option keys (for dict type)
+        :param advanced: Option advanced (True/False)
+        :param secret: Option secret (True/False)
+        :param persist: Option persist (True/False)
+        """
+        if tooltip is None:
+            tooltip = description
+
+        option = {
+            "type": type,
+            "value": value,
+            "label": label,
+            "description": description,
+            "tooltip": tooltip,
+            "min": min,
+            "max": max,
+            "multiplier": multiplier,
+            "step": step,
+            "slider": slider,
+            "keys": keys,
+            "advanced": advanced,
+            "secret": secret,
+            "persist": persist,
+        }
+        self.options[name] = option
+
+    def has_option(self, name):
+        """
+        Checks if option exists
+
+        :param name: Option name
+        :return: True if exists
+        """
+        return name in self.options
+
+    def get_option(self, name):
+        """
+        Returns option
+
+        :param name: Option name
+        :return: Option
+        """
+        if self.has_option(name):
+            return self.options[name]
+
+    def get_option_value(self, name):
+        """
+        Returns option value
+
+        :param name: Option name
+        :return: Option value
+        """
+        if self.has_option(name):
+            return self.options[name]["value"]
+
+    def attach(self, window):
+        """
+        Attaches window
+
+        :param window: Window
+        """
+        self.window = window
+
+    def on_user_send(self, text):
+        """
+        Event: On user send text
+
+        :param text: Text
+        :return: Text
+        """
+        return text
+
+    def on_ctx_begin(self, ctx):
+        """
+        Event: On new context begin
+
+        :param ctx: Context
+        :return: Context
+        """
+        return ctx
+
+    def on_ctx_end(self, ctx):
+        """
+        Event: On context end
+
+        :param ctx: Context
+        :return: Context
+        """
+        return ctx
+
+    def on_system_prompt(self, prompt):
+        """
+        Event: On prepare system prompt
+
+        :param prompt: Prompt
+        :return: Prompt
+        """
+        return prompt
+
+    def on_ai_name(self, name):
+        """
+        Event: On set AI name
+
+        :param name: Name
+        :return: Name
+        """
+        return name
+
+    def on_user_name(self, name):
+        """
+        Event: On set user name
+
+        :param name: Name
+        :return: Name
+        """
+        return name
+
+    def on_enable(self):
+        """Event: On plugin enable"""
+        pass
+
+    def on_disable(self):
+        """Event: On plugin disable"""
+        pass
+
+    def on_input_before(self, text):
+        """
+        Event: Before input
+
+        :param text: Text
+        """
+        return text
+
+    def on_ctx_before(self, ctx):
+        """
+        Event: Before ctx
+
+        :param ctx: Text
+        """
+        return ctx
+
+    def on_ctx_after(self, ctx):
+        """
+        Event: After ctx
+
+        :param ctx: ctx
+        """
+        return ctx
