a6b6f13446c97e75366ce38c8340a2982b3c7b3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-17 03:06:38 +0000

    Add package funcaptcha-challenger/0.10.0/funcaptcha_challenger-0.10.0-py3-none-any.whl

---
+++ packages/funcaptcha-challenger/funcaptcha_challenger-0.10.0-py3-none-any.whl/funcaptcha_challenger/__init__.py
@@ -0,0 +1,18 @@
+from funcaptcha_challenger.coordinatesmatch import CoordinatesMatchPredictor
+from funcaptcha_challenger.hopscotch_highsec import HopscotchHighsecPredictor
+from funcaptcha_challenger.numericalmatch import NumericalmatchPredictor
+from funcaptcha_challenger.rotate_animal import AnimalRotationPredictor
+from funcaptcha_challenger.threed_rollball_objects import ThreeDRollballObjectsPredictor
+
+arp = AnimalRotationPredictor()
+predict_3d_rollball_animals = arp.predict
+
+ocp = NumericalmatchPredictor()
+predict_numericalmatch = ocp.predict
+
+phh = HopscotchHighsecPredictor()
+
+predict_hopscotch_highsec = phh.predict
+predict_3d_rollball_objects = ThreeDRollballObjectsPredictor().predict
+
+predict_coordinatesmatch = CoordinatesMatchPredictor().predict
