3e12a3a854e5655931ef909f022dbafdea1f12a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 04:04:27 +0000

    Add package lectern/0.32.0/lectern-0.32.0.tar.gz

---
+++ packages/lectern/lectern-0.32.0.tar.gz/lectern-0.32.0/lectern/contrib/relative_location.py
@@ -0,0 +1,37 @@
+"""Plugin that handles relative resource locations."""
+
+
+__all__ = [
+    "RelativeNamespacedResourceLoader",
+]
+
+
+from dataclasses import dataclass, replace
+from typing import Mapping
+
+from beet import Context
+
+from lectern import Directive, Document, Fragment, NamespacedResourceDirective
+
+
+def beet_default(ctx: Context):
+    document = ctx.inject(Document)
+    document.loaders.append(RelativeNamespacedResourceLoader(ctx))
+
+
+@dataclass
+class RelativeNamespacedResourceLoader:
+    """Loader that resolves relative resource locations."""
+
+    ctx: Context
+
+    def __call__(
+        self,
+        fragment: Fragment,
+        directives: Mapping[str, Directive],
+    ) -> Fragment:
+        if isinstance(directives.get(fragment.directive), NamespacedResourceDirective):
+            name = fragment.expect("name")
+            if ":" not in name:
+                fragment = replace(fragment, arguments=[self.ctx.generate.path(name)])
+        return fragment
