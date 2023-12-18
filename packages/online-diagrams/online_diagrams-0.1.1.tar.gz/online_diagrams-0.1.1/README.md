daf5c630e54a36e62ed3d2e24671af9a4fed199a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 04:04:39 +0000

    Add package online-diagrams/0.1.1/online_diagrams-0.1.1.tar.gz

---
+++ packages/online-diagrams/online_diagrams-0.1.1.tar.gz/online_diagrams-0.1.1/README.md
@@ -0,0 +1,21 @@
+# diagrams
+Online diagrams-Service supporting graphviz, mscgen and plantuml markup
+
+[![Join the discussion at https://github.com/BITPlan/diagrams/discussions](https://img.shields.io/github/discussions/BITPlan/diagrams)](https://github.com/BITPlan/diagrams/discussions)
+[![pypi](https://img.shields.io/pypi/pyversions/online-diagrams)](https://pypi.org/project/online-diagrams/)
+[![Github Actions Build](https://github.com/BITPlan/diagrams/actions/workflows/build.yml/badge.svg)](https://github.com/BITPlan/diagrams/actions/workflows/build.yml)
+[![PyPI Status](https://img.shields.io/pypi/v/online-diagrams.svg)](https://pypi.python.org/pypi/online-diagrams/)
+[![GitHub issues](https://img.shields.io/github/issues/BITPlan/diagrams.svg)](https://github.com/BITPlan/diagrams/issues)
+[![GitHub issues](https://img.shields.io/github/issues-closed/BITPlan/diagrams.svg)](https://github.com/BITPlan/diagrams/issues/?q=is%3Aissue+is%3Aclosed)
+[![GitHub](https://img.shields.io/github/license/BITPlan/diagrams.svg)](https://www.apache.org/licenses/LICENSE-2.0)
+[![BITPlan](http://wiki.bitplan.com/images/wiki/thumb/3/38/BITPlanLogoFontLessTransparent.png/198px-BITPlanLogoFontLessTransparent.png)](http://www.bitplan.com)
+
+### Documentation
+* [BITPlan Wiki](http://wiki.bitplan.com/index.php/Diagrams)
+
+### Demo
+* [demo](http://diagrams.bitplan.com)
+* [demo - nicegui based](http://diagrams2.bitplan.com)
+
+### Authors
+* [Wolfgang Fahl](http://www.bitplan.com/Wolfgang_Fahl)
---
+++ packages/online-diagrams/online_diagrams-0.1.1.tar.gz/online_diagrams-0.1.1/tests/test_diagrams.py
@@ -0,0 +1,92 @@
+'''
+Created on 2020-02-13
+
+@author: wf
+'''
+import os
+from dgs.diagrams import Command,Generator,Generators,Example
+from ngwidgets.basetest import Basetest
+
+class TestDiagrams(Basetest):
+    """ Test the online diagrams service"""
+
+    def testCommands(self):
+        """
+        test some commands
+        """
+        cmds=[Command("pwd","",debug=True),Command("java","-version",debug=True),Command("dot","-V",debug=True),Command("mscgen","",debug=True)]
+        for cmd in cmds:
+            cmd.check()
+        pass
+    
+    def testGetGenerator(self):
+        generators=["graphviz","mscgen","plantuml"]
+        for generator in generators:
+            gen=Generators.get(generator)
+            assert gen is not None
+            info=gen.getHtmlInfo()
+            if self.debug:
+                print(info)
+    
+    def testGeneratorCheck(self):
+        for gen in Generators.generators():
+            gen.debug=self.debug
+            gen.check()
+            
+    def testGeneratorVersion(self):
+        for gen in Generators.generators():
+            gen.debug=self.debug
+            version=gen.getVersion()    
+            if self.debug:
+                print(version)    
+                
+    def testGeneratorForAlias(self):
+        for gen in Generators.generators():
+            gen.debug=self.debug
+            for alias in gen.aliases:
+                genid=Generators.generatorIdForAlias(alias)
+                assert genid==gen.id
+            
+    def testExamples(self):
+        for gen in Generators.generators():
+            for alias in gen.aliases:
+                txt=Example.get(alias)
+                if self.debug:
+                    print (txt)
+                assert not "no example for" in txt            
+
+    def testGenerators(self):
+        """
+        test all generators
+        """
+        if self.debug:
+            print ("outputDirectory is: %s" % (Generator.getOutputDirectory()))
+        for gen in Generators.generators():
+            gen.debug=self.debug
+            for alias in gen.aliases:
+                txt=Example.get(alias)
+                result=gen.generate(alias,txt,"png")
+                valid =result.isValid()
+                if not valid:
+                    print(result.errMsg())
+                assert valid
+            
+    def testGenerateResult(self):
+        '''
+        test provoked error
+        '''
+        debug=self.debug
+        debug=True
+        genid=Generators.generatorIdForAlias("plantuml")
+        gen=Generators.get(genid)
+        gen.debug=debug
+        # provoke an error
+        result=gen.generate('unknownalias','garbage input',"png")    
+        json=result.asJson('http://www.doe.com') 
+        if debug:
+            print(f"json:{json}")
+        # there is an image version of the error
+        assert os.path.isfile(result.path)
+        # which needs to be removed to make the test reproducible
+        os.remove(result.path)
+        assert "error" in json
