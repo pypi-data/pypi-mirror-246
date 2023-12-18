3da60e87f920f8d848abc1c2e40190e0d8195e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 03:05:59 +0000

    Add package aws-cf/0.7.1/aws_cf-0.7.1-py3-none-any.whl

---
+++ packages/aws-cf/aws_cf-0.7.1-py3-none-any.whl/aws_cf/utils/config.py
@@ -0,0 +1,75 @@
+from typing import List, Optional
+from pydantic import BaseModel
+import yaml
+import os
+from .context import Context
+
+class Stack(BaseModel):
+    path: str
+    name: str
+    parameters: Optional[dict] = None
+
+    @property
+    def _path(self):
+        return self.path.replace("$root", Context.get_root())
+        
+    @property
+    def _yml(self):
+        class SafeLoaderIgnoreUnknown(yaml.SafeLoader):
+            def ignore_unknown(self, node):
+                return None 
+
+        SafeLoaderIgnoreUnknown.add_constructor(None, SafeLoaderIgnoreUnknown.ignore_unknown)
+
+        return yaml.load(open(self._path).read(), Loader=SafeLoaderIgnoreUnknown)
+
+    @property
+    def resources(self):
+        return self._yml.get("Resources", {})
+
+class Enviroment(BaseModel):
+    artifacts: str 
+    name: str
+    region: Optional[str] = None
+    profile: Optional[str] = None
+
+
+
+class Config(BaseModel):
+    Stacks: List[Stack]
+    Environments: List[Enviroment]
+
+    @staticmethod
+    def parse(path: str = None):
+        try:
+            data = yaml.safe_load(open(path))
+        except:
+            raise IOError("Not able to find file at path: " + '"' + path + '"')
+
+        try:
+            return Config(**data)
+        except Exception as e:
+            raise Exception("Not able to pase config: " + '"' + str(e) + '"')
+
+    def setup_env(self, env=None):
+        _env = None
+
+        if env:
+            for e in self.Environments:
+                if e.name == env:
+                    _env = env
+                    break
+        else:
+            _env = self.Environments[0]
+          
+        
+        if not _env and env:
+            raise Exception("Not able to find env with name " + env)
+        
+        if not _env:
+            raise Exception("Not able to find env")
+
+
+        os.environ["AWS_PROFILE"] = _env.profile
+        os.environ["AWS_DEFAULT_REGION"] = _env.region
+        
