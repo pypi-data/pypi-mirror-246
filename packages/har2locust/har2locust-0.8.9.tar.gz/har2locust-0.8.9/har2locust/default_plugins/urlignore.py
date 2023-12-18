15951ff52ab5939e7725af1bba73fcc71a8cae61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 04:04:31 +0000

    Add package har2locust/0.8.9/har2locust-0.8.9.tar.gz

---
+++ packages/har2locust/har2locust-0.8.9.tar.gz/har2locust-0.8.9/har2locust/default_plugins/urlignore.py
@@ -0,0 +1,15 @@
+from har2locust.plugin import entriesprocessor
+import re
+import pathlib
+
+
+@entriesprocessor
+def process(entries: list[dict]):
+    urlignore_file = pathlib.Path(".urlignore")
+    filters = []
+    if urlignore_file.is_file():
+        with open(urlignore_file) as f:
+            filters = f.readlines()
+            filters = [line.rstrip() for line in filters if line.strip() and not line.strip().startswith("#")]
+
+    entries[:] = [e for e in entries if not any(re.search(r, e["request"]["url"]) for r in filters)]
