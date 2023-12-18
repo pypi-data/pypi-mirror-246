9fd72c4ea9a9936b4ebc1217bf74d8594f85caa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 03:05:36 +0000

    Add package har2locust/0.8.8/har2locust-0.8.8.tar.gz

---
+++ packages/har2locust/har2locust-0.8.8.tar.gz/har2locust-0.8.8/tests/outputs/reqres.in.py
@@ -0,0 +1,27 @@
+from locust import task, run_single_user
+from locust import FastHttpUser
+
+
+class reqres_in(FastHttpUser):
+    host = "https://reqres.in"
+    default_headers = {
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "sv,en;q=0.9",
+        "content-type": "application/json",
+        "origin": "https://reqres.in",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+    }
+
+    @task
+    def t(self):
+        with self.rest("POST", "/api/users", json={"name": "morpheus", "job": "leader"}) as resp:
+            pass
+        with self.rest("PATCH", "/api/users/2", json={"name": "morpheus", "job": "zion resident"}) as resp:
+            pass
+        with self.client.request("DELETE", "/api/users/2", catch_response=True) as resp:
+            pass
+
+
+if __name__ == "__main__":
+    run_single_user(reqres_in)
