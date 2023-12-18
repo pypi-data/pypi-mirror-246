88e0d619ed3ebbf1b076fb5fa9218a3e6cf37e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 03:05:46 +0000

    Add package anylearn/0.19.6a3/anylearn-0.19.6a3-py3-none-any.whl

---
+++ packages/anylearn/anylearn-0.19.6a3-py3-none-any.whl/anylearn/sdk/errors.py
@@ -0,0 +1,14 @@
+class AnylearnError(Exception):
+    """Base Anylearn error."""
+
+    def __init__(self, message: str):
+        super().__init__(message)
+        self.message = message
+
+
+class AnylearnInvalidResponseError(AnylearnError):
+    """Raised when the response is invalid."""
+
+
+class AnylearnArtifactDuplicationError(AnylearnError):
+    """Raised when an artifact is duplicated."""
