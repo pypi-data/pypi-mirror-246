6dfd5372b5e360e74857c0ccc066617af0487e7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 03:05:54 +0000

    Add package webchatgpt/0.0.2/webchatgpt-0.0.2-py3-none-any.whl

---
+++ packages/webchatgpt/webchatgpt-0.0.2-py3-none-any.whl/WebChatGPT/__init__.py
@@ -0,0 +1,8 @@
+from .main import ChatGPT
+
+__all__ = ["ChatGPT"]
+
+__version__ = "0.0.2"
+__author__ = "Smartwa"
+__repo__ = "https://github.com/Simatwa/WebChatGPT"
+__info__ = "Reverse Engineered ChatGPT Web-version."
