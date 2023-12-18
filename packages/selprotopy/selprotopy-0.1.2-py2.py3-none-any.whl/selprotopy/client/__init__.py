022fbcb05c9f035adc975b3ddd4fa18f8ae12ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 04:05:10 +0000

    Add package selprotopy/0.1.2/selprotopy-0.1.2-py2.py3-none-any.whl

---
+++ packages/selprotopy/selprotopy-0.1.2-py2.py3-none-any.whl/selprotopy/client/__init__.py
@@ -0,0 +1,27 @@
+################################################################################
+"""
+selprotopy: A Protocol Binding Suite for the SEL Protocol Suite.
+
+Supports:
+    - SEL Fast Meter
+    - SEL Fast Message
+    - SEL Fast Operate
+
+Author(s):
+    - Joe Stanley: engineerjoe440@yahoo.com
+
+Homepage: https://github.com/engineerjoe440/sel-proto-py
+
+SEL Protocol Application Guide: https://selinc.com/api/download/5026/
+"""
+################################################################################
+
+try:
+    from selprotopy.client.serial import *
+except ImportError:
+    pass
+
+try:
+    from selprotopy.client.ethernet import *
+except ImportError:
+    pass
