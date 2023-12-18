789a3b78b3d27572aa039e1fbe7c0f80b219d885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 04:04:40 +0000

    Add package quotexpy/1.40.4/quotexpy-1.40.4.tar.gz

---
+++ packages/quotexpy/quotexpy-1.40.4.tar.gz/quotexpy-1.40.4/quotexpy/http/login.py
@@ -0,0 +1,22 @@
+from quotexpy.http.qxbroker import Browser
+
+
+class Login(Browser):
+    """Class for Quotex login resource."""
+
+    url = ""
+    cookies = None
+    ssid = None
+    base_url = "qxbroker.com"
+    https_base_url = f"https://{base_url}"
+
+    async def __call__(self, email, password):
+        """Method to get Quotex API login http request.
+        :param str username: The username of a Quotex server.
+        :param str password: The password of a Quotex server.
+        :returns: The instance of :class:`playwright.cookies`.
+        """
+        self.email = email
+        self.password = password
+        self.ssid, self.cookies = await self.get_cookies_and_ssid()
+        return self.ssid, self.cookies
