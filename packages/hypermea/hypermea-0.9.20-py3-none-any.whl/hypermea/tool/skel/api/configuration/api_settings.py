a5d045d95eb57c073a347ec72585ebe738455ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 04:04:48 +0000

    Add package hypermea/0.9.20/hypermea-0.9.20-py3-none-any.whl

---
+++ packages/hypermea/hypermea-0.9.20-py3-none-any.whl/hypermea/tool/skel/api/configuration/api_settings.py
@@ -0,0 +1,8 @@
+from hypermea.core.settings_manager import SettingsManager
+
+PREFIX = 'API'
+SETTINGS = SettingsManager.instance()
+SETTINGS.set_prefix_description(PREFIX, 'description')
+SETTINGS.create(PREFIX, {
+    # add settings here
+})
