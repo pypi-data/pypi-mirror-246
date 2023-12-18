3cb769803f976553c192d391a86df510e00ed240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 04:05:02 +0000

    Add package pdm-dockerize/0.2.3/pdm_dockerize-0.2.3.tar.gz

---
+++ packages/pdm-dockerize/pdm_dockerize-0.2.3.tar.gz/pdm_dockerize-0.2.3/tests/__snapshots__/test_entrypoint/test_filtering[include-list].sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+set -euo pipefail
+
+export PYTHONPATH=./lib
+export PATH=./bin:$PATH
+
+cmd=$1
+shift
+
+usage() {
+    echo "Available commands"
+    echo "=================="
+    echo "test: pytest"
+    echo "ns:task1: ns:task1"
+}
+
+case $cmd in
+    test)
+        pytest "$@"
+        ;;
+    ns:task1)
+        ns:task1 "$@"
+        ;;
+    *)
+        usage
+        ;;
+esac
