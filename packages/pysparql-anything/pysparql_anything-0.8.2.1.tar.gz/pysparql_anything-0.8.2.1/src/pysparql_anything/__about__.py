789d75565a1eb1bd793490cb24335ea93e97279f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 04:04:40 +0000

    Add package pysparql-anything/0.8.2.1/pysparql_anything-0.8.2.1.tar.gz

---
+++ packages/pysparql-anything/pysparql_anything-0.8.2.1.tar.gz/pysparql_anything-0.8.2.1/src/pysparql_anything/__about__.py
@@ -0,0 +1,12 @@
+"""
+Version variable for build purposes. 'x.y.z' refers to the current release of
+SPARQL Anything, the last number to the independent PySPARQL Anything
+releases.
+"""
+
+# PySPARQL Anything version variable
+__version__ = '0.8.2.1'
+
+# SPARQL Anything metadata
+__SparqlAnything__ = 'v0.8.2'
+__uri__ = 'SPARQL-Anything/sparql.anything'
