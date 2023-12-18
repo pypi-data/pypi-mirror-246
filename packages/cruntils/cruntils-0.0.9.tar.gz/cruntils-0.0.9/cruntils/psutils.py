d2e1faf5df0039246534bd8cfdcfdacb68def8d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 03:05:52 +0000

    Add package cruntils/0.0.9/cruntils-0.0.9.tar.gz

---
+++ packages/cruntils/cruntils-0.0.9.tar.gz/cruntils-0.0.9/cruntils/psutils.py
@@ -0,0 +1,15 @@
+
+
+def CreateService():
+    """ Create a new Windows service
+
+    https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/sc-create
+
+    https://stackoverflow.com/questions/3663331/when-creating-a-service-with-sc-exe-how-to-pass-in-context-parameters
+
+    """
+    pass
+
+
+
+
