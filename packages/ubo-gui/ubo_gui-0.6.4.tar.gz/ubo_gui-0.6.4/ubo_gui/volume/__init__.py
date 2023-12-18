8c9b9893cca56badd9ef00336d0eb3942453caa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 04:04:25 +0000

    Add package ubo-gui/0.6.4/ubo_gui-0.6.4.tar.gz

---
+++ packages/ubo-gui/ubo_gui-0.6.4.tar.gz/ubo_gui-0.6.4/ubo_gui/volume/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+import pathlib
+
+from kivy.app import Builder
+from kivy.properties import ColorProperty, NumericProperty
+from kivy.uix.boxlayout import BoxLayout
+
+from ubo_gui.constants import PRIMARY_COLOR, SECONDARY_COLOR
+
+
+class VolumeWidget(BoxLayout):
+    value = NumericProperty(50)
+    fill_color = ColorProperty(PRIMARY_COLOR)
+    background_color = ColorProperty(SECONDARY_COLOR)
+
+
+Builder.load_file(pathlib.Path(
+    __file__).parent.joinpath('volume_widget.kv').resolve().as_posix())
