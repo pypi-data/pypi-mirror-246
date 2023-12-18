a35fc2af5bd97b340f595c56c6ae29e2c52257fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 04:04:58 +0000

    Add package SimpleMyAI/4.2/SimpleMyAI-4.2.tar.gz

---
+++ packages/SimpleMyAI/SimpleMyAI-4.2.tar.gz/SimpleMyAI-4.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+# Это для библиотеки SimpleMyAI
+
+setup(name="SimpleMyAI",
+      version="4.2",
+      description="Easy creation of your own simple neural network",
+      packages=["SimpleMyAI"],
+      author_email="samkirich@yandex.ru",
+      zip_safe=False)
