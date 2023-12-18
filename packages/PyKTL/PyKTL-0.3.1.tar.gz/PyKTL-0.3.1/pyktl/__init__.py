ada4c5707ac2700d3f8d2beca902614e931e15aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 04:04:40 +0000

    Add package PyKTL/0.3.1/PyKTL-0.3.1.tar.gz

---
+++ packages/PyKTL/PyKTL-0.3.1.tar.gz/PyKTL-0.3.1/pyktl/__init__.py
@@ -0,0 +1,17 @@
+from .pyktl import generate_signed_client_identifier_jwt
+from .pyktl import generate_signed_access_token_jwt
+
+
+__version__ = "0.2.0"
+
+__title__ = "PyKTL"
+__description__ = "This library provides utility methods to generate and sign Knox Cloud Tokens using Python."
+__url__ = "https://github.com/mattintech/KnoxTokenLibrary-python"
+__uri__ = __url__
+__doc__ = f"{__description__} <{__uri__}>"
+
+__author__ = "Matt Hills"
+__email__ = "mattintech@gmail.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright 2023 Matt Hills"
