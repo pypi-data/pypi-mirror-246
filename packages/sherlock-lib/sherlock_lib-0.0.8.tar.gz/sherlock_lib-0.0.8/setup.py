5f9dbb4609cee50d2a334267ae9889f0978ce431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 04:04:44 +0000

    Add package sherlock-lib/0.0.8/sherlock_lib-0.0.8.tar.gz

---
+++ packages/sherlock-lib/sherlock_lib-0.0.8.tar.gz/sherlock_lib-0.0.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(
+    name='sherlock_lib',
+    version='0.0.8',
+    license='MIT License',
+    author='Paulo Ricardo Mesquita',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='pauloricardomrs2002@gmail.com',
+    keywords='sherlock, stalking, sherlock lib, sherlock-lib',
+    description=u'🔎 Hunt down social media accounts by username across social networks',
+    packages=find_packages(),
+    install_requires=[
+        'certifi==2019.6.16',
+        'PySocks==1.7.0',
+        'requests==2.22.0',
+        'requests-futures==1.0.0',
+        'stem==1.8.0',
+        'exrex==0.11.0',
+        'urllib3==1.25.1'
+    ],
+)
