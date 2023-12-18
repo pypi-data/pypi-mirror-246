a29fd17815f30c7a8c48c517779daff3e9fd1d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 03:05:43 +0000

    Add package nvitop/1.3.2/nvitop-1.3.2.tar.gz

---
+++ packages/nvitop/nvitop-1.3.2.tar.gz/nvitop-1.3.2/setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+# To install `nvitop` with specific version of `nvidia-ml-py`, use:
+#
+#   pip install nvidia-ml-py==xx.yyy.zz nvitop
+#
+# or
+#
+#   pip install 'nvitop[pynvml-xx.yyy.zz]'
+#
+
+"""Setup script for ``nvitop``."""
+
+import pathlib
+import re
+import sys
+
+from setuptools import setup
+
+
+HERE = pathlib.Path(__file__).absolute().parent
+VERSION_FILE = HERE / 'nvitop' / 'version.py'
+
+sys.path.insert(0, str(VERSION_FILE.parent))
+# pylint: disable-next=import-error,wrong-import-position
+import version  # noqa
+
+
+VERSION_CONTENT = None
+
+try:
+    if not version.__release__:
+        try:
+            VERSION_CONTENT = VERSION_FILE.read_text(encoding='utf-8')
+            VERSION_FILE.write_text(
+                data=re.sub(
+                    r"""__version__\s*=\s*('[^']+'|"[^"]+")""",
+                    f'__version__ = {version.__version__!r}',
+                    string=VERSION_CONTENT,
+                ),
+                encoding='utf-8',
+            )
+        except OSError:
+            VERSION_CONTENT = None
+
+    setup(
+        name='nvitop',
+        version=version.__version__,
+        extras_require={
+            'lint': [
+                'black >= 22.6.0',
+                'isort',
+                'pylint[spelling] >= 2.16.0',
+                'mypy',
+                'typing-extensions',
+                'pre-commit',
+            ],
+            'cuda10': ['nvidia-ml-py == 11.450.51'],
+            **{
+                # The identifier could not start with numbers, add a prefix `pynvml-`
+                f'pynvml-{pynvml}': [f'nvidia-ml-py == {pynvml}']
+                for pynvml in version.PYNVML_VERSION_CANDIDATES
+            },
+        },
+    )
+finally:
+    if VERSION_CONTENT is not None:
+        with VERSION_FILE.open(mode='wt', encoding='utf-8', newline='') as file:
+            file.write(VERSION_CONTENT)
