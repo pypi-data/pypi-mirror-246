44a22317adb1575e588c1bb49fb2773d6fec3d14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-18 03:05:51 +0000

    Add package stackview/0.7.3/stackview-0.7.3.tar.gz

---
+++ packages/stackview/stackview-0.7.3.tar.gz/stackview-0.7.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stackview",
+    version="0.7.3",
+    author="Robert Haase",
+    author_email="robert.haase@tu-dresden.de",
+    description="Interactive image stack viewing in jupyter notebooks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/haesleinhuepf/stackview/",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=["numpy!=1.19.4", "ipycanvas", "ipywidgets", "scikit-image", "ipyevents", "toolz", "matplotlib", "ipykernel"],
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Development Status :: 3 - Alpha",
+    ],
+)
